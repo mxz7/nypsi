59ea0a43b6ab44bb463082beafbd59e73326e1e5
max <29782397+tekoh@users.noreply.github.com>
2020-02-16 20:10:52 +0000

    Initial commit

---
+++ learningdiscbot/bot.js
@@ -0,0 +1,46 @@
+/*jshint esversion: 6 */
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { prefix, token } = require("./config.json");
+const fs = require("fs");
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
+
+console.log(" -- commands -- \n");
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+
+    console.log(command.name + " ✅");
+}
+console.log("\n -- commands -- ");
+
+client.once("ready", () => {
+    client.user.setPresence({
+        status: "dnd"
+    });
+
+    console.log("- - -");
+    console.log('nypsi is online..\n\n');
+});
+
+client.on("message", message => {
+
+    if (message.member.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(`${prefix}`)) return;
+
+    const args = message.content.substring(prefix.length).split(" ");
+    const cmd = args[0].toLowerCase();
+
+    if (client.commands.get(cmd)) {
+        client.commands.get(cmd).execute(message, args);
+
+        console.log(message.member.user.tag + " ran command '" + cmd + "'");
+    }
+});
+
+client.login(token);
