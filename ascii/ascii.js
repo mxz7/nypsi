08a7e9bfaee54c29607e8b824b18fc55dcf0af12
max <maxthemango@gmail.com>
2020-02-20 22:21:59 +0000

    file update lools

--- ascii/ascii.js
+++
@@ -1,9 +0,0 @@
-const figlet = require("figlet")
-
-figlet("streaks", function(err, data) {
-    if (err) {
-        console.log("something went wrong");
-        return console.dir(err);
-    }
-    console.log(data);
-});
