9770139c703e96837f77b322b28fb120f0f25312
max <maxthemango@gmail.com>
2020-04-11 01:42:54 +0100

    ass command

---
+++ commands/ass.js
@@ -0,0 +1,84 @@
+const { MessageEmbed } = require("discord.js")
+const { redditImage, getColor } = require("../utils.js")
+
+const cooldown = new Map()
+
+module.exports = {
+    name: "ass",
+    description: "get a random ass image",
+    category: "nsfw",
+    run: async (message, args) => {
+        if (cooldown.has(message.member.id)) {
+            const init = cooldown.get(message.member.id)
+            const curr = new Date()
+            const diff = Math.round((curr - init) / 1000)
+            const time = 5 - diff
+
+            const minutes = Math.floor(time / 60)
+            const seconds = time - minutes * 60
+
+            let remaining
+
+            if (minutes != 0) {
+                remaining = `${minutes}m${seconds}s`
+            } else {
+                remaining = `${seconds}s`
+            }
+            return message.channel.send("❌\nstill on cooldown for " + remaining );
+        }
+
+        if (!message.channel.nsfw) {
+            return message.channel.send("❌\nyou must do this in an nsfw channel")
+        }
+
+        const { assCache } = require("../utils.js")
+
+        if (assCache.size <= 2) {
+            return message.channel.send("❌\nplease wait a couple more seconds..")
+        }
+
+        cooldown.set(message.member.id, new Date());
+
+        setTimeout(() => {
+            cooldown.delete(message.member.id);
+        }, 5000);
+
+        const assLinks = Array.from(assCache.keys())
+
+        const subredditChoice = assLinks[Math.floor(Math.random() * assLinks.length)]
+
+        const allowed = assCache.get(subredditChoice)
+
+        const chosen = allowed[Math.floor(Math.random() * allowed.length)]
+
+        const a = await redditImage(chosen, allowed)
+
+        if (a == "lol") {
+            return message.channel.send("❌\nunable to find bdsm image")
+        }
+
+        const image = a.split("|")[0]
+        const title = a.split("|")[1]
+        let url = a.split("|")[2]
+        const author = a.split("|")[3]
+
+        url = "https://reddit.com" + url
+
+        const color = getColor(message.member);
+
+        const subreddit = subredditChoice.split("r/")[1].split(".json")[0]
+
+        const embed = new MessageEmbed()
+            .setColor(color)
+            .setTitle(title)
+            .setAuthor("u/" + author + " | r/" + subreddit)
+            .setURL(url)
+            .setImage(image)
+            .setFooter("bot.tekoh.wtf")
+
+        message.channel.send(embed).catch(() => {
+            return message.channel.send("❌\ni may be missing permission: 'EMBED_LINKS'")
+        })
+
+    }
+}
