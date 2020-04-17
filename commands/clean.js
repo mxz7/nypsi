4e6d62e7086cedbaa1d04d6adf42ea223e52f58b
max <29782397+tekoh@users.noreply.github.com>
2020-04-17 02:24:24 +0100

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -1,6 +1,8 @@
 const cooldown = new Map();
 const { prefix } = require("../config.json")
 
+const a = new Set()
+
 module.exports = {
     name: "clean",
     description: "clean up bot commands and responses",
@@ -8,6 +10,8 @@ module.exports = {
     run: async (message, args) => {
 
         if (!message.member.hasPermission("MANAGE_MESSAGES")) return
+
+        if (a.has(message.guild.id)) return
 
         if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
             return message.channel.send("❌ i am lacking permission: 'MANAGE_MESSAGES'");
@@ -36,12 +40,17 @@ module.exports = {
             cooldown.delete(message.member.id);
         }, 15000);
 
+        a.add(message.guild.id)
+
         const collected = await message.channel.messages.fetch({limit: 25})
 
         const collecteda = collected.filter(msg => msg.member.user.id == message.client.user.id || msg.content.startsWith(prefix))
 
         for (msg of collecteda.array()) {
             await msg.delete().catch()
+        }
+        if (a.has(message.guild.id)) {
+            a.delete(message.guild.id)
         }
         message.channel.send("✅ **successfully deleted " + collecteda.array().length + " messages**").then(m => m.delete({timeout: 5000}))
     }
