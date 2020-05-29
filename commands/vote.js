0d8a994fbbbfa1d22cbb26847bb3130f5fd7afee
max <maxthemango@gmail.com>
2020-05-29 18:26:45 +0100

    added straight money bonus

--- commands/vote.js
+++ commands/vote.js
@@ -1,11 +1,12 @@
 const { MessageEmbed } = require("discord.js")
-const { getVoteMulti } = require("../economy/utils.js")
+const { getVoteMulti, getBalance, updateBalance } = require("../economy/utils.js")
 
 const cooldown = new Map()
+const bonusCooldown = new Map()
 
 module.exports = {
     name: "vote",
-    description: "vote every 12 hours to get a 10% bonus on gambling wins",
+    description: "vote every 12 hours to get a 20% bonus on gambling wins as well as a $10k reward",
     category: "money",
     run: async (message, args) => {
 
@@ -32,7 +33,7 @@ module.exports = {
 
         setTimeout(() => {
             cooldown.delete(message.member.id);
-        }, 10000);
+        }, 5);
 
         const voted = await getVoteMulti(message.member) > 0
 
@@ -42,9 +43,27 @@ module.exports = {
         embed.setFooter("bot.tekoh.wtf")
 
         if (voted) {
-            embed.setTitle("vote ✅ | " + message.member.user.username)
-            embed.setColor("#5efb8f")
-            embed.addField("status", "you currently have a 20% bonus on all gambling wins")
+
+            if (!bonusCooldown.has(message.member.id)) {
+                embed.setTitle("vote ✅ | " + message.member.user.username)
+                embed.setColor("#5efb8f")
+                embed.addField("status", "you currently have a 20% bonus on all gambling wins\nyou have been rewarded with $**10,000** for voting")
+                updateBalance(message.member, getBalance(message.member) + 10000)
+                bonusCooldown.set(message.member.id, new Date())
+                setTimeout(() => {
+                    bonusCooldown.delete(message.member.id)
+                }, 21600000)
+            } else {
+                const init = bonusCooldown.get(message.member.id)
+                const curr = new Date()
+                const diff = Math.round((curr - init))
+                const time = 21600000 - diff
+                const remaining = getUptime(time)
+
+                embed.setTitle("vote ✅ | " + message.member.user.username)
+                embed.setColor("#5efb8f")
+                embed.addField("status", "you currently have a 20% bonus on all gambling wins\nyou can receive a $**10,000** bonus in: " + remaining)
+            }
         } else {
             embed.setTitle("vote ❌ | " + message.member.user.username)
             embed.setColor("#e4334f")
@@ -56,4 +75,34 @@ module.exports = {
         })
 
     }
+}
+
+function getUptime(ms) {
+    const days = Math.floor(ms / (24 * 60 * 60 * 1000))
+    const daysms = ms % (24 * 60 * 60 * 1000)
+    const hours = Math.floor((daysms) / (60*60*1000))
+    const hoursms = ms % (60 * 60 * 1000)
+    const minutes = Math.floor((hoursms) / (60 * 1000))
+    const minutesms = ms % (60 * 1000)
+    const sec = Math.floor((minutesms) / (1000))
+
+    let output = ""
+
+    if (days > 0) {
+        output = output + days + "d "
+    }
+
+    if (hours > 0) {
+        output = output + hours + "h "
+    }
+
+    if (minutes > 0) {
+        output = output + minutes + "m "
+    }
+
+    if (sec > 0) {
+        output = output + sec + "s"
+    }
+
+    return output
 }
