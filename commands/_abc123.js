50448c90b2f27814015706aa52c75026c4a01d2a
max <maxthemango@gmail.com>
2020-06-11 16:31:18 +0100

    changed up guilds

--- commands/_abc123.js
+++ commands/_abc123.js
@@ -112,43 +112,92 @@ module.exports = {
                 return message.react("❌")
             }
 
-            const members = guild.members.cache
-            const users = members.filter(member => !member.user.bot)
-            const bots = members.filter(member => member.user.bot)
-            const online = users.filter(member => member.presence.status != "offline")
+            if (args.join("").includes("-m")) {
+                const members = guild.members.cache
 
-            const balTop = topAmount(guild, 5)
+                const names = new Map()
 
-            const filtered = balTop.filter(function (el) {
-                return el != null;
-            });
+                members.forEach(m => {
+                    if (names.size == 0) {
+                        const value1 = []
+                        value1.push("`" + m.user.tag + "`")
+                        names.set(1, value1)
+                    } else {
+                        const lastPage = names.size
+    
+                        if (names.get(lastPage).length >= 10) {
+                            const value1 = []
+                            value1.push("`" + m.user.tag + "`")
+                            names.set(lastPage + 1, value1)
+                        } else {
+                            names.get(lastPage).push("`" + m.user.tag + "`")
+                        }
+                    }
+                })
 
-            let owner
 
-            try {
-                owner = guild.owner.user.tag
-            } catch (e) {
-                owner = "`" + guild.ownerID + "`"
+                
+                const embed = new MessageEmbed()
+                    .setTitle(guild.name)
+                    .setDescription(names.get(1).join("\n"))
+                    .setFooter("bot.tekoh.wtf")
+                    .setColor("#60d16b")
+                
+                const msg = await message.channel.send(embed).catch(() => {
+                    return message.channel.send("❌ i may be lacking permission: 'EMBED_LINKS'");
+                })
+        
+                if (names.size >= 2) {
+                    await msg.react("⬅")
+                    await msg.react("➡")
+        
+                    let currentPage = 1
+                    const lastPage = names.size
+        
+                    const filter = (reaction, user) => {
+                        return ["⬅", "➡"].includes(reaction.emoji.name) && user.id == message.member.user.id
+                    }
+        
+                    async function pageManager() {
+                        const reaction = await msg.awaitReactions(filter, { max: 1, time: 30000, errors: ["time"] })
+                            .then(collected => {
+                                return collected.first().emoji.name
+                            }).catch(async () => {
+                                await msg.reactions.removeAll()
+                            })
+                        
+                        if (!reaction) return
+                
+                        if (reaction == "⬅") {
+                            if (currentPage <= 1) {
+                                return pageManager()
+                            } else {
+                                currentPage--
+                                embed.setDescription(names.get(currentPage).join("\n"))
+                                embed.setFooter("page " + currentPage)
+                                await msg.edit(embed)
+                                return pageManager()
+                            }
+                        } else if (reaction == "➡") {
+                            if (currentPage >= lastPage) {
+                                return pageManager()
+                            } else {
+                                currentPage++
+                                embed.setDescription(names.get(currentPage).join("\n"))
+                                embed.setFooter("page " + currentPage)
+                                await msg.edit(embed)
+                                return pageManager()
+                            }
+                        }
+                    }
+                    return pageManager()
+                }
+                return
             }
 
-            const embed = new MessageEmbed()
-                .setTitle(guild.name)
-                .setColor("#60d16b")
-                .setThumbnail(guild.iconURL({format: "png", dynamic: true, size: 128}))
-                .setDescription("`" + guild.id + "`")
-                .addField("info", "**owner** " + owner + "\n" + 
-                    "**created** " + formatDate(guild.createdAt) + "\n" +
-                    "**region** " + guild.region, true)
-                .addField("info", "**roles** " + guild.roles.cache.size + "\n" + 
-                    "**channels** " + guild.channels.cache.size, true)
-                .addField("member info", "**humans** " + users.size.toLocaleString() + "\n" +
-                    "**bots** " + bots.size.toLocaleString() + "\n" + 
-                    "**online** " + online.size.toLocaleString() + "\n" +
-                    "**member peak** " + getPeaks(guild).members.toLocaleString() + "\n" + 
-                    "**online peak** " + getPeaks(guild).onlines.toLocaleString(), true)
-                .addField("top " + filtered.length, filtered)
-                .setFooter("bot.tekoh.wtf")
-            message.channel.send(embed)
+            const msg = await guildInfo(guild)
+
+            message.channel.send(msg) 
         } else if (args[0] == "gname") {
             if (args.length == 1) return
 
@@ -158,43 +207,92 @@ module.exports = {
                 return message.react("❌")
             }
 
-            const members = guild.members.cache
-            const users = members.filter(member => !member.user.bot)
-            const bots = members.filter(member => member.user.bot)
-            const online = users.filter(member => member.presence.status != "offline")
+            if (args.join("").includes("-m")) {
+                const members = guild.members.cache
 
-            const balTop = topAmount(guild, 5)
+                const names = new Map()
 
-            const filtered = balTop.filter(function (el) {
-                return el != null;
-            });
+                members.forEach(m => {
+                    if (names.size == 0) {
+                        const value1 = []
+                        value1.push("`" + m.user.tag + "`")
+                        names.set(1, value1)
+                    } else {
+                        const lastPage = names.size
+    
+                        if (names.get(lastPage).length >= 10) {
+                            const value1 = []
+                            value1.push("`" + m.user.tag + "`")
+                            names.set(lastPage + 1, value1)
+                        } else {
+                            names.get(lastPage).push("`" + m.user.tag + "`")
+                        }
+                    }
+                })
 
-            let owner
 
-            try {
-                owner = guild.owner.user.tag
-            } catch (e) {
-                owner = "`" + guild.ownerID + "`"
+                
+                const embed = new MessageEmbed()
+                    .setTitle(guild.name)
+                    .setDescription(names.get(1).join("\n"))
+                    .setFooter("bot.tekoh.wtf")
+                    .setColor("#60d16b")
+                
+                const msg = await message.channel.send(embed).catch(() => {
+                    return message.channel.send("❌ i may be lacking permission: 'EMBED_LINKS'");
+                })
+        
+                if (names.size >= 2) {
+                    await msg.react("⬅")
+                    await msg.react("➡")
+        
+                    let currentPage = 1
+                    const lastPage = names.size
+        
+                    const filter = (reaction, user) => {
+                        return ["⬅", "➡"].includes(reaction.emoji.name) && user.id == message.member.user.id
+                    }
+        
+                    async function pageManager() {
+                        const reaction = await msg.awaitReactions(filter, { max: 1, time: 30000, errors: ["time"] })
+                            .then(collected => {
+                                return collected.first().emoji.name
+                            }).catch(async () => {
+                                await msg.reactions.removeAll()
+                            })
+                        
+                        if (!reaction) return
+                
+                        if (reaction == "⬅") {
+                            if (currentPage <= 1) {
+                                return pageManager()
+                            } else {
+                                currentPage--
+                                embed.setDescription(names.get(currentPage).join("\n"))
+                                embed.setFooter("page " + currentPage)
+                                await msg.edit(embed)
+                                return pageManager()
+                            }
+                        } else if (reaction == "➡") {
+                            if (currentPage >= lastPage) {
+                                return pageManager()
+                            } else {
+                                currentPage++
+                                embed.setDescription(names.get(currentPage).join("\n"))
+                                embed.setFooter("page " + currentPage)
+                                await msg.edit(embed)
+                                return pageManager()
+                            }
+                        }
+                    }
+                    return pageManager()
+                }
+                return
             }
 
-            const embed = new MessageEmbed()
-                .setTitle(guild.name)
-                .setColor("#60d16b")
-                .setThumbnail(guild.iconURL({format: "png", dynamic: true, size: 128}))
-                .setDescription("`" + guild.id + "`")
-                .addField("info", "**owner** " + owner + "\n" + 
-                    "**created** " + formatDate(guild.createdAt) + "\n" +
-                    "**region** " + guild.region, true)
-                .addField("info", "**roles** " + guild.roles.cache.size + "\n" + 
-                    "**channels** " + guild.channels.cache.size, true)
-                .addField("member info", "**humans** " + users.size.toLocaleString() + "\n" +
-                    "**bots** " + bots.size.toLocaleString() + "\n" + 
-                    "**online** " + online.size.toLocaleString() + "\n" +
-                    "**member peak** " + getPeaks(guild).members.toLocaleString() + "\n" + 
-                    "**online peak** " + getPeaks(guild).onlines.toLocaleString(), true)
-                .addField("top " + filtered.length, filtered)
-                .setFooter("bot.tekoh.wtf")
-            message.channel.send(embed)
+            const msg = await guildInfo(guild)
+
+            message.channel.send(msg)            
         } else if (args[0] == "top") {
 
             let amount = 5
@@ -218,4 +316,54 @@ module.exports = {
             message.channel.send(embed)
         }
     }
+}
+
+async function guildInfo(guild) {
+    const members = guild.members.cache
+    const users = members.filter(member => !member.user.bot)
+    const bots = members.filter(member => member.user.bot)
+    const online = users.filter(member => member.presence.status != "offline")
+
+    const balTop = topAmount(guild, 5)
+
+    const filtered = balTop.filter(function (el) {
+        return el != null;
+    });
+
+    let owner
+
+    try {
+        owner = guild.owner.user.tag
+    } catch (e) {
+        owner = "`" + guild.ownerID + "`"
+    }
+
+    const invites = (await guild.fetchInvites().catch()).keyArray()
+
+    const embed = new MessageEmbed()
+        .setTitle(guild.name)
+        .setColor("#60d16b")
+        .setThumbnail(guild.iconURL({format: "png", dynamic: true, size: 128}))
+        .setDescription("`" + guild.id + "`")
+        .addField("info", "**owner** " + owner + "\n" + 
+            "**created** " + formatDate(guild.createdAt) + "\n" +
+            "**region** " + guild.region, true)
+        .addField("info", "**roles** " + guild.roles.cache.size + "\n" + 
+            "**channels** " + guild.channels.cache.size, true)
+        .addField("member info", "**humans** " + users.size.toLocaleString() + "\n" +
+            "**bots** " + bots.size.toLocaleString() + "\n" + 
+            "**online** " + online.size.toLocaleString() + "\n" +
+            "**member peak** " + getPeaks(guild).members.toLocaleString() + "\n" + 
+            "**online peak** " + getPeaks(guild).onlines.toLocaleString(), true)
+        .setFooter("bot.tekoh.wtf")
+
+    if (invites && invites.length > 0) {
+        embed.addField("invite (" + invites.length + ")", invites[Math.floor(Math.random() * invites.length)], true)
+    }
+
+    if (filtered.length > 0) {
+        embed.addField("top " + filtered.length, filtered, true)
+    }
+
+    return embed
 }
