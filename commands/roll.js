769239d23cc7016335cda063e29758df11cc9e6f
max <29782397+tekoh@users.noreply.github.com>
2020-06-18 16:05:04 +0100

    changed a couple old commands

--- commands/roll.js
+++ commands/roll.js
@@ -1,10 +1,26 @@
+const { getColor } = require("../utils/utils")
+const { MessageEmbed } = require("discord.js")
+
 module.exports = {
     name: "roll",
     description: "roll a dice",
     category: "fun",
     run: async (message, args) => {
 
-        message.channel.send("🎲\n**you rolled a " + Math.ceil(Math.random() * 6) + "**");
+        let range = 6
 
+        if (args.length != 0) {
+            if (parseInt(args[0])) {
+                range = parseInt(args[0])
+            }
+        }
+
+        const color = getColor(message.member)
+
+        const embed = new MessageEmbed()
+            .setDescription("🎲 you rolled a **" + (Math.floor(Math.random() * range) + 1) + "**")
+            .setColor(color)
+
+        return message.channel.send(embed)
     }
 };
