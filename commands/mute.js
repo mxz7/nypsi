611fb312aea93c5339d8b80cc7de2358cebcac22
max <maxthemango@gmail.com>
2020-08-17 19:10:35 +0100

    fixed issue with command help

--- commands/mute.js
+++ commands/mute.js
@@ -20,7 +20,7 @@ module.exports = {
 
         const color = getColor(message.member)
 
-        if (args.length == 0 && (args[0].length != 18 && message.mentions.members.first() == null)) {
+        if (args.length == 0 && message.mentions.members.first() == null) {
             const embed = new MessageEmbed()
                 .setTitle("mute help")
                 .setColor(color)
@@ -48,6 +48,8 @@ module.exports = {
             }
             
             message.mentions.members.set(member.user.id, member)
+        } else if (message.mentions.members.first() == null) {
+            return message.channel.send("❌ unable to find member with ID `" + args[0] + "`")
         }
 
         const members = message.mentions.members
