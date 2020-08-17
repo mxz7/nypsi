149d8c009cd12fe95965245807f8df1266f3f090
max <maxthemango@gmail.com>
2020-08-17 19:10:35 +0100

    fixed issue with command help

--- commands/ban.js
+++ commands/ban.js
@@ -28,7 +28,7 @@ module.exports = {
             return message.channel.send("❌ i am lacking permission: 'BAN_MEMBERS'");
         }
 
-        if (args.length == 0 && (args[0].length != 18 && message.mentions.members.first() == null)) {
+        if (args.length == 0 && message.mentions.members.first() == null) {
 
             const embed = new MessageEmbed()
                 .setTitle("ban help")
@@ -60,6 +60,8 @@ module.exports = {
             }
 
             message.mentions.members.set(member.user.id, member)
+        } else if (message.mentions.members.first() == null) {
+            return message.channel.send("❌ unable to find member with ID `" + args[0] + "`")
         }
 
         const members = message.mentions.members
