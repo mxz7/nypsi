8660c94dc52b083f9f840f38460bacd1c04b9045
max <max.0x71@gmail.com>
2020-09-21 16:34:10 +0100

    support for banning unknown members (use id)

--- commands/ban.js
+++ commands/ban.js
@@ -28,6 +28,8 @@ module.exports = {
             return message.channel.send("❌ i am lacking permission: 'BAN_MEMBERS'");
         }
 
+        let idOnly = false
+
         if (args.length == 0 && message.mentions.members.first() == null) {
 
             const embed = new MessageEmbed()
@@ -57,10 +59,12 @@ module.exports = {
             const member = members.find(m => m.id == args[0])
 
             if (!member) {
-                return message.channel.send("❌ unable to find member with ID `" + args[0] + "`")
-            }
+                idOnly = true
 
-            message.mentions.members.set(member.user.id, member)
+                message.mentions.members.set(args[0], args[0])
+            } else {
+                message.mentions.members.set(member.user.id, member)
+            }
         } else if (message.mentions.members.first() == null) {
             return message.channel.send("❌ unable to find member with ID `" + args[0] + "`")
         }
@@ -84,24 +88,33 @@ module.exports = {
 
         let count = 0
         let failed = []
+        let fail = false
 
         for (member of members.keyArray()) {
-            const targetHighestRole = members.get(member).roles.highest
-            const memberHighestRole = message.member.roles.highest
+            if (!idOnly) {
+                const targetHighestRole = members.get(member).roles.highest
+                const memberHighestRole = message.member.roles.highest
 
-            if (targetHighestRole.position >= memberHighestRole.position && message.guild.owner.user.id != message.member.user.id) {
-                failed.push(members.get(member).user.tag)
-            } else {
-                await message.guild.members.ban(member, {
-                    days: days,
-                    reason: reason
-                }).then(() => {
-                    count++
-                }).catch(() => {
+                if (targetHighestRole.position >= memberHighestRole.position && message.guild.owner.user.id != message.member.user.id) {
                     failed.push(members.get(member).user.tag)
-                })
+                    continue
+                }
             }
+            await message.guild.members.ban(member, {
+                days: days,
+                reason: reason
+            }).then(() => {
+                count++
+            }).catch(() => {
+                if (idOnly) {
+                    fail = true
+                    return message.channel.send(`❌ unable to ban the id: \`${member}\``)
+                }
+                failed.push(members.get(member).user.tag)
+            })
         }
+
+        if (fail) return
 
         if (count == 0) {
             return message.channel.send("❌ i was unable to ban any users")
@@ -114,7 +127,11 @@ module.exports = {
             .setFooter("bot.tekoh.wtf")
         
         if (count == 1) {
-            embed.setDescription("✅ `" + members.first().user.tag + "` has been banned for: " + reason.split(": ")[1])
+            if (idOnly) {
+                embed.setDescription(`✅ \`${members.first()}\` has been banned for: ${reason.split(": ")[1]}`)
+            } else {
+                embed.setDescription("✅ `" + members.first().user.tag + "` has been banned for: " + reason.split(": ")[1])
+            }
         }
 
         if (failed.length != 0) {
@@ -132,7 +149,10 @@ module.exports = {
 
         for (member of members.keyArray()) {
             const m = members.get(member)
-            if (failed.indexOf(m.user.tag) == -1) {
+
+            if (idOnly) {
+                newCase(message.guild, "ban", m, message.member.user.tag, reason.split(": ")[1])
+            } else if (failed.indexOf(m.user.tag) == -1) {
                 newCase(message.guild, "ban", m.user.id, message.member.user.tag, reason.split(": ")[1])
 
                 await m.send("you have been banned in **" + message.guild.name + "** for `" + reason.split(": ")[1] + "`").catch(() => {})
