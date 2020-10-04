dd8cc501fd4299c31c6a79f0fefd7f5342540da4
max <max.0x71@gmail.com>
2020-10-04 15:08:07 +0100

    fixed issue with empty embed when just given num

--- commands/poll.js
+++ commands/poll.js
@@ -86,6 +86,10 @@ async function run(message, args) {
 
     let mode = ""
 
+    if (args.length == 0) {
+        return message.channel.send(new ErrorEmbed("missing text"))
+    }
+
     if (!message.content.includes("|")) {
         mode = "title_only"
     } else if (args.join(" ").split("|").length == 2) {
