24baa9064afadd5646d046b0a244160769877088
max <max.0x71@gmail.com>
2020-10-05 19:02:56 +0100

    updated prefix command

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,7 +1,7 @@
-const { MessageEmbed, Message } = require("discord.js");
+const { Message } = require("discord.js");
 const { getPrefix, setPrefix } = require("../guilds/utils");
 const { Command, categories } = require("../utils/classes/Command");
-const { getColor } = require("../utils/utils")
+const { CustomEmbed, ErrorEmbed } = require("../utils/classes/EmbedBuilders");
 
 const cmd = new Command("prefix", "change the bot's prefix", categories.INFO).setPermissions(["MANAGE_GUILD"])
 
@@ -17,37 +17,26 @@ async function run(message, args) {
 
     if (!message.member.hasPermission("MANAGE_GUILD")) {
         if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            const embed = new MessageEmbed()
-                .setTitle("prefix")
-                .setDescription("❌ requires permission: *MANAGE_GUILD*")
-                .setFooter("bot.tekoh.wtf")
-                .setColor(color)
-            return message.channel.send(embed)
+            return message.channel.send(new ErrorEmbed("requires permission: *MANAGE_GUILD*"))
         }
         return
     }
 
     if (args.length == 0) {
-        const embed = new MessageEmbed()
+        const embed = new CustomEmbed(message.member, false, "current prefix: `" + prefix + "`\n\nuse " + prefix + "**prefix** <new prefix> to change the current prefix")
             .setTitle("prefix")
-            .setDescription("current prefix: `" + prefix + "`\n\nuse " + prefix + "**prefix** <new prefix> to change the current prefix")
-            .setColor(color)
-            .setFooter("bot.tekoh.wtf")
 
         return message.channel.send(embed)
     }
 
     if (args.join(" ").length > 3) {
-        return message.channel.send("❌ prefix cannot be longer than 3 characters")
+        return message.channel.send(new ErrorEmbed("prefix cannot be longer than 3 characters"))
     }
 
     setPrefix(message.guild, args.join(" "))
 
-    const embed = new MessageEmbed()
+    const embed = new CustomEmbed(message.member, false, "✅ prefix changed to `" + args.join(" ") + "`")
         .setTitle("prefix")
-        .setDescription("✅ prefix changed to `" + args.join(" ") + "`")
-        .setFooter("bot.tekoh.wtf")
-        .setColor(color)
 
     return await message.channel.send(embed)
 
