25aeb4e8f9825ec3f8f1a363b265bb3dc5fae8c2
max <max.0x71@gmail.com>
2020-10-26 20:21:27 +0000

    massively improved member finding

--- utils/utils.js
+++ utils/utils.js
@@ -121,19 +121,58 @@ function getMember(message, memberName) {
 
     const members = message.guild.members.cache
 
-    let target = members.find(member => {
-        if (member.user.username.toLowerCase() == memberName.toLowerCase()) {
-            return member;
+    let target
+    let possible = new Map()
+
+    for (member of members.keyArray()) {
+        member = members.get(member)
+        
+        if (member.user.id == memberName) {
+            target = member
+            break
         } else if (member.user.tag.toLowerCase() == memberName.toLowerCase()) {
-            return member
+            target = member
+            break
+        } else if (member.user.username.toLowerCase() == memberName.toLowerCase()) {
+
+            if (member.user.bot) {
+                possible.set(3, member)
+            } else {
+                possible.set(0, member)
+            }
         } else if (member.user.tag.toLowerCase().includes(memberName.toLowerCase())) {
-            return member
+            if (member.user.bot) {
+                possible.set(4, member)
+            } else {
+                possible.set(1, member)
+            }
         } else if (member.displayName.toLowerCase().includes(memberName.toLowerCase())) {
-            return member
-        } else if (member.user.id == memberName) {
-            return member
+            if (member.user.bot) {
+                possible.set(5, member)
+            } else {
+                possible.set(2, member)
+            }
+            
+        } 
+    }
+
+    if (!target) {
+        if (possible.get(0)) {
+            target = possible.get(0)
+        } else if (possible.get(1)) {
+            target = possible.get(1)
+        } else if (possible.get(2)) {
+            target = possible.get(2)
+        } else if (possible.get(3)) {
+            target = possible.get(3)
+        } else if (possible.get(4)) {
+            target = possible.get(4)
+        } else if (possible.get(5)) {
+            target = possible.get(5)
+        } else {
+            target = null
         }
-    });
+    }
 
     return target;
 }
@@ -154,6 +193,8 @@ function getExactMember(message, memberName) {
         if (member.user.username.toLowerCase() == memberName.toLowerCase()) {
             return member;
         } else if (member.user.tag.toLowerCase() == memberName.toLowerCase()) {
+            return member
+        } else if (member.user.id == memberName) {
             return member
         }
     });
