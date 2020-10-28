2d025c8b59a5305f53a9ff0af2a4365ac86b4849
max <max.0x71@gmail.com>
2020-10-28 21:43:41 +0000

    added error catching for possible bug in games

--- commands/highlow.js
+++ commands/highlow.js
@@ -138,7 +138,10 @@ async function run(message, args) {
         await m.react("💰")
 
         await m.edit(embed)
-        playGame(message, m)
+        playGame(message, m).catch(e => {
+            console.error(e)
+            return message.channel.send(new ErrorEmbed("an error occured while running - join support server"))
+        })
     })
 
 }
