1e84eb45df584a482ecb01499d075fc6636901f2
max <29782397+tekoh@users.noreply.github.com>
2020-10-30 18:42:19 +0000

    changed withdraw tax

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -93,7 +93,7 @@ async function run(message, args) {
 
     updateBankBalance(message.member, getBankBalance(message.member) - amount)
 
-    if (amount >= 100000) {
+    if (amount >= 200000) {
         taxEnabled = true
         amount = amount - (amount * tax)
     }
