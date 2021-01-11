60c824f49913f5a97b337ed21d57ea94e35693cd
max <max.0x71@gmail.com>
2021-01-11 15:40:10 +0000

    added $height

---
+++ commands/height.js
@@ -0,0 +1,103 @@
+const { Message } = require("discord.js")
+const { Command, categories } = require("../utils/classes/Command")
+const { getMember } = require("../utils/utils")
+const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders.js")
+
+const cache = new Map()
+const cooldown = new Map()
+
+const cmd = new Command("height", "accurate prediction of your height", categories.FUN)
+
+/**
+ * @param {Message} message 
+ * @param {Array<String>} args 
+ */
+async function run(message, args) {
+
+    if (cooldown.has(message.member.id)) {
+        const init = cooldown.get(message.member.id)
+        const curr = new Date()
+        const diff = Math.round((curr - init) / 1000)
+        const time = 5 - diff
+
+        const minutes = Math.floor(time / 60)
+        const seconds = time - minutes * 60
+
+        let remaining
+
+        if (minutes != 0) {
+            remaining = `${minutes}m${seconds}s`
+        } else {
+            remaining = `${seconds}s`
+        }
+        return message.channel.send(new ErrorEmbed(`still on cooldown for \`${remaining}\``))
+    }
+
+    cooldown.set(message.member.id, new Date())
+
+    setTimeout(() => {
+        cooldown.delete(message.author.id)
+    }, 5000)
+
+    let member
+
+    if (args.length == 0) {
+        member = message.member
+    } else {
+        if (!message.mentions.members.first()) {
+            member = await getMember(message, args[0])
+        } else {
+            member = message.mentions.members.first()
+        }
+
+        if (!member) {
+            return message.channel.send(new ErrorEmbed("invalid user"))
+        }
+    }
+
+    let size
+    let feet
+    let inches
+    let sizeMsg
+
+    if (cache.has(member.user.id)) {
+        size = cache.get(member.user.id)
+        feet = size.split("'")[0]
+        inches = size.split("'")[1]
+    } else {
+        
+        feet = Math.floor(Math.random() * 6) + 4
+        inches = Math.floor(Math.random() * 12)
+
+        if (feet > 6) feet = 5
+
+        size = `${feet}'${inches}`
+        
+        cache.set(member.user.id, size)
+
+        setTimeout(() => {
+            cache.delete(member.user.id)
+        }, 60000)
+    }
+
+    if (feet == 6) {
+        sizeMsg = "yo ur tall 😳"
+    } else if (feet == 5) {
+        if (inches <= 6) {
+            sizeMsg = "kinda short.. 🤨"
+        } else {
+            sizeMsg = "average 🙄"
+        }
+    } else {
+        sizeMsg = "LOOOL UR TINY LMAO 😂🤣😆 IMAGINE"
+    }
+
+    const embed = new CustomEmbed(message.member, false, `${member.user.toString()}\n\n📏 ${size}\n${sizeMsg}`)
+        .setTitle("short person calculator")
+    
+    return message.channel.send(embed)
+}
+
+cmd.setRun(run)
+
+module.exports = cmd
