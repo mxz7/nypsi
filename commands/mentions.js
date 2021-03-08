b6840e7751d617471c11326ebf7f437922dde0a2
max <29782397+tekoh@users.noreply.github.com>
2021-03-08 19:14:15 +0000

    added pages to mentions

--- commands/mentions.js
+++ commands/mentions.js
@@ -55,15 +55,104 @@ async function run(message, args) {
 
     userMentions.reverse()
 
-    const embed = new CustomEmbed(message.member, false).setTitle("recent mentions")
+    const pages = new Map()
 
     for (let i of userMentions) {
-        embed.addField(`${timeSince(i.date)} ago`, `**${i.user}**: ${i.content}\n[jump](${i.link})`)
+        if (pages.size == 0) {
+            const page1 = []
+            page1.push(`${timeSince(i.date)} ago|6|9|**${i.user}**: ${i.content}\n[jump](${i.link})`)
+            pages.set(1, page1)
+        } else {
+            const lastPage = pages.size
+
+            if (pages.get(lastPage).length >= 3) {
+                const newPage = []
+                newPage.push(`${timeSince(i.date)} ago|6|9|**${i.user}**: ${i.content}\n[jump](${i.link})`)
+                pages.set(lastPage + 1, newPage)
+            } else {
+                pages.get(lastPage).push(`${timeSince(i.date)} ago|6|9|**${i.user}**: ${i.content}\n[jump](${i.link})`)
+            }
+        }
     }
 
     userMentions.reverse()
 
-    return message.channel.send(embed)
+    const embed = new CustomEmbed(message.member, false).setTitle("recent mentions")
+
+    for (let i of pages.get(1)) {
+        const fieldName = i.split("|6|9|")[0]
+        const fieldValue = i.split("|6|9|").splice(-1, 1).join("")
+        embed.addField(fieldName, fieldValue)
+    }
+
+    if (pages.size >= 2) {
+        embed.setFooter(`page 1/${pages.size}`)
+    }
+
+    const msg = await message.channel.send(embed)
+
+    if (pages.size >= 2) {
+
+        await msg.react("⬅")
+        await msg.react("➡")
+
+        let currentPage = 1
+        const lastPage = pages.size
+
+        const filter = (reaction, user) => {
+            return ["⬅", "➡"].includes(reaction.emoji.name) && user.id == message.member.user.id
+        }
+
+        const pageManager = async () => {
+            const reaction = await msg.awaitReactions(filter, { max: 1, time: 30000, errors: ["time"] })
+                .then(collected => {
+                    return collected.first().emoji.name
+                }).catch(async () => {
+                    await msg.reactions.removeAll()
+                })
+        
+            if (!reaction) return
+
+            const newEmbed = new CustomEmbed(message.member, false).setTitle("recent mentions")
+
+            if (reaction == "⬅") {
+                if (currentPage <= 1) {
+                    return pageManager()
+                } else {
+                    currentPage--
+
+                    for (let i of pages.get(currentPage)) {
+                        const fieldName = i.split("|6|9|")[0]
+                        const fieldValue = i.split("|6|9|").splice(-1, 1).join("")
+                        newEmbed.addField(fieldName, fieldValue)
+                    }
+
+                    newEmbed.setFooter("page " + currentPage + "/" + lastPage)
+                    await msg.edit(newEmbed)
+                    return pageManager()
+                }
+            } else if (reaction == "➡") {
+                if (currentPage >= lastPage) {
+                    return pageManager()
+                } else {
+                    currentPage++
+
+                    for (let i of pages.get(currentPage)) {
+                        const fieldName = i.split("|6|9|")[0]
+                        const fieldValue = i.split("|6|9|").splice(-1, 1).join("")
+                        newEmbed.addField(fieldName, fieldValue)
+                    }
+                    newEmbed.setFooter("page " + currentPage + "/" + lastPage)
+                    await msg.edit(newEmbed)
+                    return pageManager()
+                }
+            }
+        }
+
+        return pageManager()
+    }
+
+    
 }
 
 cmd.setRun(run)
