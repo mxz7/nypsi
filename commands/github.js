cc91bb76df1cc9e4a124b22f87ac8e040111d1fa
max <max.0x71@gmail.com>
2021-03-19 21:41:02 +0000

    updated github url

--- commands/github.js
+++ commands/github.js
@@ -11,9 +11,9 @@ const cmd = new Command("github", "view code for the bot on github", categories.
 async function run(message, args) {
 
     const embed = new CustomEmbed(message.member, true, "nypsi is open source!!\n" +
-        "click [here](https://github.com/tekohxd/nypsi) to view the source code on github")
+        "click [here](https://github.com/tekoh/nypsi) to view the source code on github")
         .setTitle("github")
-        .setURL("https://github.com/tekohxd/nypsi")
+        .setURL("https://github.com/tekoh/nypsi")
         .addField("what does this mean?", "if you know how to code, you could fix bugs, add features, create your own commands.. the list goes on.")
     
     return message.channel.send(embed)
