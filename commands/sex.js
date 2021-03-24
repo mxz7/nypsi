19c2659c62949008c93a8e09c4f55b3e0a0e808f
max <max.0x71@gmail.com>
2021-03-24 11:51:06 +0000

    created sex command

--- commands/sex.js
+++ commands/sex.js
@@ -3,7 +3,7 @@ const { isPremium } = require("../premium/utils")
 const { Command, categories } = require("../utils/classes/Command")
 const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders")
 
-const cmd = new Command("sex", "find horny milfs in ur area 😏", categories.FUN).setAliases(["findhornymilfsinmyarea"])
+const cmd = new Command("sex", "find horny milfs in ur area 😏", categories.FUN).setAliases(["findhornymilfsinmyarea", "milffinder", "findamilf"])
 
 const cooldown = new Map()
 const looking = new Map()
@@ -48,8 +48,8 @@ async function run(message, args) {
     const addToLooking = () => {
         const obj = {
             user: message.author,
-            guild: message.guild.id,
-            guildName: message.guild.name,
+            guild: message.guild,
+            channel: message.channel.id,
             date: new Date().getTime()
         }
 
@@ -58,28 +58,29 @@ async function run(message, args) {
 
     if (looking.length == 0) {
         addToLooking()
-        return message.channel.send(new CustomEmbed(message.member, false, "you have been signed up as looking for sex ✅😏 we will notify you when a match has been made").setTitle("milf finder"))
+        return message.channel.send(new CustomEmbed(message.member, false, "you're now on the milf waiting list 😏\n\nyou'll be notified when a match is found").setTitle("milf finder"))
     } else {
-
         if (looking.has(message.author.id)) {
-            return message.channel.send(new ErrorEmbed("we're already searching for a match for you.. calm down you horny shit"))
+            return message.channel.send(new ErrorEmbed("we're already searching for a match.. calm down you horny shit"))
         }
 
         for (let key of looking.keys()) {
             key = looking.get(key)
 
-            if (message.guild.id == key.guild) continue
+            if (message.guild.id == key.guild.id) continue
 
-            const embed = new CustomEmbed(message.member, true, `a match has been made from the city of **${key.guildName}**\n\n` +
-                `send **${key.user.tag}** a *private* messag 😉😏`)
+            looking.delete(key.user.id)
 
-            return message.channel.send(embed)
+            await message.channel.send(new CustomEmbed(message.member, true, `a match has been made from **${key.guild.name}**\n\n` +
+            `send **${key.user.tag}** a *private* message 😉😏`).setTitle("milf finder"))
+
+            const channel = await key.guild.channels.cache.find(ch => ch.id == key.channel)
+
+            return await channel.send(key.user.toString() + " a match has been found", new CustomEmbed(undefined, true, `a match has been made from **${message.guild.name}**\n\nsend **${message.author.tag}** a *private* message 😉😏`).setTitle("milf finder").setColor("#5efb8f"))
         }
 
         addToLooking()
-        return message.channel.send(new CustomEmbed(message.member, false, "you have been signed up as looking for sex ✅😏 we will notify you when a match has been made").setTitle("milf finder"))
-
-        
+        return message.channel.send(new CustomEmbed(message.member, false, "you're now on the milf waiting list 😏\n\nyou'll be notified when a match is found").setTitle("milf finder"))
     }
 
 }
@@ -87,3 +88,21 @@ async function run(message, args) {
 cmd.setRun(run)
 
 module.exports = cmd
+
+setInterval(() => {
+    if (looking.size == 0) return
+
+    const now = new Date().getTime()
+
+    const expire = 10800000
+
+    looking.forEach(async obj => {
+        console.log(now - obj.date)
+        if (now - obj.date >= expire) {
+            await obj.user.send(new CustomEmbed(undefined, false, "unfortunately we couldn't find you a milf 😢").setColor("#e4334f").setTitle("milf finder"))
+            looking.delete(obj.user.id)
+        }
+    })
+
+    console.log("ran interval")
+}, 600000)
