e01393e8f3f2298e1a1c3824b0ded7a9b3cdfcae
max <max.0x71@gmail.com>
2021-03-24 19:43:48 +0000

    added duck and lizard commands

---
+++ commands/duck.js
@@ -0,0 +1,89 @@
+const { Message } = require("discord.js")
+const { Command, categories } = require("../utils/classes/Command")
+const { redditImage } = require("../utils/utils")
+const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders.js")
+const { isPremium } = require("../premium/utils")
+
+const cooldown = new Map()
+
+const cmd = new Command("duck", "get a random picture of a duck", categories.ANIMALS)
+
+/**
+ * @param {Message} message 
+ * @param {Array<String>} args 
+ */
+async function run(message, args) {
+
+    let cooldownLength = 5
+
+    if (isPremium(message.author.id)) {
+        cooldownLength = 1
+    }
+
+    if (cooldown.has(message.member.id)) {
+        const init = cooldown.get(message.member.id)
+        const curr = new Date()
+        const diff = Math.round((curr - init) / 1000)
+        const time = cooldownLength - diff
+
+        const minutes = Math.floor(time / 60)
+        const seconds = time - minutes * 60
+
+        let remaining
+
+        if (minutes != 0) {
+            remaining = `${minutes}m${seconds}s`
+        } else {
+            remaining = `${seconds}s`
+        }
+        return message.channel.send(new ErrorEmbed(`still on cooldown for \`${remaining}\``))
+    }
+
+    const { duckCache } = require("../utils/imghandler")
+
+    if (duckCache.size < 1) {
+        return message.channel.send(new ErrorEmbed("please wait a couple more seconds.."))
+    }
+
+    cooldown.set(message.member.id, new Date())
+
+    setTimeout(() => {
+        cooldown.delete(message.author.id)
+    }, cooldownLength * 1000)
+
+    const duckLinks = Array.from(duckCache.keys())
+
+    const subredditChoice = duckLinks[Math.floor(Math.random() * duckLinks.length)]
+
+    const allowed = duckCache.get(subredditChoice)
+
+    const chosen = allowed[Math.floor(Math.random() * allowed.length)]
+
+    const a = await redditImage(chosen, allowed)
+
+    if (a == "lol") {
+        return message.channel.send(new ErrorEmbed("unable to find duck image"))
+    }
+
+    const image = a.split("|")[0]
+    const title = a.split("|")[1]
+    let url = a.split("|")[2]
+    const author = a.split("|")[3]
+
+    url = "https://reddit.com" + url
+
+    const subreddit = subredditChoice.split("r/")[1].split(".json")[0]
+
+    const embed = new CustomEmbed(message.member, false)
+        .setTitle(title)
+        .setHeader("u/" + author + " | r/" + subreddit)
+        .setURL(url)
+        .setImage(image)
+
+    message.channel.send(embed)
+
+}
+
+cmd.setRun(run)
+
+module.exports = cmd
