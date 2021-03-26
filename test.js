177d568cf890032cd9c34e89aa8a44ffcab4a849
Automated Publisher <actions@users.noreply.github.com>
2021-03-26 16:45:33 +0000

    automatic - prettier formattingba2113d027813f922f01c446f5746653fe5f7527

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function test() {
     console.log('"test"')
     console.log('"test69"')
-    console.log('test')
-    console.log('test LAST ONE')
+    console.log("test")
+    console.log("test LAST ONE")
 }
