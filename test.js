2bc957c38045240e4b22d3643aaf482d55bbfb86
Automated Publisher <actions@users.noreply.github.com>
2021-03-26 16:37:52 +0000

    Automated publish: Fri Mar 26 16:37:52 UTC 2021 16e656a28b5d7ad548b05bf20d02c92c44d589a9

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function test() {
-    console.log('test')
-    console.log('test69')
-    console.log('test420')
-    console.log('test last hopefully')
+    console.log("test")
+    console.log("test69")
+    console.log("test420")
+    console.log("test last hopefully")
 }
