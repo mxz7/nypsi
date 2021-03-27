3176ca2d4c1b42ab0e12d8c9fb22dc7e6338c5b6
max <29782397+tekoh@users.noreply.github.com>
2021-03-27 11:23:53 +0000

    formatting

--- commands/chatreaction.js
+++ commands/chatreaction.js
@@ -301,7 +301,9 @@ async function run(message, args) {
                 const length = parseInt(args[2])
 
                 if (!length) {
-                    return message.channel.send(new ErrorEmbed("invalid length, it must be a whole number"))
+                    return message.channel.send(
+                        new ErrorEmbed("invalid length, it must be a whole number")
+                    )
                 }
 
                 if (length > 900) {
@@ -309,14 +311,22 @@ async function run(message, args) {
                 }
 
                 if (length < 120) {
-                    return message.channel.send(new ErrorEmbed("cannot be shorter than 120 seconds"))
+                    return message.channel.send(
+                        new ErrorEmbed("cannot be shorter than 120 seconds")
+                    )
                 }
 
                 const settings = getReactionSettings(message.guild)
 
                 settings.timeBetweenEvents = length
 
-                return message.channel.send(new CustomEmbed(message.member, false, `✅ event cooldown set to \`${length}s\``))
+                return message.channel.send(
+                    new CustomEmbed(
+                        message.member,
+                        false,
+                        `✅ event cooldown set to \`${length}s\``
+                    )
+                )
             } else if (args[1].toLowerCase() == "offset") {
                 return message.channel.send(new ErrorEmbed(`${prefix}cr settings offset <number>`))
             } else if (args[1].toLowerCase() == "length") {
