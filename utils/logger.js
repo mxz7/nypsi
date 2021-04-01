3fad3b79490613d8f4180f475db56e95123467cf
max <max.0x71@gmail.com>
2021-04-01 13:13:35 +0100

    added info log

---
+++ utils/logger.js
@@ -0,0 +1,65 @@
+function info(string, type) {
+    let color
+    switch (type) {
+        case types.INFO:
+            color = "\x1b[37m"
+            break
+        case types.GUILD:
+            color = "\x1b[36m"
+            break
+        case types.ECONOMY:
+            color = "\x1b[32m"
+            break
+        case types.DATA:
+            color = "\x1b[37m"
+            break
+        case types.AUTOMATION:
+            color = "\x1b[34m"
+            break
+        case types.COMMAND:
+            color = "\x1b[33m"
+    }
+
+    const out = `${color}[${getTimestamp()}] [${type}] ${string} \x1b[0m`
+}
+
+exports.info = info
+
+const types = {
+    INFO: "info",
+    DATA: "data",
+    GUILD: "guild",
+    ECONOMY: "eco",
+    AUTOMATION: "auto",
+    COMMAND: "command"
+}
+
+exports.types = types
+
+/**
+ * @returns {String}
+ */
+function getTimestamp() {
+    const date = new Date()
+    let hours = date.getHours().toString()
+    let minutes = date.getMinutes().toString()
+    let seconds = date.getSeconds().toString()
+
+    if (hours.length == 1) {
+        hours = "0" + hours
+    }
+
+    if (minutes.length == 1) {
+        minutes = "0" + minutes
+    }
+
+    if (seconds.length == 1) {
+        seconds = "0" + seconds
+    }
+
+    const timestamp = hours + ":" + minutes + ":" + seconds
+
+    return timestamp
+}
+
+exports.getTimestamp = getTimestamp
