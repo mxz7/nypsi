843369d12b34efbf3da586799240e6f3f1e9046f
max <max.0x71@gmail.com>
2021-04-01 13:13:43 +0100

    moved gettimestamp

--- utils/utils.js
+++ utils/utils.js
@@ -264,34 +264,6 @@ function formatDate(date) {
 exports.formatDate = formatDate
 
 /**
- * @returns {String}
- */
-function getTimestamp() {
-    const date = new Date()
-    let hours = date.getHours().toString()
-    let minutes = date.getMinutes().toString()
-    let seconds = date.getSeconds().toString()
-
-    if (hours.length == 1) {
-        hours = "0" + hours
-    }
-
-    if (minutes.length == 1) {
-        minutes = "0" + minutes
-    }
-
-    if (seconds.length == 1) {
-        seconds = "0" + seconds
-    }
-
-    const timestamp = hours + ":" + minutes + ":" + seconds
-
-    return timestamp
-}
-
-exports.getTimestamp = getTimestamp
-
-/**
  * @returns {Number}
  * @param {Date} date
  */
