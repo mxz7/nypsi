f92003caba2bbe883678a78ee5dbf6240716361e
max <29782397+tekoh@users.noreply.github.com>
2021-04-21 16:32:01 +0100

    boobiesx2

--- commands/urban.js
+++ commands/urban.js
@@ -64,10 +64,7 @@ async function run(message, args) {
 
     const result = results[0]
 
-    if (!result) {
-        return message.channel.send(new ErrorEmbed("unknown definition"))
-    }
-
+    if (!result) return
     if (!result.word) return
 
     const embed = new CustomEmbed(
