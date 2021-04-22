20e886a022055ddc752fa9cf713380187fef0540
max <29782397+tekoh@users.noreply.github.com>
2021-04-22 17:30:26 +0100

    renamed original stats command

--- commands/botstats.js
+++ commands/botstats.js
@@ -5,7 +5,7 @@ const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders.js")
 
 const cooldown = new Map()
 
-const cmd = new Command("stats", "view stats for the bot", categories.INFO)
+const cmd = new Command("botstats", "view stats for the bot", categories.INFO)
 
 /**
  * @param {Message} message
