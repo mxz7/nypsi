9bdca840d60dc53fc245fc593df1d2807761d303
max <29782397+tekoh@users.noreply.github.com>
2021-04-25 21:18:15 +0100

    added servericon command

---
+++ commands/servericon.js
@@ -0,0 +1,20 @@
+const { Message } = require("discord.js")
+const { Command, categories } = require("../utils/classes/Command")
+const { CustomEmbed } = require("../utils/classes/EmbedBuilders")
+
+const cmd = new Command("servericon", "get the server icon", categories.INFO)
+
+/**
+ * @param {Message} message
+ * @param {Array<String>} args
+ */
+async function run(message, args) {
+    return message.channel.send(new CustomEmbed(message.member, false).setImage(message.guild.iconURL({
+        size: 256,
+        dynamic: true,
+    })))
+}
+
+cmd.setRun(run)
+
+module.exports = cmd
