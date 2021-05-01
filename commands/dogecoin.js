a2cdb44e99f31522fbc354741232559f83bad06b
max <max.0x71@gmail.com>
2021-05-01 16:00:29 +0100

    added titles to bitcoin and dogecoin

--- commands/dogecoin.js
+++ commands/dogecoin.js
@@ -30,7 +30,7 @@ async function run(message, args) {
         `**worth** $${dogecoin.worth.toLocaleString()}\n**owned** ${dogecoinAmount.toLocaleString()} ($${(
             dogecoinAmount * dogecoin.worth
         ).toLocaleString()})`
-    ).setFooter("not real dogecoin, although it reflects current worth in USD x 1000")
+    ).setFooter("not real dogecoin, although it reflects current worth in USD x 1000").setTitle("dogecoin | " + message.author.username)
 
     return message.channel.send(embed)
 }
