6dd7278399f543ea382c4f46f07a33152a3da206
[bot] formatting <actions@users.noreply.github.com>
2021-05-03 20:49:37 +0000

    prettier

--- events/roleDelete.js
+++ events/roleDelete.js
@@ -2,8 +2,8 @@ const { Role } = require("discord.js")
 const { getMuteRole, setMuteRole } = require("../utils/moderation/utils")
 
 /**
- * 
- * @param {Role} role 
+ *
+ * @param {Role} role
  */
 module.exports = (role) => {
     if (getMuteRole(role.guild) == role.id) {
