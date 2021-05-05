773e919eb06fcc08cae3404456c4647338e4be07
[bot] formatting <actions@users.noreply.github.com>
2021-05-05 18:01:35 +0000

    prettier

--- utils/economy/utils.js
+++ utils/economy/utils.js
@@ -246,11 +246,14 @@ async function doVote(client, vote) {
                     `+ **${getPrestige(memberID) + 1}** vote crates`
             )
 
-        await member.send("thank you for voting!", embed).then(() => {
-            info(`sent vote confirmation to ${member.tag}`, types.ECONOMY)
-        }).catch(() => {
-            error(`failed to send vote confirmation to ${member.tag}`)
-        })
+        await member
+            .send("thank you for voting!", embed)
+            .then(() => {
+                info(`sent vote confirmation to ${member.tag}`, types.ECONOMY)
+            })
+            .catch(() => {
+                error(`failed to send vote confirmation to ${member.tag}`)
+            })
     }
 }
 
