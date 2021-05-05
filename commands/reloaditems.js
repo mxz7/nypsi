19fea06273c2ad9e7983f5cacb799d3bc3258d5b
max <29782397+tekoh@users.noreply.github.com>
2021-05-05 21:10:31 +0100

    added reloaditems command

---
+++ commands/reloaditems.js
@@ -0,0 +1,22 @@
+const { Message } = require("discord.js")
+const { Command, categories } = require("../utils/classes/Command")
+const { CustomEmbed } = require("../utils/classes/EmbedBuilders")
+const { loadItems } = require("../utils/economy/utils")
+
+const cmd = new Command("reloaditems", "reload items", categories.NONE).setPermissions("bot owner")
+
+/**
+ * @param {Message} message
+ * @param {Array<String>} args
+ */
+async function run(message, args) {
+    if (message.member.user.id != "672793821850894347") return
+
+    const d = loadItems()
+
+    return message.channel.send(new CustomEmbed(message.member, false, d))
+}
+
+cmd.setRun(run)
+
+module.exports = cmd
