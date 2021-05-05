99e47fd6cb2564e438ea2957af98f48b4839b1f2
max <29782397+tekoh@users.noreply.github.com>
2021-05-05 16:58:12 +0100

    item amounts to locale string

--- commands/inventory.js
+++ commands/inventory.js
@@ -122,7 +122,7 @@ async function run(message, args) {
         item = items[item]
         embed.addField(
             item.id,
-            `${item.emoji} **${item.name}** -- ${inventory[item.id]}\n${item.description}${
+            `${item.emoji} **${item.name}** -- ${inventory[item.id].toLocaleString()}\n${item.description}${
                 item.worth ? "\n*can be sold*" : item.role == "collectable" ? "\n*collectable*" : ""
             }`,
             true
@@ -168,7 +168,7 @@ async function run(message, args) {
                         item = items[item]
                         newEmbed.addField(
                             item.id,
-                            `${item.emoji} **${item.name}** -- ${inventory[item.id]}\n${
+                            `${item.emoji} **${item.name}** -- ${inventory[item.id].toLocaleString()}\n${
                                 item.description
                             }${
                                 item.worth
@@ -197,7 +197,7 @@ async function run(message, args) {
                         item = items[item]
                         newEmbed.addField(
                             item.id,
-                            `${item.emoji} **${item.name}** -- ${inventory[item.id]}\n${
+                            `${item.emoji} **${item.name}** -- ${inventory[item.id].toLocaleString()}\n${
                                 item.description
                             }${
                                 item.worth
