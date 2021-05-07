cf5ceaf2a6c3a7bb7a3c264ae9d9527fe19be498
max <max.0x71@gmail.com>
2021-05-07 16:42:18 +0100

    no longer get a car from fishing

--- commands/fish.js
+++ commands/fish.js
@@ -131,6 +131,7 @@ async function run(message, args) {
             if (items[i]) {
                 if (items[i].role == "prey") continue
                 if (items[i].role == "tool") continue
+                if (items[i].role == "car") continue
                 if (items[i].rarity == 4) {
                     const chance = Math.floor(Math.random() * 15)
                     if (chance == 4 && fishingRod == "incredible_fishing_rod") {
