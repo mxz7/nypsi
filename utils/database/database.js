3940795c1f9d9b1f9ba9d95057908694c7a784dd
max <max.0x71@gmail.com>
2021-05-13 22:52:25 +0100

    create database tables

---
+++ utils/database/database.js
@@ -0,0 +1,28 @@
+const db = require("better-sqlite3")("./utils/database/storage.db")
+
+function createTables() {
+    db.prepare("CREATE TABLE IF NOT EXISTS economy ('id' TEXT PRIMARY KEY, 'money' INTEGER, 'bank' INTEGER, 'xp' INTEGER, 'prestige' INTEGER, 'padlock' BOOLEAN, 'dms' BOOLEAN, 'last_vote' BOOLEAN, 'inventory' TEXT, 'workers' TEXT)").run()
+
+    db.prepare("CREATE TABLE IF NOT EXISTS guilds ('id' TEXT PRIMARY KEY, 'peak' INTEGER, 'counter' TEXT, 'xmas' TEXT, 'disabled_commands' TEXT, 'snipe_filter' TEXT, 'chat_filter' TEXT, 'prefix' TEXT, 'countdowns' TEXT)").run()
+
+    db.prepare("CREATE TABLE IF NOT EXISTS moderation ('id' TEXT PRIMARY KEY, 'case_count' INTEGER, 'mute_role' TEXT)").run()
+
+    db.prepare("CREATE TABLE IF NOT EXISTS moderation_cases ('case_id' TEXT, 'type' TEXT, 'user' TEXT, 'moderatir' TEXT, 'command' TEXT, 'time' INTEGER, 'deleted' BOOLEAN, 'guild_id' TEXT, FOREIGN KEY (guild_id) REFERENCES moderation (id))").run()
+
+    db.prepare("CREATE TABLE IF NOT EXISTS moderation_mutes ('user' TEXT, 'unmute_time' NUMBER, 'guild_id' TEXT, FOREIGN KEY (guild_id) REFERENCES moderation (id))").run()
+
+    db.prepare("CREATE TABLE IF NOT EXISTS moderation_bans ('user' TEXT, 'unban_time' NUMBER, 'guild_id' TEXT, FOREIGN KEY (guild_id) REFERENCES moderation (id))").run()
+}
+
+createTables()
+
+/**
+ * 
+ * @param {String} string string from database
+ * @param {String} seperator optional seperator
+ */
+function toArray(string, seperator) {
+    return string.split(seperator || "|")
+}
+
+exports.toArray = toArray
