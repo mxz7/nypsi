bf00baeb15dbab150c9184f577e080a021ad0e09
max <max.0x71@gmail.com>
2021-05-13 23:08:38 +0100

    default values

--- utils/database/database.js
+++ utils/database/database.js
@@ -1,21 +1,22 @@
 const Database = require("better-sqlite3")
-const db = new Database("./utils/database/storage.db")
+const { info } = require("../logger")
+const db = new Database("./utils/database/storage.db", { verbose: info })
 
 function createTables() {
     db.prepare(
-        "CREATE TABLE IF NOT EXISTS economy ('id' TEXT PRIMARY KEY, 'money' INTEGER, 'bank' INTEGER, 'xp' INTEGER, 'prestige' INTEGER, 'padlock' BOOLEAN, 'dms' BOOLEAN, 'last_vote' BOOLEAN, 'inventory' TEXT, 'workers' TEXT)"
+        "CREATE TABLE IF NOT EXISTS economy ('id' TEXT PRIMARY KEY, 'money' INTEGER DEFAULT 500, 'bank' INTEGER DEFAULT 4500, 'xp' INTEGER DEFAULT 0, 'prestige' INTEGER DEFAULT 0, 'padlock' BOOLEAN DEFAULT FALSE, 'dms' BOOLEAN DEFAULT FALSE, 'last_vote' BOOLEAN DEFAULSE FALSE, 'inventory' TEXT DEFAULT '{}', 'workers' TEXT DEFAULT '{}')"
     ).run()
 
     db.prepare(
-        "CREATE TABLE IF NOT EXISTS guilds ('id' TEXT PRIMARY KEY, 'peak' INTEGER, 'counter' TEXT, 'xmas' TEXT, 'disabled_commands' TEXT, 'snipe_filter' TEXT, 'chat_filter' TEXT, 'prefix' TEXT, 'countdowns' TEXT)"
+        "CREATE TABLE IF NOT EXISTS guilds ('id' TEXT PRIMARY KEY, 'peak' INTEGER DEFAULT -, 'counter' TEXT DEFAULT '{}', 'xmas' TEXT DEFAULT '{}', 'disabled_commands' TEXT DEFAULT '', 'snipe_filter' TEXT DEFAULT '', 'chat_filter' TEXT DEFAULT '', 'prefix' TEXT DEFAULT '$', 'countdowns' TEXT DEFAULT '{}')"
     ).run()
 
     db.prepare(
-        "CREATE TABLE IF NOT EXISTS moderation ('id' TEXT PRIMARY KEY, 'case_count' INTEGER, 'mute_role' TEXT)"
+        "CREATE TABLE IF NOT EXISTS moderation ('id' TEXT PRIMARY KEY, 'case_count' INTEGER DEFAULT 0, 'mute_role' TEXT DEFAULT '')"
     ).run()
 
     db.prepare(
-        "CREATE TABLE IF NOT EXISTS moderation_cases ('case_id' TEXT, 'type' TEXT, 'user' TEXT, 'moderatir' TEXT, 'command' TEXT, 'time' INTEGER, 'deleted' BOOLEAN, 'guild_id' TEXT, FOREIGN KEY (guild_id) REFERENCES moderation (id))"
+        "CREATE TABLE IF NOT EXISTS moderation_cases ('case_id' TEXT, 'type' TEXT, 'user' TEXT, 'moderator' TEXT, 'command' TEXT, 'time' INTEGER, 'deleted' BOOLEAN, 'guild_id' TEXT, FOREIGN KEY (guild_id) REFERENCES moderation (id))"
     ).run()
 
     db.prepare(
