6ae9d71397e03708a8cf7a2093eb6a4e8a2c8405
max <max.0x71@gmail.com>
2021-05-21 21:30:26 +0100

    🤞 HOPEFULLY FIXED BOT CRASH

--- events/message.js
+++ events/message.js
@@ -1,8 +1,14 @@
-const { Message, MessageEmbed } = require("discord.js")
+const { Message, MessageEmbed, Collection } = require("discord.js")
 const { mentions } = require("../nypsi")
-const { getChatFilter, getPrefix, inCooldown } = require("../utils/guilds/utils")
+const { getChatFilter, getPrefix, inCooldown, addCooldown } = require("../utils/guilds/utils")
 const { runCommand } = require("../utils/commandhandler")
 const { info } = require("../utils/logger")
+
+/**
+ * @type {Array<{ type: String, members: Collection, message: Message, guild: String }>}
+ */
+const mentionQueue = []
+let mentionInterval
 
 /**
  * @param {Message} message
@@ -36,66 +42,51 @@ module.exports = async (message) => {
         }
     }
 
-    const addMention = (m) => {
-        if (m.user.bot || m.user.id == message.author.id) {
-            return
+    if (message.guild.memberCount < 50000) {
+        if (message.mentions.everyone) {
+            let members = message.channel.members
+
+            if (!inCooldown(message.guild)) {
+                await message.guild.members.fetch()
+                addCooldown(message.guild, 3600)
+            }
+
+            mentionQueue.push({
+                type: "collection",
+                members: members.clone(),
+                message: message
+            })
+
+            if (!mentionInterval) {
+                mentionInterval = setInterval(() => addMention(), 25)
+            }
+        } else {
+            if (message.mentions.roles.first()) {
+                message.mentions.roles.forEach((r) => {
+                    mentionQueue.push({
+                        type: "collection",
+                        members: r.members.clone(),
+                        message: message
+                    })
+                })
+
+                if (!mentionInterval) {
+                    mentionInterval = setInterval(() => addMention(), 25)
+                }
+            }
+        
+            if (message.mentions.members.first()) {
+                mentionQueue.push({
+                    type: "collection",
+                    members: message.mentions.members.clone(),
+                    message: message
+                })
+
+                if (!mentionInterval) {
+                    mentionInterval = setInterval(() => addMention(), 25)
+                }
+            }
         }
-
-        if (!message.channel.members.find((mm) => mm.id == m.user.id)) return
-
-        let content = message.content
-
-        if (content.length > 100) {
-            content = content.substr(0, 97) + "..."
-        }
-
-        content = content.replace(/(\r\n|\n|\r)/gm, " ")
-
-        const data = {
-            user: message.author.tag,
-            content: content,
-            date: message.createdTimestamp,
-            link: message.url,
-        }
-
-        if (!mentions.has(message.guild.id)) {
-            mentions.set(message.guild.id, new Map())
-        }
-
-        const guildData = mentions.get(message.guild.id)
-
-        if (!guildData.has(m.user.id)) {
-            guildData.set(m.user.id, [])
-        }
-
-        const userData = guildData.get(m.user.id)
-
-        if (userData.length >= 15) {
-            userData.shift()
-        }
-
-        userData.push(data)
-
-        guildData.set(m.user.id, userData)
-        mentions.set(message.guild.id, guildData)
-
-        exports.mentions = mentions
-    }
-
-    if (message.mentions.everyone) {
-        let members = message.channel.members
-
-        if (!inCooldown(message.guild)) {
-            await message.guild.members.fetch()
-        }
-
-        members.forEach((m) => addMention(m))
-    } else if (message.mentions.roles.first()) {
-        message.mentions.roles.forEach((r) => {
-            r.members.forEach((m) => addMention(m))
-        })
-    } else if (message.mentions.members.first()) {
-        message.mentions.members.forEach((m) => addMention(m))
     }
 
     let prefix = getPrefix(message.guild)
@@ -113,4 +104,82 @@ module.exports = async (message) => {
     const cmd = args[0].toLowerCase()
 
     return runCommand(cmd, message, args)
+}
+
+function addMention() {
+    const mention = mentionQueue.shift()
+
+    console.log(mention)
+    console.log(mentionQueue)
+
+    if (mention.type == "collection") {
+        const members = mention.members
+
+        let content = mention.message.content
+
+        if (content.length > 100) {
+            content = content.substr(0, 97) + "..."
+        }
+
+        content = content.replace(/(\r\n|\n|\r)/gm, " ")
+
+        let count = 0
+
+        for (const memberID of Array.from(members.keys())) {
+            if (count >= 15) return
+            const member = members.get(memberID)
+
+            if (member.user.bot) continue
+            if (member.user.id == mention.message.author.id) continue
+
+            const data = {
+                user: mention.message.author.tag,
+                content: content,
+                date: mention.message.createdTimestamp,
+                link: mention.message.url
+            }
+
+            const guild = mention.message.guild.id
+
+            mentionQueue.push({
+                type: "mention",
+                data: data,
+                guild: guild,
+                target: member.user.id
+            })
+            count++
+        }
+    } else {
+        const guild = mention.guild
+        const data = mention.data
+        const target = mention.target
+
+        if (!mentions.has(guild)) {
+            mentions.set(guild, new Map())
+        }
+
+        const guildData = mentions.get(guild)
+
+        if (!guildData.has(target)) {
+            guildData.set(target, [])
+        }
+
+        const userData = guildData.get(target)
+
+        if (userData.length >= 15) {
+            userData.shift()
+        }
+
+        userData.push(data)
+
+        guildData.set(target, userData)
+        mentions.set(guild, guildData)
+
+        exports.mentions = mentions
+    }
+
+    if (mentionQueue.length == 0) {
+        clearInterval(mentionInterval)
+        mentionInterval = undefined
+    }
 }
