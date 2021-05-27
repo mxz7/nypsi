8db4f38ea31facf8af265562a98a4b113af10f2c
max <max.0x71@gmail.com>
2021-05-27 18:21:57 +0100

    changed from guildmemberupdate to userupdate

---
+++ events/userUpdate.js
@@ -0,0 +1,34 @@
+const { User } = require("discord.js")
+const { usernameProfileExists, createUsernameProfile, addNewUsername, addNewAvatar } = require("../utils/users/utils")
+
+/**
+ * 
+ * @param {User} oldUser 
+ * @param {User} newUser 
+ */
+module.exports = async (oldUser, newUser) => {
+    console.log(oldUser.tag == newUser.tag)
+    console.log(
+        oldUser.displayAvatarURL({ dynamic: true, size: 256 }) ==
+            newUser.displayAvatarURL({ dynamic: true, size: 256 })
+    )
+
+    if (oldUser.tag != newUser.tag) {
+        if (!usernameProfileExists(newUser.id)) {
+            createUsernameProfile(newUser.id)
+        } else {
+            addNewUsername(newUser.id, newUser.tag)
+        }
+    }
+
+    if (
+        oldUser.displayAvatarURL({ dynamic: true, size: 256 }) !=
+        newUser.displayAvatarURL({ dynamic: true, size: 256 })
+    ) {
+        if (!usernameProfileExists(newUser.id)) {
+            createUsernameProfile(newUser.id)
+        } else {
+            addNewAvatar(newUser.id, newUser.displayAvatarURL({ dynamic: true, size: 256 }))
+        }
+    }
+}
