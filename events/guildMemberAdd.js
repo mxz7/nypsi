9988459f2fe6b2f1a6dbba83f115b87ce0640c06
max <29782397+tekoh@users.noreply.github.com>
2021-06-11 12:23:43 +0100

    prevent so many peak update logs

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,13 +2,22 @@ const { GuildMember } = require("discord.js")
 const { runCheck, hasGuild, createGuild } = require("../utils/guilds/utils")
 const { profileExists, isMuted, deleteMute, getMuteRole } = require("../utils/moderation/utils")
 
+const queue = new Set()
+
 /**
  * @param {GuildMember} member
  */
 module.exports = async (member) => {
     if (!hasGuild(member.guild)) createGuild(member.guild)
 
-    runCheck(member.guild)
+    if (!queue.has(member.guild.id)) {
+        queue.add(member.guild.id)
+
+        setTimeout(() => {
+            runCheck(member.guild)
+            queue.delete(member.guild.id)
+        }, 30000)
+    }
 
     if (!profileExists(member.guild)) return
 
