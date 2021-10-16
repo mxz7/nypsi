bdf282c904cf56944959ef20a434c57c3f1b98c7
max <max.0x71@gmail.com>
2021-10-16 15:21:21 +0100

    birb

--- commands/birb.js
+++ commands/birb.js
@@ -41,7 +41,7 @@ async function run(message, args) {
     const { birbCache } = require("../utils/imghandler")
 
     if (birbCache.size < 1) {
-        return message.channel.send(new ErrorEmbed("please wait a couple more seconds.."))
+        return message.channel.send({embeds: [new ErrorEmbed("please wait a couple more seconds..")]})
     }
 
     cooldown.set(message.member.id, new Date())
@@ -61,7 +61,7 @@ async function run(message, args) {
     const a = await redditImage(chosen, allowed)
 
     if (a == "lol") {
-        return message.channel.send(new ErrorEmbed("unable to find birb image"))
+        return message.channel.send({embeds: [new ErrorEmbed("unable to find birb image")]})
     }
 
     const image = a.split("|")[0]
