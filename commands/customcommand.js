f8209030db7e4b117cdb3e9a26317ecdba61802a
max <max.0x71@gmail.com>
2021-10-16 19:31:13 +0100

    customcommand

--- commands/customcommand.js
+++ commands/customcommand.js
@@ -46,15 +46,15 @@ const filterxd = [
  */
 async function run(message, args) {
     if (!isPremium(message.author.id)) {
-        return message.channel.send(
-            new ErrorEmbed("you must be at least GOLD tier for this command")
-        )
+        return message.channel.send({
+            embeds: [new ErrorEmbed("you must be at least GOLD tier for this command")]
+        })
     }
 
     if (getTier(message.author.id) < 3) {
-        return message.channel.send(
-            new ErrorEmbed("you must be at least GOLD tier for this command")
-        )
+        return message.channel.send({
+            embeds: [new ErrorEmbed("you must be at least GOLD tier for this command")]
+        })
     }
 
     if (args.length == 0) {
@@ -85,15 +85,15 @@ async function run(message, args) {
         const content = args.join(" ")
 
         if (content.length > 100) {
-            return message.channel.send(new ErrorEmbed("content must be 100 characters or less"))
+            return message.channel.send({embeds: [new ErrorEmbed("content must be 100 characters or less")]})
         }
 
         if (content.split("\n").length > 4) {
-            return message.channel.send(
-                new ErrorEmbed(
+            return message.channel.send({
+                embeds: [new ErrorEmbed(
                     "please make sure that your custom command cant be used to flood chat"
-                )
-            )
+                )]
+            })
         }
 
         let contentToTest = message.content.toLowerCase().normalize("NFD")
@@ -102,23 +102,23 @@ async function run(message, args) {
 
         for (const word of filterxd) {
             if (contentToTest.includes(word)) {
-                return message.channel.send(new ErrorEmbed("explicit content 🙄"))
+                return message.channel.send({embeds: [new ErrorEmbed("explicit content 🙄")]})
             }
         }
 
-        await message.channel.send(
-            new CustomEmbed(message.member, false, "please enter your command name / trigger")
-        )
+        await message.channel.send({
+            embeds: [new CustomEmbed(message.member, false, "please enter your command name / trigger")]
+        })
 
         const filter = (msg) => message.author.id == msg.author.id
 
         let fail = false
 
         let res = await message.channel
-            .awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] })
+            .awaitMessages({ filter, max: 1, time: 30000, errors: ["time"] })
             .catch(() => {
                 fail = true
-                return message.channel.send(new ErrorEmbed("you took too long"))
+                return message.channel.send({embeds: [new ErrorEmbed("you took too long")]})
             })
 
         if (fail) return
@@ -126,9 +126,9 @@ async function run(message, args) {
         res = res.first().content.split(" ")[0].toLowerCase()
 
         if (res.length > 25) {
-            return message.channel.send(
-                new ErrorEmbed("trigger cannot be longer than 25 characters")
-            )
+            return message.channel.send({
+                embeds: [new ErrorEmbed("trigger cannot be longer than 25 characters")]
+            })
         }
 
         let resToTest = res.normalize("NFD")
@@ -137,12 +137,12 @@ async function run(message, args) {
 
         for (const word of filterxd) {
             if (resToTest.includes(word)) {
-                return message.channel.send(new ErrorEmbed("explicit content 🙄"))
+                return message.channel.send({embeds: [new ErrorEmbed("explicit content 🙄")]})
             }
         }
 
         if (commandExists(res)) {
-            return message.channel.send(new ErrorEmbed("this command already exists"))
+            return message.channel.send({embeds: [new ErrorEmbed("this command already exists")]})
         }
 
         let trigger = getUserCommand(message.author.id)
@@ -152,14 +152,14 @@ async function run(message, args) {
         }
 
         if (getCommand(res) && trigger != res) {
-            return message.channel.send(new ErrorEmbed("this command already exists"))
+            return message.channel.send({embeds: [new ErrorEmbed("this command already exists")]})
         }
 
         setCommand(message.author.id, res, content)
 
-        return message.channel.send(
-            new CustomEmbed(message.member, false, "✅ your custom command has been updated")
-        )
+        return message.channel.send({
+            embeds: [new CustomEmbed(message.member, false, "✅ your custom command has been updated")]
+        })
     }
 }
 
