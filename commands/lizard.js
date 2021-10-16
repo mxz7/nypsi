5ec8512236d17937fe1b6a26f3cb7a9cd0424dae
max <29782397+tekoh@users.noreply.github.com>
2021-10-16 20:18:14 +0100

    lizard

--- commands/lizard.js
+++ commands/lizard.js
@@ -41,7 +41,7 @@ async function run(message, args) {
     const { lizardCache } = require("../utils/imghandler")
 
     if (lizardCache.size < 1) {
-        return message.channel.send(new ErrorEmbed("please wait a couple more seconds.."))
+        return message.channel.send({embeds: [new ErrorEmbed("please wait a couple more seconds..")]})
     }
 
     cooldown.set(message.member.id, new Date())
