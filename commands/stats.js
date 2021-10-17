2d8f2237615dbdceb51fe3ac9b9019f2a20a4227
max <29782397+tekoh@users.noreply.github.com>
2021-10-17 15:53:37 +0100

    stats buttons

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,4 @@
-const { Message } = require("discord.js")
+const { Message, MessageActionRow, MessageButton } = require("discord.js")
 const { Command, categories } = require("../utils/classes/Command")
 const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders")
 const { getStats, hasStatsProfile, createStatsProfile } = require("../utils/economy/utils")
@@ -123,35 +123,43 @@ async function run(message, args) {
             )
         }
 
-        const msg = await message.channel.send({ embeds: [embed] })
+        let row = new MessageActionRow().addComponents(
+            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(true),
+            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY")
+        )
+
+        /**
+         * @type {Message}
+         */
+        let msg
+
+        if (pages.size == 1) {
+            return await message.channel.send({ embeds: [embed] })
+        } else {
+            msg = await message.channel.send({ embeds: [embed], components: [row] })
+        }
 
         if (pages.size == 0) return
-
-        await msg.react("⬅")
-        await msg.react("➡")
 
         let currentPage = 1
         const lastPage = pages.size
 
-        const filter = (reaction, user) => {
-            return ["⬅", "➡"].includes(reaction.emoji.name) && user.id == message.member.user.id
-        }
+        const filter = (i) => i.user.id == message.author.id
 
-        async function pageManager() {
+        const pageManager = async () => {
             const reaction = await msg
-                .awaitReactions({ filter, max: 1, time: 30000, errors: ["time"] })
-                .then((collected) => {
-                    return collected.first().emoji.name
+                .awaitMessageComponent({ filter, time: 30000, errors: ["time"] })
+                .then(async (collected) => {
+                    await collected.deferUpdate()
+                    return collected.customId
                 })
                 .catch(async () => {
-                    await msg.reactions.removeAll()
+                    await msg.edit({ components: [] })
                 })
 
             if (!reaction) return
 
-            const newEmbed = new CustomEmbed(message.member, false).setTitle(
-                "item stats | " + message.author.username
-            )
+            const newEmbed = new CustomEmbed(message.member, false).setTitle("item stats | " + message.author.username)
 
             if (reaction == "⬅") {
                 if (currentPage <= 1) {
@@ -160,15 +168,22 @@ async function run(message, args) {
                     currentPage--
 
                     for (const item of pages.get(currentPage)) {
-                        newEmbed.addField(
-                            item,
-                            `**${stats[item].toLocaleString()}** use${stats[item] > 1 ? "s" : ""}`,
-                            true
-                        )
+                        newEmbed.addField(item, `**${stats[item].toLocaleString()}** use${stats[item] > 1 ? "s" : ""}`, true)
                     }
 
                     newEmbed.setFooter(`page ${currentPage}/${lastPage}`)
-                    await msg.edit({embeds: [newEmbed]})
+                    if (currentPage == 1) {
+                        row = new MessageActionRow().addComponents(
+                            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(true),
+                            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY").setDisabled(false)
+                        )
+                    } else {
+                        row = new MessageActionRow().addComponents(
+                            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(false),
+                            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY").setDisabled(false)
+                        )
+                    }
+                    await msg.edit({ embeds: [newEmbed], components: [row] })
                     return pageManager()
                 }
             } else if (reaction == "➡") {
@@ -178,15 +193,22 @@ async function run(message, args) {
                     currentPage++
 
                     for (const item of pages.get(currentPage)) {
-                        newEmbed.addField(
-                            item,
-                            `**${stats[item].toLocaleString()}** use${stats[item] > 1 ? "s" : ""}`,
-                            true
-                        )
+                        newEmbed.addField(item, `**${stats[item].toLocaleString()}** use${stats[item] > 1 ? "s" : ""}`, true)
                     }
 
                     newEmbed.setFooter(`page ${currentPage}/${lastPage}`)
-                    await msg.edit({embeds: [newEmbed]})
+                    if (currentPage == lastPage) {
+                        row = new MessageActionRow().addComponents(
+                            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(false),
+                            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY").setDisabled(true)
+                        )
+                    } else {
+                        row = new MessageActionRow().addComponents(
+                            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(false),
+                            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY").setDisabled(false)
+                        )
+                    }
+                    await msg.edit({ embeds: [newEmbed], components: [row] })
                     return pageManager()
                 }
             }
