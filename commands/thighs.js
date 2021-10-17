465304e76c8d17e910e5c3f86b3cc8cf0e01c913
max <29782397+tekoh@users.noreply.github.com>
2021-10-17 13:06:21 +0100

    thighs

--- commands/thighs.js
+++ commands/thighs.js
@@ -65,7 +65,7 @@ async function run(message, args) {
     const a = await redditImage(chosen, allowed)
 
     if (a == "lol") {
-        return message.channel.send(new ErrorEmbed("unable to find thighs image"))
+        return message.channel.send({embeds: [new ErrorEmbed("unable to find thighs image")]})
     }
 
     const image = a.split("|")[0]
