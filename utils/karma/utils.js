510747ae019c718d8eb15c56819f35af2aaf3ed8
max <max.0x71@gmail.com>
2021-12-16 15:48:46 +0000

    yaya

--- utils/karma/utils.js
+++ utils/karma/utils.js
@@ -62,6 +62,10 @@ function removeKarma(member, amount) {
     if (!query) {
         db.prepare("INSERT INTO karma (id, karma) VALUES (?, ?)").run(id, 1)
     } else {
+        console.log(query.karma)
+        if (amount > query.karma) {
+            amount = query.karma - 1
+        }
         db.prepare("UPDATE karma SET karma = karma - ? WHERE id = ?").run(id, amount)
     }
 }
