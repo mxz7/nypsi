adf6ecba9f71a102966de1a558531411e8e3fa9d
max <max.0x71@gmail.com>
2021-12-16 15:44:50 +0000

    add karma to owner on bot add

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,6 @@
 const { Guild, Client } = require("discord.js")
 const { hasGuild, createGuild, runCheck } = require("../utils/guilds/utils")
+const { addKarma } = require("../utils/karma/utils")
 const { info, types } = require("../utils/logger")
 
 /**
@@ -8,6 +9,10 @@ const { info, types } = require("../utils/logger")
  */
 module.exports = async (client, guild) => {
     info(`added to ${guild.name} (${guild.id}) new count: ${client.guilds.cache.size}`, types.GUILD)
+
     if (!hasGuild(guild)) createGuild(guild)
+
     runCheck(guild)
+
+    addKarma(guild.ownerId, Math.floor(guild.memberCount / 4))
 }
