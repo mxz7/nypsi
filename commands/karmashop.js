37325591bfa72084e2fa0e36ddb11623a8b43ead
max <max.0x71@gmail.com>
2022-01-06 21:52:05 +0000

    added cooldown

--- commands/karmashop.js
+++ commands/karmashop.js
@@ -6,16 +6,45 @@ const { isKarmaShopOpen } = require("../utils/karma/utils")
 
 const cmd = new Command("karmashop", "buy stuff with your karma", categories.INFO)
 
+const cooldown = new Map()
+
 /**
  * @param {Message} message
  * @param {Array<String>} args
  */
 async function run(message, args) {
+    if (cooldown.has(message.member.id)) {
+        const init = cooldown.get(message.member.id)
+        const curr = new Date()
+        const diff = Math.round((curr - init) / 1000)
+        const time = 10 - diff
+
+        const minutes = Math.floor(time / 60)
+        const seconds = time - minutes * 60
+
+        let remaining
+
+        if (minutes != 0) {
+            remaining = `${minutes}m${seconds}s`
+        } else {
+            remaining = `${seconds}s`
+        }
+        return message.channel.send({ embeds: [new ErrorEmbed(`still on cooldown for \`${remaining}\``)] })
+    }
+
+    cooldown.set(message.member.id, new Date())
+
+    setTimeout(() => {
+        cooldown.delete(message.author.id)
+    }, 10000)
+    
     if (!isKarmaShopOpen()) {
         const embed = new CustomEmbed(message.member, false).setTitle("karma shop")
         embed.setDescription("the karma shop is currently closed ❌")
         return message.channel.send({ embeds: [embed] })
     }
+
+
 }
 
 cmd.setRun(run)
