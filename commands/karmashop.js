a9c5da142496aa4d34ed2d37d18bc8b2edc21dee
max <29782397+tekoh@users.noreply.github.com>
2022-01-06 22:20:10 +0000

    weewoo

--- commands/karmashop.js
+++ commands/karmashop.js
@@ -1,18 +1,28 @@
-const { Message } = require("discord.js")
+const { Message, MessageButton, MessageActionRow } = require("discord.js")
 const { getMember } = require("../utils/utils")
 const { Command, categories } = require("../utils/classes/Command")
 const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders.js")
-const { isKarmaShopOpen } = require("../utils/karma/utils")
+const { isKarmaShopOpen, getKarma, openKarmaShop, closeKarmaShop } = require("../utils/karma/utils")
+const { inPlaceSort } = require("fast-sort")
 
 const cmd = new Command("karmashop", "buy stuff with your karma", categories.INFO)
 
 const cooldown = new Map()
+const items = require("../utils/karma/items.json")
 
 /**
  * @param {Message} message
  * @param {Array<String>} args
  */
 async function run(message, args) {
+    if (message.author.id == "672793821850894347") {
+        if (args[0] && args[0].toLowerCase() == "open") {
+            return openKarmaShop()
+        } else if (args[0] && args[0].toLowerCase() == "close") {
+            return closeKarmaShop()
+        }
+    }
+
     if (cooldown.has(message.member.id)) {
         const init = cooldown.get(message.member.id)
         const curr = new Date()
@@ -42,6 +52,147 @@ async function run(message, args) {
         const embed = new CustomEmbed(message.member, false).setTitle("karma shop")
         embed.setDescription("the karma shop is currently closed ❌")
         return message.channel.send({ embeds: [embed] })
+    }
+
+    const itemIDs = Array.from(Object.keys(items))
+
+    inPlaceSort(itemIDs).asc(i => items[i].cost)
+
+    const pages = []
+
+    let pageOfItems = []
+    for (const item of itemIDs) {
+        if (pageOfItems.length == 6) {
+            pages.push(pageOfItems)
+            pageOfItems = [item]
+        } else {
+            pageOfItems.push(item)
+        }
+    }
+
+    if (pageOfItems.length != 0) {
+        pages.push(pageOfItems)
+    }
+
+    const page = 0
+
+    const embed = new CustomEmbed(message.member).setFooter(`page ${page + 1}/${pages.length}`)
+
+    embed.setTitle("karma shop | " + message.author.username)
+    embed.setFooter(`you have ${getKarma(message.member)} karma`)
+
+    for (let item of pages[page]) {
+        item = items[item]
+        embed.addField(
+            item.id,
+            `${item.emoji} **${item.name}**\n${item.description}\n**cost** ${item.cost.toLocaleString()} karma\n*${item.items_left}* available`,
+            true
+        )
+    }
+
+    let row = new MessageActionRow().addComponents(
+        new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(true),
+        new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY")
+    )
+
+    /**
+     * @type {Message}
+     */
+    let msg
+
+    if (pages.length == 1) {
+        return await message.channel.send({ embeds: [embed] })
+    } else {
+        msg = await message.channel.send({ embeds: [embed], components: [row] })
+    }
+
+    if (pages.length > 1) {
+        let currentPage = page
+
+        const lastPage = pages.length
+
+        const filter = (i) => i.user.id == message.author.id
+
+        const pageManager = async () => {
+            const reaction = await msg
+                .awaitMessageComponent({ filter, time: 30000, errors: ["time"] })
+                .then(async (collected) => {
+                    await collected.deferUpdate()
+                    return collected.customId
+                })
+                .catch(async () => {
+                    await msg.edit({ components: [] })
+                })
+
+            const newEmbed = new CustomEmbed(message.member).setTitle("karma shop | " + message.author.username)
+
+            if (!reaction) return
+
+            if (reaction == "⬅") {
+                if (currentPage <= 0) {
+                    return pageManager()
+                } else {
+                    currentPage--
+                    for (let item of pages[currentPage]) {
+                        item = items[item]
+                        embed.addField(
+                            item.id,
+                            `${item.emoji} **${item.name}**\n${
+                                item.description
+                            }\n**cost** ${item.cost.toLocaleString()} karma\n*${item.items_left}* available`,
+                            true
+                        )
+                    }
+                    newEmbed.setFooter(`page ${currentPage + 1}/${pages.length} | you have ${getKarma(message.member)} karma`)
+                    if (currentPage == 0) {
+                        row = new MessageActionRow().addComponents(
+                            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(true),
+                            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY").setDisabled(false)
+                        )
+                    } else {
+                        row = new MessageActionRow().addComponents(
+                            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(false),
+                            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY").setDisabled(false)
+                        )
+                    }
+                    await msg.edit({ embeds: [newEmbed], components: [row] })
+                    return pageManager()
+                }
+            } else if (reaction == "➡") {
+                if (currentPage + 1 >= lastPage) {
+                    return pageManager()
+                } else {
+                    currentPage++
+                    for (let item of pages[currentPage]) {
+                        item = items[item]
+                        embed.addField(
+                            item.id,
+                            `${item.emoji} **${item.name}**\n${
+                                item.description
+                            }\n**cost** ${item.cost.toLocaleString()} karma\n*${item.items_left}* available`,
+                            true
+                        )
+                    }
+                    newEmbed.setFooter(
+                        `page ${currentPage + 1}/${pages.length} | you have ${getKarma(message.member)} karma`
+                    )
+                    if (currentPage + 1 == lastPage) {
+                        row = new MessageActionRow().addComponents(
+                            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(false),
+                            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY").setDisabled(true)
+                        )
+                    } else {
+                        row = new MessageActionRow().addComponents(
+                            new MessageButton().setCustomId("⬅").setLabel("back").setStyle("PRIMARY").setDisabled(false),
+                            new MessageButton().setCustomId("➡").setLabel("next").setStyle("PRIMARY").setDisabled(false)
+                        )
+                    }
+                    await msg.edit({ embeds: [newEmbed], components: [row] })
+                    return pageManager()
+                }
+            }
+        }
+        return pageManager()
     }
 }
 
