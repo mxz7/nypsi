ce936ed5b4898c5ffe3af557b5481d7daca4e92a
max <29782397+tekoh@users.noreply.github.com>
2022-01-06 15:53:24 +0000

    added mention id

--- utils/database/database.js
+++ utils/database/database.js
@@ -70,7 +70,7 @@ function createTables() {
     ).run()
 
     db.prepare(
-        "CREATE TABLE IF NOT EXISTS mentions ('guild_id' TEXT NOT NULL, 'target_id' TEXT NOT NULL, 'date' INTEGER NOT NULL, 'user_tag' TEXT NOT NULL, 'url' TEXT NOT NULL, 'content' TEXT NOT NULL)"
+        "CREATE TABLE IF NOT EXISTS mentions ('mention_id' INTEGER NOT NULL PRIMARY KEY, 'guild_id' TEXT NOT NULL, 'target_id' TEXT NOT NULL, 'date' INTEGER NOT NULL, 'user_tag' TEXT NOT NULL, 'url' TEXT NOT NULL, 'content' TEXT NOT NULL)"
     ).run()
 }
 
