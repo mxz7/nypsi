7d39b2e5b9f6fcfdf76698458d5057ef558355e9
max <max.0x71@gmail.com>
2022-01-10 13:31:51 +0000

    dobackup command

---
+++ commands/dobackup.js
@@ -0,0 +1,22 @@
+const { Message } = require("discord.js")
+const { Command, categories } = require("../utils/classes/Command")
+const { CustomEmbed } = require("../utils/classes/EmbedBuilders")
+const { doBackup } = require("../utils/database/database")
+
+const cmd = new Command("dobackup", "start a database backup", categories.NONE).setPermissions(["bot owner"])
+
+/**
+ * @param {Message} message
+ * @param {Array<String>} args
+ */
+async function run(message, args) {
+    if (message.member.user.id != "672793821850894347") return
+    
+    doBackup()
+
+    return message.channel.send({embeds: [new CustomEmbed(message.member, false, "backup started, check console for more info")]})
+}
+
+cmd.setRun(run)
+
+module.exports = cmd
