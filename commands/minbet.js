d761a87d7667f782b46660e17edb99a6b4d1947c
max <29782397+mxz7@users.noreply.github.com>
2022-03-25 13:04:02 +0000

    formatting

--- commands/minbet.js
+++ commands/minbet.js
@@ -6,9 +6,9 @@ const { getPrestige } = require("../utils/economy/utils")
 const cmd = new Command("minbet", "the minimum amount you need to bet to earn xp", categories.MONEY)
 
 /**
- * 
- * @param {Message} message 
- * @param {Array<String>} args 
+ *
+ * @param {Message} message
+ * @param {Array<String>} args
  */
 async function run(message, args) {
     let requiredBet = 1000
@@ -25,9 +25,11 @@ async function run(message, args) {
 
     const embed = new CustomEmbed(message.member, false)
 
-    embed.setDescription(`you must bet atleast $**${requiredBet.toLocaleString()}** to earn xp\n\nthe most xp you can earn per win is **${max}**xp`)
+    embed.setDescription(
+        `you must bet atleast $**${requiredBet.toLocaleString()}** to earn xp\n\nthe most xp you can earn per win is **${max}**xp`
+    )
 
-    return message.channel.send({embeds: [embed]})
+    return message.channel.send({ embeds: [embed] })
 }
 
 cmd.setRun(run)
