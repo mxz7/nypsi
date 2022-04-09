e382e948fc1e2d9c5cf3c4ef06389e6781b13bab
max <29782397+tekoh@users.noreply.github.com>
2022-04-09 21:02:50 +0100

    fix: final(?) argument parser

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Interaction, User, Collection } = require("discord.js")
+const { Interaction, User, Collection, CommandInteractionOption } = require("discord.js")
 const { runCommand } = require("../utils/commandhandler")
 
 /**
@@ -17,26 +17,50 @@ module.exports = async (interaction) => {
 
     const args = [""]
 
-    for (const arg of interaction.options.data) {
-        if (arg.type == "USER") {
-            const user = arg.user
-            args.push(`<@${user.id}>`)
-            const guildMember = await interaction.guild.members.fetch(user.id)
+    /**
+     * 
+     * @param {CommandInteractionOption} arg 
+     */
+    const parseArgument = async (arg) => {
+        switch (arg.type) {
+            case "USER":
+                const user = arg.user
+                args.push(`<@${user.id}>`)
+                const guildMember = await interaction.guild.members.fetch(user.id)
 
-            if (guildMember) {
-                const collection = new Collection()
-                collection.set(user.id, guildMember)
-                message.mentions = {
-                    members: collection
+                if (guildMember) {
+                    const collection = new Collection()
+                    collection.set(user.id, guildMember)
+                    message.mentions = {
+                        members: collection
+                    }
                 }
-            }
-        } else if (arg.type == "STRING") {
-            for (const str of arg.value.split(" ")) {
-                args.push(str)
-            }
-        } else if (arg.type == "INTEGER") {
-            args.push(arg.value.toString())
+                break
+            case "STRING":
+                for (const str of arg.value.split(" ")) {
+                    args.push(str)
+                }
+                break
+            case "INTEGER":
+                args.push(arg.value.toString())
+                break
+            case "SUB_COMMAND_GROUP":
+                args.push(arg.name)
+                for (const arg1 of arg.options) {
+                    await parseArgument(arg1)
+                }
+                break
+            case "SUB_COMMAND":
+                args.push(arg.name)
+                for (const arg1 of arg.options) {
+                    await parseArgument(arg1)
+                }
+                break
         }
+    }
+
+    for (const arg of interaction.options.data) {
+        await parseArgument(arg)
     }
 
     message.interaction = true
