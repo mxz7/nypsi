af2a3d4a561cb76cead0fc77bf3dc33f588c6b5e
max <max.0x71@gmail.com>
2022-04-28 19:41:42 +0100

    Revert "refactor: remove hasstatsprofile function"
    This reverts commit cd7b074d1c9b5fad1956f6a6fd5160b74e04cd90.

--- utils/economy/utils.js
+++ utils/economy/utils.js
@@ -1292,6 +1292,35 @@ function getStats(member) {
 
 exports.getStats = getStats
 
+function hasStatsProfile(member) {
+    if (stats[member.user.id]) {
+        return true
+    } else {
+        return false
+    }
+}
+
+exports.hasStatsProfile = hasStatsProfile
+
+function createStatsProfile(member) {
+    let id = member
+
+    if (member.user) {
+        id = member.user.id
+    }
+
+    stats[id] = {
+        gamble: {},
+        items: {},
+        rob: {
+            wins: 0,
+            lose: 0,
+        },
+    }
+}
+
+exports.createStatsProfile = createStatsProfile
+
 /**
  *
  * @param {GuildMember} member
@@ -1299,6 +1328,8 @@ exports.getStats = getStats
  * @param {Boolean} win
  */
 function addGamble(member, game, win) {
+    if (!hasStatsProfile(member)) createStatsProfile(member)
+
     if (stats[member.user.id].gamble[game]) {
         if (win) {
             stats[member.user.id].gamble[game].wins++
@@ -1328,6 +1359,8 @@ exports.addGamble = addGamble
  * @param {Boolean} win
  */
 function addRob(member, win) {
+    if (!hasStatsProfile(member)) createStatsProfile(member)
+
     if (win) {
         stats[member.user.id].rob.wins++
     } else {
@@ -1342,6 +1375,8 @@ exports.addRob = addRob
  * @param {GuildMember} member
  */
 function addItemUse(member, item) {
+    if (!hasStatsProfile(member)) createStatsProfile(member)
+
     if (!stats[member.user.id].items) stats[member.user.id].items = {} // remove after season 1
 
     if (stats[member.user.id].items[item]) {
