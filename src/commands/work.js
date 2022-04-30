a3b67fa85a643c10f1e809d11c9917e04d61cf29
max <29782397+tekoh@users.noreply.github.com>
2022-05-01 00:17:38 +0100

    refactor: moved json files again

--- src/commands/work.js
+++ src/commands/work.js
@@ -1,4 +1,4 @@
-const { workMessages } = require("../../lists.json")
+const { workMessages } = require("../../data/lists.json")
 const { getBalance, updateBalance, userExists, createUser } = require("../utils/economy/utils.js")
 const { Message } = require("discord.js")
 const { Command, Categories } = require("../utils/models/Command")
