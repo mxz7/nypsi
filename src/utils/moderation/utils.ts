f4ee949b5178987401880ece4a625cc77ff679f2
max <max.0x71@gmail.com>
2022-05-01 22:58:04 +0100

    refactor: convert moderation utils

--- src/utils/moderation/utils.ts
+++ src/utils/moderation/utils.ts
@@ -1,7 +1,10 @@
-const { inCooldown, addCooldown } = require("../guilds/utils")
-const { Guild, GuildMember, Client, Role } = require("discord.js")
-const { logger } = require("../logger")
-const { getDatabase } = require("../database/database")
+import { Client, Guild, GuildMember, Role } from "discord.js"
+import { getDatabase } from "../database/database"
+import { addCooldown, inCooldown } from "../guilds/utils"
+import { logger } from "../logger"
+import { Case } from "../models/GuildStorage"
+
+declare function require(name: string)
 
 const db = getDatabase()
 
@@ -10,13 +13,16 @@ setInterval(async () => {
 
     const query = db.prepare("SELECT id FROM moderation").all()
 
-    for (let guild of query) {
+    for (const guild of query) {
         const exists = await checkGuild(guild.id)
 
         if (!exists) {
             deleteServer(guild)
 
-            logger.guild(`deleted guild '${guild.id}' from moderation data`)
+            logger.log({
+                level: "guild",
+                message: `deleted guild '${guild.id}' from moderation data`,
+            })
         }
     }
 }, 24 * 60 * 60 * 1000)
@@ -25,17 +31,15 @@ setInterval(async () => {
  *
  * @param {Guild} guild guild to create profile for
  */
-function createProfile(guild) {
+export function createProfile(guild: Guild) {
     db.prepare("INSERT INTO moderation (id) VALUES (?)").run(guild.id)
 }
-
-exports.createProfile = createProfile
 
 /**
  * @returns {Boolean}
  * @param {Guild} guild check if profile exists for this guild
  */
-function profileExists(guild) {
+export function profileExists(guild: Guild): boolean {
     const query = db.prepare("SELECT id FROM moderation WHERE id = ?").get(guild.id)
 
     if (!query) {
@@ -45,19 +49,15 @@ function profileExists(guild) {
     }
 }
 
-exports.profileExists = profileExists
-
 /**
  * @returns {Number}
  * @param {Guild} guild guild to get case count of
  */
-function getCaseCount(guild) {
+export function getCaseCount(guild: Guild): number {
     const query = db.prepare("SELECT case_count FROM moderation WHERE id = ?").get(guild.id)
 
     return query.case_count
 }
-
-exports.getCaseCount = getCaseCount
 
 /**
  *
@@ -67,11 +67,11 @@ exports.getCaseCount = getCaseCount
  * @param {String} moderator moderator issuing punishment
  * @param {String} command entire message
  */
-function newCase(guild, caseType, userIDs, moderator, command) {
+export function newCase(guild: Guild, caseType: string, userIDs: Array<string>, moderator: string, command: string) {
     if (!(userIDs instanceof Array)) {
         userIDs = [userIDs]
     }
-    for (let userID of userIDs) {
+    for (const userID of userIDs) {
         const caseCount = getCaseCount(guild)
         db.prepare(
             "INSERT INTO moderation_cases (case_id, type, user, moderator, command, time, deleted, guild_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
@@ -80,30 +80,25 @@ function newCase(guild, caseType, userIDs, moderator, command) {
     }
 }
 
-exports.newCase = newCase
-
 /**
  *
  * @param {Guild} guild guild to delete case in
  * @param {String} caseID case to delete
  */
-function deleteCase(guild, caseID) {
+export function deleteCase(guild: Guild, caseID: string) {
     db.prepare("UPDATE moderation_cases SET deleted = 1 WHERE guild_id = ? AND case_id = ?").run(guild.id, caseID.toString())
 }
-
-exports.deleteCase = deleteCase
 
 /**
  *
  * @param {Guild} guild guild to delete data for
  */
-function deleteServer(guild) {
-    let id
-
-    if (!guild.id) {
-        id = guild
-    } else {
+export function deleteServer(guild: Guild | string) {
+    let id: string
+    if (guild instanceof Guild) {
         id = guild.id
+    } else {
+        id = guild
     }
 
     db.prepare("DELETE FROM moderation_cases WHERE guild_id = ?").run(id)
@@ -112,14 +107,12 @@ function deleteServer(guild) {
     db.prepare("DELETE FROM moderation WHERE id = ?").run(id)
 }
 
-exports.deleteServer = deleteServer
-
 /**
  * @returns {Array}
  * @param {Guild} guild guild to get cases of
  * @param {String} userID user to get cases of
  */
-function getCases(guild, userID) {
+export function getCases(guild: Guild, userID: string): Array<Case> {
     const query = db.prepare("SELECT * FROM moderation_cases WHERE guild_id = ? AND user = ?").all(guild.id, userID)
 
     for (const d of query) {
@@ -129,26 +122,22 @@ function getCases(guild, userID) {
     return query.reverse()
 }
 
-exports.getCases = getCases
-
 /**
  * @returns {Object}
  * @param {Guild} guild guild to get cases of
  */
-function getAllCases(guild) {
+export function getAllCases(guild: Guild): object {
     const query = db.prepare("SELECT user, moderator, type, deleted FROM moderation_cases WHERE guild_id = ?").all(guild.id)
 
     return query.reverse()
 }
-
-exports.getAllCases = getAllCases
 
 /**
  * @returns {JSON} case
  * @param {Guild} guild guild to search for case in
  * @param {Number} caseID case to fetch
  */
-function getCase(guild, caseID) {
+export function getCase(guild: Guild, caseID: number): Case {
     if (caseID > getCaseCount(guild)) return undefined
 
     let query = db
@@ -168,49 +157,43 @@ function getCase(guild, caseID) {
     return query
 }
 
-exports.getCase = getCase
-
 /**
  *
  * @param {Guild} guild
  * @param {Array<String>} userIDs
  * @param {Date} date
  */
-function newMute(guild, userIDs, date) {
+export function newMute(guild: Guild, userIDs: Array<string>, date: Date) {
     if (!(userIDs instanceof Array)) {
         userIDs = [userIDs]
     }
-    for (let userID of userIDs) {
+    for (const userID of userIDs) {
         db.prepare("INSERT INTO moderation_mutes (user, unmute_time, guild_id) VALUES (?, ?, ?)").run(userID, date, guild.id)
     }
 }
 
-exports.newMute = newMute
-
 /**
  *
  * @param {Guild} guild
  * @param {Array<String>} userIDs
  * @param {Date} date
  */
-function newBan(guild, userIDs, date) {
+export function newBan(guild: Guild, userIDs: Array<string>, date: Date) {
     if (!(userIDs instanceof Array)) {
         userIDs = [userIDs]
     }
 
-    for (let userID of userIDs) {
+    for (const userID of userIDs) {
         db.prepare("INSERT INTO moderation_bans (user, unban_time, guild_id) VALUES (?, ?, ?)").run(userID, date, guild.id)
     }
 }
-
-exports.newBan = newBan
 
 /**
  * @returns {Boolean}
  * @param {Guild} guild
  * @param {GuildMember} member
  */
-function isMuted(guild, member) {
+export function isMuted(guild: Guild, member: GuildMember): boolean {
     const query = db
         .prepare("SELECT user FROM moderation_mutes WHERE guild_id = ? AND user = ?")
         .get(guild.id, member.user.id)
@@ -222,14 +205,12 @@ function isMuted(guild, member) {
     }
 }
 
-exports.isMuted = isMuted
-
 /**
  * @returns {Boolean}
  * @param {Guild} guild
  * @param {GuildMember} member
  */
-function isBanned(guild, member) {
+export function isBanned(guild: Guild, member: GuildMember): boolean {
     const query = db
         .prepare("SELECT user FROM moderation_bans WHERE guild_id = ? AND user = ?")
         .get(guild.id, member.user.id)
@@ -241,33 +222,35 @@ function isBanned(guild, member) {
     }
 }
 
-exports.isBanned = isBanned
-
 /**
  *
  * @param {Client} client
  */
-function runModerationChecks(client) {
+export function runModerationChecks(client: Client) {
     setInterval(() => {
         const date = new Date().getTime()
 
         let query = db.prepare("SELECT user, guild_id FROM moderation_mutes WHERE unmute_time <= ?").all(date)
 
-        for (let unmute of query) {
-            logger.auto(`requesting unmute in ${unmute.guild_id} for ${unmute.user}`)
+        for (const unmute of query) {
+            logger.log({
+                level: "auto",
+                message: `requesting unmute in ${unmute.guild_id} for ${unmute.user}`,
+            })
             requestUnmute(unmute.guild_id, unmute.user, client)
         }
 
         query = db.prepare("SELECT user, guild_id FROM moderation_bans WHERE unban_time <= ?").all(date)
 
-        for (let unban of query) {
-            logger.auto(`requesting unban in ${unban.guild_id} for ${unban.user}`)
+        for (const unban of query) {
+            logger.log({
+                level: "auto",
+                message: `requesting unban in ${unban.guild_id} for ${unban.user}`,
+            })
             requestUnban(unban.guild_id, unban.user, client)
         }
     }, 30000)
 }
-
-exports.runModerationChecks = runModerationChecks
 
 /**
  *
@@ -275,42 +258,37 @@ exports.runModerationChecks = runModerationChecks
  * @param {Number} caseID
  * @param {String} reason
  */
-function setReason(guild, caseID, reason) {
+export function setReason(guild: Guild, caseID: number, reason: string) {
     db.prepare("UPDATE moderation_cases SET command = ? WHERE case_id = ? AND guild_id = ?").run(reason, caseID, guild.id)
 }
 
-exports.setReason = setReason
-
-function deleteMute(guild, member) {
-    let id = member.id
-
-    if (!id) {
+export function deleteMute(guild: Guild, member: GuildMember | string) {
+    let id: string
+    if (member instanceof GuildMember) {
+        id = member.id
+    } else {
         id = member
     }
 
     db.prepare("DELETE FROM moderation_mutes WHERE user = ? AND guild_id = ?").run(id, guild.id)
 }
 
-exports.deleteMute = deleteMute
-
-function deleteBan(guild, member) {
-    let id = member.id
-
-    if (!id) {
+export function deleteBan(guild: Guild, member: GuildMember | string) {
+    let id: string
+    if (member instanceof GuildMember) {
+        id = member.id
+    } else {
         id = member
     }
 
     db.prepare("DELETE FROM moderation_bans WHERE user = ? AND guild_id = ?").run(id, guild.id)
 }
-
-exports.deleteBan = deleteBan
-
 /**
  *
  * @param {Guild} guild
  * @returns {String}
  */
-function getMuteRole(guild) {
+export function getMuteRole(guild: Guild): string {
     const query = db.prepare("SELECT mute_role FROM moderation WHERE id = ?").get(guild.id)
 
     if (query.mute_role == "") {
@@ -320,26 +298,22 @@ function getMuteRole(guild) {
     }
 }
 
-exports.getMuteRole = getMuteRole
-
 /**
  *
  * @param {Guild} guild
  * @param {Role} role
  */
-function setMuteRole(guild, role) {
+export function setMuteRole(guild: Guild, role: Role | string) {
     const query = db.prepare("UPDATE moderation SET mute_role = ? WHERE id = ?")
 
-    if (role == "default") {
-        query.run("", guild.id)
-    } else {
+    if (role instanceof Role) {
         query.run(role.id, guild.id)
+    } else {
+        query.run("", guild.id)
     }
 }
 
-exports.setMuteRole = setMuteRole
-
-function requestUnban(guild, member, client) {
+function requestUnban(guild: string | Guild, member: string, client: Client) {
     guild = client.guilds.cache.find((g) => g.id == guild)
 
     if (!guild) {
@@ -351,10 +325,13 @@ function requestUnban(guild, member, client) {
 
     guild.members.unban(member, "ban expired")
 
-    logger.success("ban removed")
+    logger.log({
+        level: "success",
+        message: "ban removed",
+    })
 }
 
-async function requestUnmute(guild, member, client) {
+async function requestUnmute(guild: Guild | string, member: string, client: Client) {
     guild = client.guilds.cache.find((g) => g.id == guild)
 
     if (!guild) {
@@ -388,10 +365,10 @@ async function requestUnmute(guild, member, client) {
 
     const muteRoleID = getMuteRole(guild)
 
-    let muteRole = await guild.roles.cache.find((r) => r.id == muteRoleID)
+    let muteRole = guild.roles.cache.find((r) => r.id == muteRoleID)
 
     if (!muteRoleID || muteRoleID == "") {
-        muteRole = await guild.roles.cache.find((r) => r.name.toLowerCase() == "muted")
+        muteRole = guild.roles.cache.find((r) => r.name.toLowerCase() == "muted")
     }
 
     if (!muteRole) {
@@ -401,7 +378,10 @@ async function requestUnmute(guild, member, client) {
 
     deleteMute(guild, member)
 
-    logger.success("mute deleted")
+    logger.log({
+        level: "success",
+        message: "mute deleted",
+    })
 
     return await newMember.roles.remove(muteRole).catch(() => {
         logger.error("couldnt remove mute role")
