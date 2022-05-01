70bbe1cfa38d5de15eb68c958beae29b80e72be4
max <29782397+tekoh@users.noreply.github.com>
2022-05-02 00:40:55 +0100

    refactor: change Countdown class to interface

--- src/utils/models/GuildStorage.ts
+++ src/utils/models/GuildStorage.ts
@@ -1,26 +1,9 @@
-export class Countdown {
-    public date: Date | number
-    public format: string
-    public finalFormat: string
-    public channel: string
-    public id: string
-    /**
-     * @returns {Countdown}
-     * @param {Date} date
-     * @param {String} format
-     * @param {String} finalFormat
-     * @param {String} channel
-     * @param {String} id
-     */
-    constructor(date: Date | number, format: string, finalFormat: string, channel: string, id: string) {
-        this.date = date
-        this.format = format
-        this.finalFormat = finalFormat
-        this.channel = channel
-        this.id = id
-
-        return this
-    }
+export interface Countdown {
+    date: number | Date,
+    format: string,
+    finalFormat: string,
+    channel: string,
+    id: number
 }
 
 export interface Case {
