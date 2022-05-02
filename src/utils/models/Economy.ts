8aa88be5b9719e814cb8434522138b5b9e1813cc
max <max.0x71@gmail.com>
2022-05-02 01:06:22 +0100

    feat: item interface

---
+++ src/utils/models/Economy.ts
@@ -0,0 +1,11 @@
+export interface Item {
+    id: string
+    name: string
+    emoji: string
+    description: string
+    worth?: number
+    role?: number
+    aliases?: Array<string>
+    speed?: number
+    boobies?: string
+}
