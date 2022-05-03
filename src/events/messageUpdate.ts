b8e78f23b27b99eefac0951b872507106110a2bb
max <29782397+tekoh@users.noreply.github.com>
2022-05-03 18:23:47 +0100

    fix: eslint errors

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Message, Permissions } from "discord.js"
+import { Message, Permissions } from "discord.js"
 import { eSnipe } from "../nypsi"
 import { createGuild, getChatFilter, getSnipeFilter, hasGuild } from "../utils/guilds/utils"
 
