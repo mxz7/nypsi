d322ef4535a4589e3e2148411dfbcb292149c1c0
max <29782397+tekoh@users.noreply.github.com>
2022-05-03 14:58:12 +0100

    refactor: mass changes

--- src/commands/rockpaperscissors.ts
+++ src/commands/rockpaperscissors.ts
@@ -17,7 +17,7 @@ import { Command, Categories, NypsiCommandInteraction } from "../utils/models/Co
 import { ErrorEmbed, CustomEmbed } from "../utils/models/EmbedBuilders.js"
 import { getPrefix } from "../utils/guilds/utils"
 import { isPremium, getTier } from "../utils/premium/utils"
-const { gamble } = require("../utils/logger.js")
+import { gamble } from "../utils/logger.js"
 
 const cooldown = new Map()
 
