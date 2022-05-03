e2f45b222bf626090424d8a633c6ebd00368b5d2
max <29782397+tekoh@users.noreply.github.com>
2022-05-03 16:43:43 +0100

    refactor: convert github command

--- src/commands/github.ts
+++ src/commands/github.ts
@@ -1,6 +1,6 @@
 import { CommandInteraction, Message } from "discord.js"
 import { Command, Categories, NypsiCommandInteraction } from "../utils/models/Command"
-const { CustomEmbed } = require("../utils/models/EmbedBuilders.js")
+import { CustomEmbed } from "../utils/models/EmbedBuilders.js"
 
 const cmd = new Command("github", "view code for the bot on github", Categories.INFO).setAliases(["git"])
 
