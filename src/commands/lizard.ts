f55fe18f681e344982e025e0daad45966c17aec0
max <max.0x71@gmail.com>
2022-05-03 15:49:58 +0100

    fix: lizard error message

--- src/commands/lizard.ts
+++ src/commands/lizard.ts
@@ -63,7 +63,7 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     const a = await redditImage(chosen, allowed)
 
     if (a == "lol") {
-        return message.channel.send(new ErrorEmbed("unable to find lizard image"))
+        return message.channel.send({ embeds: [new ErrorEmbed("unable to find lizard image")] })
     }
 
     const image = a.split("|")[0]
