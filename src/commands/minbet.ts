b0eabfa98fd07b207fa6cd637138c586c076e6db
max <max.0x71@gmail.com>
2022-05-03 17:24:22 +0100

    refactor: convert minbet

--- src/commands/minbet.ts
+++ src/commands/minbet.ts
@@ -1,7 +1,7 @@
 import { CommandInteraction, Message } from "discord.js"
 import { Command, Categories, NypsiCommandInteraction } from "../utils/models/Command"
 import { CustomEmbed } from "../utils/models/EmbedBuilders"
-const { getPrestige } = require("../utils/economy/utils")
+import { getPrestige } from "../utils/economy/utils"
 
 const cmd = new Command("minbet", "the minimum amount you need to bet to earn xp", Categories.MONEY)
 
