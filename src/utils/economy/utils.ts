511fe255ccaa8fd69d8e3a81067d04e72d152706
max <max.0x71@gmail.com>
2022-05-04 15:37:04 +0100

    refactor: increase max coin storage

--- src/utils/economy/utils.ts
+++ src/utils/economy/utils.ts
@@ -1414,7 +1414,7 @@ export function getItems(): { [key: string]: Item } {
  * @param {GuildMember} member
  */
 export function getMaxBitcoin(member: GuildMember): number {
-    const base = 2
+    const base = 10
 
     const prestige = getPrestige(member)
 
