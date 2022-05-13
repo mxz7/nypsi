7d95704fc97f59c06413c1faa6dc712616880ecf
max <29782397+tekoh@users.noreply.github.com>
2022-05-13 21:21:35 +0100

    feat: update messageupdate filter check to same as message event

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,6 +1,9 @@
 import { Message, Permissions } from "discord.js"
 import { eSnipe } from "../nypsi"
 import { createGuild, getChatFilter, getSnipeFilter, hasGuild } from "../utils/guilds/utils"
+import { logger } from "../utils/logger"
+import { PunishmentType } from "../utils/models/GuildStorage"
+import { addModLog } from "../utils/moderation/utils"
 
 export default async function messageUpdate(message: Message, newMessage: Message) {
     if (!message) return
@@ -10,13 +13,21 @@ export default async function messageUpdate(message: Message, newMessage: Messag
     if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
         const filter = getChatFilter(message.guild)
 
-        let content = newMessage.content.toLowerCase().normalize("NFD")
+        let content: string | string[] = newMessage.content.toLowerCase().normalize("NFD")
 
         content = content.replace(/[^A-z0-9\s]/g, "")
 
+        content = content.split(" ")
+
         for (const word of filter) {
-            if (content.includes(word.toLowerCase())) {
-                return await message.delete()
+            if (content.indexOf(word.toLowerCase()) != -1) {
+                logger.debug(
+                    `(${message.guild.id}) (${message.author.id}) (${message.author.tag}) message deleted: ${content.join(
+                        " "
+                    )}`
+                )
+                addModLog(message.guild, PunishmentType.FILTER_VIOLATION, message.author.id, "nypsi", content.join(" "), -1)
+                return await message.delete().catch(() => {})
             }
         }
     }
