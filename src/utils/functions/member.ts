75eaa2e0c1855914a78c295cfd9108a90263c195
max <max.0x71@gmail.com>
2022-05-13 21:47:58 +0100

    feat: use choosemember worker when over 5k members are available

--- src/utils/functions/member.ts
+++ src/utils/functions/member.ts
@@ -1,4 +1,5 @@
 import { Collection, Guild, GuildMember } from "discord.js"
+import chooseMember from "../workers/choosemember"
 
 export async function getMember(guild: Guild, memberName: string): Promise<GuildMember> {
     if (!guild) return null
@@ -14,60 +15,64 @@ export async function getMember(guild: Guild, memberName: string): Promise<Guild
     let target: GuildMember
     const possible = new Map()
 
-    for (const m of members.keys()) {
-        const member = members.get(m)
+    if (members.size > 5000) {
+        target = await chooseMember(members, memberName)
+    } else {
+        for (const m of members.keys()) {
+            const member = members.get(m)
 
-        if (member.user.id == memberName) {
-            target = member
-            break
-        } else if (member.user.tag.toLowerCase() == memberName.toLowerCase()) {
-            target = member
-            break
-        } else if (member.user.username.toLowerCase() == memberName.toLowerCase()) {
-            if (member.user.bot) {
-                possible.set(3, member)
-            } else {
+            if (member.user.id == memberName) {
                 target = member
                 break
+            } else if (member.user.tag.toLowerCase() == memberName.toLowerCase()) {
+                target = member
+                break
+            } else if (member.user.username.toLowerCase() == memberName.toLowerCase()) {
+                if (member.user.bot) {
+                    possible.set(3, member)
+                } else {
+                    target = member
+                    break
+                }
+            } else if (member.displayName.toLowerCase() == memberName.toLowerCase()) {
+                if (member.user.bot) {
+                    possible.set(4, member)
+                } else {
+                    possible.set(1, member)
+                }
+            } else if (member.user.tag.toLowerCase().includes(memberName.toLowerCase())) {
+                if (member.user.bot) {
+                    possible.set(5, member)
+                } else {
+                    possible.set(2, member)
+                }
+            } else if (member.displayName.toLowerCase().includes(memberName.toLowerCase())) {
+                if (member.user.bot) {
+                    possible.set(6, member)
+                } else {
+                    possible.set(3, member)
+                }
             }
-        } else if (member.displayName.toLowerCase() == memberName.toLowerCase()) {
-            if (member.user.bot) {
-                possible.set(4, member)
-            } else {
-                possible.set(1, member)
-            }
-        } else if (member.user.tag.toLowerCase().includes(memberName.toLowerCase())) {
-            if (member.user.bot) {
-                possible.set(5, member)
-            } else {
-                possible.set(2, member)
-            }
-        } else if (member.displayName.toLowerCase().includes(memberName.toLowerCase())) {
-            if (member.user.bot) {
-                possible.set(6, member)
-            } else {
-                possible.set(3, member)
-            }
+
+            if (possible.size == 6) break
         }
 
-        if (possible.size == 6) break
-    }
-
-    if (!target) {
-        if (possible.get(1)) {
-            target = possible.get(1)
-        } else if (possible.get(2)) {
-            target = possible.get(2)
-        } else if (possible.get(3)) {
-            target = possible.get(3)
-        } else if (possible.get(4)) {
-            target = possible.get(4)
-        } else if (possible.get(5)) {
-            target = possible.get(5)
-        } else if (possible.get(6)) {
-            target = possible.get(6)
-        } else {
-            target = null
+        if (!target) {
+            if (possible.get(1)) {
+                target = possible.get(1)
+            } else if (possible.get(2)) {
+                target = possible.get(2)
+            } else if (possible.get(3)) {
+                target = possible.get(3)
+            } else if (possible.get(4)) {
+                target = possible.get(4)
+            } else if (possible.get(5)) {
+                target = possible.get(5)
+            } else if (possible.get(6)) {
+                target = possible.get(6)
+            } else {
+                target = null
+            }
         }
     }
 
