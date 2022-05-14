98c54f04fc5d8266c3fb693c0d3593f965024452
max <29782397+tekoh@users.noreply.github.com>
2022-05-14 15:17:44 +0100

    revert: "fix: update required balance for prestige"
    This reverts commit b90c3f8d1b83ae8af83ae08e09acb60965cb38b1.

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -104,7 +104,7 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     if (message.member == target) {
         if (
             getXp(target) >= getPrestigeRequirement(target) &&
-            getBankBalance(target) >= getPrestigeRequirementBal(target) &&
+            getBankBalance(target) >= getPrestigeRequirementBal(getXp(target)) &&
             getPrestige(target) < 20
         ) {
             return send({
