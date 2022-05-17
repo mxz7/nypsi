1bb004ad3697b1b2c2153d22d211dcf9432fbd58
max <29782397+tekoh@users.noreply.github.com>
2022-05-17 09:11:53 +0100

    fix: error spam on f command

--- src/commands/f.ts
+++ src/commands/f.ts
@@ -47,7 +47,9 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
     collector.on("collect", async (i): Promise<any> => {
         if (reactions.includes(i.user.id) || i.deferred) {
-            return await i.reply({ embeds: [new ErrorEmbed("you can only do this once")], ephemeral: true }).catch(() => {})
+            if (!(await i.fetchReply())) {
+                return await i.reply({ embeds: [new ErrorEmbed("you can only do this once")], ephemeral: true }).catch(() => {})
+            }
         }
 
         await i.deferUpdate().catch(() => {})
