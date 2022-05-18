33da9ded458ba40031a2b3f8c1a53270eb07f0f5
max <max.0x71@gmail.com>
2022-05-18 14:04:55 +0100

    feat: clear mentions queue command

---
+++ src/commands/clearqueue.ts
@@ -0,0 +1,20 @@
+import { CommandInteraction, Message } from "discord.js"
+import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command"
+import { deleteQueue, mentionQueue } from "../utils/users/utils"
+
+const cmd = new Command("clearqueue", "clear the mentions queue", Categories.NONE).setPermissions(["bot owner"])
+
+async function run(message: Message | (CommandInteraction & NypsiCommandInteraction)) {
+    if (message.author.id != "672793821850894347") return
+
+    mentionQueue.length = 0
+    deleteQueue.length = 0
+
+    if (message instanceof Message) {
+        return await message.react("✅")
+    }
+}
+
+cmd.setRun(run)
+
+module.exports = cmd
