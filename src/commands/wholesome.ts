b5bcc3e11ba7a8a1bd9390026049adf128ec4e6b
max <29782397+tekoh@users.noreply.github.com>
2022-05-22 18:38:25 +0100

    refactor: everyone can do $ws get

--- src/commands/wholesome.ts
+++ src/commands/wholesome.ts
@@ -148,8 +148,6 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
         return message.react("✅")
     } else if (args[0].toLowerCase() == "get") {
-        if (message.author.id != "672793821850894347") return
-
         if (args.length == 1) {
             return send({ embeds: [new ErrorEmbed("dumbass")] })
         }
@@ -162,9 +160,12 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
         embed.setHeader(`image #${wholesome.id}`)
 
-        embed.setDescription(
-            `**suggested by** ${wholesome.submitter} (${wholesome.submitter_id})\n**accepted by** \`${wholesome.accepter}\`\n**url** ${wholesome.image}`
-        )
+        if (message.author.id == "672793821850894347") {
+            embed.setDescription(
+                `**suggested by** ${wholesome.submitter} (${wholesome.submitter_id})\n**accepted by** \`${wholesome.accepter}\`\n**url** ${wholesome.image}`
+            )
+        }
+
         embed.setImage(wholesome.image)
         embed.setFooter(`submitted on ${formatDate(wholesome.upload)}`)
     } else if (args[0].toLowerCase() == "accept" || args[0].toLowerCase() == "a") {
