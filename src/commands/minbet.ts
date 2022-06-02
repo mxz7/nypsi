8aca09470ef3373bc583e4fa2a9db30c945915ae
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2022-06-02 09:24:33 -0400

    fix: make minbet show correct amount of max exp (#366)
    * Fix lawyer description
    
    * fix: change lawyer desc to repaint place and store
    
    * Revert "fix: change lawyer desc to repaint place and store"
    
    This reverts commit 34c529249f0ef7aa68076b36b9475e89af95fd9f.
    
    * Revert "Fix lawyer description"
    
    This reverts commit 9dc11a2936c7e6ab84f368e64457de231600a2ab.
    
    * fix: make minbet show correct amount of max exp
    
    * fix: i hate everything (fix imports)
    
    * style: prettier formatting
    
    Co-authored-by: max <max.0x71@gmail.com>
    Co-authored-by: max <29782397+tekoh@users.noreply.github.com>

--- src/commands/minbet.ts
+++ src/commands/minbet.ts
@@ -1,7 +1,7 @@
 import { CommandInteraction, Message } from "discord.js"
 import { Command, Categories, NypsiCommandInteraction } from "../utils/models/Command"
 import { CustomEmbed } from "../utils/models/EmbedBuilders"
-import { calcMinimumEarnedXp, getRequiredBetForXp } from "../utils/economy/utils"
+import { calcMinimumEarnedXp, getRequiredBetForXp, getGuildByUser } from "../utils/economy/utils"
 
 const cmd = new Command("minbet", "the minimum amount you need to bet to earn xp", Categories.MONEY)
 
@@ -13,16 +13,22 @@ const cmd = new Command("minbet", "the minimum amount you need to bet to earn xp
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction)) {
     const requiredBet = getRequiredBetForXp(message.member)
 
-    let max = calcMinimumEarnedXp(message.member) + 2
+    let earned = calcMinimumEarnedXp(message.member) + 2
 
-    if (max > 7) {
-        max = 7
+    let max = 6
+
+    const guild = getGuildByUser(message.member)
+
+    if (guild) {
+        max += guild.level - 1
     }
+
+    if (earned > max) earned = max
 
     const embed = new CustomEmbed(message.member, false)
 
     embed.setDescription(
-        `you must bet atleast $**${requiredBet.toLocaleString()}** to earn xp\n\nthe most xp you can earn per win is **${max}**xp`
+        `you must bet atleast $**${requiredBet.toLocaleString()}** to earn xp\n\nthe most xp you can earn per win is **${earned}**xp`
     )
 
     return message.channel.send({ embeds: [embed] })
