a4d1abcea33948dac6dada10a3f66a38767a3ede
max <max.0x71@gmail.com>
2022-07-03 12:24:35 +0100

    feat: functions for updating presence

---
+++ src/utils/functions/presence.ts
@@ -0,0 +1,51 @@
+import { Client } from "discord.js";
+import { getRandomCommand } from "../commandhandler";
+import { daysUntilChristmas } from "./date";
+
+let current = "";
+
+export function updatePresence(presence = "", client: Client) {
+    current = presence;
+
+    client.user.setPresence({
+        status: "dnd",
+        activities: [
+            {
+                name: current,
+                type: "PLAYING",
+            },
+        ],
+    });
+}
+
+export function randomPresence(): string {
+    const possibilities = ["$help | nypsi.xyz", "$help | tekoh.net", "$help | nypsi.xyz", "x0x", "xmas"];
+
+    const chosen = possibilities[Math.floor(Math.random() * possibilities.length)];
+
+    let game = "";
+
+    if (chosen === "x0x") {
+        const randomCommand = getRandomCommand();
+
+        game = `$${randomCommand.name} - ${randomCommand.description}`;
+    } else if (chosen === "christmas") {
+        game = `${daysUntilChristmas()} days until christmas`;
+    } else {
+        game = chosen;
+    }
+
+    return game;
+}
+
+export function getCustomPresence(): null | string {
+    if (current === "") {
+        return null;
+    } else {
+        return current;
+    }
+}
+
+export function setCustomPresence(text?: string) {
+    current = text;
+}
