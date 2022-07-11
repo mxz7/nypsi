724092ea4d9cb007ffa8ebbe5ffbc0869c124c12
max <max.0x71@gmail.com>
2022-07-11 21:53:05 +0100

    feat: create sqlite database for mentions

--- src/utils/database/database.ts
+++ src/utils/database/database.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import Database = require("better-sqlite3");
 
 const prisma = new PrismaClient();
 
 export default prisma;
+
+const db = new Database("./out/data/mentions.db");
+
+function createTable() {
+    db.prepare(
+        "CREATE TABLE IF NOT EXISTS mentions ('guild_id' TEXT NOT NULL, 'target_id' TEXT NOT NULL, 'date' INTEGER NOT NULL, 'user_tag' TEXT NOT NULL, 'url' TEXT NOT NULL, 'content' TEXT NOT NULL)"
+    ).run();
+}
+
+createTable();
