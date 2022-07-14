c7832ea686154e581e740e04e68e5490dfd7945d
max <max.0x71@gmail.com>
2022-07-14 22:26:02 +0100

    fix: close connection after ensuring table creation

--- src/utils/database/database.ts
+++ src/utils/database/database.ts
@@ -7,10 +7,8 @@ export default prisma;
 
 const db = new Database("./out/data/mentions.db");
 
-function createTable() {
-    db.prepare(
-        "CREATE TABLE IF NOT EXISTS mentions ('guild_id' TEXT NOT NULL, 'target_id' TEXT NOT NULL, 'date' INTEGER NOT NULL, 'user_tag' TEXT NOT NULL, 'url' TEXT NOT NULL, 'content' TEXT NOT NULL)"
-    ).run();
-}
+db.prepare(
+    "CREATE TABLE IF NOT EXISTS mentions ('guild_id' TEXT NOT NULL, 'target_id' TEXT NOT NULL, 'date' INTEGER NOT NULL, 'user_tag' TEXT NOT NULL, 'url' TEXT NOT NULL, 'content' TEXT NOT NULL)"
+).run();
 
-createTable();
+db.close();
