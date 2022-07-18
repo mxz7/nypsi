9837081790d857b96c056cd0ac4521276f6ac05e
max <max.0x71@gmail.com>
2022-07-18 21:16:52 +0100

    refactor: fix ascii

--- src/commands/ascii.ts
+++ src/commands/ascii.ts
@@ -97,22 +97,16 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     let asciiString = "";
     let fail = false;
 
-    await ascii(
-        text,
-        {
-            font: font,
-        },
-        async (err, data) => {
-            if (!err) {
-                asciiString = "```" + data + "```";
-            } else {
-                fail = true;
-                return message.channel.send({
-                    embeds: [new ErrorEmbed("error - maybe an incorrect font - fonts are **cAsE sEnSiTiVe**")],
-                });
-            }
+    ascii.text(text, font as ascii.Fonts, async (err, data) => {
+        if (!err) {
+            asciiString = "```" + data + "```";
+        } else {
+            fail = true;
+            return message.channel.send({
+                embeds: [new ErrorEmbed("error - maybe an incorrect font - fonts are **cAsE sEnSiTiVe**")],
+            });
         }
-    );
+    });
 
     setTimeout(() => {
         if (fail) {
