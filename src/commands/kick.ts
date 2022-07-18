11d0899666b798d5ff9b72d060ff5629e681968d
max <max.0x71@gmail.com>
2022-07-18 20:38:50 +0100

    refactor: fix kick

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Message, PermissionFlagsBits } from "discord.js";
+import { CommandInteraction, Message, MessageOptions, PermissionFlagsBits } from "discord.js";
 import { profileExists, createProfile, newCase } from "../utils/moderation/utils";
 import { inCooldown, addCooldown, getPrefix } from "../utils/guilds/utils";
 import { Command, Categories, NypsiCommandInteraction } from "../utils/models/Command";
@@ -16,7 +16,7 @@ cmd.slashData
     .addStringOption((option) => option.setName("reason").setDescription("reason for kick").setRequired(true));
 
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
-    const send = async (data) => {
+    const send = async (data: MessageOptions) => {
         if (!(message instanceof Message)) {
             return await message.editReply(data);
         } else {
