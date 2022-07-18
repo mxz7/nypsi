da18793cf3ba31fc7f3011bfbd72a7cf22f7b078
max <max.0x71@gmail.com>
2022-07-18 21:10:52 +0100

    refactor: fix bdsm

--- src/commands/bdsm.ts
+++ src/commands/bdsm.ts
@@ -1,10 +1,9 @@
 import { BaseGuildTextChannel, CommandInteraction, Message, ThreadChannel } from "discord.js";
 import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler";
 import { redditImage } from "../utils/functions/image";
+import { images } from "../utils/imghandler";
 import { Command, Categories, NypsiCommandInteraction } from "../utils/models/Command";
 import { ErrorEmbed, CustomEmbed } from "../utils/models/EmbedBuilders.js";
-
-declare function require(name: string);
 
 const cmd = new Command("bdsm", "get a random bdsm image", Categories.NSFW);
 
@@ -25,8 +24,6 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     if (!message.channel.nsfw) {
         return message.channel.send({ embeds: [new ErrorEmbed("you must do this in an nsfw channel")] });
     }
-
-    const { images } = require("../utils/imghandler");
 
     const bdsmCache = images.get("bdsm");
 
