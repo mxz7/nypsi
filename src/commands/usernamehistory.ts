6e68ec34c1aeeb6caf332a55ad7f3cb86ab26e87
max <max.0x71@gmail.com>
2022-07-18 19:43:08 +0100

    refactor: fix usernamehistory

--- src/commands/usernamehistory.ts
+++ src/commands/usernamehistory.ts
@@ -6,6 +6,7 @@ import {
     ButtonBuilder,
     ButtonStyle,
     MessageActionRowComponentBuilder,
+    Interaction,
 } from "discord.js";
 import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler";
 import { formatDate } from "../utils/functions/date";
@@ -102,7 +103,7 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
         new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary)
     );
 
-    let msg;
+    let msg: Message;
 
     if (pages.size == 1) {
         return await message.channel.send({ embeds: [embed] });
@@ -115,11 +116,11 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     let currentPage = 1;
     const lastPage = pages.size;
 
-    const filter = (i) => i.user.id == message.author.id;
+    const filter = (i: Interaction) => i.user.id == message.author.id;
 
-    const pageManager = async () => {
+    const pageManager = async (): Promise<void> => {
         const reaction = await msg
-            .awaitMessageComponent({ filter, time: 30000, errors: ["time"] })
+            .awaitMessageComponent({ filter, time: 30000 })
             .then(async (collected) => {
                 await collected.deferUpdate();
                 return collected.customId;
