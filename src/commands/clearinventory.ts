233389e0e9b5d37ef5873575a2b06458be662a2a
max <max.0x71@gmail.com>
2022-07-18 21:04:23 +0100

    refactor: fix clearinventory

--- src/commands/clearinventory.ts
+++ src/commands/clearinventory.ts
@@ -5,6 +5,7 @@ import {
     ButtonBuilder,
     MessageActionRowComponentBuilder,
     ButtonStyle,
+    Interaction,
 } from "discord.js";
 import { Command, Categories, NypsiCommandInteraction } from "../utils/models/Command";
 import { ErrorEmbed, CustomEmbed } from "../utils/models/EmbedBuilders";
@@ -39,7 +40,7 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
     const msg = await message.channel.send({ embeds: [embed], components: [row] });
 
-    const filter = (i) => i.user.id == message.author.id;
+    const filter = (i: Interaction) => i.user.id == message.author.id;
 
     const reaction = await msg
         .awaitMessageComponent({ filter, time: 15000 })
