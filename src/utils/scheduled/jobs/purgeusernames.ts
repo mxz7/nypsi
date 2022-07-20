228504a984b9d6ae8d1e9b12ad36fbc1fbdb718c
max <29782397+tekoh@users.noreply.github.com>
2022-07-20 19:10:10 +0100

    refactor: put it in right format thing to work

--- src/utils/scheduled/jobs/purgeusernames.ts
+++ src/utils/scheduled/jobs/purgeusernames.ts
@@ -1,40 +1,15 @@
 import dayjs = require("dayjs");
-import prisma from "../database/database";
-import { MStoTime } from "../functions/date";
-import { logger } from "../logger";
+import prisma from "../../database/database";
+import { logger } from "../../logger";
 
-export default function purgeUsernames() {
-    const now = new Date();
+(async () => {
+    const old = dayjs().subtract(180, "days").toDate();
 
-    let d = `${now.getMonth() + 1}/${now.getDate() + 1}/${now.getUTCFullYear()}`;
-
-    if (now.getHours() < 3) {
-        d = `${now.getMonth() + 1}/${now.getDate()}/${now.getUTCFullYear()}`;
-    }
-
-    const needed = new Date(Date.parse(d) + 10800000);
-
-    const purge = async () => {
-        const old = dayjs().subtract(180, "days").toDate();
-
-        const d = await prisma.username.deleteMany({
-            where: {
-                AND: [{ type: "username" }, { date: { lt: old } }],
-            },
-        });
-
-        logger.log("auto", `${d.count.toLocaleString()} old usernames deleted from database`);
-    };
-
-    setTimeout(async () => {
-        setInterval(() => {
-            purge();
-        }, 86400000);
-        purge();
-    }, needed.getTime() - now.getTime());
-
-    logger.log({
-        level: "auto",
-        message: `old usernames will be purged in ${MStoTime(needed.getTime() - now.getTime())}`,
+    const d = await prisma.username.deleteMany({
+        where: {
+            AND: [{ type: "username" }, { date: { lt: old } }],
+        },
     });
-}
+
+    logger.log("auto", `${d.count.toLocaleString()} old usernames deleted from database`);
+})();
