b6e025d6da836bcf8aa5fe5b181582c491df5644
max <29782397+tekoh@users.noreply.github.com>
2022-07-22 21:37:48 +0100

    fix(maybe): requestdm weird error

--- src/utils/functions/requestdm.ts
+++ src/utils/functions/requestdm.ts
@@ -13,87 +13,157 @@ interface RequestDMOptions {
 export default async function requestDM(options: RequestDMOptions): Promise<boolean> {
     logger.info(`DM requested with ${options.memberId}`);
 
-    let broadcastEval;
-
     if (options.client instanceof NypsiClient) {
-        broadcastEval = options.client.shard.broadcastEval;
-    } else {
-        broadcastEval = options.client.broadcastEval;
-    }
+        const clusterHas = await options.client.shard.broadcastEval(
+            async (c, { userId }) => {
+                const user = await c.users.fetch(userId).catch(() => {});
 
-    const clusterHas = await broadcastEval(
-        async (c, { userId }) => {
-            const user = await c.users.fetch(userId).catch(() => {});
-
-            if (user) {
-                return c.shard.ids[0];
-            } else {
-                return "not-found";
-            }
-        },
-        {
-            context: { userId: options.memberId },
-        }
-    );
-
-    let shard: number;
-
-    for (const i of clusterHas) {
-        if (i != "not-found") {
-            shard = i;
-            break;
-        }
-    }
-
-    if (isNaN(shard)) {
-        logger.warn("user not found");
-        return false;
-    }
-
-    const payload: MessageOptions = {
-        content: options.content,
-    };
-
-    if (options.embed) {
-        payload.embeds = [options.embed.toJSON()];
-    }
-
-    const res = await broadcastEval(
-        async (c, { needed, memberId, payload }) => {
-            if (!c.shard.ids.includes(needed)) return false;
-
-            const user = await c.users.fetch(memberId).catch(() => {});
-
-            if (!user) return false;
-
-            let fail = false;
-
-            await user.send(payload as MessagePayload).catch(() => {
-                fail = true;
-            });
-
-            if (fail) {
-                return false;
-            }
-            return true;
-        },
-        {
-            context: {
-                needed: shard,
-                memberId: options.memberId,
-                payload: payload,
+                if (user) {
+                    return c.shard.ids[0];
+                } else {
+                    return "not-found";
+                }
             },
-        }
-    );
+            {
+                context: { userId: options.memberId },
+            }
+        );
 
-    if (res.includes(true)) {
-        logger.log({
-            level: "success",
-            message: "DM sent",
-        });
-        return true;
+        let shard: number;
+
+        for (const i of clusterHas) {
+            if (i != "not-found") {
+                shard = i;
+                break;
+            }
+        }
+
+        if (isNaN(shard)) {
+            logger.warn("user not found");
+            return false;
+        }
+
+        const payload: MessageOptions = {
+            content: options.content,
+        };
+
+        if (options.embed) {
+            payload.embeds = [options.embed.toJSON()];
+        }
+
+        const res = await options.client.shard.broadcastEval(
+            async (c, { needed, memberId, payload }) => {
+                if (!c.shard.ids.includes(needed)) return false;
+
+                const user = await c.users.fetch(memberId).catch(() => {});
+
+                if (!user) return false;
+
+                let fail = false;
+
+                await user.send(payload as MessagePayload).catch(() => {
+                    fail = true;
+                });
+
+                if (fail) {
+                    return false;
+                }
+                return true;
+            },
+            {
+                context: {
+                    needed: shard,
+                    memberId: options.memberId,
+                    payload: payload,
+                },
+            }
+        );
+
+        if (res.includes(true)) {
+            logger.log({
+                level: "success",
+                message: "DM sent",
+            });
+            return true;
+        } else {
+            logger.error("failed to send DM");
+            return false;
+        }
     } else {
-        logger.error("failed to send DM");
-        return false;
+        const clusterHas = await options.client.broadcastEval(
+            async (c, { userId }) => {
+                const user = await c.users.fetch(userId).catch(() => {});
+
+                if (user) {
+                    return c.shard.ids[0];
+                } else {
+                    return "not-found";
+                }
+            },
+            {
+                context: { userId: options.memberId },
+            }
+        );
+
+        let shard: number;
+
+        for (const i of clusterHas) {
+            if (i != "not-found") {
+                shard = i;
+                break;
+            }
+        }
+
+        if (isNaN(shard)) {
+            logger.warn("user not found");
+            return false;
+        }
+
+        const payload: MessageOptions = {
+            content: options.content,
+        };
+
+        if (options.embed) {
+            payload.embeds = [options.embed.toJSON()];
+        }
+
+        const res = await options.client.broadcastEval(
+            async (c, { needed, memberId, payload }) => {
+                if (!c.shard.ids.includes(needed)) return false;
+
+                const user = await c.users.fetch(memberId).catch(() => {});
+
+                if (!user) return false;
+
+                let fail = false;
+
+                await user.send(payload as MessagePayload).catch(() => {
+                    fail = true;
+                });
+
+                if (fail) {
+                    return false;
+                }
+                return true;
+            },
+            {
+                context: {
+                    needed: shard,
+                    memberId: options.memberId,
+                    payload: payload,
+                },
+            }
+        );
+
+        if (res.includes(true)) {
+            logger.log({
+                level: "success",
+                message: "DM sent",
+            });
+            return true;
+        } else {
+            logger.error("failed to send DM");
+            return false;
+        }
     }
 }
