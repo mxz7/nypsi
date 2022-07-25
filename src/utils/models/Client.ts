abfc1e3f2a4b89ac064aac5e80707cb9492b769a
max <29782397+tekoh@users.noreply.github.com>
2022-07-25 14:25:29 +0100

    fix: actually run the command timers thing 🙄

--- src/utils/models/Client.ts
+++ src/utils/models/Client.ts
@@ -1,6 +1,7 @@
 import * as Cluster from "discord-hybrid-sharding";
 import { Client, ClientOptions } from "discord.js";
 import { doChatReactions } from "../chatreactions/utils";
+import { runCommandUseTimers } from "../commandhandler";
 import { runEconomySetup } from "../economy/utils";
 import { runChristmas, runCountdowns, runSnipeClearIntervals, updateCounters } from "../guilds/utils";
 import { updateCache } from "../imghandler";
@@ -32,6 +33,7 @@ export class NypsiClient extends Client {
         runChristmas(this);
         runSnipeClearIntervals();
         doChatReactions(this);
+        runCommandUseTimers(this);
 
         if (this.cluster.id != 0) return;
 
