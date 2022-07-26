8333033094e6a044e74e65a3a0a3cc0aed11bd51
max <29782397+tekoh@users.noreply.github.com>
2022-07-26 23:50:07 +0100

    feat: zero downtime restarts (#489)
    * feat: use recluster manager
    
    * refactor: move loading events to client method
    
    * feat: listener for restart request
    
    * refactor: delete restarting key on ready
    
    * feat: send restart message to manager
    
    * 4.3.1

--- src/utils/models/Client.ts
+++ src/utils/models/Client.ts
@@ -1,11 +1,25 @@
 import * as Cluster from "discord-hybrid-sharding";
 import { Client, ClientOptions } from "discord.js";
+import channelCreate from "../../events/channelCreate";
+import guildCreate from "../../events/guildCreate";
+import guildDelete from "../../events/guildDelete";
+import guildMemberAdd from "../../events/guildMemberAdd";
+import guildMemberRemove from "../../events/guildMemberRemove";
+import guildMemberUpdate from "../../events/guildMemberUpdate";
+import interactionCreate from "../../events/interactionCreate";
+import messageCreate from "../../events/message";
+import messageDelete from "../../events/messageDelete";
+import messageUpdate from "../../events/messageUpdate";
+import ready from "../../events/ready";
+import roleDelete from "../../events/roleDelete";
+import userUpdate from "../../events/userUpdate";
 import { doChatReactions } from "../chatreactions/utils";
 import { runCommandUseTimers } from "../commandhandler";
+import redis from "../database/redis";
 import { runEconomySetup } from "../economy/utils";
 import { runChristmas, runCountdowns, runSnipeClearIntervals, updateCounters } from "../guilds/utils";
 import { updateCache } from "../imghandler";
-import { getWebhooks, setClusterId } from "../logger";
+import { getWebhooks, logger, setClusterId } from "../logger";
 import { runLotteryInterval } from "../scheduled/clusterjobs/lottery";
 import { runModerationChecks } from "../scheduled/clusterjobs/moderationchecks";
 import { runPremiumChecks } from "../scheduled/clusterjobs/premiumexpire";
@@ -23,7 +37,52 @@ export class NypsiClient extends Client {
 
         runEconomySetup();
 
+        if (this.cluster.maintenance) {
+            logger.info(`started on maintenance mode with ${this.cluster.maintenance}`);
+        }
+
         return this;
+    }
+
+    public loadEvents() {
+        this.on("shardReady", (shardID) => {
+            logger.info(`shard#${shardID} ready`);
+        });
+        this.on("shardDisconnect", (s, shardID) => {
+            logger.info(`shard#${shardID} disconnected`);
+        });
+        this.on("shardError", (error1, shardID) => {
+            logger.error(`shard#${shardID} error: ${error1}`);
+        });
+        this.on("shardReconnecting", (shardID) => {
+            logger.info(`shard#${shardID} connecting`);
+        });
+        this.on("shardResume", (shardId) => {
+            logger.info(`shard#${shardId} resume`);
+        });
+
+        this.once("ready", ready.bind(null, this));
+
+        this.cluster.on("ready", async () => {
+            await redis.del("nypsi:restarting");
+            this.on("guildCreate", guildCreate.bind(null, this));
+            this.on("guildDelete", guildDelete.bind(null, this));
+            this.rest.on("rateLimited", (rate) => {
+                const a = rate.route.split("/");
+                const reason = a[a.length - 1];
+                logger.warn("rate limit: " + reason);
+            });
+            this.on("guildMemberUpdate", guildMemberUpdate.bind(null));
+            this.on("guildMemberAdd", guildMemberAdd.bind(null));
+            this.on("guildMemberRemove", guildMemberRemove.bind(null));
+            this.on("messageDelete", messageDelete.bind(null));
+            this.on("messageUpdate", messageUpdate.bind(null));
+            this.on("messageCreate", messageCreate.bind(null));
+            this.on("channelCreate", channelCreate.bind(null));
+            this.on("roleDelete", roleDelete.bind(null));
+            this.on("userUpdate", userUpdate.bind(null));
+            this.on("interactionCreate", interactionCreate.bind(null));
+        });
     }
 
     public runIntervals() {
