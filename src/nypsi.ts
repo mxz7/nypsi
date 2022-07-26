8660581e8f00ddd6f0fa9f3463dfb78c2d3d33d3
max <29782397+tekoh@users.noreply.github.com>
2022-07-26 23:50:07 +0100

    feat: zero downtime restarts (#489)
    * feat: use recluster manager
    
    * refactor: move loading events to client method
    
    * feat: listener for restart request
    
    * refactor: delete restarting key on ready
    
    * feat: send restart message to manager
    
    * 4.3.1

--- src/nypsi.ts
+++ src/nypsi.ts
@@ -42,74 +42,11 @@ const client = new NypsiClient({
     ],
 });
 
-import channelCreate from "./events/channelCreate";
-import guildCreate from "./events/guildCreate";
-import guildDelete from "./events/guildDelete";
-import guildMemberAdd from "./events/guildMemberAdd";
-import guildMemberRemove from "./events/guildMemberRemove";
-import guildMemberUpdate from "./events/guildMemberUpdate";
-import interactionCreate from "./events/interactionCreate";
-import messageCreate from "./events/message";
-import messageDelete from "./events/messageDelete";
-import messageUpdate from "./events/messageUpdate";
-import ready from "./events/ready";
-import roleDelete from "./events/roleDelete";
-import userUpdate from "./events/userUpdate";
 import { loadCommands } from "./utils/commandhandler";
 import { logger } from "./utils/logger";
 
 loadCommands();
-
-client.once("ready", ready.bind(null, client));
-if (!process.env.GITHUB_ACTION) {
-    client.on("guildCreate", guildCreate.bind(null, client));
-    client.on("guildDelete", guildDelete.bind(null, client));
-    client.rest.on("rateLimited", (rate) => {
-        const a = rate.route.split("/");
-        const reason = a[a.length - 1];
-        logger.warn("rate limit: " + reason);
-    });
-    client.on("guildMemberUpdate", guildMemberUpdate.bind(null));
-    client.on("guildMemberAdd", guildMemberAdd.bind(null));
-    client.on("guildMemberRemove", guildMemberRemove.bind(null));
-    client.on("messageDelete", messageDelete.bind(null));
-    client.on("messageUpdate", messageUpdate.bind(null));
-    client.on("messageCreate", messageCreate.bind(null));
-    client.on("channelCreate", channelCreate.bind(null));
-    client.on("roleDelete", roleDelete.bind(null));
-    client.on("userUpdate", userUpdate.bind(null));
-    client.on("interactionCreate", interactionCreate.bind(null));
-}
-
-client.on("shardReady", (shardID) => {
-    logger.info(`shard#${shardID} ready`);
-});
-client.on("shardDisconnect", (s, shardID) => {
-    logger.info(`shard#${shardID} disconnected`);
-});
-client.on("shardError", (error1, shardID) => {
-    logger.error(`shard#${shardID} error: ${error1}`);
-});
-client.on("shardReconnecting", (shardID) => {
-    logger.info(`shard#${shardID} connecting`);
-});
-client.on("shardResume", (shardId) => {
-    logger.info(`shard#${shardId} resume`);
-});
-
-// process.on("uncaughtException", (e: any) => {
-//     const excludedReasons = [50013, 10008, 50001];
-
-//     if (e.code && excludedReasons.includes(e.code)) return;
-//     logger.error(`unhandled promise rejection: ${e.stack}`);
-// });
-
-// process.on("unhandledRejection", (e: any) => {
-//     const excludedReasons = [50013, 10008, 50001];
-
-//     if (e.code && excludedReasons.includes(e.code)) return;
-//     logger.error(`unhandled promise rejection: ${e.stack}`);
-// });
+client.loadEvents();
 
 setTimeout(() => {
     logger.info("logging in...");
