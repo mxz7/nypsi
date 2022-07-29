df00fa531091986404bb1a00b34970b2e2442d3f
max <29782397+tekoh@users.noreply.github.com>
2022-07-29 22:04:21 +0100

    refactor: change time cleanmentions will run

--- src/utils/scheduled/scheduler.ts
+++ src/utils/scheduled/scheduler.ts
@@ -56,7 +56,7 @@ const bree = new Bree({
         },
         {
             name: "clean-mentions",
-            interval: "6h",
+            interval: "at 11:59pm",
             path: path.join(__dirname, "jobs", "cleanmentions.js"),
         },
         {
