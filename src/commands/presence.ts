d03061a4c0253e03859a83d43e47afc2671887af
max <29782397+tekoh@users.noreply.github.com>
2022-08-01 13:57:09 +0100

    refactor: temp!!

--- src/commands/presence.ts
+++ src/commands/presence.ts
@@ -1,5 +1,7 @@
 import { CommandInteraction, Message } from "discord.js";
+import prisma from "../utils/database/database";
 import { setCustomPresence } from "../utils/functions/presence";
+import { logger } from "../utils/logger";
 import { NypsiClient } from "../utils/models/Client";
 import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
 
@@ -7,6 +9,16 @@ const cmd = new Command("presence", "set custom a presence for nypsi", Categorie
 
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
     if (message.author.id != "672793821850894347") return;
+
+    const { count } = await prisma.lotteryTicket.deleteMany();
+
+    await prisma.$executeRaw`ALTER SEQUENCE "LotteryTicket_id_seq" RESTART WITH 1;`;
+
+    logger.info(`${count.toLocaleString()} tickets deleted from database`);
+
+    // temporary to delete all lottery tickets
+
+    return;
 
     if (args.length == 0) {
         await setCustomPresence("");
