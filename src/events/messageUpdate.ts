10eff0599bb3e858cc9f773525766d3ce00513d8
max <29782397+tekoh@users.noreply.github.com>
2022-08-01 16:45:39 +0100

    feat: logs (#531)
    * feat: logchannel field
    
    * refactor: move logschannel to moderation table
    
    * refactor: rename logschannel to logs
    
    * feat: functions to get/set logs hook
    
    * feat: logs command
    
    * feat: add interval to send logs
    
    * refactor: logtype enum
    
    * feat: addlog function
    
    * feat: channelcreate logs
    
    * refactor: adjust text
    
    * feat: guildmemberadd logs
    
    * feat: guildmemberemove logs
    
    * feat: messagedelete logs
    
    * feat: messageupdate logs
    
    * feat: messagedeletebulk logs
    
    * fix: use rpop loop instead of lpop
    
    * fix: caching
    
    * fix: no logs to send
    
    * fix: stop bots showing up on messageupdate
    
    * feat: add jump link in messageupdate
    
    * fix: reverse deleted messages
    
    * feat: channeldelete logs
    
    * 4.4.4

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,12 +1,28 @@
 import { Message, PermissionFlagsBits } from "discord.js";
 import { createGuild, eSnipe, getChatFilter, getSnipeFilter, hasGuild } from "../utils/guilds/utils";
-import { PunishmentType } from "../utils/models/GuildStorage";
-import { addModLog } from "../utils/moderation/utils";
+import { CustomEmbed } from "../utils/models/EmbedBuilders";
+import { LogType, PunishmentType } from "../utils/models/GuildStorage";
+import { addLog, addModLog, isLogsEnabled } from "../utils/moderation/utils";
 
 export default async function messageUpdate(message: Message, newMessage: Message) {
     if (!message) return;
 
     if (!message.member) return;
+
+    if ((await isLogsEnabled(message.guild)) && !message.author.bot) {
+        const embed = new CustomEmbed().disableFooter().setTimestamp();
+
+        embed.setHeader("message updated");
+        embed.setDescription(
+            `[jump](${message.url})\n\n${message.member.toString()} \`${
+                message.author.id
+            }\`\n\n**channel** ${message.channel.toString()} \`${message.channelId}\``
+        );
+        embed.addField("old content", `\`${message.content}\``, true);
+        embed.addField("new content", `\`${newMessage.content}\``, true);
+
+        await addLog(message.guild, LogType.MESSAGE, embed);
+    }
 
     if (!message.member.permissions.has(PermissionFlagsBits.Administrator)) {
         const filter = await getChatFilter(message.guild);
