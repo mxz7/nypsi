5a4043f8e2e5a004f77591a8fc137b21e59ad140
max <29782397+tekoh@users.noreply.github.com>
2022-08-01 22:38:57 +0100

    fix: prevent resetting daily streaks when not needed

--- src/utils/scheduled/jobs/dailystreak.ts
+++ src/utils/scheduled/jobs/dailystreak.ts
@@ -3,11 +3,11 @@ import { parentPort } from "worker_threads";
 import prisma from "../../database/database";
 
 (async () => {
-    const limit = dayjs().subtract(1, "day").subtract(4, "hours").toDate();
+    const limit = dayjs().subtract(1, "day").subtract(6, "hours").toDate();
 
     const query = await prisma.economy.updateMany({
         where: {
-            lastDaily: { lte: limit },
+            AND: [{ lastDaily: { lte: limit } }, { dailyStreak: 1 }],
         },
         data: {
             dailyStreak: 0,
