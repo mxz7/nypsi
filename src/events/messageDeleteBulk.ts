0bd35dc20d557437a697a4853ed94af6b33f2158
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2022-08-08 07:32:11 -0400

    feat: add time and channel name to bulk delete log (#546)

--- src/events/messageDeleteBulk.ts
+++ src/events/messageDeleteBulk.ts
@@ -2,6 +2,7 @@ import { Collection, Message, Snowflake, TextBasedChannel } from "discord.js";
 import { CustomEmbed } from "../utils/models/EmbedBuilders";
 import { LogType } from "../utils/models/GuildStorage";
 import { addLog, isLogsEnabled } from "../utils/moderation/utils";
+import dayjs = require("dayjs");
 
 export default async function messageDeleteBulk(messages: Collection<Snowflake, Message>, channel: TextBasedChannel) {
     if (channel.isDMBased()) return;
@@ -9,13 +10,17 @@ export default async function messageDeleteBulk(messages: Collection<Snowflake, 
     if (await isLogsEnabled(channel.guild)) {
         const embed = new CustomEmbed().disableFooter().setTimestamp();
 
-        embed.setHeader(`${messages.size} messages deleted [bulk delete]`);
+        embed.setHeader(`${messages.size} messages deleted in #${channel.name} [bulk delete]`);
 
         const desc: string[] = [];
 
         messages.each((message) => {
             if (message.content) {
-                desc.push(`${message.author.tag}: ${message.content}`);
+                desc.push(
+                    `[${dayjs(message.createdTimestamp).format("YYYY-MM-DD HH:mm:ss")}] ${message.author.tag}: ${
+                        message.content
+                    }`
+                );
             }
 
             if (desc.join("\n").length > 1500) {
