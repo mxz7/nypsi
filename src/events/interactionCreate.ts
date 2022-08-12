27768066e07c4bd8586f8ac82cd29df9fbd5d018
max <29782397+tekoh@users.noreply.github.com>
2022-08-12 23:08:40 +0100

    feat: thingy to buy auctions

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -8,11 +8,78 @@ import {
     InteractionType,
 } from "discord.js";
 import { runCommand } from "../utils/commandhandler";
+import prisma from "../utils/database/database";
+import { getBalance, updateBalance, userExists } from "../utils/economy/utils";
 import { logger } from "../utils/logger";
+import { NypsiClient } from "../utils/models/Client";
 import { createNypsiInteraction, NypsiCommandInteraction } from "../utils/models/Command";
-import { CustomEmbed } from "../utils/models/EmbedBuilders";
+import { CustomEmbed, ErrorEmbed } from "../utils/models/EmbedBuilders";
 
 export default async function interactionCreate(interaction: Interaction) {
+    if (interaction.type == InteractionType.MessageComponent && interaction.customId == "b") {
+        const auction = await prisma.auction.findUnique({
+            where: {
+                messageId: interaction.message.id,
+            },
+            select: {
+                bin: true,
+                messageId: true,
+                id: true,
+            },
+        });
+
+        if (auction) {
+            if (!(await userExists(interaction.user.id))) {
+                return await interaction.reply({ embeds: [new ErrorEmbed("you cannot afford this")], ephemeral: true });
+            }
+
+            const balance = await getBalance(interaction.user.id);
+
+            if (balance < Number(auction.bin)) {
+                return await interaction.reply({ embeds: [new ErrorEmbed("you cannot afford this")], ephemeral: true });
+            }
+
+            await prisma.auction
+                .delete({
+                    where: {
+                        id: auction.id,
+                    },
+                })
+                .catch(() => {});
+            await updateBalance(interaction.user.id, balance - Number(auction.bin));
+
+            await (interaction.client as NypsiClient).cluster.broadcastEval(
+                async (client, { messageId, username }) => {
+                    const guild = await client.guilds.fetch("747056029795221513");
+
+                    if (!guild) return;
+
+                    const channel = await guild.channels.fetch("819640200699052052");
+
+                    if (!channel) return;
+
+                    if (channel.isTextBased()) {
+                        const msg = await channel.messages.fetch(messageId);
+
+                        if (msg) {
+                            const embed = JSON.parse(JSON.stringify(msg.embeds[0]));
+                            const desc = embed.description.split("\n\n");
+
+                            desc[0] = `**bought** by ${username} <t:${Math.floor(Date.now() / 1000)}:R>`;
+
+                            embed.description = desc.join("\n\n");
+
+                            await msg.edit({ embeds: [embed], components: [] });
+                        }
+                    }
+                },
+                {
+                    context: { messageId: auction.messageId, username: interaction.user.username },
+                }
+            );
+        }
+    }
+
     if (interaction.type != InteractionType.ApplicationCommand) return;
 
     if (!interaction.guild) {
