33db24916f803f34245bec3451c23c4f3dc22dcc
max <29782397+tekoh@users.noreply.github.com>
2022-08-14 19:21:50 +0100

    fix: last command

--- src/commands/find.js
+++ src/commands/find.js
@@ -181,7 +181,7 @@ async function showUser(message, user) {
             `**tag** ${user.tag}
             **created** ${formatDate(user.createdAt)}${
                 (await getLastCommand(user.id))
-                    ? `\n**last command** <t:${(await (await getLastCommand(user.id)).getTime()) / 1000}:R> days ago`
+                    ? `\n**last command** <t:${(await Math.floor(await getLastCommand(user.id)).getTime()) / 1000}:R>`
                     : ""
             }`,
             true
