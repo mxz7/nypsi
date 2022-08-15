1923dcce13aebce4061ac683e966d820a15efed2
max <29782397+tekoh@users.noreply.github.com>
2022-08-15 19:41:12 +0100

    feat: auction house (#570)
    * feat: auction table
    
    * feat: createdat field
    
    * feat: make daily streak stricter
    
    * feat: functions for creating/managing auctions
    
    * fix: messageid should be unique
    
    * fix: delete auction message on delete
    
    * feat: thingy to buy auctions
    
    * fix: im so stupid bruv
    
    * feat: started auction command
    
    * refactor: reduce tax on lottery win
    
    * fix: last command
    
    * feat: return message url
    
    * feat: finish auction creation process
    
    * fix: remove item from inventory when creating auction
    
    * fix: update inventory before checks
    
    * feat: auction command
    
    * feat: check auctions every 3 hours
    
    * feat: dm auction owner when auction is bought
    
    * fix: prevent buying own auction
    
    * fix: remove row
    
    * fix: delete message
    
    * fix: stupid djs
    
    * fix: item selection with drop down
    
    * fix: ahhhh
    
    * fix: more auction fixes
    
    * fix: djs wasnt stupid im just stupid
    
    * feat: bump auction function
    
    * feat: change channel id
    
    * fix: cost processing
    
    * fix: remove console.log
    
    * fix: im so silly
    
    * fix: bitcoin/eth emoji
    
    * fix: error message with empty inventory
    
    * feat: enable slash command for auction
    
    * 4.5.0

---
+++ src/utils/scheduled/clusterjobs/checkauctions.ts
@@ -0,0 +1,61 @@
+import dayjs = require("dayjs");
+import ms = require("ms");
+import prisma from "../../database/database";
+import { deleteAuction, getInventory, getItems, userExists } from "../../economy/utils";
+import requestDM from "../../functions/requestdm";
+import { logger } from "../../logger";
+import { NypsiClient } from "../../models/Client";
+import { CustomEmbed } from "../../models/EmbedBuilders";
+
+export async function runAuctionChecks(client: NypsiClient) {
+    setInterval(async () => {
+        const limit = dayjs().subtract(3, "days").toDate();
+
+        const query = await prisma.auction.findMany({
+            where: {
+                createdAt: { lte: limit },
+            },
+            select: {
+                ownerId: true,
+                itemAmount: true,
+                itemName: true,
+                id: true,
+            },
+        });
+
+        const items = getItems();
+
+        for (const auction of query) {
+            await deleteAuction(auction.id, client);
+
+            if (!(await userExists(auction.ownerId))) continue;
+
+            const inventory = await getInventory(auction.ownerId);
+
+            if (inventory[auction.itemName]) {
+                inventory[auction.itemName] += auction.itemAmount;
+            } else {
+                inventory[auction.itemName] = auction.itemAmount;
+            }
+
+            const embed = new CustomEmbed().setColor("#36393f");
+
+            embed.setDescription(
+                `your auction for ${auction.itemAmount}x ${items[auction.itemName].emoji} ${
+                    items[auction.itemName].name
+                } has expired. you have been given back your item${auction.itemAmount > 1 ? "s" : ""}`
+            );
+
+            await requestDM({
+                client: client,
+                content: "your auction has expired",
+                memberId: auction.ownerId,
+                embed: embed,
+            });
+        }
+
+        if (query.length > 0) {
+            logger.info(`${query.length} auctions expired`);
+        }
+    }, ms("3 hours"));
+}
