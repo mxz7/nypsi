c1a07a05e7dc9f7dbd177c85e5694de609ddb7d6
max <29782397+tekoh@users.noreply.github.com>
2022-09-01 15:55:52 +0100

    feat: survey create command

---
+++ src/commands/survey.ts
@@ -0,0 +1,101 @@
+import dayjs = require("dayjs");
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    CommandInteraction,
+    InteractionReplyOptions,
+    Message,
+    MessageActionRowComponentBuilder,
+    MessageOptions,
+} from "discord.js";
+import { createSurvey, getSurveys } from "../utils/functions/surveys";
+import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
+import { CustomEmbed, ErrorEmbed } from "../utils/models/EmbedBuilders";
+import { getTier, isPremium } from "../utils/premium/utils";
+
+const cmd = new Command("survey", "create a survey for users to respond to", Categories.INFO);
+
+cmd.slashEnabled = true;
+cmd.slashData.addSubcommand((create) =>
+    create
+        .setName("create")
+        .setDescription("create a survey")
+        .addStringOption((option) =>
+            option
+                .setName("text")
+                .setDescription("the text/question asked to members")
+                .setRequired(true)
+                .setMinLength(1)
+                .setMaxLength(500)
+        )
+        .addIntegerOption((option) =>
+            option.setName("hours").setDescription("amount of hours until survey ends").setMinValue(1).setMaxValue(72)
+        )
+);
+
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction)) {
+    const send = async (data: MessageOptions) => {
+        if (!(message instanceof Message)) {
+            if (message.deferred) {
+                await message.editReply(data);
+            } else {
+                await message.reply(data as InteractionReplyOptions);
+            }
+            const replyMsg = await message.fetchReply();
+            if (replyMsg instanceof Message) {
+                return replyMsg;
+            }
+        } else {
+            return await message.channel.send(data);
+        }
+    };
+
+    if (message instanceof Message) {
+        return send({ embeds: [new CustomEmbed(message.member, "use /survey")] });
+    }
+
+    if (!message.isChatInputCommand()) return;
+
+    const surveys = await getSurveys(message.member);
+
+    let max = 1;
+
+    if (await isPremium(message.member)) {
+        max += await getTier(message.member);
+    }
+
+    if (surveys.length >= max) {
+        return send({
+            embeds: [
+                new ErrorEmbed(
+                    `you have reached your maximum amount of surveys (\`${max}\`). you can become a patreon to create more`
+                ),
+            ],
+        });
+    }
+
+    let desc = message.options.getString("string");
+    const endsAt = dayjs()
+        .add(message.options.getInteger("hours") || 12, "hours")
+        .toDate();
+
+    desc += `\n\nanswers: \`0\`\nends <t:${Math.floor(endsAt.getTime() / 1000)}:R>`;
+
+    const embed = new CustomEmbed(message.member, desc).setHeader(
+        `${message.author.username}'s survey`,
+        message.author.avatarURL()
+    );
+
+    const row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+        new ButtonBuilder().setCustomId("a").setLabel("answer").setStyle(ButtonStyle.Success)
+    );
+
+    const msg = await send({ embeds: [embed], components: [row] });
+
+    return await createSurvey(message.author.id, message.options.getString("string"), endsAt, msg.id);
+}
+
+cmd.setRun(run);
+
+module.exports = cmd;
