1cdb926dd193839f16f9be88b4de635cee078e77
max <29782397+tekoh@users.noreply.github.com>
2022-09-04 14:09:38 +0100

    feat: return true if achievement is completed

--- src/utils/economy/achievements.ts
+++ src/utils/economy/achievements.ts
@@ -58,7 +58,9 @@ export async function setAchievementProgress(userId: string, achievementId: stri
 
     if (query.progress >= achievements[achievementId].target) {
         await completeAchievement(userId, achievementId);
+        return true;
     }
+    return false;
 }
 
 export async function getAllAchievements(id: string) {
