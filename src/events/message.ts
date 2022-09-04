be3b9e06a0ecfe934dee4f3ea1d6998175f8a735
max <29782397+tekoh@users.noreply.github.com>
2022-09-04 22:41:08 +0100

    fix: error thing

--- src/events/message.ts
+++ src/events/message.ts
@@ -279,12 +279,14 @@ export default async function messageCreate(message: Message) {
                     if (message.mentions.members.size == 1) {
                         if (message.mentions.members.first().user.id == message.author.id) return;
 
-                        if (
-                            !Array.from((message.channel as TextChannel).members.keys()).includes(
-                                message.mentions.members.first().id
-                            )
-                        ) {
-                            return; // return if user doesnt have access to channel
+                        if ((message.channel as TextChannel).members) {
+                            if (
+                                !Array.from((message.channel as TextChannel).members.keys()).includes(
+                                    message.mentions.members.first().id
+                                )
+                            ) {
+                                return; // return if user doesnt have access to channel
+                            }
                         }
 
                         let content = message.content;
