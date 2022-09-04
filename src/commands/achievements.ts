e928b6caeb6b56d5662d908e7edb1ca334e89c20
max <29782397+tekoh@users.noreply.github.com>
2022-09-04 13:39:55 +0100

    feat: command to show all achievements

--- src/commands/achievements.ts
+++ src/commands/achievements.ts
@@ -1,6 +1,18 @@
-import { CommandInteraction, InteractionReplyOptions, Message, MessageOptions } from "discord.js";
-import { getResponse, onCooldown } from "../utils/cooldownhandler";
-import { getUncompletedAchievements } from "../utils/economy/achievements";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    CommandInteraction,
+    Interaction,
+    InteractionReplyOptions,
+    Message,
+    MessageActionRowComponentBuilder,
+    MessageEditOptions,
+    MessageOptions,
+} from "discord.js";
+import { inPlaceSort } from "fast-sort";
+import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler";
+import { getAllAchievements, getUncompletedAchievements } from "../utils/economy/achievements";
 import { getAchievements } from "../utils/economy/utils";
 import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
 import { CustomEmbed } from "../utils/models/EmbedBuilders";
@@ -27,23 +39,35 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
         }
     };
 
+    const edit = async (data: MessageEditOptions, msg: Message) => {
+        if (!(message instanceof Message)) {
+            await message.editReply(data);
+            return await message.fetchReply();
+        } else {
+            return await msg.edit(data);
+        }
+    };
+
     if (await onCooldown(cmd.name, message.member)) {
         const embed = await getResponse(cmd.name, message.member);
 
         return send({ embeds: [embed], ephemeral: true });
     }
 
+    await addCooldown(cmd.name, message.member, 15);
+
     const showCurrentProgress = async () => {
         const allAchievementData = getAchievements();
         const achievements = await getUncompletedAchievements(message.author.id);
 
         if (!achievements || achievements.length == 0) {
-            return send({ embeds: [new CustomEmbed(message.member, "you have no achievements in progress")] });
+            return showAllAchievements();
         }
 
         const desc: string[] = [];
 
         for (const achievement of achievements) {
+            if (achievement.completed) continue;
             desc.push(
                 `${allAchievementData[achievement.achievementId].emoji} ${
                     allAchievementData[achievement.achievementId].name
@@ -56,6 +80,8 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
             );
         }
 
+        if (desc.length == 0) return showAllAchievements();
+
         const embed = new CustomEmbed(message.member, desc.join("\n")).setHeader(
             "your achievement progress",
             message.author.avatarURL()
@@ -64,10 +90,176 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
         return send({ embeds: [embed] });
     };
 
+    const showAllAchievements = async () => {
+        const allAchievements = getAchievements();
+        const achievementIds = Object.keys(allAchievements);
+        const usersAchievements = await getAllAchievements(message.author.id);
+        const userAchievementIds = usersAchievements.map((i) => i.achievementId);
+
+        inPlaceSort(achievementIds).asc();
+
+        const pages = new Map<number, string[]>();
+
+        for (const achievementId of achievementIds) {
+            const achievement = allAchievements[achievementId];
+
+            let str = `${achievement.emoji} ${achievement.name} `;
+
+            if (userAchievementIds.includes(achievementId)) {
+                const achData = usersAchievements.find((i) => i.achievementId == achievementId);
+
+                if (!achData) continue;
+
+                if (achData.completed) {
+                    str += "`completed`";
+                } else {
+                    str += `\`${achData.progress.toLocaleString()} / ${achievement.target} (${(
+                        (achData.progress / achievement.target) *
+                        100
+                    ).toFixed(1)}%)\``;
+                }
+            } else {
+                str += `*${achievement.description}*`;
+            }
+
+            if (pages.size == 0) {
+                pages.set(1, [str]);
+            } else if (pages.get(pages.size).length >= 10) {
+                pages.set(pages.size + 1, [str]);
+            } else {
+                const arr = pages.get(pages.size);
+                arr.push(str);
+            }
+        }
+
+        const embed = new CustomEmbed(message.member, pages.get(1).join("\n"))
+            .setHeader("all achievements", message.author.avatarURL())
+            .setFooter({ text: `page 1/${pages.size}` });
+
+        let row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+            new ButtonBuilder().setCustomId("⬅").setLabel("back").setStyle(ButtonStyle.Primary).setDisabled(true),
+            new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary)
+        );
+
+        let msg: Message;
+
+        if (pages.size == 1) {
+            return await send({ embeds: [embed] });
+        } else {
+            msg = await send({ embeds: [embed], components: [row] });
+        }
+
+        const filter = (i: Interaction) => i.user.id == message.author.id;
+
+        let currentPage = 1;
+
+        const pageManager = async (): Promise<void> => {
+            const reaction = await msg
+                .awaitMessageComponent({ filter, time: 30000 })
+                .then(async (collected) => {
+                    await collected.deferUpdate();
+                    return collected.customId;
+                })
+                .catch(async () => {
+                    await edit({ components: [] }, msg).catch(() => {});
+                });
+
+            if (!reaction) return;
+
+            const newEmbed = new CustomEmbed(message.member).setHeader("most used commands", message.author.avatarURL());
+
+            if (reaction == "⬅") {
+                if (currentPage <= 1) {
+                    return pageManager();
+                } else {
+                    currentPage--;
+
+                    newEmbed.setDescription(pages.get(currentPage).join("\n"));
+
+                    newEmbed.setFooter({ text: `page ${currentPage}/${pages.size}` });
+
+                    if (currentPage == 1) {
+                        row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+                            new ButtonBuilder()
+                                .setCustomId("⬅")
+                                .setLabel("back")
+                                .setStyle(ButtonStyle.Primary)
+                                .setDisabled(true),
+                            new ButtonBuilder()
+                                .setCustomId("➡")
+                                .setLabel("next")
+                                .setStyle(ButtonStyle.Primary)
+                                .setDisabled(false)
+                        );
+                    } else {
+                        row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+                            new ButtonBuilder()
+                                .setCustomId("⬅")
+                                .setLabel("back")
+                                .setStyle(ButtonStyle.Primary)
+                                .setDisabled(false),
+                            new ButtonBuilder()
+                                .setCustomId("➡")
+                                .setLabel("next")
+                                .setStyle(ButtonStyle.Primary)
+                                .setDisabled(false)
+                        );
+                    }
+                    await edit({ embeds: [newEmbed], components: [row] }, msg);
+                    return pageManager();
+                }
+            } else if (reaction == "➡") {
+                if (currentPage >= pages.size) {
+                    return pageManager();
+                } else {
+                    currentPage++;
+
+                    newEmbed.setDescription(pages.get(currentPage).join("\n"));
+
+                    newEmbed.setFooter({ text: `page ${currentPage}/${pages.size}` });
+
+                    if (currentPage == pages.size) {
+                        row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+                            new ButtonBuilder()
+                                .setCustomId("⬅")
+                                .setLabel("back")
+                                .setStyle(ButtonStyle.Primary)
+                                .setDisabled(false),
+                            new ButtonBuilder()
+                                .setCustomId("➡")
+                                .setLabel("next")
+                                .setStyle(ButtonStyle.Primary)
+                                .setDisabled(true)
+                        );
+                    } else {
+                        row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+                            new ButtonBuilder()
+                                .setCustomId("⬅")
+                                .setLabel("back")
+                                .setStyle(ButtonStyle.Primary)
+                                .setDisabled(false),
+                            new ButtonBuilder()
+                                .setCustomId("➡")
+                                .setLabel("next")
+                                .setStyle(ButtonStyle.Primary)
+                                .setDisabled(false)
+                        );
+                    }
+                    await edit({ embeds: [newEmbed], components: [row] }, msg);
+                    return pageManager();
+                }
+            }
+        };
+
+        return pageManager();
+    };
+
     if (args.length == 0) {
         return showCurrentProgress();
     } else if (args[0].toLowerCase() == "view") {
         return showCurrentProgress();
+    } else if (args[0].toLocaleLowerCase() == "all") {
+        return showAllAchievements();
     }
 }
 
