ac4f21d2b96a15881ddbcdd3c549c2207696361d
max <29782397+tekoh@users.noreply.github.com>
2022-09-04 23:31:37 +0100

    feat: top completion command

---
+++ src/commands/completiontop.ts
@@ -0,0 +1,74 @@
+import { CommandInteraction, InteractionReplyOptions, Message, MessageOptions, PermissionFlagsBits } from "discord.js";
+import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler.js";
+import { topCompletion } from "../utils/economy/utils.js";
+import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
+import { CustomEmbed, ErrorEmbed } from "../utils/models/EmbedBuilders.js";
+
+const cmd = new Command("completiontop", "view top completion rates in the server", Categories.MONEY).setAliases([
+    "topc",
+    "topcomplete",
+]);
+
+cmd.slashEnabled = true;
+
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
+    const send = async (data: MessageOptions | InteractionReplyOptions) => {
+        if (!(message instanceof Message)) {
+            if (message.deferred) {
+                await message.editReply(data);
+            } else {
+                await message.reply(data as InteractionReplyOptions);
+            }
+            const replyMsg = await message.fetchReply();
+            if (replyMsg instanceof Message) {
+                return replyMsg;
+            }
+        } else {
+            return await message.channel.send(data as MessageOptions);
+        }
+    };
+
+    if (await onCooldown(cmd.name, message.member)) {
+        const embed = await getResponse(cmd.name, message.member);
+
+        return send({ embeds: [embed], ephemeral: true });
+    }
+
+    await addCooldown(cmd.name, message.member, 15);
+
+    let amount;
+
+    if (args.length == 0) {
+        args[0] = "5";
+    }
+
+    if (isNaN(parseInt(args[0])) || parseInt(args[0]) <= 0) {
+        args[0] = "5";
+    }
+
+    amount = parseInt(args[0]);
+
+    if (amount > 10 && !message.member.permissions.has(PermissionFlagsBits.Administrator)) amount = 10;
+
+    if (amount < 5) amount = 5;
+
+    const balTop = await topCompletion(message.guild, amount);
+
+    const filtered = balTop.filter(function (el) {
+        return el != null;
+    });
+
+    if (filtered.length == 0) {
+        return send({ embeds: [new ErrorEmbed("there are no users to show")] });
+    }
+
+    const embed = new CustomEmbed(message.member)
+        .setHeader(`top ${filtered.length} completion`)
+        .setDescription(filtered.join("\n"));
+
+    send({ embeds: [embed] });
+}
+
+cmd.setRun(run);
+
+module.exports = cmd;
--- src/utils/economy/utils.ts
+++ src/utils/economy/utils.ts
@@ -481,6 +481,88 @@ export async function topAmountGlobal(amount: number, client?: NypsiClient, anon
     return usersFinal;
 }
 
+export async function topCompletion(guild: Guild, amount = 10): Promise<string[]> {
+    let members: Collection<string, GuildMember>;
+
+    if (guild.memberCount == guild.members.cache.size) {
+        members = guild.members.cache;
+    } else {
+        members = await guild.members.fetch();
+    }
+
+    if (!members) members = guild.members.cache;
+
+    members = members.filter((m) => {
+        return !m.user.bot;
+    });
+
+    const query = await prisma.achievements.findMany({
+        where: {
+            AND: [{ completed: true }, { userId: { in: Array.from(members.keys()) } }],
+        },
+        select: {
+            userId: true,
+        },
+    });
+
+    if (query.length == 0) {
+        return [];
+    }
+
+    const allAchievements = Object.keys(getAchievements()).length;
+    let userIds = query.map((i) => i.userId);
+    const completionRate = new Map<string, number>();
+
+    userIds = [...new Set(userIds)];
+
+    for (const userId of userIds) {
+        const achievementsForUser = query.filter((i) => i.userId == userId);
+
+        completionRate.set(userId, (achievementsForUser.length / allAchievements) * 100);
+    }
+
+    if (userIds.length > 500) {
+        userIds = await workerSort(userIds, completionRate);
+        userIds.reverse();
+    } else {
+        inPlaceSort(userIds).desc((i) => completionRate.get(i));
+    }
+
+    const usersFinal = [];
+
+    let count = 0;
+
+    const getMemberID = (guild: Guild, id: string) => {
+        const target = guild.members.cache.find((member) => {
+            return member.user.id == id;
+        });
+
+        return target;
+    };
+
+    for (const user of userIds) {
+        if (count >= amount) break;
+        if (usersFinal.join().length >= 1500) break;
+
+        if (completionRate.get(user) != 0) {
+            let pos: number | string = count + 1;
+
+            if (pos == 1) {
+                pos = "🥇";
+            } else if (pos == 2) {
+                pos = "🥈";
+            } else if (pos == 3) {
+                pos = "🥉";
+            }
+
+            usersFinal[count] =
+                pos + " **" + getMemberID(guild, user).user.tag + "** " + completionRate.get(user).toFixed(1) + "%";
+            count++;
+        }
+    }
+    return usersFinal;
+}
+
 export async function topAmount(guild: Guild, amount: number): Promise<string[]> {
     let members: Collection<string, GuildMember>;
 
