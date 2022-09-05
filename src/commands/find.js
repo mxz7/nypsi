599d74e517a947e569bd6ee1b13d46752a82ab6d
max <29782397+tekoh@users.noreply.github.com>
2022-09-05 17:48:35 +0100

    fix: im so dumb bro

--- src/commands/find.js
+++ src/commands/find.js
@@ -181,7 +181,7 @@ async function showUser(message, user) {
             `**tag** ${user.tag}
             **created** ${formatDate(user.createdAt)}${
                 (await getLastCommand(user.id))
-                    ? `\n**last command** <t:${Math.floor((await getLastCommand(user.id)).getTime()) / 1000}:R>`
+                    ? `\n**last command** <t:${Math.floor((await getLastCommand(user.id)).getTime() / 1000)}:R>`
                     : ""
             }`,
             true
