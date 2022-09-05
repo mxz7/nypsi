736ee1d2a44925b0a900da064ec07b0896af2c6a
max <29782397+tekoh@users.noreply.github.com>
2022-09-05 14:18:01 +0100

    feat: redesign vote commnd

--- src/utils/economy/utils.ts
+++ src/utils/economy/utils.ts
@@ -101,6 +101,26 @@ export function runEconomySetup() {
     }, 3600000);
 }
 
+export async function getLastVote(member: GuildMember) {
+    let id: string;
+    if (member instanceof GuildMember) {
+        id = member.user.id;
+    } else {
+        id = member;
+    }
+
+    const query = await prisma.economy.findUnique({
+        where: {
+            userId: id,
+        },
+        select: {
+            lastVote: true,
+        },
+    });
+
+    return query.lastVote;
+}
+
 export async function hasVoted(member: GuildMember | string) {
     let id: string;
     if (member instanceof GuildMember) {
