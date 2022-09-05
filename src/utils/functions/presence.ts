a1a19e20312827b1d4cc9a944c4eee61a64379fd
max <29782397+tekoh@users.noreply.github.com>
2022-09-05 16:26:00 +0100

    refactor: remove command thingy for presence

--- src/utils/functions/presence.ts
+++ src/utils/functions/presence.ts
@@ -1,19 +1,14 @@
-import { getRandomCommand } from "../commandhandler";
 import redis from "../database/redis";
 import { daysUntilChristmas } from "./date";
 
 export function randomPresence(): string {
-    const possibilities = ["$help | nypsi.xyz", "$help | tekoh.net", "$help | nypsi.xyz", "x0x", "xmas"];
+    const possibilities = ["$help | nypsi.xyz", "$help | tekoh.net", "$help | nypsi.xyz", "xmas", "xmas"];
 
     const chosen = possibilities[Math.floor(Math.random() * possibilities.length)];
 
     let game = "";
 
-    if (chosen === "x0x") {
-        const randomCommand = getRandomCommand();
-
-        game = `$${randomCommand.name} - ${randomCommand.description}`;
-    } else if (chosen === "xmas") {
+    if (chosen === "xmas") {
         game = `${daysUntilChristmas()} days until christmas`;
     } else {
         game = chosen;
