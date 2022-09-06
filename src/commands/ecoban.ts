e6b69b79ecd156b70ba79b4f0b76ee89e1706991
max <29782397+tekoh@users.noreply.github.com>
2022-09-06 16:39:40 +0100

    feat: update ecoban command

--- src/commands/ecoban.ts
+++ src/commands/ecoban.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction, Message } from "discord.js";
-import { toggleBan } from "../utils/economy/utils";
+import { setEcoBan } from "../utils/economy/utils";
 import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
 
 const cmd = new Command("ecoban", "ban an account from eco", Categories.NONE);
@@ -7,11 +7,13 @@ const cmd = new Command("ecoban", "ban an account from eco", Categories.NONE);
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
     if (message.author.id != "672793821850894347") return;
 
-    if (args.length == 0 || args[0].length != 18) {
+    if (args.length != 2) {
         return message.channel.send({ content: "dumbass" });
     }
 
-    await toggleBan(args[0]);
+    const time = new Date(Date.now() + getDuration(args[1].toLowerCase()) * 1000);
+
+    await setEcoBan(args[0], time);
 
     if (!(message instanceof Message)) return;
 
@@ -21,3 +23,33 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 cmd.setRun(run);
 
 module.exports = cmd;
+
+function getDuration(duration: string): number {
+    duration.toLowerCase();
+
+    if (duration.includes("d")) {
+        if (!parseInt(duration.split("d")[0])) return undefined;
+
+        const num = parseInt(duration.split("d")[0]);
+
+        return num * 86400;
+    } else if (duration.includes("h")) {
+        if (!parseInt(duration.split("h")[0])) return undefined;
+
+        const num = parseInt(duration.split("h")[0]);
+
+        return num * 3600;
+    } else if (duration.includes("m")) {
+        if (!parseInt(duration.split("m")[0])) return undefined;
+
+        const num = parseInt(duration.split("m")[0]);
+
+        return num * 60;
+    } else if (duration.includes("s")) {
+        if (!parseInt(duration.split("s")[0])) return undefined;
+
+        const num = parseInt(duration.split("s")[0]);
+
+        return num;
+    }
+}
