b7702e93cf5d4687f9bb146775e5506b28f650cf
max <29782397+tekoh@users.noreply.github.com>
2022-09-07 16:25:33 +0100

    feat: nypsi bank (#632)
    * feat: functions for getting and setting dynamic tax
    
    * feat: dynamic tax on pay command
    
    * fix: prevent floating point errors
    
    * feat: use dynamic tax on lottery
    
    * refactor: change gif when user tries to fight themself
    
    * feat: add tax to auction sales
    
    * feat: functions to get and update nypsi bank balance
    
    * feat: add to nypsi bank when tax happens
    
    * fix: number
    
    * fix: tax return thing
    
    * feat: gettaxrefreshtime function
    
    * feat: view current tax rate and current nypsi bank balance
    
    * refactor: tax updates every 12 hours
    
    * feat: completely redo bankrob
    
    * refactor: remove storerob

--- src/commands/bankrob.ts
+++ src/commands/bankrob.ts
@@ -1,145 +1,324 @@
-import { CommandInteraction, Message } from "discord.js";
-import * as shuffle from "shuffle-array";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    CommandInteraction,
+    Interaction,
+    InteractionReplyOptions,
+    Message,
+    MessageActionRowComponentBuilder,
+    MessageOptions,
+    SelectMenuBuilder,
+    SelectMenuOptionBuilder,
+} from "discord.js";
+import { inPlaceSort } from "fast-sort";
 import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler.js";
 import { addProgress } from "../utils/economy/achievements.js";
-import {
-    addItemUse,
-    createUser,
-    getBalance,
-    getInventory,
-    setInventory,
-    updateBalance,
-    userExists,
-} from "../utils/economy/utils.js";
+import { createUser, getBalance, getInventory, setInventory, updateBalance, userExists } from "../utils/economy/utils.js";
+import { addToNypsiBank, getNypsiBankBalance, removeFromNypsiBankBalance } from "../utils/functions/tax.js";
 import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
 import { CustomEmbed, ErrorEmbed } from "../utils/models/EmbedBuilders.js";
+import ms = require("ms");
 
 const cmd = new Command("bankrob", "attempt to rob a bank for a high reward", Categories.MONEY);
 
-async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
+const bankWorths = new Map<string, { banks: Map<string, number>; createdAt: number }>();
+
+const defaults = new Map<string, number>([
+    ["bank of america", 10_000_000],
+    ["maze bank", 5_000_000],
+    ["barclays", 2_500_000],
+    ["lloyds", 1_250_000],
+    ["monzo", 1_000_000],
+    ["fleeca", 750_000],
+]);
+
+cmd.slashEnabled = true;
+
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction)) {
     if (!(await userExists(message.member))) await createUser(message.member);
 
-    if ((await getBalance(message.member)) < 1000) {
-        return await message.channel.send({
-            embeds: [new ErrorEmbed("you must have atleast $1k in your wallet to rob a bank")],
-        });
-    }
-
-    const bankWorth = new Map<string, number>();
-
-    if ((await getBalance(message.member)) > 100000000) {
-        bankWorth.set("barclays", Math.round((await getBalance(message.member)) * 0.1));
-        bankWorth.set("santander", Math.round((await getBalance(message.member)) * 0.08));
-        bankWorth.set("bankofamerica", Math.round((await getBalance(message.member)) * 0.125));
-        bankWorth.set("lloyds", Math.round((await getBalance(message.member)) * 0.075));
-        bankWorth.set("hsbc", Math.round((await getBalance(message.member)) * 0.09));
-        bankWorth.set("fleeca", Math.round((await getBalance(message.member)) * 0.05));
-        bankWorth.set("mazebank", Math.round((await getBalance(message.member)) * 0.1));
-    } else if ((await getBalance(message.member)) > 500000) {
-        bankWorth.set("barclays", Math.round((await getBalance(message.member)) * 1));
-        bankWorth.set("santander", Math.round((await getBalance(message.member)) * 0.8));
-        bankWorth.set("bankofamerica", Math.round((await getBalance(message.member)) * 1.25));
-        bankWorth.set("lloyds", Math.round((await getBalance(message.member)) * 0.75));
-        bankWorth.set("hsbc", Math.round((await getBalance(message.member)) * 0.9));
-        bankWorth.set("fleeca", Math.round((await getBalance(message.member)) * 0.5));
-        bankWorth.set("mazebank", Math.round((await getBalance(message.member)) * 1));
-    } else {
-        bankWorth.set("barclays", Math.round((await getBalance(message.member)) * 2));
-        bankWorth.set("santander", Math.round((await getBalance(message.member)) * 1.7));
-        bankWorth.set("bankofamerica", Math.round((await getBalance(message.member)) * 2.5));
-        bankWorth.set("lloyds", Math.round((await getBalance(message.member)) * 1.5));
-        bankWorth.set("hsbc", Math.round((await getBalance(message.member)) * 1.8));
-        bankWorth.set("fleeca", Math.round((await getBalance(message.member)) * 1.1));
-        bankWorth.set("mazebank", Math.round((await getBalance(message.member)) * 2));
-    }
-
-    if (args[0]) {
-        let bankList = "";
-
-        for (const bank1 of bankWorth.keys()) {
-            bankList = bankList + "**" + bank1 + "** $" + bankWorth.get(bank1).toLocaleString() + "\n";
+    const send = async (data: MessageOptions | InteractionReplyOptions) => {
+        if (!(message instanceof Message)) {
+            if (message.deferred) {
+                await message.editReply(data);
+            } else {
+                await message.reply(data as InteractionReplyOptions);
+            }
+            const replyMsg = await message.fetchReply();
+            if (replyMsg instanceof Message) {
+                return replyMsg;
+            }
+        } else {
+            return await message.channel.send(data as MessageOptions);
         }
+    };
 
-        bankList = bankList + "the most you can recieve on one robbery is 75% of the bank's balance";
-
-        const embed = new CustomEmbed(message.member, bankList).setHeader("current bank balances");
-
-        return message.channel.send({ embeds: [embed] });
+    if ((await getBalance(message.member)) < 5_000) {
+        return send({ embeds: [new ErrorEmbed("you must have atleast $5k")], ephemeral: true });
     }
 
-    if (await onCooldown(cmd.name, message.member)) {
-        const embed = await getResponse(cmd.name, message.member);
-
-        return message.channel.send({ embeds: [embed] });
-    }
-
-    await addCooldown(cmd.name, message.member, 600);
-
-    const banks = ["barclays", "santander", "bankofamerica", "lloyds", "hsbc", "fleeca", "mazebank"];
-
-    const bank = shuffle(banks)[Math.floor(Math.random() * banks.length)];
-    const amount = Math.floor(Math.random() * 60) + 15;
-    const caught = Math.floor(Math.random() * 15);
-
-    let robbedAmount = 0;
-
-    let percentLost;
-    let amountLost;
-
-    const embed = new CustomEmbed(message.member, "robbing " + bank + "..").setHeader(
-        "bank robbery",
-        message.author.avatarURL()
-    );
-
-    const embed2 = new CustomEmbed(message.member, "robbing " + bank + "..").setHeader(
-        "bank robbery",
-        message.author.avatarURL()
-    );
-
-    if (caught <= 10) {
-        percentLost = Math.floor(Math.random() * 50) + 10;
-        amountLost = Math.round((percentLost / 100) * (await getBalance(message.member)));
-
+    const getMaxValues = async (bankBalance: number) => {
+        const balance = await getBalance(message.member);
         const inventory = await getInventory(message.member);
 
-        if (inventory["lawyer"] && inventory["lawyer"] > 0) {
-            await addItemUse(message.member, "lawyer");
-            inventory["lawyer"]--;
+        let maxLoss = balance * 0.63;
+        let maxSteal = balance * 0.5;
 
-            if (inventory["lawyer"] == 0) {
-                delete inventory["lawyer"];
+        if (maxLoss > bankBalance * 0.6) {
+            maxLoss = bankBalance * 0.7;
+            maxSteal = bankBalance * 0.5;
+        } else if (maxSteal < 100_000) {
+            maxSteal = 100_000;
+            maxLoss = balance * 0.95;
+        }
+
+        let lawyer = false;
+
+        if (inventory["lawyer"] && inventory["lawyer"] > 0) {
+            lawyer = true;
+            maxLoss = maxLoss * 0.35;
+        }
+
+        return { loss: Math.floor(maxLoss), steal: Math.floor(maxSteal), lawyer };
+    };
+
+    const displayBankInfo = async (bank: string, bankWorth: Map<string, number>) => {
+        let loss: number;
+        let steal: number;
+        let lawyer: boolean;
+
+        let worth: number;
+
+        if (bank == "nypsi") {
+            worth = await getNypsiBankBalance();
+            const res = await getMaxValues(worth);
+
+            loss = res.loss;
+            steal = res.steal;
+            lawyer = res.lawyer;
+        } else {
+            worth = bankWorth.get(bank);
+            const res = await getMaxValues(bankWorth.get(bank));
+
+            loss = res.loss;
+            steal = res.steal;
+            lawyer = res.lawyer;
+        }
+
+        return `**${bank}**\n*$${worth.toLocaleString()}*\n\n**max steal** $${steal.toLocaleString()}\n**max loss** $${loss.toLocaleString()}${
+            lawyer ? " 🧑‍⚖️" : ""
+        }`;
+    };
+
+    const robBank = async (bank: string, bankWorth: Map<string, number>) => {
+        if (await onCooldown(cmd.name, message.member)) {
+            const embed = await getResponse(cmd.name, message.member);
+
+            if (message instanceof Message) {
+                message.channel.send({ embeds: [embed] });
+                return;
+            } else {
+                message.followUp({ embeds: [embed] });
+                return;
+            }
+        }
+
+        if ((await getBalance(message.member)) < 5_000) {
+            if (message instanceof Message) {
+                message.channel.send({ embeds: [new ErrorEmbed("you must have atleast $5k")] });
+                return;
+            } else {
+                message.followUp({ embeds: [new ErrorEmbed("you must have atleast $5k")] });
+                return;
+            }
+        }
+
+        await addCooldown(cmd.name, message.member, 900);
+
+        let loss: number;
+        let steal: number;
+
+        if (bank == "nypsi") {
+            const res = await getMaxValues(await getNypsiBankBalance());
+
+            loss = res.loss;
+            steal = res.steal;
+        } else {
+            const res = await getMaxValues(bankWorth.get(bank));
+
+            loss = res.loss;
+            steal = res.steal;
+        }
+
+        const chance = Math.floor(Math.random() * 100);
+
+        const embed = new CustomEmbed(message.member).setHeader("bank robbery", message.author.avatarURL());
+
+        if (chance > 65) {
+            const minStolen = Math.floor(steal * 0.5);
+
+            const stolen = Math.floor(Math.random() * (steal - minStolen)) + minStolen;
+
+            await Promise.all([
+                await updateBalance(message.member, (await getBalance(message.member)) + stolen),
+                await addProgress(message.author.id, "robber", 1),
+            ]);
+
+            if (bank == "nypsi") {
+                await removeFromNypsiBankBalance(stolen);
+            } else {
+                bankWorth.set(bank, bankWorth.get(bank) - stolen);
             }
 
-            await setInventory(message.member, inventory);
+            const embed = new CustomEmbed(message.member)
+                .setColor("#5efb8f")
+                .setDescription(`**success!**\n\n**you stole** $${stolen.toLocaleString()} from **${bank}**`);
 
-            await updateBalance(message.member, (await getBalance(message.member)) - Math.floor(amountLost * 0.1));
-
-            embed2.addField(
-                "**you were caught**",
-                `thanks to your lawyer, you only lost $**${Math.floor(amountLost * 0.1).toLocaleString()}**`
-            );
-            embed2.setColor("#e4334f");
+            return embed;
         } else {
-            await updateBalance(message.member, (await getBalance(message.member)) - amountLost);
+            const inventory = await getInventory(message.member);
+            let lawyer = false;
 
-            embed2.addField("**you were caught**", "**you lost** $" + amountLost.toLocaleString());
-            embed2.setColor("#e4334f");
+            if (inventory["lawyer"] && inventory["lawyer"] > 0) {
+                lawyer = true;
+
+                inventory["lawyer"]--;
+
+                if (inventory["lawyer"] <= 0) {
+                    delete inventory["lawyer"];
+                }
+
+                await setInventory(message.member, inventory);
+            }
+
+            const minLoss = Math.floor(loss * 0.4);
+            const totalLossed = Math.floor(Math.random() * (loss - minLoss)) + minLoss;
+
+            await updateBalance(message.member, (await getBalance(message.member)) - totalLossed);
+
+            if (bank == "nypsi") {
+                await addToNypsiBank(totalLossed);
+            } else {
+                bankWorth.set(bank, bankWorth.get(bank) + totalLossed * 0.75);
+            }
+
+            embed.setColor("#e4334f");
+
+            if (lawyer) {
+                embed.setDescription(
+                    `**you were caught**\n\nthanks to your lawyer, you only lost $**${totalLossed.toLocaleString()}**`
+                );
+            } else {
+                embed.setDescription(`**you were caught**\n\nyou lost $**${totalLossed.toLocaleString()}**`);
+            }
         }
+
+        return embed;
+    };
+
+    if (!bankWorths.has(message.author.id)) {
+        bankWorths.set(message.author.id, { banks: new Map(defaults), createdAt: Date.now() });
     } else {
-        robbedAmount = Math.round((amount / 100) * bankWorth.get(bank));
-
-        await updateBalance(message.member, (await getBalance(message.member)) + robbedAmount);
-        await addProgress(message.author.id, "robber", 1);
-
-        embed2.addField("**success!!**", "**you stole** $" + robbedAmount.toLocaleString() + " from **" + bank + "**");
-        embed2.setColor("#5efb8f");
+        if (bankWorths.get(message.author.id).createdAt < Date.now() - ms("48 hours")) {
+            bankWorths.set(message.author.id, { banks: new Map(defaults), createdAt: Date.now() });
+        }
     }
 
-    message.channel.send({ embeds: [embed] }).then((m) => {
-        setTimeout(() => {
-            m.edit({ embeds: [embed2] });
-        }, 1500);
-    });
+    const bankWorth = bankWorths.get(message.author.id).banks;
+
+    const embed = new CustomEmbed(message.member).setHeader("bank robbery", message.author.avatarURL());
+
+    const options: SelectMenuOptionBuilder[] = [];
+
+    const bankNames = Array.from(bankWorth.keys());
+
+    inPlaceSort(bankNames).desc((i) => bankWorth.get(i));
+
+    for (const bankName of bankNames) {
+        if (bankWorth.get(bankName) < defaults.get(bankName) * 0.1) continue;
+
+        options.push(
+            new SelectMenuOptionBuilder()
+                .setLabel(bankName)
+                .setValue(bankName)
+                .setDescription(`$${bankWorth.get(bankName).toLocaleString()}`)
+        );
+    }
+
+    if (options.length == 0) {
+        return send({ embeds: [new ErrorEmbed("there are no available banks for you to rob")] });
+    }
+
+    if ((await getNypsiBankBalance()) > 500_000) {
+        options.push(
+            new SelectMenuOptionBuilder()
+                .setLabel("nypsi bank")
+                .setValue("nypsi")
+                .setDefault(true)
+                .setDescription(`$${(await getNypsiBankBalance()).toLocaleString()}`)
+        );
+        embed.setDescription(await displayBankInfo("nypsi", bankWorth));
+    } else {
+        options[0].setDefault(true);
+        embed.setDescription(await displayBankInfo(bankNames[0], bankWorth));
+    }
+
+    const dropdown = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+        new SelectMenuBuilder().setCustomId("bank").setOptions(options)
+    );
+
+    const button = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+        new ButtonBuilder().setCustomId("ro").setLabel("rob").setStyle(ButtonStyle.Danger)
+    );
+
+    const msg = await send({ embeds: [embed], components: [dropdown, button] });
+
+    const filter = (i: Interaction) => i.user.id == message.author.id;
+
+    const pageManager: any = async () => {
+        const res = await msg
+            .awaitMessageComponent({ filter, time: 30_000 })
+            .then(async (i) => {
+                await i.deferUpdate();
+                return i;
+            })
+            .catch(() => {});
+
+        if (!res) {
+            msg.edit({ components: [] });
+            return;
+        }
+
+        if (res.isSelectMenu() && res.customId == "bank") {
+            embed.setDescription(await displayBankInfo(res.values[0], bankWorth));
+
+            for (const option of options) {
+                option.setDefault(false);
+
+                if (option.data.value == res.values[0]) option.setDefault(true);
+            }
+
+            const newDropdown = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+                new SelectMenuBuilder().setCustomId("bank").setOptions(options)
+            );
+
+            await res.message.edit({ embeds: [embed], components: [newDropdown, button] });
+            return pageManager();
+        } else if (res.customId == "ro") {
+            const selected = options.filter((o) => o.data.default)[0].data.value;
+
+            const newEmbed = await robBank(selected, bankWorth);
+
+            if (!newEmbed) return await res.message.edit({ components: [] });
+
+            await res.followUp({ embeds: [newEmbed] });
+            await res.message.edit({ components: [] });
+            return;
+        }
+    };
+
+    return pageManager();
 }
 
 cmd.setRun(run);
---
+++ src/utils/functions/tax.ts
@@ -0,0 +1,68 @@
+import ms = require("ms");
+import prisma from "../database/database";
+import redis from "../database/redis";
+
+export async function getTax() {
+    let tax: number;
+
+    if (!(await redis.exists("nypsi:tax"))) {
+        tax = await updateTax();
+    } else {
+        tax = parseFloat(await redis.get("nypsi:tax"));
+    }
+
+    return parseFloat(tax.toFixed(3));
+}
+
+export async function getTaxRefreshTime() {
+    return Math.floor(Date.now() / 1000 + (await redis.ttl("nypsi:tax")));
+}
+
+async function updateTax() {
+    const tax = parseFloat((Math.random() * 5 + 5).toFixed(3)) / 100;
+
+    await redis.set("nypsi:tax", tax.toFixed(3));
+    await redis.expire("nypsi:tax", Math.floor(ms("12 hours") / 1000));
+
+    return tax;
+}
+
+export async function addToNypsiBank(amount: number) {
+    await prisma.economy.upsert({
+        where: {
+            userId: "678711738845102087",
+        },
+        update: {
+            bank: { increment: amount },
+        },
+        create: {
+            bank: amount,
+            lastVote: new Date(0),
+            userId: "678711738845102087",
+        },
+    });
+}
+
+export async function getNypsiBankBalance() {
+    const query = await prisma.economy.findUnique({
+        where: {
+            userId: "678711738845102087",
+        },
+        select: {
+            bank: true,
+        },
+    });
+
+    return Number(query?.bank) || 0;
+}
+
+export async function removeFromNypsiBankBalance(amount: number) {
+    await prisma.economy.update({
+        where: {
+            userId: "678711738845102087",
+        },
+        data: {
+            bank: { decrement: amount },
+        },
+    });
+}
