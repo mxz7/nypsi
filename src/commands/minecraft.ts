a62fcf5f1bd9a6feb6069a15ae99a6d848c84fec
max <29782397+tekoh@users.noreply.github.com>
2022-09-13 16:57:54 +0100

    feat: add notice to $mc command

--- src/commands/minecraft.ts
+++ src/commands/minecraft.ts
@@ -1,30 +1,10 @@
-import {
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle,
-    CommandInteraction,
-    Interaction,
-    InteractionReplyOptions,
-    Message,
-    MessageActionRowComponentBuilder,
-    MessageEditOptions,
-    MessageOptions,
-} from "discord.js";
-import { getNameHistory } from "mc-names";
-import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler";
-import { cleanString } from "../utils/functions/string";
-import { getPrefix } from "../utils/guilds/utils";
+import { CommandInteraction, InteractionReplyOptions, Message, MessageOptions } from "discord.js";
 import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
-import { CustomEmbed, ErrorEmbed } from "../utils/models/EmbedBuilders.js";
+import { CustomEmbed } from "../utils/models/EmbedBuilders.js";
 
 const cmd = new Command("minecraft", "view information about a minecraft account", Categories.MINECRAFT).setAliases(["mc"]);
 
-cmd.slashEnabled = true;
-cmd.slashData.addStringOption((option) =>
-    option.setName("username").setDescription("username to get the name history for").setRequired(true)
-);
-
-async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction)) {
     const send = async (data: MessageOptions | InteractionReplyOptions) => {
         if (!(message instanceof Message)) {
             if (message.deferred) {
@@ -41,162 +21,14 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
         }
     };
 
-    if (await onCooldown(cmd.name, message.member)) {
-        const embed = await getResponse(cmd.name, message.member);
-
-        return send({ embeds: [embed], ephemeral: true });
-    }
-
-    const prefix = await getPrefix(message.guild);
-
-    if (args.length == 0) {
-        return send({ embeds: [new ErrorEmbed(`${prefix}minecraft <name>`)] });
-    }
-
-    await addCooldown(cmd.name, message.member, 10);
-
-    let username = cleanString(args[0]);
-
-    const nameHistory = await getNameHistory(username);
-
-    if (!nameHistory) {
-        return await send({ embeds: [new ErrorEmbed("invalid account")] });
-    }
-
-    const skin = `https://mc-heads.net/avatar/${nameHistory.uuid}/256`;
-
-    username = nameHistory.username;
-
-    const names = nameHistory.toPages(7, "`$username` | `$date`");
-
-    const embed = new CustomEmbed(message.member, names.get(1).join("\n"))
-        .setTitle(username)
-        .setURL("https://namemc.com/profile/" + username)
-        .setThumbnail(skin);
-
-    if (names.size >= 2) {
-        embed.setFooter({ text: `page 1/${names.size}` });
-    }
-
-    let msg: Message;
-
-    let row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-        new ButtonBuilder().setCustomId("⬅").setLabel("back").setStyle(ButtonStyle.Primary).setDisabled(true),
-        new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary)
-    );
-
-    if (names.size >= 2) {
-        msg = await send({ embeds: [embed], components: [row] });
-    } else {
-        return await send({ embeds: [embed] });
-    }
-
-    if (names.size >= 2) {
-        let currentPage = 1;
-        const lastPage = names.size;
-
-        const filter = (i: Interaction) => i.user.id == message.author.id;
-
-        const edit = async (data: MessageEditOptions, msg: Message) => {
-            if (!(message instanceof Message)) {
-                await message.editReply(data);
-                return await message.fetchReply();
-            } else {
-                return await msg.edit(data);
-            }
-        };
-
-        const pageManager = async (): Promise<void> => {
-            const reaction = await msg
-                .awaitMessageComponent({ filter, time: 30000 })
-                .then(async (collected) => {
-                    await collected.deferUpdate();
-                    return collected.customId;
-                })
-                .catch(async () => {
-                    await edit({ components: [] }, msg);
-                });
-
-            if (!reaction) return;
-
-            if (reaction == "⬅") {
-                if (currentPage <= 1) {
-                    return pageManager();
-                } else {
-                    currentPage--;
-                    embed.setDescription(names.get(currentPage).join("\n"));
-                    embed.setFooter({ text: "page " + currentPage + "/" + lastPage });
-                    if (currentPage == 1) {
-                        row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-                            new ButtonBuilder()
-                                .setCustomId("⬅")
-                                .setLabel("back")
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(true),
-                            new ButtonBuilder()
-                                .setCustomId("➡")
-                                .setLabel("next")
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(false)
-                        );
-                    } else {
-                        row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-                            new ButtonBuilder()
-                                .setCustomId("⬅")
-                                .setLabel("back")
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(false),
-                            new ButtonBuilder()
-                                .setCustomId("➡")
-                                .setLabel("next")
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(false)
-                        );
-                    }
-                    await edit({ embeds: [embed], components: [row] }, msg);
-                    return pageManager();
-                }
-            } else if (reaction == "➡") {
-                if (currentPage >= lastPage) {
-                    return pageManager();
-                } else {
-                    currentPage++;
-                    embed.setDescription(names.get(currentPage).join("\n"));
-                    embed.setFooter({ text: "page " + currentPage + "/" + lastPage });
-                    if (currentPage == lastPage) {
-                        row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-                            new ButtonBuilder()
-                                .setCustomId("⬅")
-                                .setLabel("back")
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(false),
-                            new ButtonBuilder()
-                                .setCustomId("➡")
-                                .setLabel("next")
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(true)
-                        );
-                    } else {
-                        row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-                            new ButtonBuilder()
-                                .setCustomId("⬅")
-                                .setLabel("back")
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(false),
-                            new ButtonBuilder()
-                                .setCustomId("➡")
-                                .setLabel("next")
-                                .setStyle(ButtonStyle.Primary)
-                                .setDisabled(false)
-                        );
-                    }
-                    await edit({ embeds: [embed], components: [row] }, msg);
-                    return pageManager();
-                }
-            }
-        };
-        return pageManager();
-    }
+    return send({
+        embeds: [
+            new CustomEmbed(
+                message.member,
+                "sadly, minecraft has removed access to the name history api, meaning it is now impossible for nypsi to retreive name history data.\n\nhttps://help.minecraft.net/hc/en-us/articles/8969841895693-Username-History-API-Removal-FAQ-"
+            ),
+        ],
+    });
 }
 
 cmd.setRun(run);
