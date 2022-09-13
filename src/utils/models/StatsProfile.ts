93ea814d46efb262369ae5d4498acb8d2b67e8c2
max <29782397+tekoh@users.noreply.github.com>
2022-09-13 16:14:37 +0100

    feat: bankrob stats (#644)
    * feat: bankrob stats
    
    * feat: update schema to use bigints
    
    * 4.8.4

--- src/utils/models/StatsProfile.ts
+++ src/utils/models/StatsProfile.ts
@@ -1,4 +1,4 @@
-type StatsData = { economyUserId: string; type: string; win: number; lose: number; gamble: boolean };
+type StatsData = { economyUserId: string; type: string; win: number | bigint; lose: number | bigint; gamble: boolean };
 
 export class StatsProfile {
     public gamble: { [key: string]: { wins: number; lose: number } };
@@ -26,14 +26,14 @@ export class StatsProfile {
                     wins: 0,
                     lose: 0,
                 };
-                this.gamble[d.type].wins = d.win;
-                this.gamble[d.type].lose = d.lose;
+                this.gamble[d.type].wins = Number(d.win);
+                this.gamble[d.type].lose = Number(d.lose);
             } else {
                 if (d.type == "rob") {
-                    this.rob.wins = d.win;
-                    this.rob.lose = d.lose;
+                    this.rob.wins = Number(d.win);
+                    this.rob.lose = Number(d.lose);
                 } else {
-                    this.items[d.type] = d.win;
+                    this.items[d.type] = Number(d.win);
                 }
             }
         }
