b036b51e0c953bcad02f90e6be39065386b2f80b
max <29782397+tekoh@users.noreply.github.com>
2022-09-15 17:49:29 +0100

    refactor: update word list links

--- src/commands/wordle.ts
+++ src/commands/wordle.ts
@@ -67,7 +67,7 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
         embed.setHeader("wordle help");
         embed.setDescription(
-            `you have 6 attempts to guess the word\n\ngreen letters indicate that the letter is in the correct spot\nyellow letters indicate that the letter is in the word, but in the wrong spot\ngrey letters arent in the word at all\n\n**${prefix}wordle play**`
+            `you have 6 attempts to guess the word\n\ngreen letters indicate that the letter is in the correct spot\nyellow letters indicate that the letter is in the word, but in the wrong spot\ngrey letters arent in the word at all\n\n[wordlist](https://github.com/tekoh/nypsi/blob/main/data/wordle.txt)\n**${prefix}wordle play**`
         );
         embed.setFooter({ text: "type 'stop' to cancel the game when you're playing" });
 
