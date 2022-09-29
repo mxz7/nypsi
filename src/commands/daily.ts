f65f0db4362708378cc7d2f2faec052fa1301e19
max <29782397+tekoh@users.noreply.github.com>
2022-09-29 01:10:43 +0100

    feat: improve rewards from daily

--- src/commands/daily.ts
+++ src/commands/daily.ts
@@ -4,6 +4,7 @@ import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler";
 import { MStoTime } from "../utils/functions/date";
 import { setProgress } from "../utils/functions/economy/achievements";
 import { getBalance, updateBalance } from "../utils/functions/economy/balance";
+import { getInventory, setInventory } from "../utils/functions/economy/inventory";
 import { createUser, getDailyStreak, getLastDaily, updateLastDaily, userExists } from "../utils/functions/economy/utils";
 import { getXp, updateXp } from "../utils/functions/economy/xp";
 import { getTier, isPremium } from "../utils/functions/premium/premium";
@@ -54,23 +55,23 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
   const base = 20000;
 
-  let streakBonus = 850;
+  let streakBonus = 5000;
 
   if (await isPremium(message.member)) {
     const tier = await getTier(message.member);
 
     switch (tier) {
       case 1:
-        streakBonus = 900;
+        streakBonus = 5500;
         break;
       case 2:
-        streakBonus = 950;
+        streakBonus = 6000;
         break;
       case 3:
-        streakBonus = 1000;
+        streakBonus = 6500;
         break;
       case 4:
-        streakBonus = 1100;
+        streakBonus = 7500;
         break;
     }
   }
@@ -78,9 +79,26 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
   const total = base + streakBonus * streak;
 
   let xp = 1;
+  let crate = 0;
 
-  if (streak > 20) {
-    xp = Math.floor((streak - 20) / 15);
+  if (streak > 5) {
+    xp = Math.floor((streak - 5) / 10);
+  }
+
+  if (streak > 0 && streak % 7 == 0) {
+    crate++;
+
+    crate += Math.floor(streak / 30);
+
+    const inventory = await getInventory(message.member);
+
+    if (inventory["basic_crate"]) {
+      inventory["basic_crate"] += crate;
+    } else {
+      inventory["basic_crate"] = crate;
+    }
+
+    await setInventory(message.member, inventory);
   }
 
   await updateBalance(message.member, (await getBalance(message.member)) + total);
@@ -88,7 +106,11 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
   const embed = new CustomEmbed(message.member);
   embed.setHeader("daily", message.author.avatarURL());
-  embed.setDescription(`+$**${total.toLocaleString()}**\ndaily streak: \`${streak + 1}\``);
+  embed.setDescription(
+    `+$**${total.toLocaleString()}**${crate ? `\n+ **${crate}** basic crate${crate > 1 ? "s" : ""}` : ""}\ndaily streak: \`${
+      streak + 1
+    }\``
+  );
 
   if (xp > 0) {
     await updateXp(message.member, (await getXp(message.member)) + xp);
