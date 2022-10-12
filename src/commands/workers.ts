eca54a35f2061a8d66d22b84c54fbd62bff6dad1
max <29782397+tekoh@users.noreply.github.com>
2022-10-12 19:58:53 +0100

    feat: can view and buy workers with new system

--- src/commands/workers.ts
+++ src/commands/workers.ts
@@ -1,20 +1,26 @@
+import dayjs = require("dayjs");
 import {
+  ActionRowBuilder,
   APIApplicationCommandOptionChoice,
   BaseMessageOptions,
+  ButtonBuilder,
+  ButtonStyle,
   CommandInteraction,
+  Interaction,
   InteractionReplyOptions,
   Message,
+  MessageActionRowComponentBuilder,
+  SelectMenuBuilder,
+  SelectMenuOptionBuilder,
 } from "discord.js";
 import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler";
-import { getBalance, updateBalance } from "../utils/functions/economy/balance";
+import { getBalance } from "../utils/functions/economy/balance";
 import { getPrestige } from "../utils/functions/economy/prestige";
 import { createUser, userExists } from "../utils/functions/economy/utils";
-import { addWorker, emptyWorkersStored, getWorkers, upgradeWorker } from "../utils/functions/economy/workers";
-import { getPrefix } from "../utils/functions/guilds/utils";
-import { getTier, isPremium } from "../utils/functions/premium/premium";
+import { addWorker, calcWorkerValues, getBaseWorkers, getWorkers } from "../utils/functions/economy/workers";
 import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
 import { CustomEmbed, ErrorEmbed } from "../utils/models/EmbedBuilders";
-import { getAllWorkers, Worker } from "../utils/models/Workers";
+import { Worker } from "../utils/models/Workers";
 
 const cmd = new Command("workers", "view the available workers and manage your own", Categories.MONEY).setAliases([
   "worker",
@@ -65,7 +71,7 @@ cmd.slashData
   .addSubcommand((reclaim) => reclaim.setName("reclaim").setDescription("obtain workers from your premium subscription"));
 
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
-  const workers = getAllWorkers();
+  const baseWorkers = getBaseWorkers();
 
   if (!(await userExists(message.member))) await createUser(message.member);
 
@@ -93,365 +99,148 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
   await addCooldown(cmd.name, message.member, 5);
 
-  const prefix = await getPrefix(message.guild);
+  let userWorkers = await getWorkers(message.member);
+  const prestige = await getPrestige(message.member);
 
-  const listAllWorkers = () => {
-    const embed = new CustomEmbed(message.member, "workers create items over time, which you can sell for money")
-      .setHeader("workers", message.author.avatarURL())
-      .setFooter({ text: `${prefix}workers help` });
-
-    for (const w of Array.from(workers.keys())) {
-      const Worker = workers.get(w);
-      const worker = new Worker();
-      embed.addField(
-        `${worker.name}`,
-        `**cost** $${worker.cost.toLocaleString()}\n**prestige** ${
-          worker.prestige
-        }\n**item worth** $${worker.perItem.toLocaleString()} / ${worker.itemName}\n**rate** ${worker
-          .getHourlyRate()
-          .toLocaleString()} ${worker.itemName} / hour`,
-        true
-      );
+  const isOwned = (workerId: string) => {
+    for (const worker of userWorkers) {
+      if (worker.workerId == workerId) return true;
     }
 
-    return send({ embeds: [embed] });
+    return false;
   };
 
-  const listPersonalWorkers = async () => {
-    const personalWorkers = await getWorkers(message.member);
-
-    const embed = new CustomEmbed(
-      message.member,
-      `you have ${Object.keys(personalWorkers).length} worker${Object.keys(personalWorkers).length == 1 ? "" : "s"}`
-    )
-      .setHeader("your workers", message.author.avatarURL())
-      .setFooter({ text: `${prefix}workers help` });
-
-    for (const w of Object.keys(personalWorkers)) {
-      const worker = Worker.fromStorage(personalWorkers[w]);
-      embed.addField(
-        `${worker.name}`,
-        `**inventory** ${worker.stored.toLocaleString()} ${worker.itemName} / ${worker.maxStorage.toLocaleString()} ($${(
-          worker.stored * worker.perItem
-        ).toLocaleString()})\n` +
-          `**level** ${worker.level}${
-            worker.level >= 5 ? "" : `\n**upgrade cost** $${worker.getUpgradeCost().toLocaleString()}`
-          }\n**item worth** $${worker.perItem.toLocaleString()} / ${worker.itemName}\n**rate** ${worker
-            .getHourlyRate()
-            .toLocaleString()} ${worker.itemName} / hour`,
-        true
-      );
-    }
-
-    return send({ embeds: [embed] });
-  };
-
-  if (args.length == 0) {
-    if (Object.keys(await getWorkers(message.member)).length == 0) {
-      return listAllWorkers();
-    } else {
-      return listPersonalWorkers();
-    }
-  } else {
-    if (args[0].toLowerCase() == "buy") {
-      if (args.length == 1) {
-        return send({
-          embeds: [new ErrorEmbed(`${prefix}workers buy <worker name>`)],
-        });
-      }
-
-      let worker;
-
-      if (args.length == 2) {
-        if (args[1].length == 1) {
-          if (workers.get(parseInt(args[1]))) {
-            worker = workers.get(parseInt(args[1]));
-          }
-        }
-      }
-
-      if (worker) {
-        worker = new worker();
-      }
-
-      if (!worker) {
-        args.shift();
-        const name = args.join(" ").toLowerCase();
-        for (const w of Array.from(workers.keys())) {
-          const Worker1 = workers.get(w);
-          const worker1 = new Worker1();
-          if (worker1.name == name) {
-            worker = worker1;
-            break;
-          }
-        }
-      }
-
-      if (!worker) {
-        return send({
-          embeds: [new ErrorEmbed("invalid worker, please use the worker name")],
-        });
-      }
-
-      if (!(worker instanceof Worker)) {
-        return send({
-          embeds: [new ErrorEmbed("invalid worker, please use the worker name")],
-        });
-      }
-
-      if (worker.prestige > (await getPrestige(message.member))) {
-        return send({
-          embeds: [
-            new ErrorEmbed(
-              `you need to be prestige **${worker.prestige}** to buy this worker, you are prestige **${await getPrestige(
-                message.member
-              )}**`
-            ),
-          ],
-        });
-      }
-
-      if ((await getBalance(message.member)) < worker.cost) {
-        return send({ embeds: [new ErrorEmbed("you cannot afford this worker")] });
-      }
-
-      const personalWorkers = await getWorkers(message.member);
-
-      for (const w of Object.keys(personalWorkers)) {
-        const worker1 = personalWorkers[w];
-
-        if (worker1.id == worker.id) {
-          return send({ embeds: [new ErrorEmbed("you already have this worker")] });
-        }
-      }
-
-      await updateBalance(message.member, (await getBalance(message.member)) - worker.cost);
-
-      await addWorker(message.member, worker.id);
-
-      return send({
-        embeds: [new CustomEmbed(message.member, `✅ you have bought a **${worker.name}**`)],
-      });
-    } else if (args[0].toLowerCase() == "claim" || args[0].toLowerCase() == "sell") {
-      const personalWorkers = await getWorkers(message.member);
-
-      let amountEarned = 0;
-      let earnedBreakdown = "";
-
-      for (const w of Object.keys(personalWorkers)) {
-        const worker = Worker.fromStorage(personalWorkers[w]);
-
-        amountEarned += Math.floor(worker.perItem * worker.stored);
-        earnedBreakdown += `\n${worker.name} +$${Math.floor(
-          worker.perItem * worker.stored
-        ).toLocaleString()} (${worker.stored.toLocaleString()} ${worker.itemName})`;
-      }
-
-      if (amountEarned == 0) {
-        return send({
-          embeds: [new ErrorEmbed("you have no money to claim from your workers")],
-        });
-      }
-
-      await emptyWorkersStored(message.member);
-      await updateBalance(message.member, (await getBalance(message.member)) + amountEarned);
-
-      const embed = new CustomEmbed(message.member, `+$**${amountEarned.toLocaleString()}**\n${earnedBreakdown}`).setHeader(
-        "workers",
-        message.author.avatarURL()
-      );
-
-      return send({ embeds: [embed] });
-    } else if (args[0].toLowerCase() == "upgrade") {
-      if (args.length == 1) {
-        return send({
-          embeds: [new ErrorEmbed(`${prefix}workers upgrade <name>`)],
-        });
-      }
-
-      let worker;
-
-      if (args.length == 2) {
-        if (args[1].length == 1) {
-          if (workers.get(parseInt(args[1]))) {
-            worker = workers.get(parseInt(args[1]));
-          }
-        }
-      }
-
-      if (worker) {
-        worker = new worker();
-      }
-
-      if (!worker) {
-        args.shift();
-        const name = args.join(" ").toLowerCase();
-        for (const w of Array.from(workers.keys())) {
-          const Worker1 = workers.get(w);
-          const worker1 = new Worker1();
-          if (worker1.name == name) {
-            worker = worker1;
-            break;
-          }
-        }
-      }
-
-      if (!worker) {
-        return send({
-          embeds: [new ErrorEmbed("invalid worker, please use the worker name")],
-        });
-      }
-
-      const memberWorkers = await getWorkers(message.member);
-
-      if (!(worker instanceof Worker)) {
-        return send({
-          embeds: [new ErrorEmbed("invalid worker, please use the worker name")],
-        });
-      }
-
-      worker = memberWorkers[worker.id];
-
-      if (!worker) {
-        return send({ embeds: [new ErrorEmbed("you don't have this worker")] });
-      }
-
-      worker = Worker.fromStorage(worker);
-
-      if (worker.level >= 5) {
-        return send({ embeds: [new ErrorEmbed("this worker is already max level")] });
-      }
-
-      if ((await getBalance(message.member)) < worker.getUpgradeCost()) {
-        return send({
-          embeds: [
-            new ErrorEmbed(
-              `the upgrade cost for \`${worker.name}\` is $${worker
-                .getUpgradeCost()
-                .toLocaleString()}, you can't afford this`
-            ),
-          ],
-        });
-      }
-
-      await updateBalance(message.member, (await getBalance(message.member)) - worker.getUpgradeCost());
-
-      await upgradeWorker(message.member, worker.id.toString());
-
+  const showWorkers = async (defaultWorker = "potato_farmer", msg?: Message) => {
+    const displayWorker = async (worker: Worker) => {
       const embed = new CustomEmbed(message.member);
 
-      embed.setHeader("workers", message.author.avatarURL());
+      embed.setTitle(`${worker.name}${prestige < worker.prestige_requirement ? " [locked]" : ""}`);
 
-      worker = (await getWorkers(message.member))[worker.id];
+      const row = new ActionRowBuilder<MessageActionRowComponentBuilder>();
 
-      worker = Worker.fromStorage(worker);
+      if (isOwned(worker.id)) {
+        const userWorker = userWorkers.find((w) => w.workerId == worker.id);
 
-      embed.setDescription(
-        `your ${worker.name} has been upgraded to level ${worker.level}\n\n` +
-          `**item worth** $${worker.perItem.toLocaleString()} / ${worker.itemName}\n` +
-          `**rate** ${worker.getHourlyRate()} ${worker.itemName} / hour\n` +
-          `**inventory** ${worker.stored.toLocaleString()} ${worker.itemName} / ${worker.maxStorage.toLocaleString()}`
-      );
+        const { maxStorage, perInterval, perItem } = await calcWorkerValues(userWorker);
 
-      return send({ embeds: [embed] });
-    } else if (args[0].toLowerCase() == "list") {
-      return listAllWorkers();
-    } else if (
-      args[0].toLowerCase() == "reclaim" ||
-      args[0].toLowerCase() == "patreon" ||
-      args[0].toLowerCase() == "premium"
-    ) {
-      if (!(await isPremium(message.author.id))) {
-        return send({
-          embeds: [
-            new ErrorEmbed("you must have a premium membership for this").setFooter({
-              text: `${prefix}patreon`,
-            }),
-          ],
-        });
-      }
+        let desc =
+          `**inventory** ${userWorker.stored.toLocaleString()} ${worker.item_emoji} / ${maxStorage.toLocaleString()} ${
+            worker.item_emoji
+          }\n` +
+          `**item worth** $${perItem.toLocaleString()} / ${worker.item_emoji}\n` +
+          `**rate** ${perInterval.toLocaleString()} ${worker.item_emoji} / hour`;
 
-      let msg = "";
+        if (userWorker.stored < maxStorage) {
+          let hours = Math.ceil((maxStorage - userWorker.stored) / perInterval);
 
-      const personalWorkers = await getWorkers(message.member);
+          const diff = dayjs().add(hours, "hours").unix() - dayjs().unix();
+          hours = diff / 3600;
 
-      if ((await getTier(message.author.id)) >= 2) {
-        let has = false;
-        for (const w of Object.keys(personalWorkers)) {
-          const worker1 = personalWorkers[w];
-
-          if (worker1.id == 1) {
-            has = true;
-            break;
-          }
+          desc += `\n\n\`${hours.toLocaleString()} hour${hours > 1 ? "s" : ""}\` until full`;
         }
-        if (!has) {
-          await addWorker(message.member, 1);
-          let name: any = workers.get(1);
-          name = new name().name;
-          msg += "+ " + name + "\n";
-        }
-      }
 
-      if ((await getTier(message.author.id)) >= 3) {
-        let has = false;
-        for (const w of Object.keys(personalWorkers)) {
-          const worker1 = personalWorkers[w];
+        embed.setDescription(desc);
 
-          if (worker1.id == 3) {
-            has = true;
-            break;
-          }
-        }
-        if (!has) {
-          await addWorker(message.member, 3);
-          let name: any = workers.get(3);
-          name = new name().name;
-          msg += "+ " + name + "\n";
-        }
-      }
-
-      if ((await getTier(message.author.id)) >= 4) {
-        let has = false;
-        for (const w of Object.keys(personalWorkers)) {
-          const worker1 = personalWorkers[w];
-
-          if (worker1.id == 6) {
-            has = true;
-            break;
-          }
-        }
-        if (!has) {
-          await addWorker(message.member, 6);
-          let name: any = workers.get(6);
-          name = new name().name;
-          msg += "+ " + name + "\n";
-        }
-      }
-
-      if (msg == "") {
-        msg = "you weren't able to claim any free workers";
-      }
-
-      return send({ embeds: [new CustomEmbed(message.member, msg)] });
-    } else if (args[0].toLowerCase() == "view") {
-      if (Object.keys(await getWorkers(message.member)).length == 0) {
-        return listAllWorkers();
+        row.addComponents(new ButtonBuilder().setCustomId("upg").setLabel("upgrades").setStyle(ButtonStyle.Primary));
       } else {
-        return listPersonalWorkers();
+        embed.setDescription(
+          `**cost** $${worker.cost.toLocaleString()}\n` +
+            `**required prestige** ${worker.prestige_requirement}\n\n` +
+            `**item worth** $${worker.base.per_item.toLocaleString()} / ${worker.item_emoji}\n` +
+            `**rate** ${worker.base.per_interval.toLocaleString()} ${worker.item_emoji} / hour`
+        );
+
+        embed.setFooter({ text: `you are prestige ${prestige}` });
+
+        if (prestige >= worker.prestige_requirement) {
+          row.addComponents(new ButtonBuilder().setCustomId("bu").setLabel("buy").setStyle(ButtonStyle.Primary));
+        } else {
+          row.addComponents(
+            new ButtonBuilder().setCustomId("bu").setLabel("buy").setStyle(ButtonStyle.Primary).setDisabled(true)
+          );
+        }
       }
-    } else {
-      const embed = new CustomEmbed(message.member).setHeader("workers", message.author.avatarURL());
 
-      embed.setDescription(
-        `${prefix}**workers list** *list all available workers*\n` +
-          `${prefix}**workers buy** *buy a worker*\n` +
-          `${prefix}**workers claim** *claim money from your workers*\n` +
-          `${prefix}**workers upgrade** *upgrade a worker*`
+      return { embed: embed, buttonRow: row };
+    };
+
+    const options: SelectMenuOptionBuilder[] = [];
+
+    for (const worker of Object.keys(baseWorkers)) {
+      options.push(
+        new SelectMenuOptionBuilder()
+          .setLabel(`${baseWorkers[worker].name}${baseWorkers[worker].prestige_requirement > prestige ? " [locked]" : ""}`)
+          .setValue(baseWorkers[worker].id)
+          .setDefault(baseWorkers[worker].id == defaultWorker ? true : false)
       );
-
-      return send({ embeds: [embed] });
     }
+
+    let workersList = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+      new SelectMenuBuilder().setCustomId("worker").setOptions(options)
+    );
+
+    const { buttonRow, embed } = await displayWorker(baseWorkers[defaultWorker]);
+
+    if (msg) {
+      msg = await msg.edit({ embeds: [embed], components: [workersList, buttonRow] });
+    } else {
+      msg = await send({ embeds: [embed], components: [workersList, buttonRow] });
+    }
+
+    const filter = (i: Interaction) => i.user.id == message.author.id;
+
+    const pageManager: any = async () => {
+      const res = await msg
+        .awaitMessageComponent({ filter, time: 30_000 })
+        .then(async (i) => {
+          await i.deferUpdate();
+          return i;
+        })
+        .catch(() => {});
+
+      if (!res) {
+        msg.edit({ components: [] });
+        return;
+      }
+
+      if (res.isSelectMenu()) {
+        const { buttonRow, embed } = await displayWorker(baseWorkers[res.values[0]]);
+
+        for (const option of options) {
+          option.setDefault(false);
+
+          if (option.data.value == res.values[0]) option.setDefault(true);
+        }
+
+        workersList = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+          new SelectMenuBuilder().setCustomId("worker").setOptions(options)
+        );
+
+        await res.message.edit({ embeds: [embed], components: [workersList, buttonRow] });
+        return pageManager();
+      } else if (res.customId == "bu") {
+        const balance = await getBalance(message.member);
+
+        const selected = options.filter((o) => o.data.default)[0].data.value;
+
+        if (balance < baseWorkers[selected].cost) {
+          await res.followUp({ embeds: [new ErrorEmbed("you cannot afford this worker")], ephemeral: true });
+          return pageManager();
+        } else {
+          await addWorker(message.member, selected);
+
+          userWorkers = await getWorkers(message.member);
+
+          return showWorkers(selected, msg);
+        }
+      }
+    };
+
+    return pageManager();
+  };
+
+  if (args.length == 0 || args[0].toLowerCase() == "view") {
+    return showWorkers();
   }
 }
 
