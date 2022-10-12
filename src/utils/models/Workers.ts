8940285e61c4917101e7e00dcf6762e76c2b7b3b
max <29782397+tekoh@users.noreply.github.com>
2022-10-12 16:40:55 +0100

    feat: interface for worker upgrades

--- src/utils/models/Workers.ts
+++ src/utils/models/Workers.ts
@@ -1,6 +1,6 @@
 export interface Worker {
   name: string;
-  id: string
+  id: string;
   item_emoji: string;
   prestige_requirement: number;
   cost: number;
@@ -9,4 +9,18 @@ export interface Worker {
     max_storage: number;
     per_interval: number;
   };
+}
+
+export interface WorkerUpgrades {
+  name: string;
+  id: string;
+  upgrades: PossibleUpgrade;
+  effect: number; // decimal
+  stack_limit: number;
+}
+
+enum PossibleUpgrade {
+  PER_ITEM,
+  PER_INTERVAL,
+  MAX_STORAGE,
 }
