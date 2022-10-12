f3d5ecbc71b745edf76cf028ed214974f2be744c
max <29782397+tekoh@users.noreply.github.com>
2022-10-12 18:52:18 +0100

    feat: created function to calculate worker vales

--- src/utils/functions/economy/workers.ts
+++ src/utils/functions/economy/workers.ts
@@ -1,6 +1,9 @@
+import { EconomyWorker, EconomyWorkerUpgrades } from "@prisma/client";
 import { GuildMember } from "discord.js";
 import prisma from "../../database/database";
 import { Worker, WorkerUpgrades } from "../../models/Workers";
+import { getBoosters } from "./boosters";
+import { getItems } from "./utils";
 
 declare function require(name: string): any;
 
@@ -67,4 +70,52 @@ export async function emptyWorkersStored(member: GuildMember | string) {
       stored: 0,
     },
   });
+}
+
+export async function calcWorkerValues(
+  worker: EconomyWorker & {
+    upgrades: EconomyWorkerUpgrades[];
+  }
+) {
+  const boosters = await getBoosters(worker.userId);
+  const items = getItems();
+
+  let perIntervalBonus = 0;
+  let perItemBonus = 0;
+  let maxStoredBonus = 0;
+
+  for (const upgrade of worker.upgrades) {
+    switch (baseUpgrades[upgrade.upgradeId].upgrades) {
+      case 0:
+        perItemBonus += baseUpgrades[upgrade.upgradeId].effect * upgrade.amount * baseWorkers[worker.workerId].base.per_item;
+        break;
+      case 1:
+        perIntervalBonus +=
+          baseUpgrades[upgrade.upgradeId].effect * upgrade.amount * baseWorkers[worker.workerId].base.per_interval;
+        break;
+      case 2:
+        maxStoredBonus +=
+          baseUpgrades[upgrade.upgradeId].effect * upgrade.amount * baseWorkers[worker.workerId].base.max_storage;
+        break;
+    }
+  }
+
+  for (const boosterId of boosters.keys()) {
+    if (items[boosterId].role != "booster") return;
+
+    switch (items[boosterId].boosterEffect.boosts[0]) {
+      case "per_interval":
+        perIntervalBonus +=
+          items[boosterId].boosterEffect.effect *
+          boosters.get(boosterId).length *
+          baseWorkers[worker.workerId].base.per_interval;
+        break;
+    }
+  }
+
+  return {
+    perInterval: baseWorkers[worker.workerId].base.per_interval + perIntervalBonus,
+    perItem: baseWorkers[worker.workerId].base.per_item + perItemBonus,
+    maxStorage: baseWorkers[worker.workerId].base.max_storage + maxStoredBonus,
+  };
 }
--- src/utils/scheduled/jobs/workers.ts
+++ src/utils/scheduled/jobs/workers.ts
@@ -1,8 +1,5 @@
 import prisma from "../../database/database";
-import { getBoosters } from "../../functions/economy/boosters";
-import { getItems } from "../../functions/economy/utils";
-import { getBaseUpgrades, getBaseWorkers } from "../../functions/economy/workers";
-import { Booster } from "../../models/Economy";
+import { calcWorkerValues } from "../../functions/economy/workers";
 
 (async () => {
   const query = await prisma.economyWorker.findMany({
@@ -11,54 +8,14 @@ import { Booster } from "../../models/Economy";
     },
   });
 
-  const boosters = new Map<string, Map<string, Booster[]>>();
-  const baseWorkers = getBaseWorkers();
-  const baseUpgrades = getBaseUpgrades();
-  const items = getItems();
-
   const promises = [];
 
   for (const worker of query) {
-    if (!boosters.has(worker.userId)) {
-      boosters.set(worker.userId, await getBoosters(worker.userId));
-    }
-
-    const usersBoosters = boosters.get(worker.userId);
-
-    let perIntervalBonus = 0;
-    let maxStoredBonus = 0;
-
-    for (const upgrade of worker.upgrades) {
-      switch (baseUpgrades[upgrade.upgradeId].upgrades) {
-        case 1:
-          perIntervalBonus +=
-            baseUpgrades[upgrade.upgradeId].effect * upgrade.amount * baseWorkers[worker.workerId].base.per_interval;
-          break;
-        case 2:
-          maxStoredBonus +=
-            baseUpgrades[upgrade.upgradeId].effect * upgrade.amount * baseWorkers[worker.workerId].base.max_storage;
-          break;
-      }
-    }
-
-    for (const boosterId of usersBoosters.keys()) {
-      if (items[boosterId].role != "booster") return;
-
-      switch (items[boosterId].boosterEffect.boosts[0]) {
-        case "per_interval":
-          perIntervalBonus +=
-            items[boosterId].boosterEffect.effect *
-            usersBoosters.get(boosterId).length *
-            baseWorkers[worker.workerId].base.per_interval;
-          break;
-      }
-    }
-
-    const maxStorage = baseWorkers[worker.workerId].base.max_storage + maxStoredBonus;
+    const { maxStorage, perInterval } = await calcWorkerValues(worker);
 
     if (worker.stored >= maxStorage) continue;
 
-    let incrementAmount = baseWorkers[worker.workerId].base.per_interval + perIntervalBonus;
+    let incrementAmount = perInterval;
 
     if (worker.stored + incrementAmount > maxStorage) incrementAmount = maxStorage - worker.stored;
 
