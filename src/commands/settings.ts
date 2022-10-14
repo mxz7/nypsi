744e0ac8fa94c76b5a0f9559463221bad56ce59a
max <29782397+tekoh@users.noreply.github.com>
2022-10-14 20:43:40 +0100

    feat: move defaultbet to settings command

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -10,17 +10,29 @@ import {
   MessageActionRowComponentBuilder,
   SelectMenuOptionBuilder,
 } from "discord.js";
+import { addCooldown, getResponse, onCooldown } from "../utils/cooldownhandler";
+import { calcMaxBet, getDefaultBet, setDefaultBet } from "../utils/functions/economy/balance";
+import { createUser, formatNumber, userExists } from "../utils/functions/economy/utils";
 import { getDmSettings, getNotificationsData, updateDmSettings } from "../utils/functions/users/notifications";
 import { Categories, Command, NypsiCommandInteraction } from "../utils/models/Command";
-import { CustomEmbed } from "../utils/models/EmbedBuilders";
+import { CustomEmbed, ErrorEmbed } from "../utils/models/EmbedBuilders";
 
 const cmd = new Command("settings", "manage nypsi settings for your server and you", Categories.UTILITY);
 
 cmd.slashEnabled = true;
 cmd.slashData.addSubcommandGroup((me) =>
-  me.addSubcommand((notifications) =>
-    notifications.setName("notifications").setDescription("manage your notifications settings")
-  )
+  me
+    .addSubcommand((notifications) =>
+      notifications.setName("notifications").setDescription("manage your notifications settings")
+    )
+    .addSubcommand((defaultbet) =>
+      defaultbet
+        .setName("defaultbet")
+        .setDescription("set or reset your default bet")
+        .addStringOption((option) =>
+          option.setName("bet").setDescription("use reset to disable your default bet").setRequired(true)
+        )
+    )
 );
 
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
@@ -39,6 +51,14 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
       return await message.channel.send(data as BaseMessageOptions);
     }
   };
+
+  if (await onCooldown(cmd.name, message.member)) {
+    const embed = await getResponse(cmd.name, message.member);
+
+    return send({ embeds: [embed] });
+  }
+
+  await addCooldown(cmd.name, message.member, 5);
 
   const showDmSettings = async (settingId?: string) => {
     const notificationsData = getNotificationsData();
@@ -155,11 +175,80 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     return pageManager();
   };
 
+  const defaultBet = async () => {
+    if (!(await userExists(message.member))) await createUser(message.member);
+
+    const defaultBet = await getDefaultBet(message.member);
+
+    if (args.length == 2) {
+      if (!defaultBet) {
+        const embed = new CustomEmbed(message.member).setHeader("default bet", message.author.avatarURL());
+
+        embed.setDescription(
+          "you do not currently have a default bet. use `/settings me defaultbet <amount/reset>` to set your default bet"
+        );
+
+        return send({ embeds: [embed] });
+      } else {
+        const embed = new CustomEmbed(message.member).setHeader("default bet", message.author.avatarURL());
+
+        embed.setDescription(
+          `your default bet is $**${defaultBet.toLocaleString()}**` +
+            "\n\nuse `/settings me defaultbet <amount/reset>` to change this"
+        );
+
+        return send({ embeds: [embed] });
+      }
+    }
+
+    if (args[2].toLocaleLowerCase() == "reset") {
+      await setDefaultBet(message.member, null);
+
+      const embed = new CustomEmbed(message.member);
+
+      embed.setDescription(":white_check_mark: your default bet has been reset");
+
+      return send({ embeds: [embed] });
+    }
+
+    const maxBet = await calcMaxBet(message.member);
+
+    const bet = formatNumber(args[2]);
+
+    if (!bet || isNaN(bet)) {
+      return send({ embeds: [new ErrorEmbed("invalid amount")] });
+    }
+
+    if (bet <= 0) {
+      return send({ embeds: [new ErrorEmbed("your default bet must be greater than 0")] });
+    }
+
+    if (bet > maxBet) {
+      return send({
+        embeds: [
+          new ErrorEmbed(`your max bet is $**${maxBet.toLocaleString()}**\nyou can upgrade this by prestiging and voting`),
+        ],
+      });
+    }
+
+    await addCooldown(cmd.name, message.member, 5);
+
+    await setDefaultBet(message.member, bet);
+
+    const embed = new CustomEmbed(message.member);
+
+    embed.setDescription(`:white_check_mark: your default bet has been set to $${bet.toLocaleString()}`);
+
+    return send({ embeds: [embed] });
+  };
+
   if (args.length == 0) {
     return send({ embeds: [new CustomEmbed(message.member, "/settings me\n/settings server")] });
   } else if (args[0].toLowerCase() == "me") {
     if (args[1].toLowerCase() == "notifications") {
       return showDmSettings();
+    } else if (args[1].toLowerCase() == "defaultbet") {
+      return defaultBet();
     }
   }
 }
