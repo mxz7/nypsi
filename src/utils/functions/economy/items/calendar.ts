4130b1fa926918fbb4cc6437ff46ed2b1b738d93
max <29782397+tekoh@users.noreply.github.com>
2022-10-17 18:21:09 +0100

    feat: calendar item function

---
+++ src/utils/functions/economy/items/calendar.ts
@@ -0,0 +1,47 @@
+import { BaseMessageOptions, CommandInteraction, InteractionReplyOptions, Message, MessageEditOptions } from "discord.js";
+import { NypsiCommandInteraction } from "../../../../models/Command";
+import { CustomEmbed } from "../../../../models/EmbedBuilders";
+import { ItemUse } from "../../../../models/ItemUse";
+import sleep from "../../sleep";
+
+module.exports = new ItemUse("calendar", async (message: Message | (NypsiCommandInteraction & CommandInteraction)) => {
+  const send = async (data: BaseMessageOptions | InteractionReplyOptions) => {
+    if (!(message instanceof Message)) {
+      if (message.deferred) {
+        await message.editReply(data);
+      } else {
+        await message.reply(data as InteractionReplyOptions);
+      }
+      const replyMsg = await message.fetchReply();
+      if (replyMsg instanceof Message) {
+        return replyMsg;
+      }
+    } else {
+      return await message.channel.send(data as BaseMessageOptions);
+    }
+  };
+
+  const edit = async (data: MessageEditOptions, msg: Message) => {
+    if (!(message instanceof Message)) {
+      await message.editReply(data);
+      return await message.fetchReply();
+    } else {
+      return await msg.edit(data);
+    }
+  };
+
+  const embed = new CustomEmbed(message.member, "you look down at your calendar to check the date..");
+
+  const msg = await send({ embeds: [embed] });
+
+  await sleep(2000);
+
+  return edit(
+    {
+      embeds: [
+        embed.setDescription(`you look down at your calendar to check the date..\n\nits ${new Date().toDateString()}`),
+      ],
+    },
+    msg
+  );
+});
