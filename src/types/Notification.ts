10a44a265029baceed273038a87199e3bc25bdf4
max <29782397+tekoh@users.noreply.github.com>
2022-10-17 21:19:57 +0100

    refactor: notification queue function (#699)
    * feat: type for notificationpayload
    
    * feat: function to push to notification queue
    
    * refactor: use function in auction wach
    
    * fix: update to use new notiicationpayload format
    
    * refactor: use new function where applicable

---
+++ src/types/Notification.ts
@@ -0,0 +1,11 @@
+import { ActionRowBuilder, MessageActionRowComponentBuilder } from "discord.js";
+import { CustomEmbed } from "../models/EmbedBuilders";
+
+export interface NotificationPayload {
+  memberId: string;
+  payload: {
+    embed?: CustomEmbed;
+    components?: ActionRowBuilder<MessageActionRowComponentBuilder>;
+    content?: string;
+  };
+}
