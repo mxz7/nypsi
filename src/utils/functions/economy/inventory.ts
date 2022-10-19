082708da354cb28a62aa7011708dd287a0e90970
max <29782397+tekoh@users.noreply.github.com>
2022-10-19 14:47:34 +0100

    feat: maxbet booster (#711)
    * feat: max bet without boosters is now 1m
    
    * feat: high roller ite
    
    * feat: logic for max bet boosters
    
    * fix: improve nypsi crate odds
    
    * feat: better description

--- src/utils/functions/economy/inventory.ts
+++ src/utils/functions/economy/inventory.ts
@@ -338,8 +338,8 @@ export async function openCrate(member: GuildMember, item: Item): Promise<string
 
     for (const i of crateItems) {
       if (items[i]) {
-        if (item.id == "nypsi_crate" && items[i].role == "collectable") {
-          const chance = Math.floor(Math.random() * 3);
+        if (item.id == "nypsi_crate" && ["collectable", "sellable", "item", "car"].includes(item.role)) {
+          const chance = Math.floor(Math.random() * 6);
 
           if (chance != 2) continue;
         }
