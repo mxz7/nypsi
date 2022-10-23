4b9ee2a7f5214c02d8d2bde2e02c08dcd3bb3937
max <29782397+tekoh@users.noreply.github.com>
2022-10-23 19:33:53 +0100

    fix: adjust karma from reset

--- src/commands/reset.ts
+++ src/commands/reset.ts
@@ -29,7 +29,7 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
   let earnedKarma = 0;
 
-  earnedKarma += (await calcNetWorth(message.member)) / 1000;
+  earnedKarma += (await calcNetWorth(message.member)) / 100000;
   earnedKarma += (await getPrestige(message.member)) * 100;
   earnedKarma += (await getXp(message.member)) / 50;
 
