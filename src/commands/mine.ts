9ece0fe83ddfb9b9c8772cf4303569b1b12b9af2
max <29782397+tekoh@users.noreply.github.com>
2022-10-26 16:10:40 +0100

    fix: worse odds for wooden pickaxe

--- src/commands/mine.ts
+++ src/commands/mine.ts
@@ -197,10 +197,16 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
         } else if (items[i].rarity == 3) {
           if (pickaxe == "wooden_pickaxe" && items[i].id != "mineshaft_chest") continue;
 
-          for (let x = 0; x < 10; x++) {
-            mineItemsModified.push(i);
+          if (pickaxe == "wooden_pickaxe") {
+            for (let x = 0; x < 5; x++) {
+              mineItemsModified.push(i);
+            }
+          } else {
+            for (let x = 0; x < 10; x++) {
+              mineItemsModified.push(i);
 
-            if (pickaxe == "diamond_pickaxe") mineItemsModified.push(i);
+              if (pickaxe == "diamond_pickaxe") mineItemsModified.push(i);
+            }
           }
         } else if (items[i].rarity == 2 && pickaxe != "wooden_pickaxe") {
           for (let x = 0; x < 15; x++) {
