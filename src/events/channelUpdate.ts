cba7b79605bbe59dadf04a3bb7a560def649af09
max <29782397+tekoh@users.noreply.github.com>
2022-11-02 19:38:40 +0000

    feat: more logs (#747)
    * feat: channel update event
    
    * feat: emoji create evet
    
    * feat: emoji delete event
    
    * feat: emoji update event
    
    * feat: moved category event
    
    * feat: role add/remove logs
    
    * ci: events label

---
+++ src/events/channelUpdate.ts
@@ -0,0 +1,24 @@
+import { GuildChannel } from "discord.js";
+import { CustomEmbed } from "../models/EmbedBuilders";
+import { LogType } from "../types/Moderation";
+import { addLog, isLogsEnabled } from "../utils/functions/moderation/logs";
+
+export default async function channelUpdate(oldChannel: GuildChannel, newChannel: GuildChannel) {
+  if (oldChannel.name != newChannel.name && (await isLogsEnabled(newChannel.guild))) {
+    const embed = new CustomEmbed().disableFooter().setTimestamp();
+
+    embed.setHeader("channel renamed");
+    embed.setDescription(`${newChannel.toString()} \`${newChannel.id}\`\n` + `${oldChannel.name} -> ${newChannel.name}`);
+
+    await addLog(newChannel.guild, LogType.CHANNEL, embed);
+  } else if (oldChannel.parentId != newChannel.parentId && (await isLogsEnabled(newChannel.guild))) {
+    const embed = new CustomEmbed().disableFooter().setTimestamp();
+
+    embed.setHeader("channel moved category");
+    embed.setDescription(
+      `${newChannel.toString()} \`${newChannel.id}\`\n` + `${oldChannel.parent.name} -> ${newChannel.parent.name}`
+    );
+
+    await addLog(newChannel.guild, LogType.CHANNEL, embed);
+  }
+}
