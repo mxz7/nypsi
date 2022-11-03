c08644cf95e044ecd20ac7810f076b260f8e77bc
max <29782397+tekoh@users.noreply.github.com>
2022-11-03 16:50:40 +0000

    feat: use random catppuccin colour

--- src/utils/functions/color.ts
+++ src/utils/functions/color.ts
@@ -1,9 +1,20 @@
 import { variants } from "@catppuccin/palette";
 import { ColorResolvable, GuildMember } from "discord.js";
 
+const colors: string[] = [
+  variants.mocha.flamingo.hex,
+  variants.mocha.mauve.hex,
+  variants.mocha.maroon.hex,
+  variants.mocha.peach.hex,
+  variants.mocha.yellow.hex,
+  variants.mocha.green.hex,
+  variants.mocha.sky.hex,
+  variants.mocha.lavender.hex,
+];
+
 export function getColor(member: GuildMember) {
   if (member.displayHexColor == "#ffffff") {
-    return variants.latte.base.hex as ColorResolvable;
+    return colors[Math.floor(Math.random() * colors.length)] as ColorResolvable;
   } else {
     return member.displayHexColor;
   }
