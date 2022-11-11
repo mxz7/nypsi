8729c0ce20af3b3697b4e3df34fa9cef181eadcc
max <29782397+tekoh@users.noreply.github.com>
2022-11-11 17:34:19 +0000

    feat: x for showing users

---
+++ src/commands/x.ts
@@ -0,0 +1,192 @@
+import { CommandInteraction, Message, User } from "discord.js";
+import * as fs from "fs/promises";
+import prisma from "../init/database";
+import { NypsiClient } from "../models/Client";
+import { Categories, Command, NypsiCommandInteraction } from "../models/Command";
+import { CustomEmbed, ErrorEmbed } from "../models/EmbedBuilders";
+import Constants from "../utils/Constants";
+import { formatDate } from "../utils/functions/date";
+import { fetchUsernameHistory } from "../utils/functions/users/history";
+import { logger } from "../utils/logger";
+
+const cmd = new Command("x", "admincmd", Categories.NONE).setPermissions(["bot owner"]);
+
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
+  if (!(message instanceof Message)) return;
+  if (!Constants.ADMIN_IDS.includes(message.author.id)) return message.react("🫦");
+
+  const showUser = async (id: string) => {
+    const res = await (message.client as NypsiClient).cluster.broadcastEval(
+      async (c, { userId }) => {
+        const g = await c.users.fetch(userId);
+
+        return g;
+      },
+      { context: { userId: id } }
+    );
+
+    let user: User;
+
+    for (const i of res) {
+      if ((i as any).username) {
+        user = i as User;
+        break;
+      }
+    }
+
+    if (!user) return message.react("❌");
+
+    const embed = new CustomEmbed(message.member, `tag: ${user.username}#${user.discriminator}\nid: ${user.id}`).setHeader(
+      "discord"
+    );
+
+    const usernameHistory = await fetchUsernameHistory(user.id);
+
+    if (usernameHistory.length > 0) {
+      let msg = "";
+
+      let count = 0;
+      for (const un of usernameHistory) {
+        if (count >= 10) break;
+        msg += `\`${un.value}\` | \`${formatDate(un.date)}\`\n`;
+        count++;
+      }
+
+      embed.addField("username history", msg, true);
+    }
+
+    logger.info(`fetching data for ${user.id}...`);
+    const userData = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+      include: {
+        Economy: {
+          include: {
+            Inventory: true,
+            Boosters: true,
+            EconomyStats: true,
+            Crafting: true,
+            LotteryTicket: true,
+            EconomyWorker: {
+              include: {
+                upgrades: true,
+              },
+            },
+          },
+        },
+        EconomyGuild: true,
+        EconomyGuildMember: true,
+        Premium: {
+          include: {
+            PremiumCommand: true,
+          },
+        },
+        Username: true,
+        WordleStats: true,
+        Auction: true,
+        CommandUse: true,
+        Survey: true,
+        Achievements: true,
+        DMSettings: true,
+      },
+    });
+
+    const moderationCases = await prisma.moderationCase.findMany({
+      where: {
+        user: user.id,
+      },
+    });
+
+    const moderationCasesModerator = await prisma.moderationCase.findMany({
+      where: {
+        moderator: user.tag,
+      },
+    });
+
+    const moderationMutes = await prisma.moderationMute.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    const moderationBans = await prisma.moderationBan.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    const chatReactionStats = await prisma.chatReactionStats.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    const file = `temp/${user.id}.txt`;
+
+    logger.info(`packing into text file for ${user.id}...`);
+
+    await fs.writeFile(
+      file,
+      `nypsi data for ${user.username}#${user.username} (${user.id} at time of request) requested by ${message.author.tag} ${
+        message.author.id
+      } - ${new Date().toUTCString()}\n\n----------\nYOUR USER DATA\n----------\n\n`
+    );
+    await fs.appendFile(file, JSON.stringify(userData, null, 2));
+
+    await fs.appendFile(
+      file,
+      "\n----------------------------------------------\n\n----------\nYOUR MODERATION CASE DATA WHERE YOU GOT PUNISHED\n----------\n\n"
+    );
+    await fs.appendFile(file, JSON.stringify(moderationCases, null, 2));
+
+    await fs.appendFile(
+      file,
+      "\n----------------------------------------------\n\n----------\nYOUR MODERATION CASE DATA WHERE YOU WERE THE MODERATOR\n----------\n\n"
+    );
+    await fs.appendFile(file, JSON.stringify(moderationCasesModerator, null, 2));
+
+    await fs.appendFile(
+      file,
+      "\n----------------------------------------------\n\n----------\nYOUR MODERATION MUTE DATA\n----------\n\n"
+    );
+    await fs.appendFile(file, JSON.stringify(moderationMutes, null, 2));
+
+    await fs.appendFile(
+      file,
+      "\n----------------------------------------------\n\n----------\nYOUR MODERATION BAN DATA\n----------\n\n"
+    );
+    await fs.appendFile(file, JSON.stringify(moderationBans, null, 2));
+
+    await fs.appendFile(
+      file,
+      "\n----------------------------------------------\n\n----------\nYOUR CHAT REACTION DATA\n----------\n\n"
+    );
+    await fs.appendFile(file, JSON.stringify(chatReactionStats, null, 2));
+
+    const buffer = await fs.readFile(file);
+
+    return message.channel.send({ embeds: [embed], files: [{ attachment: buffer, name: `data_for_${user.id}.txt` }] });
+  };
+
+  if (args.length == 0) {
+    const embed = new CustomEmbed(
+      message.member,
+      "$x userid (id) - view disc info and db info" +
+        "\n$x removeitem (id) (item_id) (amount) - remove item from user inventory" +
+        "\n$x findid (tag/username) - will attempt to find user id from cached users and database"
+    );
+
+    return message.channel.send({ embeds: [embed] });
+  } else if (args[0].toLowerCase() == "userid") {
+    if (args.length == 1) {
+      return message.channel.send({ embeds: [new ErrorEmbed("$x userid (id)")] });
+    }
+
+    return showUser(args[1]);
+  }
+}
+
+cmd.setRun(run);
+
+module.exports = cmd;
