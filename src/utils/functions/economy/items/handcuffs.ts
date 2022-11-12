51ebbdfccb86defa934d787b5a19f459ac726386
max <29782397+tekoh@users.noreply.github.com>
2022-11-12 15:47:53 +0000

    feat: remove hardcode block for rob

--- src/utils/functions/economy/items/handcuffs.ts
+++ src/utils/functions/economy/items/handcuffs.ts
@@ -2,6 +2,7 @@ import { BaseMessageOptions, CommandInteraction, InteractionReplyOptions, Messag
 import { NypsiCommandInteraction } from "../../../../models/Command";
 import { CustomEmbed, ErrorEmbed } from "../../../../models/EmbedBuilders";
 import { ItemUse } from "../../../../models/ItemUse";
+import { getDisabledCommands } from "../../guilds/disabledcommands";
 import { getMember } from "../../member";
 import { getInventory, setInventoryItem } from "../inventory";
 import { addHandcuffs, isHandcuffed } from "../utils";
@@ -34,8 +35,8 @@ module.exports = new ItemUse(
       }
     };
 
-    if (message.guild.id == "747056029795221513") {
-      return send({ embeds: [new ErrorEmbed("this has been disabled in the support server")] });
+    if ((await getDisabledCommands(message.guild)).includes("rob")) {
+      return send({ embeds: [new ErrorEmbed("that command has been disabled")] });
     }
 
     if (args.length == 1) {
