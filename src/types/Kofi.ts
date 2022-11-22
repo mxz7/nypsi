f6b188d1fd4df8ac4ca1ad3682e5ad552c8b2e65
max <29782397+tekoh@users.noreply.github.com>
2022-11-22 00:43:44 +0000

    fix: update for kofi api respons

--- src/types/Kofi.ts
+++ src/types/Kofi.ts
@@ -2,6 +2,6 @@ export interface KofiResponse {
   type: string;
   email: string;
   tier_name?: string;
-  shop_items?: { direct_link_code: string }[];
+  shop_items?: { direct_link_code: string; quantity: number }[];
   verification_token: string;
 }
