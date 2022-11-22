7abcd32d94a4bbeb884f076a163c81880542b5ae
max <29782397+tekoh@users.noreply.github.com>
2022-11-22 14:12:44 +0000

    feat: checkautomute func

--- src/utils/functions/guilds/filters.ts
+++ src/utils/functions/guilds/filters.ts
@@ -1,8 +1,11 @@
-import { Guild, Message } from "discord.js";
+import { Guild, Message, Role, ThreadChannel } from "discord.js";
 import * as stringSimilarity from "string-similarity";
 import prisma from "../../../init/database";
 import { PunishmentType } from "../../../types/Moderation";
+import { logger } from "../../logger";
+import { newCase } from "../moderation/cases";
 import { addModLog } from "../moderation/logs";
+import { deleteMute, getAutoMuteLevels, getMuteRole, getMuteViolations, isMuted, newMute } from "../moderation/mute";
 import { cleanString } from "../string";
 import { getPercentMatch } from "./utils";
 
@@ -131,4 +134,98 @@ export async function checkMessageContent(message: Message) {
     }
   }
   return true;
+}
+
+export async function checkAutoMute(message: Message) {
+  const vl = getMuteViolations(message.guild, message.member);
+
+  console.log(vl);
+
+  const muteLevels = await getAutoMuteLevels(message.guild);
+
+  const muteUser = async (length: number) => {
+    const guildMuteRole = await getMuteRole(message.guild);
+
+    let muteRole: Role;
+    let mode = "role";
+
+    if (!guildMuteRole || guildMuteRole == "default") {
+      muteRole = message.guild.roles.cache.find((r) => r.name.toLowerCase() == "muted");
+
+      if (!muteRole) {
+        let channelError = false;
+        try {
+          const newMuteRole = await message.guild.roles
+            .create({
+              name: "muted",
+            })
+            .catch(() => {
+              channelError = true;
+            });
+
+          if (newMuteRole instanceof Role) {
+            muteRole = newMuteRole;
+          }
+
+          message.guild.channels.cache.forEach(async (channel) => {
+            if (channel instanceof ThreadChannel) return;
+            await channel.permissionOverwrites
+              .edit(muteRole, {
+                SendMessages: false,
+                Speak: false,
+                AddReactions: false,
+                SendMessagesInThreads: false,
+                CreatePublicThreads: false,
+                CreatePrivateThreads: false,
+              })
+              .catch(() => {
+                channelError = true;
+              });
+          });
+        } catch (e) {
+          channelError = true;
+          return logger.warn(`error creating mute role ${message.guild.id}`);
+        }
+        if (channelError) {
+          return logger.warn(`error creating mute role ${message.guild.id}`);
+        }
+      }
+    } else if (guildMuteRole == "timeout") {
+      mode = "timeout";
+    } else {
+      muteRole = await message.guild.roles.fetch(guildMuteRole);
+
+      if (!muteRole) {
+        logger.warn(`failed to find muterole ${message.guild.id} ${guildMuteRole}`);
+        return;
+      }
+    }
+
+    if (await isMuted(message.guild, message.member)) {
+      await deleteMute(message.guild, message.member);
+    }
+
+    await newCase(
+      message.guild,
+      PunishmentType.MUTE,
+      message.author.id,
+      message.guild.members.me.user.tag,
+      "filter violation"
+    );
+    await newMute(message.guild, [message.author.id], new Date(Date.now() + length * 1000));
+
+    if (mode == "timeout") {
+      return await message.member.timeout(length, "filter violation auto mute").catch(() => {
+        logger.warn(`error timing out user ${message.guild.id} ${message.author.id}`);
+      });
+    } else {
+      return await message.member.roles.add(muteRole, "filter violation auto mute");
+    }
+  };
+
+  if (muteLevels[vl]) {
+    await muteUser(muteLevels[vl]);
+  } else if (muteLevels[muteLevels.length - 1]) {
+    await muteUser(muteLevels[muteLevels.length - 1]);
+  }
 }
