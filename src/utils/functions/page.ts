a47921102637e1bb7888b7e9a76edb8f16c00336
max <29782397+tekoh@users.noreply.github.com>
2022-11-24 12:59:06 +0000

    refactor: static func in class

--- src/utils/functions/page.ts
+++ src/utils/functions/page.ts
@@ -1,17 +1,19 @@
-export function arrayToPage<T>(arr: T[], pageLength = 10): Map<number, T[]> {
-  const map = new Map<number, T[]>();
+export default class PageManager {
+  static createPages<T>(arr: T[], pageLength = 10): Map<number, T[]> {
+    const map = new Map<number, T[]>();
 
-  for (const item of arr) {
-    if (map.size == 0) {
-      map.set(1, [item]);
-    } else {
-      if (map.get(map.size).length >= pageLength) {
-        map.set(map.size + 1, [item]);
+    for (const item of arr) {
+      if (map.size == 0) {
+        map.set(1, [item]);
       } else {
-        map.get(map.size).push(item);
+        if (map.get(map.size).length >= pageLength) {
+          map.set(map.size + 1, [item]);
+        } else {
+          map.get(map.size).push(item);
+        }
       }
     }
-  }
 
-  return map;
+    return map;
+  }
 }
