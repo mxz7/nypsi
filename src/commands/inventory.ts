fe7656c48940a4618f5a3edf06eaf75c07accafb
max <29782397+tekoh@users.noreply.github.com>
2022-11-29 13:15:16 +0000

    refactor: use pagemanager for inventory

--- src/commands/inventory.ts
+++ src/commands/inventory.ts
@@ -4,17 +4,17 @@ import {
   ButtonBuilder,
   ButtonStyle,
   CommandInteraction,
-  Interaction,
   InteractionReplyOptions,
   Message,
   MessageActionRowComponentBuilder,
-  MessageEditOptions,
 } from "discord.js";
 import { inPlaceSort } from "fast-sort";
 import { Categories, Command, NypsiCommandInteraction } from "../models/Command";
 import { CustomEmbed } from "../models/EmbedBuilders";
+import { Item } from "../types/Economy";
 import { getInventory } from "../utils/functions/economy/inventory";
 import { createUser, getItems, userExists } from "../utils/functions/economy/utils";
+import PageManager from "../utils/functions/page";
 import { addCooldown, getResponse, onCooldown } from "../utils/handlers/cooldownhandler";
 
 const cmd = new Command("inventory", "view items in your inventory", Categories.MONEY).setAliases(["inv"]);
@@ -22,7 +22,7 @@ const cmd = new Command("inventory", "view items in your inventory", Categories.
 cmd.slashEnabled = true;
 cmd.slashData.addIntegerOption((option) => option.setName("page").setDescription("page number"));
 
-async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction)) {
   if (!(await userExists(message.member))) await createUser(message.member);
 
   const send = async (data: BaseMessageOptions | InteractionReplyOptions) => {
@@ -51,25 +51,10 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
   await addCooldown(cmd.name, message.member, 10);
 
-  let page = 0;
-
-  if (args.length == 1) {
-    if (!parseInt(args[0])) {
-      page = 1;
-    } else {
-      page = parseInt(args[0]) - 1;
-      if (page < 0) {
-        page = 0;
-      }
-    }
-  }
-
   const inventory = await getInventory(message.member, true);
   const items = getItems();
 
-  const itemIDs = inventory.map((i) => i.item);
-
-  if (itemIDs.length == 0) {
+  if (inventory.length == 0) {
     return send({
       embeds: [
         new CustomEmbed(message.member, "your inventory is empty").setHeader("your inventory", message.author.avatarURL()),
@@ -77,157 +62,64 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     });
   }
 
-  inPlaceSort(itemIDs).asc();
+  inPlaceSort(inventory).asc((i) => i.item);
 
-  const pages: string[][] = [];
-  let pageOfItems: string[] = [];
-
-  for (const item of itemIDs) {
-    if (pageOfItems.length == 6) {
-      pages.push(pageOfItems);
-      pageOfItems = [item];
-    } else {
-      pageOfItems.push(item);
-    }
-  }
-
-  if (pageOfItems.length != 0) {
-    pages.push(pageOfItems);
-  }
+  const pages = PageManager.createPages(
+    inventory.map((i) => items[i.item]),
+    6
+  );
 
   const embed = new CustomEmbed(message.member).setFooter({
-    text: `page ${page + 1}/${pages.length}`,
+    text: `page 1/${pages.size}`,
   });
 
   embed.setHeader("your inventory", message.author.avatarURL());
 
-  if (!pages[page]) {
-    page = 0;
-  }
+  const updatePage = (page: Item[], embed: CustomEmbed) => {
+    if (embed.data.fields?.length) embed.data.fields.length = 0;
 
-  for (const i of pages[page]) {
-    const item = items[i];
-    embed.addField(
-      item.id,
-      `${item.emoji} **${item.name}** ~~--~~ *${inventory.find((i) => i.item == item.id).amount.toLocaleString()}*${
-        item.shortDesc ? `\n${item.shortDesc}` : ""
-      }`,
-      true
-    );
-  }
+    for (const item of page) {
+      embed.addField(
+        item.id,
+        `${item.emoji} **${item.name}** ~~--~~ *${inventory.find((i) => i.item == item.id).amount.toLocaleString()}*${
+          item.shortDesc ? `\n${item.shortDesc}` : ""
+        }`,
+        true
+      );
+    }
 
-  let row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+    return embed;
+  };
+
+  updatePage(pages.get(1), embed);
+
+  const row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
     new ButtonBuilder().setCustomId("⬅").setLabel("back").setStyle(ButtonStyle.Primary).setDisabled(true),
     new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary)
   );
 
   let msg: Message;
 
-  if (pages.length == 1) {
+  if (pages.size == 1) {
     return await send({ embeds: [embed] });
   } else {
     msg = await send({ embeds: [embed], components: [row] });
   }
 
-  const edit = async (data: MessageEditOptions, msg: Message) => {
-    if (!(message instanceof Message)) {
-      await message.editReply(data);
-      return await message.fetchReply();
-    } else {
-      return await msg.edit(data);
-    }
-  };
+  const manager = new PageManager({
+    embed: embed,
+    message: msg,
+    userId: message.author.id,
+    row: row,
+    pages,
+    onPageUpdate(manager) {
+      manager.embed.setFooter({ text: `page ${manager.currentPage}/${manager.lastPage}` });
+      return manager.embed;
+    },
+    updateEmbed: updatePage,
+  });
 
-  if (pages.length > 1) {
-    let currentPage = page;
-
-    const lastPage = pages.length;
-
-    const filter = (i: Interaction) => i.user.id == message.author.id;
-
-    const pageManager = async (): Promise<void> => {
-      const reaction = await msg
-        .awaitMessageComponent({ filter, time: 30000 })
-        .then(async (collected) => {
-          await collected.deferUpdate();
-          return collected.customId;
-        })
-        .catch(async () => {
-          await edit({ components: [] }, msg);
-        });
-
-      const newEmbed = new CustomEmbed(message.member).setHeader("your inventory", message.author.avatarURL());
-
-      if (!reaction) return;
-
-      if (reaction == "⬅") {
-        if (currentPage <= 0) {
-          return pageManager();
-        } else {
-          currentPage--;
-          for (const i of pages[currentPage]) {
-            const item = items[i];
-            newEmbed.addField(
-              item.id,
-              `${item.emoji} **${item.name}** ~~--~~ *${inventory.find((i) => i.item == item.id).amount.toLocaleString()}*${
-                item.shortDesc ? `\n${item.shortDesc}` : ""
-              }`,
-              true
-            );
-          }
-          newEmbed.setFooter({
-            text: `page ${currentPage + 1}/${pages.length}`,
-          });
-          if (currentPage == 0) {
-            row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-              new ButtonBuilder().setCustomId("⬅").setLabel("back").setStyle(ButtonStyle.Primary).setDisabled(true),
-              new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary).setDisabled(false)
-            );
-          } else {
-            row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-              new ButtonBuilder().setCustomId("⬅").setLabel("back").setStyle(ButtonStyle.Primary).setDisabled(false),
-              new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary).setDisabled(false)
-            );
-          }
-          await edit({ embeds: [newEmbed], components: [row] }, msg);
-          return pageManager();
-        }
-      } else if (reaction == "➡") {
-        if (currentPage + 1 >= lastPage) {
-          return pageManager();
-        } else {
-          currentPage++;
-          for (const i of pages[currentPage]) {
-            const item = items[i];
-            newEmbed.addField(
-              item.id,
-              `${item.emoji} **${item.name}** ~~--~~ *${inventory.find((i) => i.item == item.id).amount.toLocaleString()}*${
-                item.shortDesc ? `\n${item.shortDesc}` : ""
-              }`,
-              true
-            );
-          }
-          newEmbed.setFooter({
-            text: `page ${currentPage + 1}/${pages.length}`,
-          });
-          if (currentPage + 1 == lastPage) {
-            row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-              new ButtonBuilder().setCustomId("⬅").setLabel("back").setStyle(ButtonStyle.Primary).setDisabled(false),
-              new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary).setDisabled(true)
-            );
-          } else {
-            row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-              new ButtonBuilder().setCustomId("⬅").setLabel("back").setStyle(ButtonStyle.Primary).setDisabled(false),
-              new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary).setDisabled(false)
-            );
-          }
-          await edit({ embeds: [newEmbed], components: [row] }, msg);
-          return pageManager();
-        }
-      }
-    };
-    return pageManager();
-  }
+  return manager.listen();
 }
 
 cmd.setRun(run);
