c37d611db43d224ea8642708806a27ffa20f9caa
max <29782397+tekoh@users.noreply.github.com>
2022-11-29 19:52:23 +0000

    feat: use crypto randomint

--- src/utils/functions/random.ts
+++ src/utils/functions/random.ts
@@ -1,6 +1,8 @@
+import { randomInt } from "crypto";
+
 export function shuffle<T>(array: T[]): T[] {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = randomInt(i + 1);
     const temp = array[i];
     array[i] = array[j];
     array[j] = temp;
