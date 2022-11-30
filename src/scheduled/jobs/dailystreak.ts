59d1fb426e7bb9f4510b7e59228798a5fba22f94
max <29782397+tekoh@users.noreply.github.com>
2022-11-30 15:54:09 +0000

    feat: white gem has chance to save streak

--- src/scheduled/jobs/dailystreak.ts
+++ src/scheduled/jobs/dailystreak.ts
@@ -31,15 +31,13 @@ import { addNotificationToQueue } from "../../utils/functions/users/notification
     },
   });
 
-  const savedList: string[] = [];
-  let savedCount = 0;
-  const resetList: string[] = [];
-  let resetCount = 0;
+  const calendarSaved: string[] = [];
+  const gemSaved: string[] = [];
+  const resetStreaks: string[] = [];
 
   for (const user of users) {
-    if (user.Inventory.find((i) => i.item == "calendar").amount > 0) {
-      if (user.user.DMSettings.other) savedList.push(user.userId);
-      savedCount++;
+    if (user.Inventory.find((i) => i.item == "calendar")?.amount > 0) {
+      if (user.user.DMSettings.other) calendarSaved.push(user.userId);
 
       if (user.Inventory.find((i) => i.item == "calendar").amount == 1) {
         await prisma.inventory.delete({
@@ -63,26 +61,40 @@ import { addNotificationToQueue } from "../../utils/functions/users/notification
           },
         });
       }
-    } else {
-      if (user.user.DMSettings.other) resetList.push(user.userId);
-      resetCount++;
+      continue;
+    } else if (user.Inventory.find((i) => i.item == "white_gem")?.amount > 0) {
+      const gemSaveChance = Math.floor(Math.random() * 10);
 
-      await prisma.economy.update({
-        where: {
-          userId: user.userId,
-        },
-        data: {
-          dailyStreak: 0,
-        },
-      });
+      if (gemSaveChance < 5) {
+        gemSaved.push(user.userId);
+        continue;
+      }
     }
+
+    if (user.user.DMSettings.other) resetStreaks.push(user.userId);
+
+    await prisma.economy.update({
+      where: {
+        userId: user.userId,
+      },
+      data: {
+        dailyStreak: 0,
+      },
+    });
   }
 
-  const savedEmbed = new CustomEmbed()
+  const calendarSavedEmbed = new CustomEmbed()
     .setColor(Constants.TRANSPARENT_EMBED_COLOR)
     .setTitle("your daily streak has been saved by a calendar!")
     .setDescription(
       "calendars in your inventory protect your daily streak, make sure to do `/daily` to continue your streak"
+    );
+
+  const gemSavedEmbed = new CustomEmbed()
+    .setColor(Constants.TRANSPARENT_EMBED_COLOR)
+    .setTitle("your daily streak was saved by your white gem!")
+    .setDescription(
+      "<:nypsi_gem_white:1046933670436552725> white gems have a chance to protect your daily streak. make sure to do /daily to continue your streak"
     );
 
   const resetEmbed = new CustomEmbed()
@@ -92,16 +104,25 @@ import { addNotificationToQueue } from "../../utils/functions/users/notification
       "you have lost your daily streak by not doing `/daily`. calendars can be used to protect your daily streak from being reset"
     );
 
-  for (const userId of savedList) {
+  for (const userId of calendarSaved) {
     await addNotificationToQueue({
       memberId: userId,
       payload: {
-        embed: savedEmbed,
+        embed: calendarSavedEmbed,
       },
     });
   }
 
-  for (const userId of resetList) {
+  for (const userId of gemSaved) {
+    await addNotificationToQueue({
+      memberId: userId,
+      payload: {
+        embed: gemSavedEmbed,
+      },
+    });
+  }
+
+  for (const userId of resetStreaks) {
     await addNotificationToQueue({
       memberId: userId,
       payload: {
@@ -110,8 +131,9 @@ import { addNotificationToQueue } from "../../utils/functions/users/notification
     });
   }
 
-  if (savedCount) parentPort.postMessage(`${savedCount} daily streaks saved by calendars`);
-  if (resetCount) parentPort.postMessage(`${resetCount} daily streaks reset`);
+  if (calendarSaved.length > 0) parentPort.postMessage(`${calendarSaved.length} daily streaks saved by calendars`);
+  if (gemSaved.length > 0) parentPort.postMessage(`${gemSaved.length} daily streaks saved by gems`);
+  if (resetStreaks.length > 0) parentPort.postMessage(`${resetStreaks.length} daily streaks reset`);
 
   process.exit(0);
 })();
