9f53e81b951460fda514369b99425a4a497918ee
max <29782397+tekoh@users.noreply.github.com>
2022-12-01 22:58:21 +0000

    feat: add dragon tower game (#805)
    * feat: add dragon tower (unfinished_
    
    * refactor: reduce mines win multi
    
    * refactor: rename map
    
    * feat: add gems to hard
    
    * feat: createrows func
    
    * fix: gem ocd
    
    * feat: dragons tower achievement
    
    * feat: game done
    
    * fix: prevent row overflow
    
    * fix: rename mines to tower
    
    * fix: top item search tag
    
    * fix: pages on top
    
    * fix: difficulty
    
    * feat: kofi prefix
    
    * fix: boobs
    
    * feat: bonus at top of tower
    
    * refactor: adjust gem odds
    
    * fix: top of board
    
    * feat: scaling increment
    
    * 5.7.0
    
    * feat: prevent playing multiple games at once
    
    * feat: add help msg to tower
    
    * refactor: achievement is to complete the tower
    
    * fix: linting

--- src/commands/highlow.ts
+++ src/commands/highlow.ts
@@ -183,8 +183,15 @@ async function prepareGame(
     }
   }
 
-  await addCooldown(cmd.name, message.member, 25);
+  if (await redis.sismember(Constants.redis.nypsi.USERS_PLAYING, message.author.id)) {
+    if (msg) {
+      return msg.edit({ embeds: [new ErrorEmbed("you are already playing something")], components: [] });
+    }
+    return send({ embeds: [new ErrorEmbed("you are already playing something")] });
+  }
 
+  await addCooldown(cmd.name, message.member, 25);
+  await redis.sadd(Constants.redis.nypsi.USERS_PLAYING, message.author.id);
   await updateBalance(message.member, (await getBalance(message.member)) - bet);
 
   const id = Math.random();
@@ -259,6 +266,7 @@ async function prepareGame(
     if (games.has(message.author.id)) {
       if (games.get(message.author.id).id == id) {
         games.delete(message.author.id);
+        await redis.srem(Constants.redis.nypsi.USERS_PLAYING, message.author.id);
         await updateBalance(message.member, (await getBalance(message.member)) + bet);
       }
     }
@@ -285,6 +293,7 @@ async function prepareGame(
   playGame(message, msg, args).catch((e: string) => {
     logger.error(`error occured playing highlow - ${message.author.tag} (${message.author.id})`);
     logger.error(e);
+    redis.srem(Constants.redis.nypsi.USERS_PLAYING, message.author.id);
     return send({
       embeds: [new ErrorEmbed("an error occured while running - join support server")],
     });
@@ -350,6 +359,7 @@ async function playGame(
   };
 
   const replay = async (embed: CustomEmbed) => {
+    await redis.srem(Constants.redis.nypsi.USERS_PLAYING, message.author.id);
     if (!(await isPremium(message.member)) || (await getBalance(message.member)) < bet) {
       return m.edit({ embeds: [embed], components: [] });
     }
@@ -505,6 +515,7 @@ async function playGame(
     .catch(() => {
       fail = true;
       games.delete(message.author.id);
+      redis.srem(Constants.redis.nypsi.USERS_PLAYING, message.author.id);
       message.channel.send({ content: message.author.toString() + " highlow game expired" });
     });
 
@@ -631,6 +642,7 @@ async function playGame(
     }
   } else {
     games.delete(message.author.id);
+    redis.srem(Constants.redis.nypsi.USERS_PLAYING, message.author.id);
     m.reactions.removeAll();
     return;
   }
