54fe352a43d2f708a28108fee3211863c87ded04
max <29782397+tekoh@users.noreply.github.com>
2022-12-04 21:32:51 +0000

    feat: it works!

---
+++ src/commands/getcsvdata.ts
@@ -0,0 +1,20 @@
+import { CommandInteraction, Message } from "discord.js";
+import { Categories, Command, NypsiCommandInteraction } from "../models/Command";
+import Constants from "../utils/Constants";
+import graphToCsv from "../utils/functions/workers/graphtocsv";
+
+const cmd = new Command("getcsvdata", "get csv data", Categories.NONE).setPermissions(["bot owner"]);
+
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction)) {
+  if (message.member.user.id != Constants.TEKOH_ID) return;
+
+  const msg = await message.channel.send({ content: "processing..." });
+
+  await graphToCsv();
+
+  msg.edit({ content: "done (/temp)" });
+}
+
+cmd.setRun(run);
+
+module.exports = cmd;
---
+++ src/utils/functions/workers/graphtocsv.ts
@@ -0,0 +1,92 @@
+import dayjs = require("dayjs");
+import { inPlaceSort } from "fast-sort";
+import { appendFile, writeFile } from "fs/promises";
+import { isMainThread, parentPort, Worker } from "worker_threads";
+import prisma from "../../../init/database";
+
+export default function graphToCsv(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(__filename);
+    worker.on("message", resolve);
+    worker.on("error", reject);
+    worker.on("exit", (code) => {
+      if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
+    });
+  });
+}
+
+if (!isMainThread) {
+  process.title = "nypsi: graph to csv";
+
+  const toCsv = async (fileName: string, data: { userId: string; balance: bigint; date: Date; id: number }[]) => {
+    const dates: number[] = [];
+    const map = new Map<string, { value: number; date: number }[]>();
+
+    for (const user of data) {
+      if (!dates.includes(user.date.getTime())) dates.push(user.date.getTime());
+      if (map.has(user.userId)) {
+        map.get(user.userId).push({ value: Number(user.balance), date: user.date.getTime() });
+      } else {
+        map.set(user.userId, [{ value: Number(user.balance), date: user.date.getTime() }]);
+      }
+    }
+
+    inPlaceSort(dates).asc();
+
+    const header: string[] = ["user"];
+
+    header.push(...dates.map((d) => dayjs(d).format("YYYY-MM-DD")));
+
+    await writeFile(`temp/${fileName}`, header.join(","));
+
+    const users = new Map<string, string[]>();
+
+    for (const date of dates) {
+      for (const [userId, values] of map.entries()) {
+        if (values.find((v) => v.date === date)) {
+          const balance = values.find((v) => v.date === date).value;
+
+          if (!users.has(userId)) {
+            users.set(userId, [balance.toString()]);
+          } else {
+            users.get(userId).push(balance.toString());
+          }
+        } else {
+          if (!users.has(userId)) {
+            users.set(userId, ["0"]);
+          } else {
+            users.get(userId).push("0");
+          }
+        }
+      }
+    }
+
+    for (const [userId, balances] of users.entries()) {
+      const tag = await prisma.user
+        .findUnique({
+          where: {
+            id: userId,
+          },
+          select: {
+            lastKnownTag: true,
+          },
+        })
+        .then((r) => r?.lastKnownTag);
+
+      await appendFile(`temp/${fileName}`, `\n${tag || userId},${balances.join(",")}`);
+    }
+  };
+
+  (async () => {
+    const topBalance = await prisma.graphTopBalance.findMany({
+      orderBy: {
+        date: "asc",
+      },
+    });
+
+    await toCsv("topbalance.csv", topBalance);
+
+    parentPort.postMessage(true);
+    process.exit(0);
+  })();
+}
