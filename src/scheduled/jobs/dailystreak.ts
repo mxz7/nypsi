761c2535f7e62f3ccbd8d45e38d4c71f95f1a2be
max <29782397+tekoh@users.noreply.github.com>
2022-12-11 21:21:38 +0000

    feat: gem has chance of breaking

--- src/scheduled/jobs/dailystreak.ts
+++ src/scheduled/jobs/dailystreak.ts
@@ -2,7 +2,9 @@ import dayjs = require("dayjs");
 import { parentPort } from "worker_threads";
 import prisma from "../../init/database";
 import { CustomEmbed } from "../../models/EmbedBuilders";
+import { NotificationPayload } from "../../types/Notification";
 import Constants from "../../utils/Constants";
+import { percentChance } from "../../utils/functions/random";
 import { addNotificationToQueue } from "../../utils/functions/users/notifications";
 
 (async () => {
@@ -31,13 +33,39 @@ import { addNotificationToQueue } from "../../utils/functions/users/notification
     },
   });
 
-  const calendarSaved: string[] = [];
-  const gemSaved: string[] = [];
-  const resetStreaks: string[] = [];
+  const calendarSavedEmbed = new CustomEmbed()
+    .setColor(Constants.TRANSPARENT_EMBED_COLOR)
+    .setTitle("your daily streak has been saved by a calendar!")
+    .setDescription(
+      "calendars in your inventory protect your daily streak, make sure to do `/daily` to continue your streak"
+    );
+
+  const gemSavedEmbed = new CustomEmbed()
+    .setColor(Constants.TRANSPARENT_EMBED_COLOR)
+    .setTitle("your daily streak was saved by your white gem!")
+    .setDescription(
+      "<:nypsi_gem_white:1046933670436552725> white gems have a chance to protect your daily streak. make sure to do /daily to continue your streak"
+    );
+
+  const whiteGemBrokeEmbed = new CustomEmbed()
+    .setColor(Constants.TRANSPARENT_EMBED_COLOR)
+    .setTitle("your white gem has shattered")
+    .setDescription(
+      "<:nypsi_gem_white:1046933670436552725> the power exerted by your white gem to save your streak has unfortunately caused it to shatter..."
+    );
+
+  const resetEmbed = new CustomEmbed()
+    .setColor(Constants.TRANSPARENT_EMBED_COLOR)
+    .setTitle("you have lost your daily streak!")
+    .setDescription(
+      "you have lost your daily streak by not doing `/daily`. calendars can be used to protect your daily streak from being reset"
+    );
+
+  const notifications: NotificationPayload[] = [];
 
   for (const user of users) {
     if (user.Inventory.find((i) => i.item == "calendar")?.amount > 0) {
-      if (user.user.DMSettings.other) calendarSaved.push(user.userId);
+      if (user.user.DMSettings.other) notifications.push({ memberId: user.userId, payload: { embed: calendarSavedEmbed } });
 
       if (Number(user.Inventory.find((i) => i.item == "calendar").amount) == 1) {
         await prisma.inventory.delete({
@@ -62,16 +90,42 @@ import { addNotificationToQueue } from "../../utils/functions/users/notification
         });
       }
       continue;
-    } else if (user.Inventory.find((i) => i.item == "white_gem")?.amount > 0) {
+    } else if (user.Inventory.find((i) => i.item == "white_gem")?.amount > 0n) {
       const gemSaveChance = Math.floor(Math.random() * 10);
 
       if (gemSaveChance < 5) {
-        gemSaved.push(user.userId);
+        notifications.push({ memberId: user.userId, payload: { embed: gemSavedEmbed } });
+
+        if (percentChance(7)) {
+          if (user.Inventory.find((i) => i.item === "white_gem")?.amount === 1n) {
+            await prisma.inventory.delete({
+              where: {
+                userId_item: {
+                  userId: user.userId,
+                  item: "white_gem",
+                },
+              },
+            });
+          } else {
+            await prisma.inventory.update({
+              where: {
+                userId_item: {
+                  userId: user.userId,
+                  item: "white_gem",
+                },
+              },
+              data: {
+                amount: { decrement: 1 },
+              },
+            });
+          }
+          notifications.push({ memberId: user.userId, payload: { embed: whiteGemBrokeEmbed } });
+        }
         continue;
       }
     }
 
-    if (user.user.DMSettings.other) resetStreaks.push(user.userId);
+    if (user.user.DMSettings.other) notifications.push({ memberId: user.userId, payload: { embed: resetEmbed } });
 
     await prisma.economy.update({
       where: {
@@ -83,57 +137,11 @@ import { addNotificationToQueue } from "../../utils/functions/users/notification
     });
   }
 
-  const calendarSavedEmbed = new CustomEmbed()
-    .setColor(Constants.TRANSPARENT_EMBED_COLOR)
-    .setTitle("your daily streak has been saved by a calendar!")
-    .setDescription(
-      "calendars in your inventory protect your daily streak, make sure to do `/daily` to continue your streak"
-    );
-
-  const gemSavedEmbed = new CustomEmbed()
-    .setColor(Constants.TRANSPARENT_EMBED_COLOR)
-    .setTitle("your daily streak was saved by your white gem!")
-    .setDescription(
-      "<:nypsi_gem_white:1046933670436552725> white gems have a chance to protect your daily streak. make sure to do /daily to continue your streak"
-    );
-
-  const resetEmbed = new CustomEmbed()
-    .setColor(Constants.TRANSPARENT_EMBED_COLOR)
-    .setTitle("you have lost your daily streak!")
-    .setDescription(
-      "you have lost your daily streak by not doing `/daily`. calendars can be used to protect your daily streak from being reset"
-    );
-
-  for (const userId of calendarSaved) {
-    await addNotificationToQueue({
-      memberId: userId,
-      payload: {
-        embed: calendarSavedEmbed,
-      },
-    });
+  for (const notif of notifications) {
+    await addNotificationToQueue(notif);
   }
 
-  for (const userId of gemSaved) {
-    await addNotificationToQueue({
-      memberId: userId,
-      payload: {
-        embed: gemSavedEmbed,
-      },
-    });
-  }
-
-  for (const userId of resetStreaks) {
-    await addNotificationToQueue({
-      memberId: userId,
-      payload: {
-        embed: resetEmbed,
-      },
-    });
-  }
-
-  if (calendarSaved.length > 0) parentPort.postMessage(`${calendarSaved.length} daily streaks saved by calendars`);
-  if (gemSaved.length > 0) parentPort.postMessage(`${gemSaved.length} daily streaks saved by gems`);
-  if (resetStreaks.length > 0) parentPort.postMessage(`${resetStreaks.length} daily streaks reset`);
+  parentPort.postMessage(`${notifications.length} streak notifications sent`);
 
   process.exit(0);
 })();
