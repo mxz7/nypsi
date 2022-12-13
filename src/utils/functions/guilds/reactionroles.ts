550686af13550ee6e6d63941aaa7282eb8fba1a8
max <29782397+tekoh@users.noreply.github.com>
2022-12-13 17:44:13 +0000

    feat: sendreactionrole func

--- src/utils/functions/guilds/reactionroles.ts
+++ src/utils/functions/guilds/reactionroles.ts
@@ -1,5 +1,13 @@
 import { ReactionRole, ReactionRoleMode, ReactionRoleRoles } from "@prisma/client";
-import { Guild, GuildTextBasedChannel, Role } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Guild,
+  GuildTextBasedChannel,
+  MessageActionRowComponentBuilder,
+  Role,
+} from "discord.js";
 import prisma from "../../../init/database";
 import redis from "../../../init/redis";
 import { CustomEmbed } from "../../../models/EmbedBuilders";
@@ -9,7 +17,7 @@ import ms = require("ms");
 export async function getReactionRolesByGuild(guild: Guild) {
   if (await redis.exists(`${Constants.redis.cache.guild.REACTION_ROLES}:${guild.id}`)) {
     return JSON.parse(await redis.get(`${Constants.redis.cache.guild.REACTION_ROLES}:${guild.id}`)) as (ReactionRole & {
-      ReactionRoleRoles: ReactionRoleRoles[];
+      roles: ReactionRoleRoles[];
     })[];
   }
 
@@ -100,4 +108,51 @@ export async function deleteReactionRole(guildId: string, messageId: string) {
   });
 
   await redis.del(`${Constants.redis.cache.guild.REACTION_ROLES}:${guildId}`);
+}
+
+export async function sendReactionRole(
+  reactionRole: ReactionRole & { roles: ReactionRoleRoles[] },
+  channel: GuildTextBasedChannel
+) {
+  const embed = new CustomEmbed().setColor(Constants.TRANSPARENT_EMBED_COLOR);
+
+  if (reactionRole.title) embed.setHeader(reactionRole.title);
+  embed.setDescription(reactionRole.description);
+
+  const components: ActionRowBuilder<MessageActionRowComponentBuilder>[] = [];
+
+  for (const role of reactionRole.roles) {
+    const button = new ButtonBuilder().setCustomId(role.roleId).setStyle(ButtonStyle.Secondary);
+
+    if (role.label.match(Constants.EMOJI_REGEX)) {
+      button.setEmoji(role.label);
+    } else {
+      button.setLabel(role.label);
+    }
+
+    if (components.length === 0) {
+      components[0] = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(button);
+    } else if (components[components.length - 1].components.length >= 5) {
+      components[components.length] = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(button);
+    } else {
+      components[components.length - 1].addComponents(button);
+    }
+  }
+
+  const oldMessage = await channel.messages.fetch(reactionRole.messageId);
+
+  if (oldMessage) await oldMessage.delete().catch(() => {});
+
+  const msg = await channel.send({ embeds: [embed], components });
+
+  await prisma.reactionRole.update({
+    where: {
+      messageId: reactionRole.messageId,
+    },
+    data: {
+      messageId: msg.id,
+    },
+  });
+
+  await redis.del(`${Constants.redis.cache.guild.REACTION_ROLES}:${reactionRole.guildId}`);
 }
