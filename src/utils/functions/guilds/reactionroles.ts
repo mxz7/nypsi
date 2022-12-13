7c4c9c687d4eccd2a3305d28900d39a4740f03ae
max <29782397+tekoh@users.noreply.github.com>
2022-12-13 17:20:29 +0000

    feat: createreactionrole func

--- src/utils/functions/guilds/reactionroles.ts
+++ src/utils/functions/guilds/reactionroles.ts
@@ -1,7 +1,8 @@
-import { ReactionRole, ReactionRoleRoles } from "@prisma/client";
-import { Guild } from "discord.js";
+import { ReactionRole, ReactionRoleMode, ReactionRoleRoles } from "@prisma/client";
+import { Guild, GuildTextBasedChannel } from "discord.js";
 import prisma from "../../../init/database";
 import redis from "../../../init/redis";
+import { CustomEmbed } from "../../../models/EmbedBuilders";
 import Constants from "../../Constants";
 import ms = require("ms");
 
@@ -25,4 +26,31 @@ export async function getReactionRolesByGuild(guild: Guild) {
   await redis.expire(`${Constants.redis.cache.guild.REACTION_ROLES}:${guild.id}`, Math.floor(ms("1 hour") / 1000));
 
   return query;
+}
+
+export async function createReactionRole(options: {
+  guildId: string;
+  channel: GuildTextBasedChannel;
+  title?: string;
+  messageDescription: string;
+  mode: ReactionRoleMode;
+}) {
+  const embed = new CustomEmbed().setColor(Constants.TRANSPARENT_EMBED_COLOR);
+
+  if (options.title) embed.setHeader(options.title);
+  embed.setDescription(options.messageDescription);
+
+  const msg = await options.channel.send({ embeds: [embed] });
+
+  await prisma.reactionRole.create({
+    data: {
+      channelId: options.channel.id,
+      messageId: msg.id,
+      mode: options.mode,
+      guildId: options.guildId,
+      title: options.title,
+    },
+  });
+
+  await redis.del(`${Constants.redis.cache.guild.REACTION_ROLES}:${options.guildId}`);
 }
