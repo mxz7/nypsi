fc74f80dd22e776ecd85c5c20be817e1d5bb5272
max <29782397+tekoh@users.noreply.github.com>
2022-12-19 20:57:59 +0000

    fix: clear reset / deleted members from cache

--- src/commands/deletemydata.ts
+++ src/commands/deletemydata.ts
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import {
   ActionRowBuilder,
   ButtonBuilder,
@@ -174,6 +175,8 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
         id: message.author.id,
       },
     });
+
+    exec(`redis-cli KEYS "*:${message.author.id}:*" | xargs redis-cli DEL`);
 
     logger.info(`data deleted for ${message.author.id}`);
 
