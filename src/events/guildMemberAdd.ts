8523a0f94ffcdb6af5558cf9cb8d52caf4e80cba
max <29782397+tekoh@users.noreply.github.com>
2022-12-23 12:35:39 +0000

    test: log weird shit

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -66,6 +66,8 @@ export default async function guildMemberAdd(member: GuildMember) {
   const userRoles = await getPersistantRolesForUser(member.guild, member.id);
 
   if (userRoles.length > 0 && persistantRoles.length > 0) {
+    logger.info(userRoles);
+    logger.info(persistantRoles);
     for (const roleId of userRoles) {
       if (persistantRoles.includes(roleId)) {
         await member.roles.add(persistantRoles).catch(async () => {
