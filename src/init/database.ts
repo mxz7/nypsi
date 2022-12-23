4867c7a83c5a426496e7847b9e9aec6b79fefe12
max <29782397+tekoh@users.noreply.github.com>
2022-12-23 15:21:49 +0000

    feat: database query timing

--- src/init/database.ts
+++ src/init/database.ts
@@ -1,5 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { logger } from "../utils/logger";
 
 const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  const before = Date.now();
+
+  const result = await next(params);
+
+  const timeTaken = Date.now() - before;
+
+  if (timeTaken > 50) {
+    logger.warn(`query ${params.model}.${params.action} took ${timeTaken}ms`);
+  }
+
+  return result;
+});
 
 export default prisma;
