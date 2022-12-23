5c795bed87929b0e63f54daaec7ea8b60d81d373
max <29782397+tekoh@users.noreply.github.com>
2022-12-23 19:08:15 +0000

    feat: updatechannel func

--- src/utils/functions/guilds/counters.ts
+++ src/utils/functions/guilds/counters.ts
@@ -1,9 +1,276 @@
 import { GuildCounter, TrackingType } from "@prisma/client";
 import { ChannelType, Guild, PermissionFlagsBits } from "discord.js";
 import prisma from "../../../init/database";
+import { NypsiClient } from "../../../models/Client";
+import { logger } from "../../logger";
+import { getItems } from "../economy/utils";
 
-export async function updateChannel(data: GuildCounter) {
-  // update channel name init.
+export async function updateChannel(data: GuildCounter, client: NypsiClient) {
+  const clusterHas = await client.cluster.broadcastEval(
+    async (c, { channelId }) => {
+      const client = c as NypsiClient;
+      const channel = await client.channels.fetch(channelId).catch(() => {});
+
+      if (channel) {
+        return client.cluster.id;
+      } else {
+        return "not-found";
+      }
+    },
+    {
+      context: { channelId: data.channel },
+    }
+  );
+
+  let shard: number;
+
+  for (const i of clusterHas) {
+    if (i != "not-found") {
+      shard = i;
+      break;
+    }
+  }
+
+  if (isNaN(shard)) {
+    logger.warn(`counter channel not found: ${JSON.stringify(data)}`);
+    await prisma.guildCounter.delete({
+      where: {
+        tracks_guildId: {
+          guildId: data.guildId,
+          tracks: data.tracks,
+        },
+      },
+    });
+    return;
+  }
+
+  let value: string;
+
+  if (data.tracks === TrackingType.HUMANS) {
+    value = await client.cluster
+      .broadcastEval(
+        async (c, { channelId, shard }) => {
+          const client = c as NypsiClient;
+
+          if (client.cluster.id != shard) return;
+
+          const channel = await client.channels.fetch(channelId);
+
+          if (channel.isDMBased()) return;
+
+          if (channel.guild.memberCount != channel.guild.members.cache.size) {
+            return await channel.guild.members.fetch().then((m) => m.size.toLocaleString());
+          }
+          return channel.guild.memberCount.toLocaleString();
+        },
+        { context: { channelId: data.channel, shard } }
+      )
+      .then((res) => {
+        for (const r of res) {
+          if (r) return r;
+        }
+      });
+  } else if (data.tracks === TrackingType.MEMBERS) {
+    value = await client.cluster
+      .broadcastEval(
+        async (c, { channelId, shard }) => {
+          const client = c as NypsiClient;
+
+          if (client.cluster.id != shard) return;
+
+          const channel = await client.channels.fetch(channelId);
+
+          if (channel.isDMBased()) return;
+
+          return channel.guild.memberCount.toLocaleString();
+        },
+        { context: { channelId: data.channel, shard } }
+      )
+      .then((res) => {
+        for (const r of res) {
+          if (r) return r;
+        }
+      });
+  } else if (data.tracks === TrackingType.BOOSTS) {
+    value = await client.cluster
+      .broadcastEval(
+        async (c, { channelId, shard }) => {
+          const client = c as NypsiClient;
+
+          if (client.cluster.id != shard) return;
+
+          const channel = await client.channels.fetch(channelId);
+
+          if (channel.isDMBased()) return;
+
+          return channel.guild.premiumSubscriptionCount.toLocaleString();
+        },
+        { context: { channelId: data.channel, shard } }
+      )
+      .then((res) => {
+        for (const r of res) {
+          if (r) return r;
+        }
+      });
+  } else if (data.tracks === TrackingType.RICHEST_MEMBER) {
+    const members = await client.cluster
+      .broadcastEval(
+        async (c, { channelId, shard }) => {
+          const client = c as NypsiClient;
+
+          if (client.cluster.id != shard) return;
+
+          const channel = await client.channels.fetch(channelId);
+
+          if (channel.isDMBased()) return;
+
+          if (channel.guild.memberCount !== channel.guild.members.cache.size) {
+            return Array.from(await channel.guild.members.fetch().then((members) => members.keys()));
+          }
+          return Array.from(channel.guild.members.cache.keys());
+        },
+        { context: { channelId: data.channel, shard } }
+      )
+      .then((res) => {
+        for (const r of res) {
+          if (r) return r;
+        }
+      });
+
+    const topMember = await prisma.economy.findFirst({
+      where: {
+        userId: { in: members },
+      },
+      select: {
+        user: {
+          select: {
+            lastKnownTag: true,
+          },
+        },
+      },
+      orderBy: { money: "desc" },
+    });
+
+    value = topMember?.user?.lastKnownTag || "null";
+  } else if (data.tracks === TrackingType.TOTAL_BALANCE) {
+    const members = await client.cluster
+      .broadcastEval(
+        async (c, { channelId, shard }) => {
+          const client = c as NypsiClient;
+
+          if (client.cluster.id != shard) return;
+
+          const channel = await client.channels.fetch(channelId);
+
+          if (channel.isDMBased()) return;
+
+          if (channel.guild.memberCount !== channel.guild.members.cache.size) {
+            return Array.from(await channel.guild.members.fetch().then((members) => members.keys()));
+          }
+          return Array.from(channel.guild.members.cache.keys());
+        },
+        { context: { channelId: data.channel, shard } }
+      )
+      .then((res) => {
+        for (const r of res) {
+          if (r) return r;
+        }
+      });
+
+    const total = await prisma.economy.aggregate({
+      where: {
+        userId: { in: members },
+      },
+      _sum: {
+        money: true,
+      },
+    });
+
+    value = total._sum.money.toLocaleString();
+  } else if (data.tracks === TrackingType.TOTAL_ITEM) {
+    if (!data.totalItem || !getItems()[data.totalItem]) {
+      logger.warn(`invalid item: ${JSON.stringify(data)}`);
+      return;
+    }
+    const members = await client.cluster
+      .broadcastEval(
+        async (c, { channelId, shard }) => {
+          const client = c as NypsiClient;
+
+          if (client.cluster.id != shard) return;
+
+          const channel = await client.channels.fetch(channelId);
+
+          if (channel.isDMBased()) return;
+
+          if (channel.guild.memberCount !== channel.guild.members.cache.size) {
+            return Array.from(await channel.guild.members.fetch().then((members) => members.keys()));
+          }
+          return Array.from(channel.guild.members.cache.keys());
+        },
+        { context: { channelId: data.channel, shard } }
+      )
+      .then((res) => {
+        for (const r of res) {
+          if (r) return r;
+        }
+      });
+
+    const query = await prisma.inventory.aggregate({
+      where: {
+        AND: [{ userId: { in: members } }, { item: data.totalItem }],
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+
+    value = query?._sum.amount.toLocaleString() || "0";
+  }
+
+  const format = data.format.replace("%value%", value);
+
+  if (format.length > 164) {
+    logger.warn(`channel name too long: ${JSON.stringify(data)}`);
+    return;
+  }
+
+  const res = await client.cluster.broadcastEval(
+    async (c, { shard, channelId, format }) => {
+      const client = c as NypsiClient;
+
+      if (client.cluster.id != shard) return;
+
+      const channel = await client.channels.fetch(channelId);
+
+      if (channel.isDMBased()) return;
+
+      if (channel.name == format) return "no-update";
+
+      let fail = false;
+
+      await channel.setName(format).catch(() => {
+        fail = true;
+      });
+
+      if (fail) return "failed";
+      return "updated";
+    },
+    { context: { shard, channelId: data.channel, format } }
+  );
+
+  for (const r of res) {
+    if (r) {
+      if (r === "failed") {
+        logger.warn(`failed to upload counter ${data}`);
+      } else if (r === "updated") {
+        logger.info({
+          message: `updated counter for ${data.guildId} type: ${data.tracks}`,
+          level: "success",
+        });
+      }
+    }
+  }
 }
 
 export async function createGuildCounter(guild: Guild, mode: TrackingType, item?: string, format?: string) {
@@ -39,7 +306,7 @@ export async function createGuildCounter(guild: Guild, mode: TrackingType, item?
     },
   });
 
-  await updateChannel(res);
+  await updateChannel(res, channel.client as NypsiClient);
 
   return true;
 }
