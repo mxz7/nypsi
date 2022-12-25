ad9775c5ac77269f41d228fccc450b77cf6aab6d
max <29782397+tekoh@users.noreply.github.com>
2022-12-25 13:31:02 +0000

    fix: reset back to default colour

--- src/commands/premium.ts
+++ src/commands/premium.ts
@@ -188,7 +188,7 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
       color = color.substr(0, 6);
     }
 
-    if (!color.startsWith("#")) color = `#${color}`;
+    if (!color.startsWith("#") && color != "default") color = `#${color}`;
 
     if (!color.match(Constants.COLOUR_REGEX))
       return send({ embeds: [new ErrorEmbed("invalid hex color code. example: #abcdef")] });
