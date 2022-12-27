6a67fde543f772357e3bc1f498b91d5f789366d4
max <29782397+tekoh@users.noreply.github.com>
2022-12-27 14:24:15 +0000

    feat: specific crate items (#847)
    * feat: update type for items key
    
    * feat: allow filtering based on items allowed in crate
    
    * feat: set items for mineshaft chest
    
    * fix: reset back to default colour
    
    * fix: wording
    
    * chore(deps-dev): bump @types/node from 18.11.17 to 18.11.18 (#844)
    
    Bumps [@types/node](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/node) from 18.11.17 to 18.11.18.
    - [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)
    - [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/node)
    
    ---
    updated-dependencies:
    - dependency-name: "@types/node"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps-dev): bump @typescript-eslint/eslint-plugin from 5.47.0 to 5.47.1 (#845)
    
    chore(deps-dev): bump @typescript-eslint/eslint-plugin
    
    Bumps [@typescript-eslint/eslint-plugin](https://github.com/typescript-eslint/typescript-eslint/tree/HEAD/packages/eslint-plugin) from 5.47.0 to 5.47.1.
    - [Release notes](https://github.com/typescript-eslint/typescript-eslint/releases)
    - [Changelog](https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/CHANGELOG.md)
    - [Commits](https://github.com/typescript-eslint/typescript-eslint/commits/v5.47.1/packages/eslint-plugin)
    
    ---
    updated-dependencies:
    - dependency-name: "@typescript-eslint/eslint-plugin"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Revert "refactor: use id instead of unique"
    
    This reverts commit 19b2f34caaca7c0c9d4905898f76a1e4dfeda0c7.
    
    * feat: add crate_runs field thing
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/types/Economy.ts
+++ src/types/Economy.ts
@@ -28,6 +28,8 @@ export interface Item {
   };
   in_crates: boolean;
   account_locked?: boolean;
+  items?: string[]; // used for crates with specific items format: <id|role>:(value)
+  crate_runs?: number; // how many times to do crate thing
 }
 
 export interface LotteryTicket {
