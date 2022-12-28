554984783f0c839bc79ea533d8f9d475299dede4
max <29782397+tekoh@users.noreply.github.com>
2022-12-28 17:55:40 +0000

    fix: improve xp earned

--- src/utils/functions/economy/xp.ts
+++ src/utils/functions/economy/xp.ts
@@ -78,8 +78,8 @@ export async function calcEarnedXp(member: GuildMember, bet: number, multiplier:
 
   if (prestige) {
     if (prestige > 15) prestige = 15;
-    min += prestige / 2.7;
-    max += prestige / 1.1;
+    min += prestige / 3.5;
+    max += prestige / 0.77;
   }
 
   if (await isPremium(member)) {
@@ -89,7 +89,7 @@ export async function calcEarnedXp(member: GuildMember, bet: number, multiplier:
   min += bet / 75_000;
   max += multiplier;
 
-  if (inventory.find((i) => i.item === "crystal_heart")?.amount > 0) min += Math.floor(Math.random() * 5);
+  if (inventory.find((i) => i.item === "crystal_heart")?.amount > 0) max += Math.floor(Math.random() * 7);
   if (inventory.find((i) => i.item == "white_gem")?.amount > 0) {
     const chance = Math.floor(Math.random() * 10);
 
@@ -97,7 +97,7 @@ export async function calcEarnedXp(member: GuildMember, bet: number, multiplier:
       max -= Math.floor(Math.random() * 7) + 1;
     } else {
       gemBreak(member.user.id, 0.007, "white_gem");
-      max += Math.floor(Math.random() * 13) + 1;
+      max += Math.floor(Math.random() * 17) + 1;
     }
   } else if (inventory.find((i) => i.item == "pink_gem")?.amount > 0) {
     gemBreak(member.user.id, 0.07, "pink_gem");
