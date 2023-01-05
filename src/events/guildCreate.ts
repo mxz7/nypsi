0e115b998064749f48af3d099439cee3670dd6ed
max <29782397+tekoh@users.noreply.github.com>
2023-01-05 20:39:19 +0000

    refactor: remove guild related karma

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,7 +1,5 @@
 import { Client, Guild } from "discord.js";
 import { createGuild, hasGuild, runCheck } from "../utils/functions/guilds/utils";
-import { addKarma } from "../utils/functions/karma/karma";
-import { createProfile, hasProfile } from "../utils/functions/users/utils";
 import { logger } from "../utils/logger";
 
 export default async function guildCreate(client: Client, guild: Guild) {
@@ -13,14 +11,4 @@ export default async function guildCreate(client: Client, guild: Guild) {
   if (!(await hasGuild(guild))) await createGuild(guild);
 
   await runCheck(guild);
-
-  let amount = Math.floor(guild.memberCount / 10);
-
-  if (amount > 500) {
-    amount = 500;
-  }
-
-  if (!(await hasProfile(guild.ownerId))) await createProfile(guild.ownerId);
-
-  await addKarma(guild.ownerId, amount);
 }
--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,7 +1,6 @@
 import { Client, Guild } from "discord.js";
 import { updateDisabledCommands } from "../utils/functions/guilds/disabledcommands";
 import { setPrefix } from "../utils/functions/guilds/utils";
-import { removeKarma } from "../utils/functions/karma/karma";
 import { setMuteRole } from "../utils/functions/moderation/mute";
 import { profileExists } from "../utils/functions/moderation/utils";
 import { logger } from "../utils/logger";
@@ -21,12 +20,4 @@ export default async function guildDelete(client: Client, guild: Guild) {
   if (await profileExists(guild)) {
     await setMuteRole(guild, "");
   }
-
-  let amount = Math.floor(guild.memberCount / 10);
-
-  if (amount > 500) {
-    amount = 500;
-  }
-
-  await removeKarma(guild.ownerId, amount);
 }
