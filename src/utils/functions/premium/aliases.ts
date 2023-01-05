abbbd6caba00d22357164a6544e8f3dd2d362fc3
max <29782397+tekoh@users.noreply.github.com>
2023-01-05 15:33:47 +0000

    feat: add custom per user aliases (#859)
    * feat: useralias model
    
    * feat: get aliases func
    
    * feat: add/delete func
    
    * fix: always array
    
    * feat: custom alias support in handler
    
    * feat: added ability to edit/view custom aliases in premium command
    
    * fix: error msg on list when having none
    
    * fix: custom alias handling
    
    * feat: migration
    
    * 6.1.0

---
+++ src/utils/functions/premium/aliases.ts
@@ -0,0 +1,69 @@
+import { UserAlias } from "@prisma/client";
+import { GuildMember } from "discord.js";
+import prisma from "../../../init/database";
+import redis from "../../../init/redis";
+import Constants from "../../Constants";
+import ms = require("ms");
+
+export async function getUserAliases(member: GuildMember | string) {
+  let id: string;
+  if (member instanceof GuildMember) {
+    id = member.user.id;
+  } else {
+    id = member;
+  }
+
+  if (await redis.exists(`${Constants.redis.cache.premium.ALIASES}:${id}`)) {
+    return JSON.parse(await redis.get(`${Constants.redis.cache.premium.ALIASES}:${id}`)) as UserAlias[];
+  }
+
+  const query = await prisma.userAlias.findMany({
+    where: {
+      userId: id,
+    },
+  });
+
+  await redis.set(`${Constants.redis.cache.premium.ALIASES}:${id}`, JSON.stringify(query || []));
+  await redis.expire(`${Constants.redis.cache.premium.ALIASES}:${id}`, Math.floor(ms("12 hour") / 1000));
+
+  return query;
+}
+
+export async function addUserAlias(member: GuildMember | string, alias: string, command: string) {
+  let id: string;
+  if (member instanceof GuildMember) {
+    id = member.user.id;
+  } else {
+    id = member;
+  }
+
+  await prisma.userAlias.create({
+    data: {
+      alias,
+      command,
+      userId: id,
+    },
+  });
+
+  await redis.del(`${Constants.redis.cache.premium.ALIASES}:${id}`);
+}
+
+export async function removeUserAlias(member: GuildMember | string, alias: string) {
+  let id: string;
+  if (member instanceof GuildMember) {
+    id = member.user.id;
+  } else {
+    id = member;
+  }
+
+  await prisma.userAlias.delete({
+    where: {
+      userId_alias: {
+        alias,
+        userId: id,
+      },
+    },
+  });
+
+  await redis.del(`${Constants.redis.cache.premium.ALIASES}:${id}`);
+}
