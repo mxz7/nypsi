be64b3819a68acaabb3be7219a3a6f454ef45ccb
max <29782397+tekoh@users.noreply.github.com>
2023-01-18 13:22:41 +0000

    feat: passive mode (#881)
    * feat: add passive field to schema
    
    * feat: migration file
    
    * feat: passive util functions
    
    * feat: lower multiplier
    
    * feat: reduced xp gain
    
    * fix: no less than 0
    
    * feat: prevent passive people being attacked
    
    * fix: cache expire
    
    * feat: added passive to /settings
    
    * refactor: embed appearance
    
    * refactor: remove karma from user command
    
    * 6.3.0

---
+++ src/utils/functions/economy/passive.ts
@@ -0,0 +1,54 @@
+import { GuildMember } from "discord.js";
+import prisma from "../../../init/database";
+import redis from "../../../init/redis";
+import Constants from "../../Constants";
+import ms = require("ms");
+
+export async function isPassive(member: GuildMember | string) {
+  let id: string;
+  if (member instanceof GuildMember) {
+    id = member.user.id;
+  } else {
+    id = member;
+  }
+
+  const cache = await redis.get(`${Constants.redis.cache.economy.PASSIVE}:${id}`);
+
+  if (cache) {
+    return cache == "t";
+  }
+
+  const query = await prisma.economy.findUnique({
+    where: {
+      userId: id,
+    },
+    select: {
+      passive: true,
+    },
+  });
+
+  await redis.set(`${Constants.redis.cache.economy.PASSIVE}:${id}`, query.passive ? "t" : "f");
+  await redis.expire(`${Constants.redis.cache.economy.PASSIVE}:${id}`, Math.floor(ms("6 hours") / 1000));
+
+  return query.passive;
+}
+
+export async function setPassive(member: GuildMember | string, value: boolean) {
+  let id: string;
+  if (member instanceof GuildMember) {
+    id = member.user.id;
+  } else {
+    id = member;
+  }
+
+  await prisma.economy.update({
+    where: {
+      userId: id,
+    },
+    data: {
+      passive: value,
+    },
+  });
+
+  await redis.del(`${Constants.redis.cache.economy.PASSIVE}:${id}`);
+}
