ff84c4a09df9b330c1afc90d66d73b3421d3833c
max <29782397+tekoh@users.noreply.github.com>
2023-01-22 19:51:14 +0000

    feat: reformat logs

--- src/events/channelDelete.ts
+++ src/events/channelDelete.ts
@@ -9,7 +9,7 @@ export default async function channelDelete(channel: GuildChannel) {
   if (await isLogsEnabled(channel.guild)) {
     const embed = new CustomEmbed().disableFooter().setTimestamp();
 
-    embed.setHeader("channel deleted");
+    embed.setTitle("channel deleted");
     embed.setDescription(
       `${channel.toString()} \`${channel.id}\`\n\n**name** ${channel.name}\n**category** ${channel.parent.name}\n**type** ${
         channel.type
