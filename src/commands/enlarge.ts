371922daa0603227cb0f4229f5df8b160a99ec98
max <29782397+tekoh@users.noreply.github.com>
2023-02-04 19:29:57 +0000

    fix: no cooldown for enlarge cmd

--- src/commands/enlarge.ts
+++ src/commands/enlarge.ts
@@ -2,7 +2,6 @@ import { CommandInteraction, Message } from "discord.js";
 import { Categories, Command, NypsiCommandInteraction } from "../models/Command";
 import { CustomEmbed, ErrorEmbed } from "../models/EmbedBuilders";
 import { getPrefix } from "../utils/functions/guilds/utils";
-import { addCooldown, getResponse, onCooldown } from "../utils/handlers/cooldownhandler";
 
 const cmd = new Command("enlarge", "enlarge a custom emoji to its full size", Categories.UTILITY).setAliases([
   "emoji",
@@ -10,12 +9,6 @@ const cmd = new Command("enlarge", "enlarge a custom emoji to its full size", Ca
 ]);
 
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
-  if (await onCooldown(cmd.name, message.member)) {
-    const embed = await getResponse(cmd.name, message.member);
-
-    return message.channel.send({ embeds: [embed] });
-  }
-
   const prefix = await getPrefix(message.guild);
 
   if (args.length == 0) {
@@ -31,8 +24,6 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
   if (!emoji[2]) {
     return message.channel.send({ embeds: [new ErrorEmbed("invalid emoji - please use a custom emoji")] });
   }
-
-  await addCooldown(cmd.name, message.member, 5);
 
   const emojiID = emoji[2].slice(0, emoji[2].length - 1);
 
