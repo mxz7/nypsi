1ea26c4511b486b137a603c5445bf7c631624891
max <29782397+tekoh@users.noreply.github.com>
2023-02-06 21:42:47 +0000

    feat: hacky way to change log color

--- src/utils/logger/pino-pretty-transport.ts
+++ src/utils/logger/pino-pretty-transport.ts
@@ -11,5 +11,29 @@ module.exports = (opts: PrettyOptions) =>
       time: (timestamp) => `${chalk.blackBright.italic(dayjs(Number(timestamp)).format("MM-DD hh:mm:ss"))}`,
       // level: (level) => `${levels.labels[level as unknown as number]}`,
     },
-    messageFormat: (log, msgKey) => chalk.reset(log[msgKey]),
+    messageFormat: (log, msgKey) => {
+      let color = chalk.reset;
+
+      if ((log[msgKey] as string).startsWith("::")) {
+        const category = (log[msgKey] as string).split(" ").splice(0, 1)[0].substring(2);
+        log[msgKey] = (log[msgKey] as string).split(" ").slice(1).join(" ");
+
+        switch (category.toLowerCase()) {
+          case "guild":
+            color = chalk.magenta;
+            break;
+          case "auto":
+            color = chalk.blue;
+            break;
+          case "cmd":
+            color = chalk.cyan;
+            break;
+          case "success":
+            color = chalk.green;
+            break;
+        }
+      }
+
+      return color(log[msgKey]);
+    },
   });
