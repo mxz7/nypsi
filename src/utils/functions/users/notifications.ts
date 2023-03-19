a6ec4e3ff91121cf2f80c27b542e04461aa816c3
max <29782397+tekoh@users.noreply.github.com>
2023-03-19 13:20:38 +0000

    feat: preferences functions

--- src/utils/functions/users/notifications.ts
+++ src/utils/functions/users/notifications.ts
@@ -1,4 +1,4 @@
-import { DMSettings } from "@prisma/client";
+import { DMSettings, Preferences } from "@prisma/client";
 import { GuildMember } from "discord.js";
 import prisma from "../../../init/database";
 import redis from "../../../init/redis";
@@ -15,7 +15,8 @@ interface NotificationData {
   types?: { name: string; description: string; value: string }[];
 }
 
-const notificationsData: { [key: string]: NotificationData } = require("../../../../data/notifications.json");
+const notificationsData: { [key: string]: NotificationData } = require("../../../../data/notifications.json").notifications;
+const preferencesData: { [key: string]: NotificationData } = require("../../../../data/notifications.json").preferences;
 
 export async function getDmSettings(member: GuildMember | string) {
   let id: string;
@@ -71,6 +72,62 @@ export async function updateDmSettings(member: GuildMember | string, data: DMSet
 
 export function getNotificationsData() {
   return notificationsData;
+}
+
+export function getPreferencesData() {
+  return preferencesData;
+}
+
+export async function getPreferences(member: GuildMember | string) {
+  let id: string;
+  if (member instanceof GuildMember) {
+    id = member.user.id;
+  } else {
+    id = member;
+  }
+
+  if (await redis.exists(`${Constants.redis.cache.user.PREFERENCES}:${id}`)) {
+    return (await JSON.parse(await redis.get(`${Constants.redis.cache.user.PREFERENCES}:${id}`))) as DMSettings;
+  }
+
+  let query = await prisma.preferences.findUnique({
+    where: {
+      userId: id,
+    },
+  });
+
+  if (!query) {
+    query = await prisma.preferences.create({
+      data: {
+        userId: id,
+      },
+    });
+  }
+
+  await redis.set(`${Constants.redis.cache.user.PREFERENCES}:${id}`, JSON.stringify(query));
+  await redis.expire(`${Constants.redis.cache.user.PREFERENCES}:${id}`, ms("12 hour") / 1000);
+
+  return query;
+}
+
+export async function updatePreferences(member: GuildMember | string, data: Preferences) {
+  let id: string;
+  if (member instanceof GuildMember) {
+    id = member.user.id;
+  } else {
+    id = member;
+  }
+
+  const query = await prisma.preferences.update({
+    where: {
+      userId: id,
+    },
+    data,
+  });
+
+  await redis.del(`${Constants.redis.cache.user.PREFERENCES}:${id}`);
+
+  return query;
 }
 
 export async function addNotificationToQueue(...payload: NotificationPayload[]) {
