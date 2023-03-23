fadaa4cfb65044f5dae464fc7418aaadeffc85b5
max <29782397+tekoh@users.noreply.github.com>
2023-03-23 21:16:12 +0000

    fix: remove stupid errors ik what im doing

--- src/utils/functions/random.ts
+++ src/utils/functions/random.ts
@@ -13,8 +13,6 @@ export function shuffle<T>(array: T[]): T[] {
 }
 
 export function percentChance(percent: number) {
-  if (percent >= 100) throw new TypeError("percent must be less than 100");
-  if (percent < 0.0001) throw new TypeError("cannot accurately create a chance less than 0.0001%");
   let max = 100;
 
   while (percent < 1 || Boolean(percent % 1)) {
