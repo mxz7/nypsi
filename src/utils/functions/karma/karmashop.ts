88390e34537e882658ece01ec4246679bc7b785e
max <29782397+tekoh@users.noreply.github.com>
2023-03-24 12:51:45 +0000

    feat: auto opening karma shop

--- src/utils/functions/karma/karmashop.ts
+++ src/utils/functions/karma/karmashop.ts
@@ -1,23 +1,119 @@
+import ms = require("ms");
+import redis from "../../../init/redis";
+import { NypsiClient } from "../../../models/Client";
 import { KarmaShopItem } from "../../../types/Karmashop";
+import Constants from "../../Constants";
+import { logger } from "../../logger";
+import { MStoTime } from "../date";
 
 declare function require(name: string): any;
 
-const items: { [key: string]: KarmaShopItem } = require("../../../../data/karmashop.json");
+let items: { [key: string]: KarmaShopItem } = require("../../../../data/karmashop.json");
 
-let karmaShop = false;
+async function createNextDate() {
+  const nextOpen = new Date(Date.now() + (Math.floor(Math.random() * ms("7 days")) + ms("7 days")));
 
-export function isKarmaShopOpen(): boolean {
-  return karmaShop;
+  await redis.set(Constants.redis.nypsi.KARMA_NEXT_OPEN, nextOpen.getTime());
 }
 
-export function openKarmaShop() {
-  karmaShop = true;
+export async function getNextKarmaShopOpen() {
+  if (!(await redis.exists(Constants.redis.nypsi.KARMA_NEXT_OPEN))) await createNextDate();
+
+  const ms = await redis.get(Constants.redis.nypsi.KARMA_NEXT_OPEN);
+
+  return new Date(parseInt(ms));
 }
 
-export function closeKarmaShop() {
-  karmaShop = false;
+export async function getLastKarmaShopOpen() {
+  const ms = await redis.get(Constants.redis.nypsi.KARMA_LAST_OPEN);
+
+  if (!ms) return new Date(0);
+  return new Date(parseInt(ms));
+}
+
+export async function isKarmaShopOpen() {
+  const v = await redis.get(Constants.redis.nypsi.KARMA_SHOP_OPEN);
+
+  if (typeof v === "string" && v === "t") return true;
+  return false;
+}
+
+export async function closeKarmaShop() {
+  await redis.del(Constants.redis.nypsi.KARMA_SHOP_OPEN);
+  restock();
+}
+
+function restock() {
+  items = require("../../../../data/karmashop.json");
 }
 
 export function getKarmaShopItems() {
   return items;
+}
+
+export async function openKarmaShop(client: NypsiClient, now = false) {
+  const open = async () => {
+    if (!now && (await getNextKarmaShopOpen()).getTime() > Date.now()) return;
+    restock();
+    await createNextDate();
+    await redis.set(Constants.redis.nypsi.KARMA_LAST_OPEN, Date.now());
+
+    await redis.set(Constants.redis.nypsi.KARMA_SHOP_OPEN, "t");
+    await redis.expire(Constants.redis.nypsi.KARMA_SHOP_OPEN, Math.floor(ms("1 hour") / 1000));
+
+    logger.info("karma shop has been opened");
+
+    const clusters = await client.cluster.broadcastEval(async (client) => {
+      const guild = await client.guilds.fetch("747056029795221513");
+
+      if (guild) return (client as unknown as NypsiClient).cluster.id;
+      return "not-found";
+    });
+
+    let cluster: number;
+
+    for (const i of clusters) {
+      if (i != "not-found") {
+        cluster = i;
+        break;
+      }
+    }
+
+    await client.cluster
+      .broadcastEval(
+        async (client, { content, cluster }) => {
+          if ((client as unknown as NypsiClient).cluster.id != cluster) return;
+          const guild = await client.guilds.fetch("747056029795221513");
+
+          if (!guild) return;
+
+          const channel = await guild.channels.fetch("747057465245564939");
+
+          if (!channel) return;
+
+          if (channel.isTextBased()) {
+            await channel.send({ content });
+          }
+        },
+        {
+          context: {
+            content: `🔮 <@&1088800175532806187> karma shop has been opened!! it will next open on <t:${Math.floor(
+              (await getNextKarmaShopOpen()).getTime() / 1000
+            )}>`,
+            cluster: cluster,
+          },
+        }
+      )
+      .then((res) => {
+        res.filter((i) => Boolean(i));
+        return res[0];
+      });
+  };
+
+  const nextOpen = (await getNextKarmaShopOpen()).getTime();
+
+  const needed = nextOpen - Date.now();
+
+  setTimeout(open, now ? 1000 : needed);
+  if (!now) logger.info(`::auto karma shop will open in ${MStoTime(needed, true)}`);
 }
