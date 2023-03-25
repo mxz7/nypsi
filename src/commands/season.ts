35981078c03a2d18a3d6067758cbe802811e1bf3
max <29782397+tekoh@users.noreply.github.com>
2023-03-25 00:44:58 +0000

    feat: add season info to constants

---
+++ src/commands/season.ts
@@ -0,0 +1,23 @@
+import { CommandInteraction, Message } from "discord.js";
+import { Command, NypsiCommandInteraction } from "../models/Command";
+import { CustomEmbed } from "../models/EmbedBuilders.js";
+import Constants from "../utils/Constants";
+
+const cmd = new Command("season", "view current season", "money");
+
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction)) {
+  return message.channel.send({
+    embeds: [
+      new CustomEmbed(
+        message.member,
+        `currently on season ${Constants.SEASON_NUMBER}\n\nstarted on <t:${Math.floor(
+          Constants.SEASON_START.getTime() / 1000
+        )}>`
+      ),
+    ],
+  });
+}
+
+cmd.setRun(run);
+
+module.exports = cmd;
