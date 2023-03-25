6eb4250e90852b0075da426be5840d95efadc1e8
max <29782397+tekoh@users.noreply.github.com>
2023-03-25 18:11:08 +0000

    feat: item sale history (#952)
    * feat: auction history worker
    
    * fix: aaa
    
    * feat: add totalsales
    
    * feat: show button
    
    * feat: caching
    
    * fix: aaa
    
    * feat: better formatting
    
    * fix: aa
    
    * feat: better formatting

--- src/types/Chart.ts
+++ src/types/Chart.ts
@@ -1,4 +1,18 @@
 export interface ChartData {
   type: string;
-  data: { labels: string[]; datasets: { label: string; data: number[]; fill?: boolean }[] };
+  data: { labels: string[]; datasets: { label: string; data: number[]; fill?: boolean; lineTension?: number }[] };
+  options?: {
+    elements?: {
+      point: {
+        pointStyle: "line";
+      };
+    };
+    plugins: {
+      tickFormat: {
+        style: "currency";
+        currency: "USD";
+        minimumFractionDigits: 0;
+      };
+    };
+  };
 }
