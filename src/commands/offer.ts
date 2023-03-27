93061b8374f1a267e7046215b21a04c4d7c219a1
max <29782397+tekoh@users.noreply.github.com>
2023-03-27 18:48:51 +0100

    feat: offer command

---
+++ src/commands/offer.ts
@@ -0,0 +1,377 @@
+import dayjs = require("dayjs");
+import {
+  ActionRowBuilder,
+  BaseMessageOptions,
+  ButtonBuilder,
+  ButtonStyle,
+  CommandInteraction,
+  GuildMember,
+  Interaction,
+  InteractionReplyOptions,
+  Message,
+  MessageActionRowComponentBuilder,
+} from "discord.js";
+import { NypsiClient } from "../models/Client";
+import { Command, NypsiCommandInteraction } from "../models/Command";
+import { CustomEmbed, ErrorEmbed } from "../models/EmbedBuilders";
+import { getBalance, updateBalance } from "../utils/functions/economy/balance";
+import { getInventory, selectItem } from "../utils/functions/economy/inventory";
+import {
+  createOffer,
+  deleteOffer,
+  getBlockedList,
+  getOwnedOffers,
+  getTargetedOffers,
+  setBlockedList,
+} from "../utils/functions/economy/offers";
+import { getPrestige } from "../utils/functions/economy/prestige";
+import { formatNumber, getItems, isEcoBanned } from "../utils/functions/economy/utils";
+import { getXp } from "../utils/functions/economy/xp";
+import { getMember } from "../utils/functions/member";
+import { getPreferences } from "../utils/functions/users/notifications";
+import { addCooldown, getResponse, onCooldown } from "../utils/handlers/cooldownhandler";
+
+const cmd = new Command("offer", "create and manage offers", "money").setAliases(["offers", "of"]);
+
+cmd.slashEnabled = true;
+cmd.slashData;
+
+async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
+  const send = async (data: BaseMessageOptions | InteractionReplyOptions) => {
+    if (!(message instanceof Message)) {
+      let usedNewMessage = false;
+      let res;
+
+      if (message.deferred) {
+        res = await message.editReply(data).catch(async () => {
+          usedNewMessage = true;
+          return await message.channel.send(data as BaseMessageOptions);
+        });
+      } else {
+        res = await message.reply(data as InteractionReplyOptions).catch(() => {
+          return message.editReply(data).catch(async () => {
+            usedNewMessage = true;
+            return await message.channel.send(data as BaseMessageOptions);
+          });
+        });
+      }
+
+      if (usedNewMessage && res instanceof Message) return res;
+
+      const replyMsg = await message.fetchReply();
+      if (replyMsg instanceof Message) {
+        return replyMsg;
+      }
+    } else {
+      return await message.channel.send(data as BaseMessageOptions);
+    }
+  };
+
+  if (await onCooldown(cmd.name, message.member)) {
+    const embed = await getResponse(cmd.name, message.member);
+
+    return send({ embeds: [embed], ephemeral: true });
+  }
+
+  await addCooldown(cmd.name, message.member, 7);
+
+  if (message.author.createdTimestamp > dayjs().subtract(1, "day").unix() * 1000) {
+    return send({
+      embeds: [new ErrorEmbed("you cannot use this command yet. u might be an alt. or a bot 😳")],
+    });
+  }
+
+  if ((await getPrestige(message.member)) < 1) {
+    if ((await getXp(message.member)) < 30) {
+      return send({
+        embeds: [new ErrorEmbed("you need at least 30xp before you can create an offer")],
+      });
+    }
+  }
+
+  const items = getItems();
+
+  const manageOffers = async (msg?: Message) => {
+    const offers = await getOwnedOffers(message.author.id);
+
+    const embed = new CustomEmbed(message.member).setHeader("your offers", message.author.avatarURL());
+
+    const row = new ActionRowBuilder<MessageActionRowComponentBuilder>();
+
+    let currentPage = 0;
+    const maxPage = offers.length - 1;
+
+    const displayOffer = (page: number) => {
+      embed.setFields(
+        {
+          name: "item",
+          value: `**${offers[page].itemAmount}x** ${items[offers[page].itemId].emoji} ${items[offers[page].itemId].name}`,
+          inline: true,
+        },
+        {
+          name: "cost",
+          value: `$**${offers[page].money.toLocaleString()}**`,
+          inline: true,
+        },
+        {
+          name: "target",
+          value: `<@${offers[page].targetId}> (${offers[page].targetId})`,
+          inline: true,
+        }
+      );
+      embed.setFooter({ text: `page ${page + 1}/${maxPage + 1}` });
+    };
+
+    const updateButtons = (page: number) => {
+      if (offers.length > 0) {
+        row.setComponents(
+          new ButtonBuilder().setCustomId("⬅").setLabel("back").setStyle(ButtonStyle.Primary),
+          new ButtonBuilder().setCustomId("➡").setLabel("next").setStyle(ButtonStyle.Primary),
+          new ButtonBuilder().setCustomId("del").setLabel("delete").setStyle(ButtonStyle.Danger)
+        );
+
+        if (offers.length == 1) {
+          row.components[0].setDisabled(true);
+          row.components[1].setDisabled(true);
+        } else {
+          if (page === 0) {
+            row.components[0].setDisabled(true);
+          } else if (page === offers.length - 1) {
+            row.components[1].setDisabled(true);
+          }
+        }
+      }
+    };
+
+    if (offers.length == 0) {
+      embed.setDescription("you don't have any active offers");
+    } else if (offers.length > 1) {
+      displayOffer(0);
+    } else {
+      row.addComponents(new ButtonBuilder().setCustomId("del").setLabel("delete").setStyle(ButtonStyle.Danger));
+      displayOffer(0);
+    }
+
+    updateButtons(0);
+
+    if (msg) {
+      msg = await msg.edit({ embeds: [embed], components: [row] });
+    } else {
+      msg = await send({ embeds: [embed], components: [row] });
+    }
+
+    const filter = (i: Interaction) => i.user.id == message.author.id;
+
+    const pageManager: any = async () => {
+      let fail = false;
+
+      const response = await msg
+        .awaitMessageComponent({ filter, time: 60000 })
+        .then(async (collected) => {
+          await collected.deferUpdate().catch(() => {
+            fail = true;
+            return pageManager();
+          });
+          return { res: collected.customId, interaction: collected };
+        })
+        .catch(async () => {
+          fail = true;
+          await msg.edit({ embeds: [embed], components: [] });
+        });
+
+      if (fail) return;
+      if (!response) return;
+
+      const { res, interaction } = response;
+
+      if (res == "⬅") {
+        if (currentPage == 0) {
+          return pageManager();
+        }
+
+        currentPage--;
+
+        displayOffer(currentPage);
+
+        updateButtons(currentPage);
+
+        await msg.edit({ embeds: [embed], components: [row] });
+        return pageManager();
+      } else if (res == "➡") {
+        if (currentPage == maxPage) {
+          return pageManager();
+        }
+
+        currentPage++;
+
+        displayOffer(currentPage);
+        updateButtons(currentPage);
+
+        await msg.edit({ embeds: [embed], components: [row] });
+        return pageManager();
+      } else if (res == "del") {
+        const res = await deleteOffer(offers[currentPage], message.client as NypsiClient).catch(() => {});
+
+        if (res) {
+          await interaction.followUp({
+            embeds: [new CustomEmbed(message.member, "✅ your offer has been deleted")],
+            ephemeral: true,
+          });
+        } else {
+          await interaction.followUp({
+            embeds: [new CustomEmbed(message.member, "failed to delete that offer")],
+            ephemeral: true,
+          });
+        }
+
+        return manageOffers(msg);
+      }
+    };
+
+    return pageManager();
+  };
+
+  if (args[0].toLowerCase() === "create") args.shift();
+
+  if (args.length == 0 || args[0].toLowerCase() == "manage") {
+    return manageOffers();
+  } else if (args[0].toLowerCase() == "block") {
+    let current = await getBlockedList(message.author.id);
+    const max = 100;
+
+    const items = getItems();
+
+    if (args.length == 1) {
+      if (current.length == 0) {
+        return send({ embeds: [new CustomEmbed(message.member, "you are have no items or people blocked")] });
+      }
+
+      return send({
+        embeds: [
+          new CustomEmbed(
+            message.member,
+            `blocklist: \n\n${current
+              .map((i) => {
+                if (items[i]) return `${items[i].emoji} ${items[i].name}`;
+                if (message.guild.members.cache.has(i)) return `\`${i}\` (${message.guild.members.cache.get(i).toString()})`;
+                return `\`${i}\``;
+              })
+              .join("\n")}`
+          ).setHeader("offer blocklist", message.author.avatarURL()),
+        ],
+      });
+    }
+
+    const searchTag = args[1].toLowerCase();
+
+    let desc = "";
+
+    if (current.includes(searchTag)) {
+      desc = `✅ removed \`${searchTag}\``;
+
+      current.splice(current.indexOf(searchTag), 1);
+
+      current = await setBlockedList(message.author.id, current);
+    } else {
+      if (current.length >= max) {
+        return send({ embeds: [new ErrorEmbed("you have reached the limit of your blocklist")] });
+      }
+
+      desc = `✅ added \`${searchTag}\``;
+
+      current.push(searchTag);
+      current = await setBlockedList(message.author.id, current);
+    }
+
+    const embed = new CustomEmbed(message.member, desc).setHeader("offer blocklist", message.author.avatarURL());
+
+    if (current.length > 0) {
+      embed.addField(
+        "blocklist:",
+        current
+          .map((i) => {
+            if (items[i]) return `${items[i].emoji} ${items[i].name}`;
+            if (message.guild.members.cache.has(i)) return `\`${i}\` (${message.guild.members.cache.get(i).toString()})`;
+            return `\`${i}\``;
+          })
+          .join("\n")
+      );
+    }
+
+    return send({ embeds: [embed] });
+  } else {
+    if (args.length != 4) return send({ embeds: [new ErrorEmbed("/offer create <target> <item> <amount> <money>")] });
+    let target: GuildMember;
+
+    if (!message.mentions.members.first()) {
+      target = await getMember(message.guild, args[0].toLowerCase());
+    } else {
+      target = message.mentions.members.first();
+    }
+
+    if (!target) {
+      return message.channel.send({ embeds: [new ErrorEmbed("invalid user")] });
+    }
+
+    if (await isEcoBanned(target.user.id))
+      return send({ embeds: [new ErrorEmbed("theyre banned lol xd be mean to them for me ty")] });
+
+    if ((await getPreferences(target)).offers <= (await getTargetedOffers(target.user.id)).length) {
+      if ((await getPreferences(target)).offers === 0)
+        return send({ embeds: [new ErrorEmbed(`${target.toString()} has disabled offers`)] });
+      return send({
+        embeds: [
+          new ErrorEmbed(
+            `${target.toString()} has already received their max amount of offers (${(await getPreferences(target)).offers})`
+          ),
+        ],
+      });
+    }
+
+    const selected = selectItem(args[1].toLowerCase());
+
+    if (!selected) return send({ embeds: [new ErrorEmbed("invalid item")] });
+
+    const blocked = await getBlockedList(target.user.id);
+
+    if (blocked.includes(selected.id))
+      return send({
+        embeds: [new ErrorEmbed(`${target.toString()} has blocked offers for ${selected.emoji} ${selected.name}`)],
+      });
+
+    if (blocked.includes(message.author.id))
+      return send({ embeds: [new ErrorEmbed(`${target.toString()} has blocked offers from you`)] });
+
+    const amount = parseInt(args[2]);
+
+    if (!amount || isNaN(amount) || amount < 1) return send({ embeds: [new ErrorEmbed("invalid amount")] });
+
+    const inventory = await getInventory(target);
+
+    if (!inventory.find((i) => i.item === selected.id) || inventory.find((i) => i.item === selected.id).amount < amount)
+      return send({
+        embeds: [new ErrorEmbed(`${target.toString()} doesnt have ${amount}x ${selected.emoji} ${selected.name}`)],
+      });
+
+    const money = formatNumber(args[3]);
+
+    if (!money || money < 1 || isNaN(money)) return send({ embeds: [new ErrorEmbed("invalid amount")] });
+
+    const balance = await getBalance(message.member);
+
+    if (balance < money) return send({ embeds: [new ErrorEmbed("you cant afford this")] });
+    await updateBalance(message.member, balance - money);
+    const res = await createOffer(target.user, selected.id, amount, money, message.member);
+
+    if (!res) {
+      await updateBalance(message.member, balance + money);
+      return send({ embeds: [new ErrorEmbed("failed to create offer")] });
+    } else {
+      return send({ embeds: [new CustomEmbed(message.member, `✅ offer has been sent to ${target.toString()}`)] });
+    }
+  }
+}
+
+cmd.setRun(run);
+
+module.exports = cmd;
