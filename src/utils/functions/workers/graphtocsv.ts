220b08e5fb960db4cf428d2a52ed6253cd6d3423
max <29782397+tekoh@users.noreply.github.com>
2023-03-28 15:19:46 +0100

    fix: ugh stupid types

--- src/utils/functions/workers/graphtocsv.ts
+++ src/utils/functions/workers/graphtocsv.ts
@@ -18,7 +18,7 @@ export default function graphToCsv(): Promise<void> {
 if (!isMainThread) {
   process.title = "nypsi: graph to csv";
 
-  const toCsv = async (fileName: string, data: { userId: string; value: bigint | number; date: Date; id: number }[]) => {
+  const toCsv = async (fileName: string, data: { userId: string; value: bigint | number; date: Date; id: string }[]) => {
     const dates: number[] = [];
     const map = new Map<string, { value: number; date: number }[]>();
 
