d0d67a24504082f087af599c46877d50c0b8c4e5
max <29782397+tekoh@users.noreply.github.com>
2023-04-01 13:18:37 +0100

    fix: update boosters link

--- src/utils/functions/premium/boosters.ts
+++ src/utils/functions/premium/boosters.ts
@@ -59,7 +59,7 @@ export async function setBooster(userId: string, value: boolean): Promise<void> 
       payload: {
         embed: new CustomEmbed(
           null,
-          "thank you for boosting the nypsi server, you can see your rewards [here](https://discord.com/channels/747056029795221513/1031950370206924903/1089545304099016765)"
+          "thank you for boosting the nypsi server, you can see your rewards [here](https://discord.com/channels/747056029795221513/1031950370206924903/1091698012817985676)"
         )
           .setColor(Constants.EMBED_SUCCESS_COLOR)
           .setHeader("thank you for supporting nypsi!!"),
