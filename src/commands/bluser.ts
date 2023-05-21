6fdaa5463673e0f647d5d62bf906b492bac9fe38
max <29782397+mxz7@users.noreply.github.com>
2023-05-21 01:15:21 +0000

    style: formatting

--- src/commands/bluser.ts
+++ src/commands/bluser.ts
@@ -8,7 +8,7 @@ const cmd = new Command("bluser", "blacklist account from nypsi", "none").setPer
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
   if ((await getAdminLevel(message.author.id)) < 3) return;
 
-  if (args.length === 0) return message.channel.send({ content: "are you stupid or some shit lol lol ol ol ol ol"});
+  if (args.length === 0) return message.channel.send({ content: "are you stupid or some shit lol lol ol ol ol ol" });
 
   if (await isUserBlacklisted(args[0])) {
     await setUserBlacklist(args[0], false);
