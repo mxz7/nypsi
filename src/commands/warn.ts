5c2513ff455ceb33688346fce410e0d252efc044
max <29782397+tekoh@users.noreply.github.com>
2023-05-28 19:36:25 +0100

    feat: modernise moderation (#1056)
    * fix: prevent dupes
    
    * feat: update ban command
    
    * fix: oooops
    
    * feat: banned commadn
    
    * fix: ugh
    
    * fix: owner
    
    * feat: modernise mute cmd
    
    * feat: modernise kick command
    
    * feat: modernise warn command
    
    * 6.6.10

--- src/commands/warn.ts
+++ src/commands/warn.ts
@@ -1,14 +1,7 @@
-import {
-  BaseMessageOptions,
-  CommandInteraction,
-  InteractionReplyOptions,
-  Message,
-  PermissionFlagsBits,
-  User,
-} from "discord.js";
+import { BaseMessageOptions, CommandInteraction, InteractionReplyOptions, Message, PermissionFlagsBits } from "discord.js";
 import { Command, NypsiCommandInteraction } from "../models/Command";
 import { CustomEmbed, ErrorEmbed } from "../models/EmbedBuilders.js";
-import { addCooldown, getPrefix, inCooldown } from "../utils/functions/guilds/utils";
+import { getPrefix } from "../utils/functions/guilds/utils";
 import { getExactMember } from "../utils/functions/member";
 import { newCase } from "../utils/functions/moderation/cases";
 import { createProfile, profileExists } from "../utils/functions/moderation/utils";
@@ -60,11 +53,11 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
   if (args.length == 0 || !args[0]) {
     const embed = new CustomEmbed(message.member)
       .setHeader("warn help")
-      .addField("usage", `${prefix}warn <@user(s)> (reason) [-s`)
+      .addField("usage", `${prefix}warn <user> (reason) [-s`)
       .addField(
         "help",
         "**<>** required | **()** optional | **[]** parameter\n" +
-          "**<@users>** you can warn one or more members in one command (must tag them)\n" +
+          "**<user>** can tag them or use their username\n" +
           "**(reason)** reason for the warn, will be given to all warned members\n" +
           "**[-s]** if used, command message will be deleted and the output will be sent to moderator as a DM if possible\n\n" +
           "if the bot was unable to DM a user on warn, the warning will still be logged"
@@ -74,103 +67,40 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     return send({ embeds: [embed] });
   }
 
-  if ((await message.guild.members.fetch(args[0]).catch(() => {})) && message.mentions.members.first() == null) {
-    let members;
+  const target = await getExactMember(message.guild, args[0]);
 
-    if (inCooldown(message.guild)) {
-      members = message.guild.members.cache;
-    } else {
-      members = await message.guild.members.fetch();
-      addCooldown(message.guild, 3600);
-    }
-
-    const member = members.find((m) => m.id == args[0]);
-
-    if (!member) {
-      return send({
-        embeds: [new ErrorEmbed("unable to find member with ID `" + args[0] + "`")],
-      });
-    }
-
-    message.mentions.members.set(member.user.id, member);
-  } else if (message.mentions.members.first() == null) {
-    const member = await getExactMember(message.guild, args[0]);
-
-    if (!member) {
-      return send({ embeds: [new ErrorEmbed("unable to find member `" + args[0] + "`")] });
-    }
-
-    message.mentions.members.set(member.user.id, member);
-  }
-
-  const members = message.mentions.members;
   let reason;
 
-  if (args.length != members.size) {
-    for (let i = 0; i < members.size; i++) {
-      args.shift();
-    }
-    reason = args.join(" ");
+  if (args.length > 1) {
+    reason = args.slice(1).join(" ");
   } else {
     return send({ embeds: [new ErrorEmbed("you must include a warn reason")] });
   }
 
-  let count = 0;
-  const failed: User[] = [];
-  const error: User[] = [];
-
-  for (const member of members.keys()) {
-    if (members.get(member).user.id == message.client.user.id) {
-      await send({ content: "wow... 😢" });
-      continue;
-    }
-
-    const targetHighestRole = members.get(member).roles.highest;
-    const memberHighestRole = message.member.roles.highest;
-
-    if (targetHighestRole.position >= memberHighestRole.position && message.guild.ownerId != message.member.user.id) {
-      failed.push(members.get(member).user);
-    } else {
-      const embed = new CustomEmbed(members.get(member))
-        .setTitle(`warned in ${message.guild.name}`)
-        .addField("reason", `\`${reason}\``);
-
-      await members
-        .get(member)
-        .send({ content: `you have been warned in ${message.guild.name}`, embeds: [embed] })
-        .catch(() => {
-          error.push(members.get(member).user);
-        });
-      count++;
-    }
+  if (target.user.id == message.client.user.id) {
+    await send({ content: "wow... 😢" });
+    return;
   }
 
-  if (count == 0) {
-    return send({ embeds: [new ErrorEmbed("i was unable to warn any users")] });
+  const targetHighestRole = target.roles.highest;
+  const memberHighestRole = message.member.roles.highest;
+
+  let dmFail = false;
+
+  if (targetHighestRole.position >= memberHighestRole.position && message.guild.ownerId != message.member.user.id) {
+    return send({ embeds: [new ErrorEmbed(`your role is not high enough to punish ${target.toString()}`)] });
+  } else {
+    const embed = new CustomEmbed(target).setTitle(`warned in ${message.guild.name}`).addField("reason", `\`${reason}\``);
+
+    await target.send({ content: `you have been warned in ${message.guild.name}`, embeds: [embed] }).catch(() => {
+      dmFail = true;
+    });
   }
 
-  const embed = new CustomEmbed(message.member, "✅ **" + count + "** members warned for: " + reason);
+  const embed = new CustomEmbed(message.member, `✅ \`${target.user.tag}\` has been warned for **${reason}**`);
 
-  if (count == 1 && failed.length == 0) {
-    embed.setDescription("✅ `" + members.first().user.tag + "` has been warned for: " + reason);
-  }
-
-  if (failed.length != 0) {
-    const failedTags = [];
-    for (const fail of failed) {
-      failedTags.push(fail.tag);
-    }
-
-    embed.addField("error", "unable to warn: " + failedTags.join(", "));
-  }
-
-  if (error.length != 0) {
-    const errorTags = [];
-    for (const err of error) {
-      errorTags.push(err.tag);
-    }
-
-    embed.addField("warning", "unable to DM: " + errorTags.join(", "));
+  if (dmFail) {
+    embed.setDescription(`⚠️ failed to send message to \`${target.user.tag}\`. warn has still been logged`);
   }
 
   if (args.join(" ").includes("-s")) {
@@ -184,17 +114,7 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     await send({ embeds: [embed] });
   }
 
-  const members1 = Array.from(members.keys());
-
-  if (failed.length != 0) {
-    for (const fail of failed) {
-      if (members1.includes(fail.id)) {
-        members1.splice(members1.indexOf(fail.id), 1);
-      }
-    }
-  }
-
-  await newCase(message.guild, "warn", members1, message.author.tag, reason);
+  await newCase(message.guild, "warn", target.user.id, message.author.tag, reason);
 }
 
 cmd.setRun(run);
