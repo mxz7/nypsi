95a8c3c29fa2fbea2eb9fedd65a32f8ba5a3a906
max <29782397+tekoh@users.noreply.github.com>
2023-05-28 18:53:53 +0100

    feat: update ban command

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -8,7 +8,7 @@ import {
 } from "discord.js";
 import { Command, NypsiCommandInteraction } from "../models/Command";
 import { CustomEmbed, ErrorEmbed } from "../models/EmbedBuilders.js";
-import { getPrefix } from "../utils/functions/guilds/utils";
+import Constants from "../utils/Constants";
 import { getExactMember } from "../utils/functions/member";
 import { newBan } from "../utils/functions/moderation/ban";
 import { newCase } from "../utils/functions/moderation/cases";
@@ -19,7 +19,7 @@ const cmd = new Command("ban", "ban one or more users from the server", "moderat
 cmd.slashEnabled = true;
 cmd.slashData
   .addUserOption((option) => option.setName("user").setDescription("member to ban from the server").setRequired(true))
-  .addStringOption((option) => option.setName("reason").setDescription("reason for the ban").setRequired(true));
+  .addStringOption((option) => option.setName("reason").setDescription("reason for the ban").setRequired(false));
 
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) {
   const send = async (data: BaseMessageOptions | InteractionReplyOptions) => {
@@ -67,64 +67,42 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
   if (!(await profileExists(message.guild))) await createProfile(message.guild);
 
-  let idOnly = false;
-  let idUser: string;
-  let id: string;
-
-  const prefix = await getPrefix(message.guild);
-
   if (args.length == 0 || !args[0]) {
     const embed = new CustomEmbed(message.member)
       .setHeader("ban help")
-      .addField("usage", `${prefix}ban <@user(s)> (reason) [-s]`)
+      .addField("usage", "/ban <user> (reason) [-s]")
       .addField(
         "help",
         "**<>** required | **()** optional | **[]** parameter\n" +
-          "**<@users>** you can ban one or more members in one command (must tag them)\n" +
+          "**<user>** you can either tag the user or use their username\n" +
           "**(reason)** reason for the ban, will be given to all banned members\n" +
           "**[-s]** if used, command message will be deleted and the output will be sent to moderator as a DM if possible"
-      )
-      .addField(
-        "examples",
-        `${prefix}ban @member hacking\n${prefix}ban @member @member2 @member3 hacking\n${prefix}ban @member hacking -s\n${prefix}ban @member 1d annoying`
       );
 
     return send({ embeds: [embed] });
   }
 
-  if ((await message.guild.members.fetch(args[0]).catch(() => {})) && message.mentions.members.first() == null) {
-    const member = await message.guild.members.fetch(args[0]).catch(() => {});
+  const target = await getExactMember(message.guild, args[0]);
+  let mode: "target" | "id" = "target";
+  let userId: string;
 
-    if (!member) {
-      idOnly = true;
-
-      id = args.shift();
-    } else {
-      message.mentions.members.set(member.user.id, member);
+  if (!target) {
+    if (args[0].match(Constants.SNOWFLAKE_REGEX)) {
+      mode = "id";
+      userId = args[0];
     }
-  } else if (message.mentions.members.first() == null) {
-    const member = await getExactMember(message.guild, args[0]);
-
-    if (!member) {
-      return send({ embeds: [new ErrorEmbed("unable to find member `" + args[0] + "`")] });
-    }
-
-    message.mentions.members.set(member.user.id, member);
   }
 
-  const members = message.mentions.members;
-  let reason = message.member.user.tag + ": ";
+  console.log(mode);
+
+  let reason = message.author.tag + ": ";
   let unbanDate: Date;
   let temporary = false;
   let duration;
 
-  if (args.length != members.size && args.length != 0) {
-    for (let i = 0; i < members.size; i++) {
-      args.shift();
-    }
-
+  if (args.length > 1) {
     try {
-      duration = getDuration(args[0].toLowerCase());
+      duration = getDuration(args[1].toLowerCase());
       unbanDate = new Date(Date.now() + duration * 1000);
     } catch {
       // eslint happy
@@ -135,18 +113,17 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
       args.shift();
     }
 
-    reason = reason + args.join(" ");
+    reason = reason + args.slice(1);
   } else {
     reason = reason + "no reason given";
   }
 
-  let count = 0;
-  const failed = [];
   let fail = false;
+  let idUser: string;
 
-  if (idOnly) {
+  if (mode === "id") {
     await message.guild.members
-      .ban(id, {
+      .ban(userId, {
         reason: reason,
       })
       .then((banned) => {
@@ -157,92 +134,61 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
         } else {
           idUser = `${banned.user.tag}`;
         }
-        count++;
       })
       .catch(() => {
-        if (idOnly) {
-          fail = true;
-          return send({
-            embeds: [new ErrorEmbed(`unable to ban the id: \`${id}\``)],
-          });
-        }
-        failed.push(id);
+        fail = true;
+        return send({
+          embeds: [new ErrorEmbed(`failed to ban: \`${userId}\``)],
+        });
       });
   } else {
-    for (const member of members.keys()) {
-      const targetHighestRole = members.get(member).roles.highest;
-      const memberHighestRole = message.member.roles.highest;
+    const targetHighestRole = target.roles.highest;
+    const memberHighestRole = message.member.roles.highest;
 
-      if (targetHighestRole.position >= memberHighestRole.position && message.guild.ownerId != message.member.user.id) {
-        failed.push(members.get(member).user);
-        continue;
-      }
-
-      if (members.get(member).user.id == message.client.user.id) {
-        await send({ content: "NICE TRY LOSER CANT KICK THE BEST WORLDWIDE" });
-        return;
-      }
-
-      await message.guild.members
-        .ban(member, {
-          reason: reason,
-        })
-        .then(() => {
-          count++;
-        })
-        .catch(() => {
-          if (idOnly) {
-            fail = true;
-            return send({
-              embeds: [new ErrorEmbed(`unable to ban the id: \`${member}\``)],
-            });
-          }
-          failed.push(members.get(member).user);
-        });
+    if (targetHighestRole >= memberHighestRole) {
+      return send({ embeds: [new ErrorEmbed(`your role is not high enough to punish ${target.toString()}`)] });
     }
+
+    if (target.user.id == message.client.user.id) {
+      await send({ content: "NICE TRY LOSER CANT BAN THE BEST BOT" });
+      return;
+    }
+
+    // await message.guild.members
+    //   .ban(target, {
+    //     reason: reason,
+    //   })
+    //   .catch(() => {
+    //     fail = true;
+    //     return send({
+    //       embeds: [new ErrorEmbed(`unable to ban ${target.toString()}`)],
+    //     });
+    //   });
   }
 
   if (fail) return;
 
-  if (count == 0) {
-    return send({ embeds: [new ErrorEmbed("i was unable to ban any users")] });
-  }
-
   let banLength = "";
 
-  const embed = new CustomEmbed(message.member).setDescription("✅ **" + count + "** members banned");
+  if (temporary) banLength = getTime(duration * 1000);
+
+  const embed = new CustomEmbed(message.member);
+
+  let msg = "";
+
+  if (mode === "id") {
+    msg = `✅ \`${idUser}\` has been banned`;
+  } else {
+    msg = `✅ \`${target.user.tag}\` has been banned`;
+  }
 
   if (temporary) {
-    banLength = getTime(duration * 1000);
-    embed.setDescription(`✅ **${count}** members banned for **${banLength}**`);
-  } else {
-    embed.setDescription(`✅ **${count}** members banned`);
+    msg += ` for **${banLength}**`;
+  } else if (reason.split(": ")[1] !== "no reason given") {
+    msg += ` for **${reason.split(": ")[1]}**`;
   }
 
-  if (count == 1 && failed.length == 0) {
-    if (idOnly) {
-      if (temporary) {
-        embed.setDescription(`✅ \`${idUser}\` has been banned for **${banLength}**`);
-      } else {
-        embed.setDescription(`✅ \`${idUser}\` has been banned`);
-      }
-    } else {
-      if (temporary) {
-        embed.setDescription(`✅ \`${members.first().user.tag}\` has been banned for **${banLength}**`);
-      } else {
-        embed.setDescription("✅ `" + members.first().user.tag + "` has been banned");
-      }
-    }
-  }
-
-  if (failed.length != 0) {
-    const failedTags = [];
-    for (const fail1 of failed) {
-      failedTags.push(fail1.tag);
-    }
-
-    embed.addField("error", "unable to ban: " + failedTags.join(", "));
-  }
+  embed.setDescription(msg);
 
   if (args.join(" ").includes("-s")) {
     if (message instanceof Message) {
@@ -262,51 +208,38 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
     storeReason = `[perm] ${storeReason}`;
   }
 
-  if (idOnly) {
-    await newCase(message.guild, "ban", id, message.member.user.tag, storeReason);
+  if (mode === "id") {
+    await newCase(message.guild, "ban", userId, message.member.user.tag, storeReason);
     if (temporary) {
-      await newBan(message.guild, id, unbanDate);
+      await newBan(message.guild, userId, unbanDate);
     }
   } else {
-    const members1 = Array.from(members.keys());
-
-    if (failed.length != 0) {
-      for (const fail of failed) {
-        if (members1.includes(fail.id)) {
-          members1.splice(members1.indexOf(fail.id), 1);
-        }
-      }
-    }
-
-    await newCase(message.guild, "ban", members1, message.author.tag, reason.split(": ")[1]);
+    await newCase(message.guild, "ban", target.user.id, message.author.tag, reason.split(": ")[1]);
 
     if (temporary) {
-      await newBan(message.guild, members1, unbanDate);
+      await newBan(message.guild, target.user.id, unbanDate);
     }
 
     if (args.join(" ").includes("-s")) return;
-    for (const member of members1) {
-      const m = members.get(member);
 
-      if (reason.split(": ")[1] == "no reason given") {
-        await m
-          .send({
-            content: `you have been banned from ${message.guild.name}${temporary ? `\n\nexpires in **${banLength}**}` : ""}`,
-          })
-          .catch(() => {});
-      } else {
-        const embed = new CustomEmbed(m)
-          .setTitle(`banned from ${message.guild.name}`)
-          .addField("reason", `\`${reason.split(": ")[1]}\``, true);
+    if (reason.split(": ")[1] == "no reason given") {
+      await target
+        .send({
+          content: `you have been banned from ${message.guild.name}${temporary ? `\n\nexpires in **${banLength}**}` : ""}`,
+        })
+        .catch(() => {});
+    } else {
+      const embed = new CustomEmbed(target)
+        .setTitle(`banned from ${message.guild.name}`)
+        .addField("reason", `\`${reason.split(": ")[1]}\``, true);
 
-        if (temporary) {
-          embed.addField("length", `\`${banLength}\``, true);
-          embed.setFooter({ text: "unbanned at:" });
-          embed.setTimestamp(unbanDate);
-        }
-
-        await m.send({ content: `you have been banned from ${message.guild.name}`, embeds: [embed] }).catch(() => {});
+      if (temporary) {
+        embed.addField("length", `\`${banLength}\``, true);
+        embed.setFooter({ text: "unbanned at:" });
+        embed.setTimestamp(unbanDate);
       }
+
+      await target.send({ content: `you have been banned from ${message.guild.name}`, embeds: [embed] }).catch(() => {});
     }
   }
 }
