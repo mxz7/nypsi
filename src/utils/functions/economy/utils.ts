7b1189e16a68cf9ec1df0282e571924476996f31
max <29782397+tekoh@users.noreply.github.com>
2023-05-31 17:33:14 +0100

    fix: delete old graph metrics

--- src/utils/functions/economy/utils.ts
+++ src/utils/functions/economy/utils.ts
@@ -310,6 +310,7 @@ export async function reset() {
   await prisma.inventory.deleteMany();
   await prisma.crafting.deleteMany();
   await prisma.bakeryUpgrade.deleteMany();
+  await prisma.graphMetrics.deleteMany({ where: { OR: [{ category: "networth" }, { category: "balance" }] } });
 
   await prisma.economy.deleteMany({
     where: {
