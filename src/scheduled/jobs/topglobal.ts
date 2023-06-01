1e6f044fe0517ce2bb2fd06cf894962cdff32967
max <29782397+tekoh@users.noreply.github.com>
2023-06-01 01:02:01 +0100

    fix: sorry

--- src/scheduled/jobs/topglobal.ts
+++ src/scheduled/jobs/topglobal.ts
@@ -69,9 +69,11 @@ async function topAmountGlobal(amount: number): Promise<string[]> {
     }
 
     usersFinal[count] =
-      pos + " **" + user.user?.Preferences?.leaderboards
-        ? user.user.lastKnownTag.split("#")[0]
-        : "[hidden]" + "** $" + user.money.toLocaleString();
+      pos +
+      " **" +
+      (user.user?.Preferences?.leaderboards ? user.user.lastKnownTag.split("#")[0] : "[hidden]") +
+      "** $" +
+      user.money.toLocaleString();
     count++;
   }
   return usersFinal;
