fb3cf93bb6c3a5cfdf87faa91786a92a5435ba59
max <29782397+tekoh@users.noreply.github.com>
2023-07-04 13:06:56 +0100

    feat: use redis for mentions queue (#1107)
    * feat: mention queue redis key
    
    * feat: use redis for mention queue
    
    * feat: debug info
    
    * fix: encrypt
    
    * fix: inline
    
    * fix: use a proper id for mentions (will change to uuid after update)
    
    * feat: it works !
    
    * 6.7.5

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import Constants from "./utils/Constants";
 import { addFailedHeatbeat, sendHeartbeat } from "./utils/functions/heartbeat";
 import { updateStats } from "./utils/functions/topgg";
 import { getVersion } from "./utils/functions/version";
+import { startMentionInterval } from "./utils/handlers/mentions";
 import { listenForDms } from "./utils/handlers/notificationhandler";
 import { listen } from "./utils/handlers/webhookhandler";
 import { getWebhooks, logger, setClusterId } from "./utils/logger";
@@ -110,9 +111,10 @@ listen(manager);
 setTimeout(async () => {
   await startJobs();
   listenForDms(manager);
+  startMentionInterval();
   logger.info("jobs triggered");
-}, 300000);
-// }, 15000);
+  // }, 300000);
+}, 15000);
 
 setTimeout(async () => {
   const userId = await manager.fetchClientValues("user.id");
