85e5b53b772c7e5c3ff9075f3af5fe28ebb5d15f
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2023-07-20 14:05:28 -0400

    feat: make fun of ppl using $support in nypsi server

--- src/commands/support.ts
+++ src/commands/support.ts
@@ -1,9 +1,12 @@
 import { CommandInteraction, Message } from "discord.js";
 import { Command, NypsiCommandInteraction } from "../models/Command";
+import { ErrorEmbed } from "../models/EmbedBuilders";
+import Constants from "../utils/Constants";
 
 const cmd = new Command("support", "join the nypsi support server", "info").setAliases(["discord"]);
 
 async function run(message: Message | (NypsiCommandInteraction & CommandInteraction)) {
+  if (message.guildId == Constants.NYPSI_SERVER_ID) return message.channel.send({ embeds: [new ErrorEmbed(`this is the support server dumbass`)] });
   return message.channel.send({ content: "discord.gg/hJTDNST" });
 }
 
