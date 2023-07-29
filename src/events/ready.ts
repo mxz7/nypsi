c10a09a1781e631418adb6ced34b57f9ea010c0e
max <29782397+tekoh@users.noreply.github.com>
2023-07-29 18:44:08 +0100

    fix: better presence handling

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,20 +1,16 @@
 import { Client } from "discord.js";
-import { getCustomPresence, randomPresence } from "../utils/functions/presence";
+import { getCustomPresence, randomPresence, setCustomPresence } from "../utils/functions/presence";
 
-export default function ready(client: Client) {
-  setTimeout(async () => {
-    if (await getCustomPresence()) return;
-    const presence = randomPresence();
-
-    client.user.setPresence({
-      status: "dnd",
-      activities: [
-        {
-          name: presence,
-        },
-      ],
-    });
-  }, 15000);
+export default async function ready(client: Client) {
+  await setCustomPresence();
+  client.user.setPresence({
+    status: "dnd",
+    activities: [
+      {
+        name: "nypsi.xyz",
+      },
+    ],
+  });
 
   setInterval(
     async () => {
