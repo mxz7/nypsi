be34575a99669f71bc05e779ace7b9cdf9d118de
max <29782397+tekoh@users.noreply.github.com>
2023-08-03 19:04:24 +0100

    Revert "refactor: use default caching to see how that goes?"
    This reverts commit 5af5904ea21a79e07c1773d21d26f2c7b79ef22e.

--- src/nypsi.ts
+++ src/nypsi.ts
@@ -1,58 +1,58 @@
 import { getInfo } from "discord-hybrid-sharding";
-import { GatewayIntentBits, Partials } from "discord.js";
+import { GatewayIntentBits, Options, Partials } from "discord.js";
 import { NypsiClient } from "./models/Client";
-import { loadCommands } from "./utils/handlers/commandhandler";
-import { loadInteractions } from "./utils/handlers/interactions";
-import { logger } from "./utils/logger";
-import ms = require("ms");
 
 const client = new NypsiClient({
   allowedMentions: {
     parse: ["users", "roles"],
   },
-  // sweepers: {
-  //   messages: {
-  //     interval: 3600,
-  //     lifetime: 900,
-  //     filter: () => (msg) =>
-  //       msg.author.id === msg.client.user.id &&
-  //       (msg.editedTimestamp || msg.createdTimestamp) > Date.now() - ms("10 minutes"),
-  //   },
-  //   guildMembers: {
-  //     interval: 3600,
-  //     filter: () => (member) => member.id !== member.client.user.id,
-  //   },
-  // },
-  // makeCache: Options.cacheWithLimits({
-  //   ApplicationCommandManager: 0,
-  //   BaseGuildEmojiManager: 0,
-  //   GuildBanManager: 0,
-  //   GuildInviteManager: 0,
-  //   GuildStickerManager: 0,
-  //   GuildScheduledEventManager: 0,
-  //   MessageManager: {
-  //     maxSize: 25,
-  //     keepOverLimit: (msg) =>
-  //       (msg.author.id === msg.client.user.id &&
-  //         (msg.editedTimestamp || msg.createdTimestamp) > Date.now() - ms("10 minutes")) ||
-  //       msg.createdTimestamp > Date.now() - ms("5 minutes"),
-  //   },
-  //   PresenceManager: 0,
-  //   ReactionManager: 0,
-  //   ReactionUserManager: 0,
-  //   StageInstanceManager: 0,
-  //   ThreadManager: 0,
-  //   ThreadMemberManager: 0,
-  //   VoiceStateManager: 0,
-  //   GuildEmojiManager: 0,
-  //   AutoModerationRuleManager: 0,
-  //   GuildForumThreadManager: 0,
-  //   GuildTextThreadManager: 0,
-  //   UserManager: {
-  //     maxSize: 25_000,
-  //     keepOverLimit: (user) => user.id === user.client.user.id,
-  //   },
-  // }),
+  sweepers: {
+    messages: {
+      interval: 3600,
+      lifetime: 900,
+      filter: () => (msg) =>
+        msg.author.id === msg.client.user.id &&
+        (msg.editedTimestamp || msg.createdTimestamp) > Date.now() - ms("10 minutes"),
+    },
+    guildMembers: {
+      interval: 3600,
+      filter: () => (member) => member.id !== member.client.user.id,
+    },
+  },
+  makeCache: Options.cacheWithLimits({
+    ApplicationCommandManager: 0,
+    BaseGuildEmojiManager: 0,
+    GuildBanManager: 0,
+    GuildInviteManager: 0,
+    GuildStickerManager: 0,
+    GuildScheduledEventManager: 0,
+    MessageManager: {
+      maxSize: 25,
+      keepOverLimit: (msg) =>
+        (msg.author.id === msg.client.user.id &&
+          (msg.editedTimestamp || msg.createdTimestamp) > Date.now() - ms("10 minutes")) ||
+        msg.createdTimestamp > Date.now() - ms("5 minutes"),
+    },
+    PresenceManager: 0,
+    ReactionManager: 0,
+    ReactionUserManager: 0,
+    StageInstanceManager: 0,
+    ThreadManager: 0,
+    ThreadMemberManager: 0,
+    VoiceStateManager: 0,
+    GuildEmojiManager: 0,
+    AutoModerationRuleManager: 0,
+    GuildForumThreadManager: 0,
+    GuildTextThreadManager: 0,
+    UserManager: {
+      maxSize: 10_000,
+      keepOverLimit: (user) => user.id === user.client.user.id,
+    },
+    GuildMemberManager: {
+      maxSize: 10_000,
+      keepOverLimit: (user) => user.id === user.client.user.id,
+    },
+  }),
   presence: {
     status: "dnd",
     activities: [
@@ -79,6 +79,11 @@ const client = new NypsiClient({
   ],
   partials: [Partials.Channel], // for direct messages
 });
+
+import { loadCommands } from "./utils/handlers/commandhandler";
+import { loadInteractions } from "./utils/handlers/interactions";
+import { logger } from "./utils/logger";
+import ms = require("ms");
 
 loadCommands();
 loadInteractions();
