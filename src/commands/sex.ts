dfc92fd9724dc9f15b64510c17b486f8dc1c80aa
max <29782397+tekoh@users.noreply.github.com>
2023-08-06 12:29:54 +0100

    refactor: ughhh

--- src/commands/sex.ts
+++ src/commands/sex.ts
@@ -284,7 +284,7 @@ async function run(
 
           if (!guild) return;
 
-          const channel = await guild.channels.cache.get(channelId);
+          const channel = guild.channels.cache.get(channelId);
 
           if (!channel) return;
 
