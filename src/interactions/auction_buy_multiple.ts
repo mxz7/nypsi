50e29c649c23a940e0573bc31178597f40d0678b
tekoh <tekoh@users.noreply.github.com>
2023-08-07 01:10:16 +0000

    style: formatting

--- src/interactions/auction_buy_multiple.ts
+++ src/interactions/auction_buy_multiple.ts
@@ -1,4 +1,11 @@
-import { ActionRowBuilder, ButtonInteraction, Interaction, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonInteraction,
+  Interaction,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} from "discord.js";
 import prisma from "../init/database";
 import { ErrorEmbed } from "../models/EmbedBuilders";
 import { InteractionHandler } from "../types/InteractionHandler";
@@ -28,13 +35,13 @@ export default {
       const res = await showMultiBuyModal(interaction, Number(auction.itemAmount)).catch(
         () => null,
       );
-  
-      if (!res || !res.isModalSubmit()) return;
-    
-      const amount = parseInt(res.fields.fields.first().value)
 
-      if (!amount) return res.reply({ embeds: [new ErrorEmbed("invalid amount")], ephemeral: true})
-    
+      if (!res || !res.isModalSubmit()) return;
+
+      const amount = parseInt(res.fields.fields.first().value);
+
+      if (!amount)
+        return res.reply({ embeds: [new ErrorEmbed("invalid amount")], ephemeral: true });
 
       if (auction.itemAmount == BigInt(amount)) {
         res.deferReply({ ephemeral: true });
@@ -42,8 +49,7 @@ export default {
         return buyFullAuction(interaction as ButtonInteraction, auction);
       }
 
-      return buyAuctionMulti(BigInt(amount), res, auction)
-      
+      return buyAuctionMulti(BigInt(amount), res, auction);
     } else if (auction.sold || Number(auction.itemAmount) === 0) {
       return await interaction.reply({
         embeds: [new ErrorEmbed("too slow ):").removeTitle()],
@@ -57,23 +63,23 @@ export default {
 } as InteractionHandler;
 
 async function showMultiBuyModal(interaction: ButtonInteraction, maxAmount: number) {
-    const modal = new ModalBuilder().setCustomId("auction-multi-buy").setTitle("buy multiple");
+  const modal = new ModalBuilder().setCustomId("auction-multi-buy").setTitle("buy multiple");
 
-    modal.addComponents(
-        new ActionRowBuilder<TextInputBuilder>().addComponents(
-        new TextInputBuilder()
-            .setCustomId("amount")
-            .setLabel("how many do you want to buy?")
-            .setPlaceholder(`maximum amount: ${maxAmount}`)
-            .setStyle(TextInputStyle.Short)
-            .setRequired(true)
-            .setMaxLength(10),
-        ),
-    );
+  modal.addComponents(
+    new ActionRowBuilder<TextInputBuilder>().addComponents(
+      new TextInputBuilder()
+        .setCustomId("amount")
+        .setLabel("how many do you want to buy?")
+        .setPlaceholder(`maximum amount: ${maxAmount}`)
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true)
+        .setMaxLength(10),
+    ),
+  );
 
-    await interaction.showModal(modal);
+  await interaction.showModal(modal);
 
-    const filter = (i: Interaction) => i.user.id == interaction.user.id;
+  const filter = (i: Interaction) => i.user.id == interaction.user.id;
 
-    return await interaction.awaitModalSubmit({ filter, time: 30000 }).catch(() => {});
+  return await interaction.awaitModalSubmit({ filter, time: 30000 }).catch(() => {});
 }
