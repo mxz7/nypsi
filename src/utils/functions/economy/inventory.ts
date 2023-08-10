195a5a6eca0e686485b401a61cc7f3608c03db6b
max <29782397+tekoh@users.noreply.github.com>
2023-08-10 20:42:51 +0100

    fix: allow proper item variety when opening multiple crates

--- src/utils/functions/economy/inventory.ts
+++ src/utils/functions/economy/inventory.ts
@@ -310,34 +310,6 @@ export async function openCrate(
     return new Map();
   }
 
-  const crateItems: string[] = [];
-
-  if (item.items) {
-    for (const itemFilter of item.items) {
-      let filteredItems: string[];
-      if (itemFilter.startsWith("id:")) {
-        filteredItems = Object.keys(items).filter((i) => i === itemFilter.substring(3));
-      } else if (itemFilter.startsWith("role:")) {
-        filteredItems = Object.keys(items).filter((i) => items[i].role === itemFilter.substring(5));
-      } else {
-        crateItems.push(itemFilter);
-        continue;
-      }
-      for (const i of filteredItems) {
-        crateItems.push(i);
-      }
-    }
-  } else {
-    crateItems.push(...["money:50000", "money:100000", "xp:25", "xp:50"]);
-
-    for (const i of Object.keys(items)) {
-      if (!items[i].in_crates) continue;
-      crateItems.push(i);
-    }
-  }
-
-  await setInventoryItem(member, item.id, inventory.find((i) => i.item == item.id).amount - 1);
-
   const times = item.crate_runs || 1;
   const found = new Map<string, number>();
 
@@ -346,141 +318,223 @@ export async function openCrate(
     found.set("money", 69420);
   }
 
-  for (let i = 0; i < times; i++) {
-    const crateItemsModified = [];
+  await setInventoryItem(member, item.id, inventory.find((i) => i.item == item.id).amount - 1);
 
-    for (const i of crateItems) {
-      if (items[i]) {
+  if (item.items) {
+    for (let i = 0; i < times; i++) {
+      const crateItems: string[] = [];
+      for (const itemFilter of item.items) {
+        let filteredItems: string[] = [];
+        if (itemFilter.startsWith("id:")) {
+          if (itemFilter.split(":")[2]) {
+            if (percentChance(parseInt(itemFilter.split(":")[2]) || 0)) {
+              filteredItems = Object.keys(items).filter((i) => i === itemFilter.split(";")[1]);
+            }
+          } else {
+            filteredItems = Object.keys(items).filter((i) => i === itemFilter.substring(3));
+          }
+        } else if (itemFilter.startsWith("role:")) {
+          filteredItems = Object.keys(items).filter(
+            (i) => items[i].role === itemFilter.substring(5),
+          );
+        } else {
+          crateItems.push(itemFilter);
+          continue;
+        }
+        for (const i of filteredItems) {
+          crateItems.push(i);
+        }
+      }
+
+      const chosen = crateItems[Math.floor(Math.random() * crateItems.length)];
+
+      if (chosen.includes("money:") || chosen.includes("xp:")) {
+        if (chosen.includes("money:")) {
+          const amount = parseInt(chosen.substring(6));
+
+          await updateBalance(member, (await getBalance(member)) + amount);
+          found.set("money", found.has("money") ? found.get("money") + amount : amount);
+        } else if (chosen.includes("xp:")) {
+          const amount = parseInt(chosen.substring(3));
+
+          await updateXp(member, (await getXp(member)) + amount);
+          found.set("xp", found.has("xp") ? found.get("xp") + amount : amount);
+        }
+      } else {
+        let amount = 1;
+
         if (
-          item.id == "nypsi_crate" &&
-          (["collectable", "sellable", "item", "car"].includes(items[i].role) || items[i].buy)
+          chosen == "terrible_fishing_rod" ||
+          chosen == "terrible_gun" ||
+          chosen == "wooden_pickaxe"
         ) {
-          const chance = Math.floor(Math.random() * 7);
-
-          if (chance != 2) continue;
+          amount = 5;
+        } else if (chosen == "fishing_rod" || chosen == "gun" || chosen == "iron_pickaxe") {
+          amount = 10;
+        } else if (
+          chosen == "incredible_fishing_rod" ||
+          chosen == "incredible_gun" ||
+          chosen == "diamond_pickaxe"
+        ) {
+          amount = 10;
+        } else if (chosen == "gem_shard" && item.id === "gem_crate") {
+          amount = Math.floor(Math.random() * 15) + 5;
         }
 
-        if (items[i].rarity === 6) {
-          const chance = Math.floor(Math.random() * 500);
+        await addInventoryItem(member, chosen, amount);
 
-          if (chance == 7) crateItemsModified.push(i);
-        } else if (items[i].rarity == 5) {
-          const chance = Math.floor(Math.random() * 50);
+        if (chosen.includes("_gem"))
+          await addProgress(typeof member === "string" ? member : member.user.id, "gem_hunter", 1);
 
-          if (chance == 7) crateItemsModified.push(i);
-        } else if (items[i].rarity == 4) {
-          const chance = Math.floor(Math.random() * 15);
-          if (chance == 4) {
+        found.set(chosen, found.has(chosen) ? found.get(chosen) + amount : amount);
+      }
+    }
+  } else {
+    const crateItems: string[] = [];
+
+    crateItems.push(...["money:50000", "money:100000", "xp:25", "xp:50"]);
+
+    for (const i of Object.keys(items)) {
+      if (!items[i].in_crates) continue;
+      crateItems.push(i);
+    }
+
+    for (let i = 0; i < times; i++) {
+      const crateItemsModified = [];
+
+      for (const i of crateItems) {
+        if (items[i]) {
+          if (
+            item.id == "nypsi_crate" &&
+            (["collectable", "sellable", "item", "car"].includes(items[i].role) || items[i].buy)
+          ) {
+            const chance = Math.floor(Math.random() * 7);
+
+            if (chance != 2) continue;
+          }
+
+          if (items[i].rarity === 6) {
+            const chance = Math.floor(Math.random() * 500);
+
+            if (chance == 7) crateItemsModified.push(i);
+          } else if (items[i].rarity == 5) {
+            const chance = Math.floor(Math.random() * 50);
+
+            if (chance == 7) crateItemsModified.push(i);
+          } else if (items[i].rarity == 4) {
+            const chance = Math.floor(Math.random() * 15);
+            if (chance == 4) {
+              crateItemsModified.push(i);
+            } else if (chance > 7 && item.id == "nypsi_crate") {
+              for (let x = 0; x < 3; x++) {
+                crateItemsModified.push(i);
+              }
+            }
+          } else if (items[i].rarity == 3) {
+            const chance = Math.floor(Math.random() * 3);
+            if (chance == 2) {
+              crateItemsModified.push(i);
+            } else if (item.id == "nypsi_crate") {
+              for (let x = 0; x < 3; x++) {
+                crateItemsModified.push(i);
+              }
+            }
+          } else if (items[i].rarity == 2) {
+            if (item.id == "nypsi_crate") {
+              for (let x = 0; x < 5; x++) {
+                crateItemsModified.push(i);
+              }
+            }
             crateItemsModified.push(i);
-          } else if (chance > 7 && item.id == "nypsi_crate") {
-            for (let x = 0; x < 3; x++) {
+          } else if (items[i].rarity == 1) {
+            for (let x = 0; x < 2; x++) {
+              if (items[i].role == "collectable" && item.id != "nypsi_crate") {
+                const chance = Math.floor(Math.random() * 3);
+
+                if (chance == 2) {
+                  crateItemsModified.push(i);
+                }
+              } else {
+                if (item.id == "nypsi_crate") {
+                  const chance = Math.floor(Math.random() * 10);
+
+                  if (chance < 7) {
+                    crateItemsModified.push(i);
+                  }
+                } else {
+                  crateItemsModified.push(i);
+                }
+              }
               crateItemsModified.push(i);
             }
-          }
-        } else if (items[i].rarity == 3) {
-          const chance = Math.floor(Math.random() * 3);
-          if (chance == 2) {
-            crateItemsModified.push(i);
-          } else if (item.id == "nypsi_crate") {
-            for (let x = 0; x < 3; x++) {
-              crateItemsModified.push(i);
-            }
-          }
-        } else if (items[i].rarity == 2) {
-          if (item.id == "nypsi_crate") {
-            for (let x = 0; x < 5; x++) {
-              crateItemsModified.push(i);
-            }
-          }
-          crateItemsModified.push(i);
-        } else if (items[i].rarity == 1) {
-          for (let x = 0; x < 2; x++) {
-            if (items[i].role == "collectable" && item.id != "nypsi_crate") {
+          } else if (items[i].rarity == 0 && item.id != "nypsi_crate") {
+            if (items[i].role == "collectable") {
               const chance = Math.floor(Math.random() * 3);
 
               if (chance == 2) {
                 crateItemsModified.push(i);
               }
             } else {
-              if (item.id == "nypsi_crate") {
-                const chance = Math.floor(Math.random() * 10);
-
-                if (chance < 7) {
-                  crateItemsModified.push(i);
-                }
-              } else {
-                crateItemsModified.push(i);
-              }
-            }
-            crateItemsModified.push(i);
-          }
-        } else if (items[i].rarity == 0 && item.id != "nypsi_crate") {
-          if (items[i].role == "collectable") {
-            const chance = Math.floor(Math.random() * 3);
-
-            if (chance == 2) {
               crateItemsModified.push(i);
             }
-          } else {
             crateItemsModified.push(i);
           }
-          crateItemsModified.push(i);
-        }
-      } else {
-        if (item.id == "nypsi_crate") {
-          for (let x = 0; x < 6; x++) {
-            crateItemsModified.push("money:10000000");
-            crateItemsModified.push("xp:750");
+        } else {
+          if (item.id == "nypsi_crate") {
+            for (let x = 0; x < 6; x++) {
+              crateItemsModified.push("money:10000000");
+              crateItemsModified.push("xp:750");
+            }
+          }
+          for (let x = 0; x < 2; x++) {
+            crateItemsModified.push(i);
+            crateItemsModified.push(i);
           }
         }
-        for (let x = 0; x < 2; x++) {
-          crateItemsModified.push(i);
-          crateItemsModified.push(i);
+      }
+
+      const chosen = crateItemsModified[Math.floor(Math.random() * crateItemsModified.length)];
+
+      if (chosen.includes("money:") || chosen.includes("xp:")) {
+        if (chosen.includes("money:")) {
+          const amount = parseInt(chosen.substring(6));
+
+          await updateBalance(member, (await getBalance(member)) + amount);
+          found.set("money", found.has("money") ? found.get("money") + amount : amount);
+        } else if (chosen.includes("xp:")) {
+          const amount = parseInt(chosen.substring(3));
+
+          await updateXp(member, (await getXp(member)) + amount);
+          found.set("xp", found.has("xp") ? found.get("xp") + amount : amount);
         }
+      } else {
+        let amount = 1;
+
+        if (
+          chosen == "terrible_fishing_rod" ||
+          chosen == "terrible_gun" ||
+          chosen == "wooden_pickaxe"
+        ) {
+          amount = 5;
+        } else if (chosen == "fishing_rod" || chosen == "gun" || chosen == "iron_pickaxe") {
+          amount = 10;
+        } else if (
+          chosen == "incredible_fishing_rod" ||
+          chosen == "incredible_gun" ||
+          chosen == "diamond_pickaxe"
+        ) {
+          amount = 10;
+        } else if (chosen == "gem_shard" && item.id === "gem_crate") {
+          amount = Math.floor(Math.random() * 15) + 5;
+        }
+
+        await addInventoryItem(member, chosen, amount);
+
+        if (chosen.includes("_gem"))
+          await addProgress(typeof member === "string" ? member : member.user.id, "gem_hunter", 1);
+
+        found.set(chosen, found.has(chosen) ? found.get(chosen) + amount : amount);
       }
-    }
-
-    const chosen = crateItemsModified[Math.floor(Math.random() * crateItemsModified.length)];
-
-    if (chosen.includes("money:") || chosen.includes("xp:")) {
-      if (chosen.includes("money:")) {
-        const amount = parseInt(chosen.substring(6));
-
-        await updateBalance(member, (await getBalance(member)) + amount);
-        found.set("money", found.has("money") ? found.get("money") + amount : amount);
-      } else if (chosen.includes("xp:")) {
-        const amount = parseInt(chosen.substring(3));
-
-        await updateXp(member, (await getXp(member)) + amount);
-        found.set("xp", found.has("xp") ? found.get("xp") + amount : amount);
-      }
-    } else {
-      let amount = 1;
-
-      if (
-        chosen == "terrible_fishing_rod" ||
-        chosen == "terrible_gun" ||
-        chosen == "wooden_pickaxe"
-      ) {
-        amount = 5;
-      } else if (chosen == "fishing_rod" || chosen == "gun" || chosen == "iron_pickaxe") {
-        amount = 10;
-      } else if (
-        chosen == "incredible_fishing_rod" ||
-        chosen == "incredible_gun" ||
-        chosen == "diamond_pickaxe"
-      ) {
-        amount = 10;
-      } else if (chosen == "gem_shard" && item.id === "gem_crate") {
-        amount = Math.floor(Math.random() * 15) + 5;
-      }
-
-      await addInventoryItem(member, chosen, amount);
-
-      if (chosen.includes("_gem"))
-        await addProgress(typeof member === "string" ? member : member.user.id, "gem_hunter", 1);
-
-      found.set(chosen, found.has(chosen) ? found.get(chosen) + amount : amount);
     }
   }
 
