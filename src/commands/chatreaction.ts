c32c7fff3d56a6278d51b4443081d80cb12848a1
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2023-08-14 07:08:11 -0400

    fix: proper unban & unmute reasons and others (#1187)
    * fix: proper unban & unmute reasons and others
    
    * fix: bob message

--- src/commands/chatreaction.ts
+++ src/commands/chatreaction.ts
@@ -253,7 +253,7 @@ async function run(
         `${prefix}**cr settings** *view/modify the chat reaction settings for your server*\n` +
         `${prefix}**cr words** *view/modify the chat reaction word list*\n` +
         `${prefix}**cr blacklist** *add/remove people to the blacklist*\n` +
-        `${prefix}**cr stats reset** *reset stats for the server*` +
+        `${prefix}**cr stats reset** *reset stats for the server*\n` +
         `${prefix}**cr stats** *view your chat reaction stats*\n` +
         `${prefix}**cr lb** *view the server leaderboard*\n` +
         `${prefix}**cr duel** *duel a member at a chat reaction*`,
