f1e269db51fa82742edfa60d69cb38680774f79c
max <29782397+tekoh@users.noreply.github.com>
2023-08-23 18:49:03 +0100

    refactor: random presences include activities data

--- src/utils/functions/presence.ts
+++ src/utils/functions/presence.ts
@@ -1,33 +1,47 @@
+import { ActivitiesOptions, ActivityType } from "discord.js";
 import redis from "../../init/redis";
 import Constants from "../Constants";
 import { daysUntilChristmas } from "./date";
 
-export function randomPresence(): string {
-  const possibilities = [
-    "nypsi.xyz",
-    "nypsi.xyz",
-    "xmas",
-    "nypsi.xyz",
-    "nothing. me? just hanging around",
+export function randomPresence(): ActivitiesOptions {
+  const possibilities: ActivitiesOptions[] = [
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "nypsi.xyz", type: ActivityType.Custom },
+    { name: "xmas", type: ActivityType.Custom },
+    { name: "with your mother", type: ActivityType.Playing },
+    {
+      name: "nothing. me? just hanging around",
+      type: ActivityType.Watching,
+      url: "https://www.youtube.com/watch?v=d2r_LU6beVw",
+    },
+    {
+      name: "ronnie pickering",
+      type: ActivityType.Watching,
+      url: "https://youtube.com/watch?v=r0dcv6GKNNw&t=102s",
+    },
   ];
 
   const chosen = possibilities[Math.floor(Math.random() * possibilities.length)];
 
-  let game = "";
-
-  if (chosen === "xmas") {
+  if (chosen.name === "xmas") {
     const days = daysUntilChristmas();
 
     if (days == "ITS CHRISTMAS") {
-      game = "MERRY CHRISTMAS";
+      chosen.name = "MERRY CHRISTMAS";
     } else {
-      game = `${days} day${parseInt(days) > 1 ? "s" : ""} until christmas`;
+      chosen.name = `${days} day${parseInt(days) > 1 ? "s" : ""} until christmas`;
     }
-  } else {
-    game = chosen;
   }
 
-  return game;
+  return chosen;
 }
 
 export async function getCustomPresence() {
