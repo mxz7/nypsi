f1b50a8826d045fabb69f69d937446a343d063d7
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2023-09-04 15:22:17 -0400

    fix: be able to remove alias thats just $

---
+++ src/interactions/aliases.ts
@@ -0,0 +1,22 @@
+import { AutocompleteHandler } from "../types/InteractionHandler";
+import { getPrefix } from "../utils/functions/guilds/utils";
+import { getUserAliases } from "../utils/functions/premium/aliases";
+
+export default {
+  name: "alias",
+  type: "autocomplete",
+  async run(interaction) {
+    const aliases = await getUserAliases(interaction.user.id);
+
+    if (aliases.length == 0) return await interaction.respond([]);
+    
+    const prefix = await getPrefix(interaction.guild);
+
+    const formatted = aliases.map((i) => ({
+      name: `${prefix}${i.alias} (${prefix}${i.command})`,
+      value: i.alias,
+    }));
+
+    return await interaction.respond(formatted);
+  },
+} as AutocompleteHandler;
