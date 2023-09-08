6b9cb07a784cf70628aa79e32c22a2821097b8c1
max <29782397+tekoh@users.noreply.github.com>
2023-09-08 13:16:18 +0100

    feat: migration

---
+++ prisma/migrations/20230908121612_levelling/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE "Economy" ADD COLUMN     "level" INTEGER NOT NULL DEFAULT 0,
+ADD COLUMN     "prestigeDates" TIMESTAMP(3)[];
+
+-- CreateTable
+CREATE TABLE "Upgrades" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "upgradeId" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+
+    CONSTRAINT "Upgrades_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Upgrades" ADD CONSTRAINT "Upgrades_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Economy"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
