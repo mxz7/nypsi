05dc0ce92f66aa3e97a67a38feb46d8c11c17c33
Bulvius1 <92026386+Bulvius1@users.noreply.github.com>
2023-10-06 21:48:39 +0300

    Update funny.ts

--- src/commands/funny.ts
+++ src/commands/funny.ts
@@ -18,7 +18,7 @@ const cache = new Map<string, number>();
 const cmd = new Command("funny", "Measures how funny you are", "fun").setAliases([
   "howfunny",
   "unfunny",
-);
+]);
 
 async function run(
   message: Message | (NypsiCommandInteraction & CommandInteraction),
