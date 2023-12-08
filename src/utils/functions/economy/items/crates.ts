b0debfe44e19502d17d08fb958ceb5b816f978f5
max <29782397+tekoh@users.noreply.github.com>
2023-12-08 15:55:55 +0000

    still old send functions!!

--- src/utils/functions/economy/items/crates.ts
+++ src/utils/functions/economy/items/crates.ts
@@ -25,13 +25,25 @@ module.exports = new ItemUse(
   async (message: Message | (NypsiCommandInteraction & CommandInteraction), args: string[]) => {
     const send = async (data: BaseMessageOptions | InteractionReplyOptions) => {
       if (!(message instanceof Message)) {
+        let usedNewMessage = false;
+        let res;
+
         if (message.deferred) {
-          await message.editReply(data);
+          res = await message.editReply(data).catch(async () => {
+            usedNewMessage = true;
+            return await message.channel.send(data as BaseMessageOptions);
+          });
         } else {
-          await message.reply(data as InteractionReplyOptions).catch(() => {
-            return message.editReply(data);
+          res = await message.reply(data as InteractionReplyOptions).catch(() => {
+            return message.editReply(data).catch(async () => {
+              usedNewMessage = true;
+              return await message.channel.send(data as BaseMessageOptions);
+            });
           });
         }
+
+        if (usedNewMessage && res instanceof Message) return res;
+
         const replyMsg = await message.fetchReply();
         if (replyMsg instanceof Message) {
           return replyMsg;
