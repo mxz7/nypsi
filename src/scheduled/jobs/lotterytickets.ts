3828dc654ae6651a1ad0d1d9ed2ae048889a5bba
max <29782397+tekoh@users.noreply.github.com>
2023-12-17 15:30:01 +0000

    use cron instead of bree to run jobs (#1376)
    * install cron and uninstall bree
    
    * yay cron works!!!!!!!
    
    * it works tadaaaa
    
    * 6.14.1

--- src/scheduled/jobs/lotterytickets.ts
+++ src/scheduled/jobs/lotterytickets.ts
@@ -1,43 +1,40 @@
 import { variants } from "@catppuccin/palette";
 import { ColorResolvable, EmbedBuilder, WebhookClient } from "discord.js";
 import redis from "../../init/redis";
-import { getVersion } from "../../utils/functions/version";
+import { Job } from "../../types/Jobs";
 
-(async () => {
-  process.title = `nypsi v${getVersion()}: lottery tickets job`;
+export default {
+  name: "lottery tickets",
+  cron: "*/30 * * * *",
+  async run() {
+    const hook = new WebhookClient({
+      url: process.env.LOTTERY_HOOK,
+    });
 
-  const hook = new WebhookClient({
-    url: process.env.LOTTERY_HOOK,
-  });
+    const tickets = await redis.hgetall("lotterytickets:queue");
 
-  const tickets = await redis.hgetall("lotterytickets:queue");
+    if (Object.keys(tickets).length == 0) return;
 
-  if (Object.keys(tickets).length == 0) {
-    process.exit(0);
-    return;
-  }
+    const desc = [];
 
-  const desc = [];
+    for (const username of Object.keys(tickets)) {
+      const amount = parseInt(tickets[username]);
 
-  for (const username of Object.keys(tickets)) {
-    const amount = parseInt(tickets[username]);
+      desc.push(`**${username}** has bought **${amount}** lottery ticket${amount > 1 ? "s" : ""}`);
 
-    desc.push(`**${username}** has bought **${amount}** lottery ticket${amount > 1 ? "s" : ""}`);
+      await redis.hdel("lotterytickets:queue", username);
 
-    await redis.hdel("lotterytickets:queue", username);
+      if (desc.join("\n").length >= 500) break;
+    }
 
-    if (desc.join("\n").length >= 500) break;
-  }
+    const embed = new EmbedBuilder();
 
-  const embed = new EmbedBuilder();
+    embed.setColor(variants.latte.base.hex as ColorResolvable);
+    embed.setDescription(desc.join("\n"));
+    embed.setTimestamp();
 
-  embed.setColor(variants.latte.base.hex as ColorResolvable);
-  embed.setDescription(desc.join("\n"));
-  embed.setTimestamp();
+    await hook.send({ embeds: [embed] });
 
-  await hook.send({ embeds: [embed] });
-
-  hook.destroy();
-
-  process.exit(0);
-})();
+    hook.destroy();
+  },
+} satisfies Job;
--- src/scheduled/jobs/topcommands.ts
+++ src/scheduled/jobs/topcommands.ts
@@ -1,84 +1,83 @@
 import { variants } from "@catppuccin/palette";
 import { ColorResolvable, EmbedBuilder, WebhookClient } from "discord.js";
 import { inPlaceSort } from "fast-sort";
-import { parentPort } from "worker_threads";
 import redis from "../../init/redis";
+import { Job } from "../../types/Jobs";
 import Constants from "../../utils/Constants";
 import { getPreferences } from "../../utils/functions/users/notifications";
 import { getLastKnownUsername } from "../../utils/functions/users/tag";
-import { getVersion } from "../../utils/functions/version";
 
-(async () => {
-  process.title = `nypsi v${getVersion()}: top commands job`;
+export default {
+  name: "top commands",
+  cron: "0 0 * * *",
+  async run(log) {
+    const [topCommands, topUsers] = await Promise.all([
+      redis.hgetall(Constants.redis.nypsi.TOP_COMMANDS),
+      redis.hgetall(Constants.redis.nypsi.TOP_COMMANDS_USER),
+    ]);
+    await redis.del(Constants.redis.nypsi.TOP_COMMANDS);
+    await redis.del(Constants.redis.nypsi.TOP_COMMANDS_USER);
 
-  const [topCommands, topUsers] = await Promise.all([
-    redis.hgetall(Constants.redis.nypsi.TOP_COMMANDS),
-    redis.hgetall(Constants.redis.nypsi.TOP_COMMANDS_USER),
-  ]);
-  await redis.del(Constants.redis.nypsi.TOP_COMMANDS);
-  await redis.del(Constants.redis.nypsi.TOP_COMMANDS_USER);
+    const commands: string[] = [];
 
-  const commands: string[] = [];
-
-  for (const cmd of Object.keys(topCommands)) {
-    commands.push(cmd);
-  }
-
-  const users: string[] = [];
-
-  for (const user of Object.keys(topUsers)) {
-    users.push(user);
-  }
-
-  inPlaceSort(commands).desc((i) => parseInt(topCommands[i]));
-  inPlaceSort(users).desc((i) => parseInt(topUsers[i]));
-
-  const msg: string[] = [];
-
-  let count = 0;
-  for (const cmd of commands) {
-    if (count >= 10) break;
-
-    let pos: number | string = count + 1;
-
-    if (pos == 1) {
-      pos = "🥇";
-    } else if (pos == 2) {
-      pos = "🥈";
-    } else if (pos == 3) {
-      pos = "🥉";
+    for (const cmd of Object.keys(topCommands)) {
+      commands.push(cmd);
     }
 
-    msg.push(`${pos} \`$${cmd}\` used **${parseInt(topCommands[cmd]).toLocaleString()}** times`);
-    count++;
-  }
+    const users: string[] = [];
 
-  const embed = new EmbedBuilder();
+    for (const user of Object.keys(topUsers)) {
+      users.push(user);
+    }
 
-  embed.setTitle("top 10 commands");
-  embed.setDescription(msg.join("\n"));
-  embed.setColor(variants.latte.base.hex as ColorResolvable);
+    inPlaceSort(commands).desc((i) => parseInt(topCommands[i]));
+    inPlaceSort(users).desc((i) => parseInt(topUsers[i]));
 
-  if ((await getPreferences(users[0]).catch(() => null))?.leaderboards || false) {
-    embed.setFooter({
-      text: `${await getLastKnownUsername(users[0])} has no life (${parseInt(
-        topUsers[users[0]],
-      ).toLocaleString()} commands)`,
-    });
-  }
+    const msg: string[] = [];
 
-  const hook = new WebhookClient({ url: process.env.TOPCOMMANDS_HOOK });
+    let count = 0;
+    for (const cmd of commands) {
+      if (count >= 10) break;
 
-  await hook
-    .send({ embeds: [embed] })
-    .then(() => {
-      parentPort.postMessage("sent top commands");
-      process.exit(0);
-    })
-    .catch(() => {
-      parentPort.postMessage("failed to send top commands");
-      process.exit(1);
-    });
+      let pos: number | string = count + 1;
 
-  hook.destroy();
-})();
+      if (pos == 1) {
+        pos = "🥇";
+      } else if (pos == 2) {
+        pos = "🥈";
+      } else if (pos == 3) {
+        pos = "🥉";
+      }
+
+      msg.push(`${pos} \`$${cmd}\` used **${parseInt(topCommands[cmd]).toLocaleString()}** times`);
+      count++;
+    }
+
+    const embed = new EmbedBuilder();
+
+    embed.setTitle("top 10 commands");
+    embed.setDescription(msg.join("\n"));
+    embed.setColor(variants.latte.base.hex as ColorResolvable);
+
+    if ((await getPreferences(users[0]).catch(() => null))?.leaderboards || false) {
+      embed.setFooter({
+        text: `${await getLastKnownUsername(users[0])} has no life (${parseInt(
+          topUsers[users[0]],
+        ).toLocaleString()} commands)`,
+      });
+    }
+
+    const hook = new WebhookClient({ url: process.env.TOPCOMMANDS_HOOK });
+
+    await hook
+      .send({ embeds: [embed] })
+      .then(() => {
+        log("sent top commands");
+      })
+      .catch(() => {
+        log("failed to send top commands");
+      });
+
+    hook.destroy();
+  },
+} satisfies Job;
