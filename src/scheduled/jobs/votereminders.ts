cee907ecaf3f0ff16757658e4ff8aaccfeb78090
max <29782397+tekoh@users.noreply.github.com>
2023-12-31 23:47:44 +0000

    better vote reminders handling

--- src/scheduled/jobs/votereminders.ts
+++ src/scheduled/jobs/votereminders.ts
@@ -13,9 +13,27 @@ import Constants from "../../utils/Constants";
 import { addNotificationToQueue } from "../../utils/functions/users/notifications";
 import dayjs = require("dayjs");
 
+const data: NotificationPayload = {
+  memberId: "boob",
+  payload: {
+    embed: new CustomEmbed()
+      .setDescription("it has been more than 12 hours since you last voted")
+      .setColor(Constants.TRANSPARENT_EMBED_COLOR),
+
+    components: new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+      new ButtonBuilder()
+        .setStyle(ButtonStyle.Link)
+        .setURL("https://top.gg/bot/678711738845102087/vote")
+        .setLabel("top.gg"),
+    ),
+  },
+};
+
+const queued = new Set<string>();
+
 export default {
   name: "vote reminders",
-  cron: "*/15 * * * *",
+  cron: "0 * * * *",
   run: async (log) => {
     const userIds = await prisma.dMSettings.findMany({
       where: {
@@ -24,7 +42,7 @@ export default {
           {
             user: {
               Economy: {
-                lastVote: { lte: dayjs().subtract(12, "hours").toDate() },
+                lastVote: { lte: dayjs().subtract(11, "hours").toDate() },
               },
             },
           },
@@ -32,36 +50,49 @@ export default {
       },
       select: {
         userId: true,
+        user: {
+          select: {
+            Economy: {
+              select: {
+                lastVote: true,
+              },
+            },
+          },
+        },
       },
     });
-
-    const data: NotificationPayload = {
-      memberId: "boob",
-      payload: {
-        embed: new CustomEmbed()
-          .setDescription("it has been more than 12 hours since you last voted")
-          .setColor(Constants.TRANSPARENT_EMBED_COLOR),
-
-        components: new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-          new ButtonBuilder()
-            .setStyle(ButtonStyle.Link)
-            .setURL("https://top.gg/bot/678711738845102087/vote")
-            .setLabel("top.gg"),
-        ),
-      },
-    };
 
     let amount = 0;
 
     for (const user of userIds) {
-      if (await redis.sismember(Constants.redis.nypsi.VOTE_REMINDER_RECEIVED, user.userId))
+      if (
+        (await redis.sismember(Constants.redis.nypsi.VOTE_REMINDER_RECEIVED, user.userId)) ||
+        queued.has(user.userId)
+      )
         continue;
-      data.memberId = user.userId;
 
-      await addNotificationToQueue(data);
-
-      await redis.sadd(Constants.redis.nypsi.VOTE_REMINDER_RECEIVED, user.userId);
       amount++;
+
+      if (
+        user.user.Economy.lastVote.getTime() <= dayjs().subtract(12, "hours").toDate().getTime()
+      ) {
+        data.memberId = user.userId;
+
+        await addNotificationToQueue(data);
+
+        await redis.sadd(Constants.redis.nypsi.VOTE_REMINDER_RECEIVED, user.userId);
+      } else {
+        queued.add(user.userId);
+        setTimeout(
+          () => {
+            queued.delete(user.userId);
+            redis.sadd(Constants.redis.nypsi.VOTE_REMINDER_RECEIVED, user.userId);
+            data.memberId = user.userId;
+            addNotificationToQueue(data);
+          },
+          user.user.Economy.lastVote.getTime() - dayjs().subtract(12, "hours").toDate().getTime(),
+        );
+      }
     }
 
     if (amount > 0) log(`${amount} vote reminders queued`);
