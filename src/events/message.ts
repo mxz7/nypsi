87e5631fc9cb63e03755fc6b6e2a14157cd68463
max <29782397+tekoh@users.noreply.github.com>
2024-02-06 15:18:27 +0000

    make dm message more clear

--- src/events/message.ts
+++ src/events/message.ts
@@ -72,7 +72,8 @@ export default async function messageCreate(message: Message) {
         .setHeader("support")
         .setColor(Constants.TRANSPARENT_EMBED_COLOR)
         .setDescription(
-          "if you need support, join the [**official nypsi server**](https://discord.gg/hJTDNST) or click the button below to talk to a staff member. only click the button if you actually need support" +
+          "if you need support, join the [**official nypsi server**](https://discord.gg/hJTDNST) or click the button below to talk to a staff member" +
+            "\n\nthis is **NOT** support for if you have been punished in an unrelated server" +
             "\n\n**ONLY CLICK IF YOU WISH TO TALK TO A NYPSI STAFF MEMBER**",
         );
 
