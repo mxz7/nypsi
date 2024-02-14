3f12e3f2dcc1b14c8edc333fd5460b47cc38b1d7
max <29782397+tekoh@users.noreply.github.com>
2024-02-14 16:29:06 +0000

    task streak loss notification

--- src/scheduled/jobs/weeklytasks.ts
+++ src/scheduled/jobs/weeklytasks.ts
@@ -1,6 +1,13 @@
 import { exec } from "child_process";
 import prisma from "../../init/database";
+import { CustomEmbed } from "../../models/EmbedBuilders";
 import { Job } from "../../types/Jobs";
+import Constants from "../../utils/Constants";
+import {
+  addInlineNotification,
+  addNotificationToQueue,
+  getDmSettings,
+} from "../../utils/functions/users/notifications";
 
 export default {
   name: "weekly tasks",
@@ -18,16 +25,35 @@ export default {
         where: { AND: [{ user_id: user.user_id }, { type: "weekly" }, { completed: true }] },
       });
 
-      if (completed === 3)
+      if (completed === 3) {
         await prisma.economy.update({
           where: { userId: user.user_id },
           data: { weeklyTaskStreak: { increment: 1 } },
         });
-      else
+      } else {
+        const streak = await prisma.economy.findUnique({
+          where: { userId: user.user_id },
+          select: { weeklyTaskStreak: true },
+        });
+
         await prisma.economy.update({
           where: { userId: user.user_id },
           data: { weeklyTaskStreak: 0 },
         });
+
+        if (streak.weeklyTaskStreak > 3) {
+          const embed = new CustomEmbed()
+            .setColor(Constants.EMBED_FAIL_COLOR)
+            .setDescription(
+              `**you lost your weekly task streak ):**\n\n` +
+                `you didnt complete all of your weekly tasks and lost your streak of ${streak.weeklyTaskStreak.toLocaleString()} ):`,
+            );
+
+          if ((await getDmSettings(user.user_id)).other)
+            addNotificationToQueue({ memberId: user.user_id, payload: { embed } });
+          else addInlineNotification({ memberId: user.user_id, embed });
+        }
+      }
     }
 
     const count = await prisma.task.deleteMany({ where: { type: "weekly" } });
