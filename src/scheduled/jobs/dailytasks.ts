08fa808e985e07057a5e3a67b9f1ef88c3ffefe5
max <29782397+tekoh@users.noreply.github.com>
2024-02-14 16:29:06 +0000

    task streak loss notification

--- src/scheduled/jobs/dailytasks.ts
+++ src/scheduled/jobs/dailytasks.ts
@@ -1,6 +1,13 @@
 import { exec } from "child_process";
 import prisma from "../../init/database";
+import { CustomEmbed } from "../../models/EmbedBuilders";
 import { Job } from "../../types/Jobs";
+import Constants from "../../utils/Constants";
+import {
+  addInlineNotification,
+  addNotificationToQueue,
+  getDmSettings,
+} from "../../utils/functions/users/notifications";
 
 export default {
   name: "daily tasks",
@@ -18,16 +25,35 @@ export default {
         where: { AND: [{ user_id: user.user_id }, { type: "daily" }, { completed: true }] },
       });
 
-      if (completed === 3)
+      if (completed === 3) {
         await prisma.economy.update({
           where: { userId: user.user_id },
           data: { dailyTaskStreak: { increment: 1 } },
         });
-      else
+      } else {
+        const streak = await prisma.economy.findUnique({
+          where: { userId: user.user_id },
+          select: { dailyTaskStreak: true },
+        });
+
         await prisma.economy.update({
           where: { userId: user.user_id },
           data: { dailyTaskStreak: 0 },
         });
+
+        if (streak.dailyTaskStreak > 5) {
+          const embed = new CustomEmbed()
+            .setColor(Constants.EMBED_FAIL_COLOR)
+            .setDescription(
+              `**you lost your daily task streak ):**\n\n` +
+                `you didnt complete all of your daily tasks and lost your streak of ${streak.dailyTaskStreak.toLocaleString()} ):`,
+            );
+
+          if ((await getDmSettings(user.user_id)).other)
+            addNotificationToQueue({ memberId: user.user_id, payload: { embed } });
+          else addInlineNotification({ memberId: user.user_id, embed });
+        }
+      }
     }
 
     const count = await prisma.task.deleteMany({ where: { type: "daily" } });
