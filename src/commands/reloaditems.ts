6012a24838ac6a36190f17db61ee90231ae4f672
max <29782397+tekoh@users.noreply.github.com>
2024-02-20 16:25:29 +0000

    remove invalid tasks

--- src/commands/reloaditems.ts
+++ src/commands/reloaditems.ts
@@ -1,8 +1,10 @@
 import { CommandInteraction, Message } from "discord.js";
+import prisma from "../init/database";
 import { NypsiClient } from "../models/Client";
 import { Command, NypsiCommandInteraction } from "../models/Command";
 import Constants from "../utils/Constants";
-import { loadItems } from "../utils/functions/economy/utils";
+import { getTasksData, loadItems } from "../utils/functions/economy/utils";
+import { logger } from "../utils/logger";
 
 const cmd = new Command("reloaditems", "reload items", "none").setPermissions(["bot owner"]);
 
@@ -11,6 +13,16 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
 
   loadItems();
   (message.client as NypsiClient).cluster.send("reload_items");
+
+  prisma.task
+    .deleteMany({
+      where: {
+        task_id: { notIn: Object.values(getTasksData()).map((i) => i.id) },
+      },
+    })
+    .then((count) => {
+      if (count.count > 0) logger.info(`${count} invalid tasks deleted`);
+    });
 
   return (message as Message).react("✅");
 }
