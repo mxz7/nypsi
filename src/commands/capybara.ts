7e89c84d5f38082b6a8592ad5ec2e79f41318dfb
max <29782397+tekoh@users.noreply.github.com>
2024-04-09 21:19:40 +0100

    use animals api

--- src/commands/capybara.ts
+++ src/commands/capybara.ts
@@ -21,10 +21,15 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
   if (!image)
     return message.channel.send({ embeds: [new ErrorEmbed("failed to find a capybara image")] });
 
-  const embed = new CustomEmbed(message.member)
-    .disableFooter()
-    .setImage(image.url)
-    .setFooter({ text: `#${image.id}` });
+  const embed = new CustomEmbed(message.member).disableFooter().setImage(image.url);
+
+  if (image.name) {
+    embed.setTitle(image.name);
+    embed.setURL(`https://animals.maxz.dev/capybara/${image.id}`);
+  }
+
+  if (Math.floor(Math.random() * 25) === 7)
+    embed.setFooter({ text: `powered by animals.maxz.dev` });
 
   message.channel.send({ embeds: [embed] });
 
--- src/commands/cat.ts
+++ src/commands/cat.ts
@@ -2,9 +2,9 @@ import { CommandInteraction, Message } from "discord.js";
 import { Command, NypsiCommandInteraction } from "../models/Command";
 import { CustomEmbed, ErrorEmbed } from "../models/EmbedBuilders";
 import { addProgress } from "../utils/functions/economy/achievements";
+import { addTaskProgress } from "../utils/functions/economy/tasks";
 import { getRandomImage } from "../utils/functions/image";
 import { getResponse, onCooldown } from "../utils/handlers/cooldownhandler";
-import { addTaskProgress } from "../utils/functions/economy/tasks";
 
 const cmd = new Command("cat", "get a random picture of a cat", "animals").setAliases([
   "kitty",
@@ -24,10 +24,15 @@ async function run(message: Message | (NypsiCommandInteraction & CommandInteract
   if (!image)
     return message.channel.send({ embeds: [new ErrorEmbed("failed to find a cat image")] });
 
-  const embed = new CustomEmbed(message.member)
-    .disableFooter()
-    .setImage(image.url)
-    .setFooter({ text: `#${image.id}` });
+  const embed = new CustomEmbed(message.member).disableFooter().setImage(image.url);
+
+  if (image.name) {
+    embed.setTitle(image.name);
+    embed.setURL(`https://animals.maxz.dev/cat/${image.id}`);
+  }
+
+  if (Math.floor(Math.random() * 25) === 7)
+    embed.setFooter({ text: `powered by animals.maxz.dev` });
 
   message.channel.send({ embeds: [embed] });
 
