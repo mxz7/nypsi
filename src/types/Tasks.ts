8195c146fe00489cb925592bb551286168590116
max <29782397+tekoh@users.noreply.github.com>
2024-04-21 13:40:51 +0100

    task complete gif

--- src/types/Tasks.ts
+++ src/types/Tasks.ts
@@ -2,6 +2,7 @@ export interface Task {
   id: string;
   name: string;
   description: string;
+  complete_gif?: string;
   target: number[]; // random from the list;
   prizes: string[]; // random prize will be chosen. in format of (id/money/xp/karma):(item/amount):(amount if item)
   type: "daily" | "weekly";
