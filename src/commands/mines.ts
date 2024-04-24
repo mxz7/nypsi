97b2534d6cacb5790740cc7c7fe60c9400655d2b
max <29782397+tekoh@users.noreply.github.com>
2024-04-24 17:34:07 +0100

    buff mines

--- src/commands/mines.ts
+++ src/commands/mines.ts
@@ -65,16 +65,16 @@ const abcde = new Map<string, number>();
 const possibleLetters = ["a", "b", "c", "d", "e"];
 const possibleNumbers = ["1", "2", "3", "4", "5"];
 const mineIncrements = new Map<number, number>([
-  [2, 0.2],
-  [3, 0.25],
-  [4, 0.3],
+  [2, 0.25],
+  [3, 0.3],
+  [4, 0.4],
   [5, 0.45],
-  [6, 0.5],
-  [7, 0.6],
-  [10, 1],
-  [15, 1.8],
-  [20, 4],
-  [23, 13],
+  [6, 0.6],
+  [7, 0.7],
+  [10, 1.3],
+  [15, 1.9],
+  [20, 4.5],
+  [23, 14],
 ]);
 
 abcde.set("a", 0);
@@ -356,7 +356,7 @@ async function prepareGame(
   ];
 
   let bombCount: number;
-  let incrementAmount = 0.5;
+  let incrementAmount = 0.75;
 
   if (chosenMinesCount == 0) {
     bombCount = Math.floor(Math.random() * 4) + 3;
