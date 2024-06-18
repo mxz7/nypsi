0ac5081e92337cbcce3d5c16823289fecbfa771a
max <29782397+tekoh@users.noreply.github.com>
2024-06-18 16:05:16 +0100

    remove old case id column

---
+++ prisma/migrations/20240618150510_moderation_restructure_9_delete_old/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `caseId_old` on the `ModerationCase` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ModerationCase" DROP COLUMN "caseId_old";
