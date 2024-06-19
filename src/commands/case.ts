7443640c6aab1d19ee1ec0384f7d01835f02f448
max <29782397+tekoh@users.noreply.github.com>
2024-06-19 19:36:05 +0100

    new case command

--- src/commands/case.ts
+++ src/commands/case.ts
@@ -18,6 +18,13 @@ import Constants from "../utils/Constants";
 import { getPrefix } from "../utils/functions/guilds/utils";
 import { deleteCase, getCase, restoreCase } from "../utils/functions/moderation/cases";
 
+import {
+  createEvidence,
+  deleteEvidence,
+  getMaxEvidenceBytes,
+  getUsedEvidenceBytes,
+} from "../utils/functions/guilds/evidence";
+import { formatBytes } from "../utils/functions/string";
 import { getLastKnownUsername } from "../utils/functions/users/tag";
 
 const cmd = new Command("case", "get information about a given case", "moderation")
@@ -93,8 +100,8 @@ async function run(
       embeds: [new ErrorEmbed("couldn't find a case with the id `" + args[0] + "`")],
     });
 
-  async function displayCase(msg?: Message, interaction?: ButtonInteraction) {
-    const caseData = await getCase(message.guild, caseId);
+  async function displayCase(caseMsg?: Message, interaction?: ButtonInteraction) {
+    let caseData = await getCase(message.guild, caseId);
 
     if (!caseData)
       return send({
@@ -118,7 +125,7 @@ async function run(
     }
 
     const embed = new CustomEmbed(message.member)
-      .setHeader("case " + caseData.caseId)
+      .setHeader("case " + caseData.caseId, message.guild.iconURL())
       .addField("type", "`" + caseData.type + "`", true)
       .addField("moderator", moderator, true)
       .addField("date/time", `<t:${Math.floor(caseData.time.getTime() / 1000)}>`, true)
@@ -131,7 +138,7 @@ async function run(
         .setCustomId("evidence")
         .setEmoji("📋")
         .setLabel("evidence")
-        .setStyle(ButtonStyle.Success),
+        .setStyle(caseData.evidence ? ButtonStyle.Success : ButtonStyle.Secondary),
     );
 
     if (message.member.permissions.has(PermissionFlagsBits.ManageGuild)) {
@@ -154,25 +161,25 @@ async function run(
       }
     }
 
-    if (msg) {
+    if (caseMsg) {
       if (interaction) {
         await interaction
           .update({ embeds: [embed], components: [row] })
-          .catch(() => msg.edit({ embeds: [embed], components: [row] }));
+          .catch(() => caseMsg.edit({ embeds: [embed], components: [row] }));
       } else {
-        await msg.edit({ embeds: [embed], components: [row] });
+        await caseMsg.edit({ embeds: [embed], components: [row] });
       }
     } else {
-      msg = await send({ embeds: [embed], components: [row] });
+      caseMsg = await send({ embeds: [embed], components: [row] });
     }
 
     async function listen() {
       const filter = (i: Interaction) => i.user.id == message.author.id;
 
-      const interaction = await msg
+      const interaction = await caseMsg
         .awaitMessageComponent({ filter, time: 15000, componentType: ComponentType.Button })
         .catch(async () => {
-          await msg.edit({ components: [] });
+          await caseMsg.edit({ components: [] });
         });
 
       if (!interaction) return;
@@ -192,7 +199,7 @@ async function run(
           ephemeral: true,
         });
 
-        return displayCase(msg);
+        return displayCase(caseMsg);
       } else if (interaction.customId === "restore") {
         if (!caseData.deleted) {
           await interaction.reply({
@@ -208,7 +215,175 @@ async function run(
           embeds: [new CustomEmbed(message.member, "✅ case restored")],
           ephemeral: true,
         });
+
+        return displayCase(caseMsg);
+      } else if (interaction.customId === "evidence") {
+        showEvidence(interaction);
+        return listen();
       }
+    }
+
+    async function showEvidence(interaction?: ButtonInteraction, evidenceMsg?: Message) {
+      const embed = new CustomEmbed(message.member).setHeader(
+        `case ${caseData.caseId} evidence`,
+        message.guild.iconURL(),
+      );
+      const row = new ActionRowBuilder<MessageActionRowComponentBuilder>();
+
+      if (caseData.evidence) {
+        const lastKnownUsername = await getLastKnownUsername(caseData.evidence.userId);
+
+        embed
+          .setImage(`https://cdn.nypsi.xyz/evidence/${caseData.guildId}/${caseData.evidence.id}`)
+          .setDescription(
+            `uploaded by: ${lastKnownUsername ? `${lastKnownUsername} ` : ""}\`${caseData.evidence.userId}\`\n` +
+              `uploaded at: <t:${Math.floor(caseData.evidence.createdAt.getTime() / 1000)}>`,
+          );
+
+        if (
+          message.member.permissions.has(PermissionFlagsBits.ManageGuild) ||
+          message.author.id === caseData.evidence.userId
+        ) {
+          row.addComponents(
+            new ButtonBuilder()
+              .setLabel("delete")
+              .setEmoji("🗑️")
+              .setCustomId("delete")
+              .setStyle(ButtonStyle.Danger),
+          );
+        }
+      } else {
+        embed.setDescription("no evidence found");
+        row.addComponents(
+          new ButtonBuilder()
+            .setLabel("upload")
+            .setCustomId("upload")
+            .setEmoji("✉️")
+            .setStyle(ButtonStyle.Success),
+        );
+      }
+
+      const payload: BaseMessageOptions = { embeds: [embed], components: [] };
+
+      if (row.components.length > 0) payload.components = [row];
+
+      if (evidenceMsg) {
+        if (interaction) await interaction.update(payload).catch(() => evidenceMsg.edit(payload));
+        else evidenceMsg.edit(payload);
+      } else {
+        if (interaction)
+          evidenceMsg = await interaction
+            .reply(payload)
+            .then((m) => m.fetch())
+            .catch(() => interaction.message.edit(payload));
+        else evidenceMsg = await evidenceMsg.channel.send(payload);
+      }
+
+      async function evidenceListen() {
+        const filter = (i: Interaction) => i.user.id == message.author.id;
+
+        const interaction = await evidenceMsg
+          .awaitMessageComponent({ filter, time: 15000, componentType: ComponentType.Button })
+          .catch(async () => {
+            await evidenceMsg.edit({ components: [] });
+          });
+
+        if (!interaction) return;
+
+        if (interaction.customId === "delete") {
+          let perms = false;
+          if (
+            message.member.permissions.has(PermissionFlagsBits.ManageGuild) ||
+            message.author.id === caseData.evidence.userId
+          )
+            perms = true;
+
+          if (!perms)
+            return interaction.reply({
+              embeds: [new ErrorEmbed("you don't have permission to do this")],
+              ephemeral: true,
+            });
+
+          await deleteEvidence(message.guild, caseData.caseId);
+          caseData = await getCase(message.guild, caseData.caseId);
+          showEvidence(interaction, evidenceMsg);
+          displayCase(caseMsg);
+        } else if (interaction.customId === "upload") {
+          const check = await getCase(message.guild, caseId);
+
+          if (check.evidence)
+            return interaction.reply({
+              embeds: [new ErrorEmbed("this case already has evidence")],
+              ephemeral: true,
+            });
+
+          const [used, max] = await Promise.all([
+            getUsedEvidenceBytes(message.guild),
+            getMaxEvidenceBytes(message.guild),
+          ]);
+
+          if (used > max) {
+            return interaction.reply({
+              embeds: [
+                new ErrorEmbed(
+                  "you have used all of your evidence storage\n\n" +
+                    `\`${formatBytes(used)}/${formatBytes(max)}\`\n\n` +
+                    `to get more storage, join the [official server](https://discord.gg/hJTDNST") and contact **max**`,
+                ),
+              ],
+            });
+          }
+
+          const embed = new CustomEmbed(
+            message.member,
+            `storage: \`${formatBytes(used)}/${formatBytes(max)}\`\n` +
+              "max file size: `10 MB`\n\n" +
+              "send your evidence in chat (jpeg/png/webp)",
+          ).setHeader(`evidence upload for case ${caseData.caseId}`, message.guild.iconURL());
+
+          const evidencePrompt = await interaction
+            .reply({ embeds: [embed] })
+            .catch(async () => message.channel.send({ embeds: [embed] }));
+
+          const filter = (m: Message) => m.author.id == message.author.id;
+
+          const evidenceMessage = await message.channel
+            .awaitMessages({ filter, time: 60000, max: 1 })
+            .then((r) => r.first())
+            .catch(async () => {
+              embed.data.description += "\n\nexpired";
+              await evidencePrompt.edit({ embeds: [embed] });
+            });
+
+          if (!evidenceMessage) return;
+
+          const attachment = evidenceMessage.attachments.first();
+
+          if (!attachment) {
+            return message.channel.send({ embeds: [new ErrorEmbed("you must send an image")] });
+          }
+
+          if (attachment.size > 10e6)
+            return message.channel.send({
+              embeds: [new ErrorEmbed("file too big. max size: 10MB")],
+            });
+
+          if (!["jpeg", "jpg", "gif", "png", "webp"].includes(attachment.contentType.split("/")[1]))
+            return message.channel.send({
+              embeds: [new ErrorEmbed("invalid file type. must be an image")],
+            });
+
+          await createEvidence(message.guild, caseData.caseId, message.author.id, attachment.url);
+
+          evidenceMessage.delete().catch(() => null);
+          evidencePrompt.delete().catch(() => null);
+          caseData = await getCase(message.guild, caseData.caseId);
+          showEvidence(null, evidenceMsg);
+          displayCase(caseMsg);
+        }
+      }
+
+      evidenceListen();
     }
 
     listen();
