1fa891ba36b6268e009ccfa7ba378858e7eadef5
max <29782397+mxz7@users.noreply.github.com>
2024-07-11 01:19:44 +0100

    feat: run task reset concurrently

--- src/scheduled/jobs/weeklytasks.ts
+++ src/scheduled/jobs/weeklytasks.ts
@@ -21,39 +21,41 @@ export default {
     });
 
     for (const user of query) {
-      const completed = await prisma.task.count({
-        where: { AND: [{ user_id: user.user_id }, { type: "weekly" }, { completed: true }] },
-      });
-
-      if (completed === 3) {
-        await prisma.economy.update({
-          where: { userId: user.user_id },
-          data: { weeklyTaskStreak: { increment: 1 } },
-        });
-      } else {
-        const streak = await prisma.economy.findUnique({
-          where: { userId: user.user_id },
-          select: { weeklyTaskStreak: true },
+      (async () => {
+        const completed = await prisma.task.count({
+          where: { AND: [{ user_id: user.user_id }, { type: "weekly" }, { completed: true }] },
         });
 
-        await prisma.economy.update({
-          where: { userId: user.user_id },
-          data: { weeklyTaskStreak: 0 },
-        });
+        if (completed === 3) {
+          await prisma.economy.update({
+            where: { userId: user.user_id },
+            data: { weeklyTaskStreak: { increment: 1 } },
+          });
+        } else {
+          const streak = await prisma.economy.findUnique({
+            where: { userId: user.user_id },
+            select: { weeklyTaskStreak: true },
+          });
 
-        if (streak.weeklyTaskStreak > 3) {
-          const embed = new CustomEmbed()
-            .setColor(Constants.EMBED_FAIL_COLOR)
-            .setDescription(
-              `**you lost your weekly task streak ):**\n\n` +
-                `you didnt complete all of your weekly tasks and lost your streak of ${streak.weeklyTaskStreak.toLocaleString()} ):`,
-            );
+          await prisma.economy.update({
+            where: { userId: user.user_id },
+            data: { weeklyTaskStreak: 0 },
+          });
 
-          if ((await getDmSettings(user.user_id)).other)
-            addNotificationToQueue({ memberId: user.user_id, payload: { embed } });
-          else addInlineNotification({ memberId: user.user_id, embed });
+          if (streak.weeklyTaskStreak > 3) {
+            const embed = new CustomEmbed()
+              .setColor(Constants.EMBED_FAIL_COLOR)
+              .setDescription(
+                `**you lost your weekly task streak ):**\n\n` +
+                  `you didnt complete all of your weekly tasks and lost your streak of ${streak.weeklyTaskStreak.toLocaleString()} ):`,
+              );
+
+            if ((await getDmSettings(user.user_id)).other)
+              addNotificationToQueue({ memberId: user.user_id, payload: { embed } });
+            else addInlineNotification({ memberId: user.user_id, embed });
+          }
         }
-      }
+      })();
     }
 
     const count = await prisma.task.deleteMany({ where: { type: "weekly" } });
