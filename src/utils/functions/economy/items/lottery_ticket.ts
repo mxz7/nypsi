444b2950341f151b747aac1be41f42124722b2f0
max <29782397+mxz7@users.noreply.github.com>
2024-07-29 14:39:14 +0100

    feat: unlimited lottery tickets (#1549)
    * lottery ticket item
    
    * remove lottery ticket max
    
    * change job
    
    * update lottery command
    
    * remove lotterytickets job
    
    * buy confirmation message
    
    * lottery alias
    
    * 8.5.0
    
    * informational item function

---
+++ src/utils/functions/economy/items/lottery_ticket.ts
@@ -0,0 +1,54 @@
+import {
+  BaseMessageOptions,
+  CommandInteraction,
+  InteractionReplyOptions,
+  Message,
+} from "discord.js";
+import { NypsiCommandInteraction } from "../../../../models/Command";
+import { CustomEmbed } from "../../../../models/EmbedBuilders";
+import { ItemUse } from "../../../../models/ItemUse";
+import { getPrefix } from "../../guilds/utils";
+
+module.exports = new ItemUse(
+  "lottery_ticket",
+  async (message: Message | (NypsiCommandInteraction & CommandInteraction)) => {
+    const send = async (data: BaseMessageOptions | InteractionReplyOptions) => {
+      if (!(message instanceof Message)) {
+        let usedNewMessage = false;
+        let res;
+
+        if (message.deferred) {
+          res = await message.editReply(data).catch(async () => {
+            usedNewMessage = true;
+            return await message.channel.send(data as BaseMessageOptions);
+          });
+        } else {
+          res = await message.reply(data as InteractionReplyOptions).catch(() => {
+            return message.editReply(data).catch(async () => {
+              usedNewMessage = true;
+              return await message.channel.send(data as BaseMessageOptions);
+            });
+          });
+        }
+
+        if (usedNewMessage && res instanceof Message) return res;
+
+        const replyMsg = await message.fetchReply();
+        if (replyMsg instanceof Message) {
+          return replyMsg;
+        }
+      } else {
+        return await message.channel.send(data as BaseMessageOptions);
+      }
+    };
+
+    return send({
+      embeds: [
+        new CustomEmbed(
+          message.member,
+          `lottery tickets will automatically be used. ${(await getPrefix(message.guild))[0]}**lotto** for more information`,
+        ),
+      ],
+    });
+  },
+);
