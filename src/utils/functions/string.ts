a9acd7e73411cc10f2a41e10914a899864d67450
max <29782397+mxz7@users.noreply.github.com>
2024-09-02 01:05:50 +0100

    fix: only return suffix not number

--- src/utils/functions/string.ts
+++ src/utils/functions/string.ts
@@ -53,17 +53,17 @@ export function getOrdinalSuffix(num: number): string {
   const lastTwoDigits = num % 100;
 
   if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
-    return `${num}th`;
+    return "th";
   }
 
   switch (lastDigit) {
     case 1:
-      return `${num}st`;
+      return "st";
     case 2:
-      return `${num}nd`;
+      return "nd";
     case 3:
-      return `${num}rd`;
+      return "rd";
     default:
-      return `${num}th`;
+      return "th";
   }
 }
