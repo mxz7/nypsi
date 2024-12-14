6f2cf69a6e3a681331fe719b2ce952e3169764d6
max <29782397+mxz7@users.noreply.github.com>
2024-12-14 16:13:35 +0000

    group net updates by last commadn

--- src/scheduled/jobs/netupdate-3.ts
+++ src/scheduled/jobs/netupdate-3.ts
@@ -1,3 +1,4 @@
+import dayjs = require("dayjs");
 import prisma from "../../init/database";
 import redis from "../../init/redis";
 import { Job } from "../../types/Jobs";
@@ -8,13 +9,18 @@ import sleep from "../../utils/functions/sleep";
 import pAll = require("p-all");
 
 export default {
-  name: "netupdate",
-  cron: "0 */6 * * *",
+  name: "netupdate-3",
+  cron: "0 0 * * *",
   async run(log) {
     const start = Date.now();
     const query = await prisma.economy.findMany({
       select: {
         userId: true,
+      },
+      where: {
+        user: {
+          lastCommand: { gt: dayjs().subtract(1, "day").toDate() },
+        },
       },
     });
 
