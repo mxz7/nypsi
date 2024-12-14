d363c33994da43918efd302df5b621215b94b580
max <29782397+mxz7@users.noreply.github.com>
2024-12-14 16:13:35 +0000

    group net updates by last commadn

---
+++ src/scheduled/jobs/netupdate-1.ts
@@ -0,0 +1,42 @@
+import dayjs = require("dayjs");
+import prisma from "../../init/database";
+import redis from "../../init/redis";
+import { Job } from "../../types/Jobs";
+import Constants from "../../utils/Constants";
+import { MStoTime } from "../../utils/functions/date";
+import { calcNetWorth } from "../../utils/functions/economy/balance";
+import sleep from "../../utils/functions/sleep";
+import pAll = require("p-all");
+
+export default {
+  name: "netupdate-3",
+  cron: "0 0 */7 * *",
+  async run(log) {
+    const start = Date.now();
+    const query = await prisma.economy.findMany({
+      select: {
+        userId: true,
+      },
+      where: {
+        user: {
+          lastCommand: { lt: dayjs().subtract(7, "day").toDate() },
+        },
+      },
+    });
+
+    const actions = [];
+
+    for (const user of query) {
+      await sleep(25);
+      if (await redis.exists(`${Constants.redis.cache.economy.NETWORTH}:${user.userId}`)) continue;
+
+      actions.push(async () => {
+        await calcNetWorth("job", user.userId);
+      });
+    }
+
+    await pAll(actions, { concurrency: 2 });
+
+    log(`net worth updated for ${actions.length} members in ${MStoTime(Date.now() - start)}`);
+  },
+} satisfies Job;
