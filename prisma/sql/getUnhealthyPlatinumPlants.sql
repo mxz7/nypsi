dce739b57db627a0cf85ede57f86204a4ca51305
max <29782397+mxz7@users.noreply.github.com>
2024-12-16 11:33:58 +0000

    I FUCKING HATE PRISMA

--- prisma/sql/getUnhealthyPlatinumPlants.sql
+++
@@ -1,5 +0,0 @@
-select "Farm"."userId", "Farm"."plantId", "Farm"."wateredAt", "Farm"."fertilisedAt", "Farm".id
-  from "Farm"
-  inner join "Premium" on "Premium"."userId" = "Farm"."userId" and "Premium".level = 4
-  left join "DMSettings" on "DMSettings"."userId" = "Farm"."userId"
-  where "Farm"."wateredAt" < now() - interval '1 day' and "Farm"."fertilisedAt" < now() - interval '5 day' and "DMSettings"."farmHealth" = true
