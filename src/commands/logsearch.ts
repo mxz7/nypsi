36c2dc7618e63be5a344f78f5048ab714849a508
max <29782397+mxz7@users.noreply.github.com>
2024-12-22 16:49:12 +0000

    use separate server for logsearch

--- src/commands/logsearch.ts
+++ src/commands/logsearch.ts
@@ -1,8 +1,12 @@
+import { exec } from "child_process";
 import { CommandInteraction, Message } from "discord.js";
 import { readFile } from "fs/promises";
+import { nanoid } from "nanoid";
+import { promisify } from "util";
 import { Command, NypsiCommandInteraction, NypsiMessage } from "../models/Command";
+import { uploadImage } from "../utils/functions/image";
 import { getAdminLevel } from "../utils/functions/users/admin";
-import searchLogs from "../utils/functions/workers/logsearch";
+import { logger } from "../utils/logger";
 
 const cmd = new Command("logsearch", "search through logs", "none").setPermissions(["bot owner"]);
 
@@ -14,17 +18,63 @@ async function run(
 
   if (args.length == 0) return;
 
-  const path = await searchLogs(args.join(" "));
+  const before = performance.now();
+
+  const path = `/tmp/nypsi_logsearch_results_${Date.now()}.txt`;
+
+  const execCmd = promisify(exec);
+
+  logger.debug("grep");
+
+  const msg = message.channel.send({ content: "searching..." });
+
+  await execCmd(
+    `grep -rh "${args.join(" ").replaceAll('"', "").replaceAll("\\", "")}" out > ${path}`,
+  );
+
+  logger.debug("processing");
+  (await msg).edit({ content: "processing..." });
+
+  const res = await fetch("https://nypsi-logprocess.fly.dev/process", {
+    method: "POST",
+    headers: { authorization: process.env.LOGSEARCH_TOKEN },
+    body: await readFile(path),
+  });
+
+  if (!res.ok) {
+    console.error(res);
+    logger.error("failed processing logs", res);
+    return (await msg).edit({ content: "failed processing logs" });
+  }
+
+  logger.debug("uploading");
+  (await msg).edit({ content: "uploading..." });
+
+  const buffer = Buffer.from(await res.arrayBuffer());
 
   if (!path) {
     if (!(message instanceof Message)) return;
     return message.react("❌");
   }
 
-  return message.channel.send({
-    content: `results for \`${args.join(" ")}\``,
-    files: [{ name: "search_results.txt", attachment: await readFile(path) }],
-  });
+  const after = performance.now();
+
+  if (buffer.byteLength > 1e7) {
+    const id = `search_result/${nanoid()}.txt`;
+
+    await uploadImage(id, buffer, "text/plain");
+
+    return (await msg).edit({
+      content:
+        `results for \`${args.join(" ")}\` in ${((after - before) / 1000).toFixed(2)}s\n\n` +
+        `https://cdn.nypsi.xyz/${id}`,
+    });
+  } else {
+    return (await msg).edit({
+      content: `results for \`${args.join(" ")}\` in ${((after - before) / 1000).toFixed(2)}s`,
+      files: [{ attachment: buffer, name: "search_results.txt" }],
+    });
+  }
 }
 
 cmd.setRun(run);
