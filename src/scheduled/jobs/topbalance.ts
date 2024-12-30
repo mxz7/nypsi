807cf7a877a9231a10d9ec1487bb1e1f54b9562f
max <29782397+mxz7@users.noreply.github.com>
2024-12-30 16:29:34 +0000

    send guilds with top balance

--- src/scheduled/jobs/topbalance.ts
+++ src/scheduled/jobs/topbalance.ts
@@ -1,19 +1,27 @@
 import { flavors } from "@catppuccin/palette";
-import { ColorResolvable, EmbedBuilder, WebhookClient } from "discord.js";
+import { ColorResolvable, WebhookClient } from "discord.js";
+import { CustomEmbed } from "../../models/EmbedBuilders";
 import { Job } from "../../types/Jobs";
-import { topBalanceGlobal } from "../../utils/functions/economy/top";
+import { topBalanceGlobal, topGuilds } from "../../utils/functions/economy/top";
 
 export default {
   name: "top balance",
   cron: "0 0 * * *",
   async run(log) {
     const baltop = await topBalanceGlobal(10);
+    const guilds = await topGuilds();
 
-    const embed = new EmbedBuilder();
+    const embed = new CustomEmbed();
 
-    embed.setTitle("top 10 richest users");
-    embed.setDescription(baltop.join("\n"));
+    embed.setHeader(
+      "top",
+      "https://cdn.discordapp.com/avatars/678711738845102087/cb2dcd61010f2b89ceb1cd5ff15816cf.png?size=256",
+    );
+
     embed.setColor(flavors.latte.colors.base.hex as ColorResolvable);
+
+    embed.addField("balance", baltop.join("\n"), true);
+    embed.addField("guild", guilds.pages.get(1).join("\n"), true);
 
     const hook = new WebhookClient({ url: process.env.TOPGLOBAL_HOOK });
 
