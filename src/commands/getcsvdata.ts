6588cb2ce32030fa3c6cefc5c68e64345ac37e38
fedpol1 <fedpolone@gmail.com>
2025-01-15 19:03:24 -0500

    refactor: remove unused or redundant constants

--- src/commands/getcsvdata.ts
+++ src/commands/getcsvdata.ts
@@ -6,7 +6,7 @@ import graphToCsv from "../utils/functions/workers/graphtocsv";
 const cmd = new Command("getcsvdata", "get csv data", "none").setPermissions(["bot owner"]);
 
 async function run(message: NypsiMessage | (NypsiCommandInteraction & CommandInteraction)) {
-  if (!Constants.ADMIN_IDS.includes(message.author.id)) return;
+  if (message.author.id !== Constants.TEKOH_ID) return;
 
   const msg = await message.channel.send({ content: "processing..." });
 
