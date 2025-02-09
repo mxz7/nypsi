2fefc99250963123f54089e4ea7e010fed62964f
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-02-09 15:21:13 +0000

    fix: image uploads

--- src/init/s3.ts
+++ src/init/s3.ts
@@ -3,6 +3,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 const s3 = new S3Client({
   endpoint: process.env.S3_ENDPOINT,
   region: process.env.S3_REGION,
+  requestChecksumCalculation: "WHEN_REQUIRED",
+  responseChecksumValidation: "WHEN_REQUIRED",
   credentials: {
     accessKeyId: process.env.S3_KEY_ID,
     secretAccessKey: process.env.S3_KEY,
