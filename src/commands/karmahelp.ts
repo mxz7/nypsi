6614bd72f0efcefbb8ea75dc0c37174de898196b
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-03-28 16:17:13 +0000

    add /karmashop help and update karma help information

--- src/commands/karmahelp.ts
+++ src/commands/karmahelp.ts
@@ -9,7 +9,7 @@ async function run(message: NypsiMessage | (NypsiCommandInteraction & CommandInt
   const embed = new CustomEmbed(message.member).setTitle("karma help");
 
   embed.setDescription(
-    "karma is an xp-like system that rewards you for simply using nypsi\n\ninteraction with nypsi in different ways rewards you with different amounts of karma, but your karma will not update instantly\n\nif you stop using nypsi for a period of time, your karma will deteriorate over time\n\n**what is karma used for?**\noccasionally, the karma shop will be opened, allowing you to buy things with your karma, such as premium membership, economy xp and crates",
+    "karma is an xp-like system that rewards you for simply using nypsi\n\ninteraction with nypsi in different ways rewards you with different amounts of karma\n\nif you stop using nypsi for a period of time, your karma will deteriorate over time\n\n**what is karma used for?**\noccasionally, the karma shop will be opened, allowing you to buy things with your karma, such as premium membership, xp, crates and rare items\n\nhttps://nypsi.xyz/docs/economy/karma",
   );
 
   embed.setFooter({ text: `${(await getPrefix(message.guild))[0]}karmashop` });
