0854a8f2ec10e3cad5908f8019000c3de3239dea
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-04-18 13:25:40 +0100

    add button to generate another colour

--- src/commands/color.ts
+++ src/commands/color.ts
@@ -28,10 +28,7 @@ async function run(
   let member: GuildMember;
 
   if (args.length == 0) {
-    color = Math.floor(Math.random() * 16777215).toString(16);
-    while (color.length != 6) {
-      color = Math.floor(Math.random() * 16777215).toString(16);
-    }
+    color = getRandomColour();
   }
 
   if (args.length != 0) {
@@ -65,13 +62,26 @@ async function run(
 
   embed.setImage(`https://singlecolorimage.com/get/${color}/54x42`);
 
-  const row = new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+  const row = new ActionRowBuilder<MessageActionRowComponentBuilder>();
+
+  if (args.length === 0)
+    row.addComponents(
+      new ButtonBuilder()
+        .setLabel("another")
+        .setEmoji("🔁")
+        .setCustomId("another")
+        .setStyle(ButtonStyle.Secondary),
+    );
+
+  row.addComponents(
     new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId("circle").setLabel("circle"),
   );
 
   const msg = await message.channel
     .send({ embeds: [embed], components: [row] })
     .catch(async () => message.channel.send({ embeds: [new ErrorEmbed("invalid color")] }));
+
+  let generating = false;
 
   const listen = async () => {
     const interaction = await msg
@@ -84,59 +94,78 @@ async function run(
 
     if (!interaction || interaction instanceof Message) return;
 
-    setTimeout(() => {
-      if (msg.components.length > 0) msg.edit({ components: [] });
-      interaction.deferReply().catch(() => {});
-    }, 1000);
+    if (interaction.customId === "circle") {
+      if (generating)
+        return interaction.reply({
+          embeds: [new ErrorEmbed("circle already being generated")],
+          ephemeral: true,
+        });
 
-    const circleId = `colour/${color}/128x128-circle`;
+      generating = true;
+      await interaction.deferReply();
+      const circleId = `colour/${color}/128x128-circle`;
 
-    if (!(await imageExists(circleId))) {
-      const res = await fetch(`https://singlecolorimage.com/get/${color}/32x32`);
+      if (!(await imageExists(circleId))) {
+        const res = await fetch(`https://singlecolorimage.com/get/${color}/32x32`);
 
-      if (!res.ok || res.status !== 200) {
-        msg.edit({ components: [] });
-        logger.error("invalid response from singlecolorimage.com");
-        return interaction
-          .reply({ embeds: [new ErrorEmbed("failed to generate circle")] })
-          .catch(() =>
-            interaction.editReply({ embeds: [new ErrorEmbed("failed to generate circle")] }),
-          );
+        if (!res.ok || res.status !== 200) {
+          generating = false;
+          msg.edit({ components: [] });
+          logger.error("invalid response from singlecolorimage.com");
+          return interaction
+            .editReply({ embeds: [new ErrorEmbed("failed to generate circle")] })
+            .catch(() =>
+              interaction.editReply({ embeds: [new ErrorEmbed("failed to generate circle")] }),
+            );
+        }
+
+        const arrayBuffer = await res.arrayBuffer();
+
+        const roundedEdges = Buffer.from(
+          '<svg><rect x="0" y="0" width="128" height="128" rx="128" ry="128"/></svg>',
+        );
+
+        const circleImage = await sharp(arrayBuffer)
+          .resize(128, 128, { fit: "fill" })
+          .composite([{ input: roundedEdges, blend: "dest-in" }])
+          .png()
+          .toBuffer();
+
+        await uploadImage(circleId, circleImage, "image/png");
       }
 
-      const arrayBuffer = await res.arrayBuffer();
+      generating = false;
 
-      const roundedEdges = Buffer.from(
-        '<svg><rect x="0" y="0" width="128" height="128" rx="128" ry="128"/></svg>',
-      );
-
-      const circleImage = await sharp(arrayBuffer)
-        .resize(128, 128, { fit: "fill" })
-        .composite([{ input: roundedEdges, blend: "dest-in" }])
-        .png()
-        .toBuffer();
-
-      await uploadImage(circleId, circleImage, "image/png");
-    }
-
-    msg.edit({ components: [] });
-    interaction
-      .reply({
-        embeds: [
-          new EmbedBuilder()
-            .setColor(color as ColorResolvable)
-            .setImage(`https://cdn.nypsi.xyz/${circleId}`),
-        ],
-      })
-      .catch(() =>
-        interaction.editReply({
+      interaction
+        .reply({
           embeds: [
             new EmbedBuilder()
               .setColor(color as ColorResolvable)
               .setImage(`https://cdn.nypsi.xyz/${circleId}`),
           ],
-        }),
-      );
+        })
+        .catch(() =>
+          interaction.editReply({
+            embeds: [
+              new EmbedBuilder()
+                .setColor(color as ColorResolvable)
+                .setImage(`https://cdn.nypsi.xyz/${circleId}`),
+            ],
+          }),
+        );
+
+      return listen();
+    } else if (interaction.customId === "another") {
+      color = getRandomColour();
+
+      embed.setHeader(`#${color}`);
+      embed.setColor(`#${color}`);
+      embed.setImage(`https://singlecolorimage.com/get/${color}/54x42`);
+
+      await interaction.update({ embeds: [embed], components: [row] });
+
+      return listen();
+    }
   };
 
   listen();
@@ -145,3 +174,12 @@ async function run(
 cmd.setRun(run);
 
 module.exports = cmd;
+
+function getRandomColour() {
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+  while (color.length != 6) {
+    color = Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  return color;
+}
