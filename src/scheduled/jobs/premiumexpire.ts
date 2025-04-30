ed020ce11ef436c68f4b76b4d74cf47f870015e1
Fraimer De La Cruz <77998859+fraimerr@users.noreply.github.com>
2025-04-30 14:13:39 -0400

    revert premiumexpire

--- src/scheduled/jobs/premiumexpire.ts
+++ src/scheduled/jobs/premiumexpire.ts
@@ -1,7 +1,6 @@
 import prisma from "../../init/database";
 import { Job } from "../../types/Jobs";
 import { expireUser } from "../../utils/functions/premium/premium";
-import dayjs = require("dayjs");
 
 export default {
   name: "premiumexpire",
@@ -9,7 +8,7 @@ export default {
   async run(log, manager) {
     const query = await prisma.premium.findMany({
       where: {
-        AND: [{ level: { gt: 0 } }, { credit: { lt: 1 } }, { expireDate: { lt: new Date() } }],
+        AND: [{ credit: { lt: 1 } }, { expireDate: { lt: new Date() } }],
       },
       select: {
         userId: true,
@@ -21,37 +20,6 @@ export default {
     }
 
     log(`${query.length} members expired`);
-
-    const gracePeriod = dayjs().subtract(21, "days").toDate();
-
-    const pastGracePeriod = await prisma.premium.findMany({
-      where: {
-        AND: [{ level: 0 }, { credit: { lt: 1 } }, { expireDate: { lt: gracePeriod } }],
-      },
-      select: {
-        userId: true,
-      },
-    });
-
-    for (const user of pastGracePeriod) {
-      await prisma.premiumCommand
-        .delete({
-          where: {
-            owner: user.userId,
-          },
-        })
-        .catch(() => {
-          // doesnt need to find one
-        });
-
-      await prisma.premium.delete({
-        where: {
-          userId: user.userId,
-        },
-      });
-    }
-
-    log(`${pastGracePeriod.length} members fully expired (past grace period)`);
 
     const reduceCredits = await prisma.premium.updateMany({
       where: {
