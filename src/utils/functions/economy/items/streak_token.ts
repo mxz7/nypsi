e951415470543432996c07e669b5fa63b225de73
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2025-05-12 00:23:17 -0400

    be able to use multiple streaks at once

--- src/utils/functions/economy/items/streak_token.ts
+++ src/utils/functions/economy/items/streak_token.ts
@@ -7,12 +7,16 @@ import {
 } from "discord.js";
 import { NypsiCommandInteraction, NypsiMessage } from "../../../../models/Command";
 import { ItemUse } from "../../../../models/ItemUse";
-import { getInventory, removeInventoryItem } from "../inventory";
+import { getInventory, removeInventoryItem, selectItem } from "../inventory";
 import { doDaily } from "../utils";
+import { ErrorEmbed } from "../../../../models/EmbedBuilders";
 
 module.exports = new ItemUse(
   "streak_token",
-  async (message: NypsiMessage | (NypsiCommandInteraction & CommandInteraction)) => {
+  async (
+    message: NypsiMessage | (NypsiCommandInteraction & CommandInteraction),
+    args: string[],
+  ) => {
     const send = async (data: BaseMessageOptions | InteractionReplyOptions) => {
       if (!(message instanceof Message)) {
         let usedNewMessage = false;
@@ -45,9 +49,22 @@ module.exports = new ItemUse(
 
     const inventory = await getInventory(message.member);
 
-    await removeInventoryItem(message.member, "streak_token", 1);
+    const selected = selectItem("streak_token");
 
-    const embed = await doDaily(message.member);
+    let amount = 1;
+
+    if (args[1]?.toLowerCase() === "all") {
+      amount = inventory.find((i) => i.item === selected.id).amount;
+    } else if (parseInt(args[1])) {
+      amount = parseInt(args[1]);
+    }
+
+    if (amount > (inventory.find((i) => i.item === selected.id)?.amount || 0))
+      return send({ embeds: [new ErrorEmbed(`you don't have ${amount} ${selected.name}`)] });
+
+    await removeInventoryItem(message.member, "streak_token", amount);
+
+    const embed = await doDaily(message.member, amount);
 
     return send({ embeds: [embed] });
   },
