b491fcd818f5da9d70c88a9848c71fa94ffbee81
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-05-13 18:32:52 +0100

    maybe fix data deletions? stupid aws sdk

--- src/init/s3.ts
+++ src/init/s3.ts
@@ -11,4 +11,19 @@ const s3 = new S3Client({
   },
 });
 
+s3.middlewareStack.add(
+  (next, context) => async (args) => {
+    // @ts-expect-error
+    delete args.request.headers["x-amz-checksum-crc32"];
+    // @ts-expect-error
+    delete args.request.headers["x-amz-checksum-sha256"];
+    return next(args);
+  },
+  {
+    step: "build",
+    name: "removeChecksumHeaders",
+    priority: "high",
+  },
+);
+
 export default s3;
