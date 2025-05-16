146837495157ba9effdf72b167359831624c9b54
max <29782397+mxz7@users.noreply.github.com>
2025-05-16 08:57:24 +0100

    remove lastweekly migration

---
+++ prisma/migrations/20250516075717_remove_lastweekly/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lastWeekly` on the `Premium` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Premium" DROP COLUMN "lastWeekly";
