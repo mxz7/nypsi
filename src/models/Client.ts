6cfe9c306d170156e20ee7f949eeafbf0915d0cd
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2025-05-18 12:06:01 -0400

    remove runMarketChecks from Client.ts

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -23,7 +23,6 @@ import roleDelete from "../events/roleDelete";
 import userUpdate from "../events/userUpdate";
 import redis from "../init/redis";
 import { runBirthdays } from "../scheduled/clusterjobs/birthdays";
-import { runMarketChecks } from "../scheduled/jobs/checkmarket";
 import { runLogs, runModerationChecks } from "../scheduled/clusterjobs/moderationchecks";
 import startRandomDrops from "../scheduled/clusterjobs/random-drops";
 import Constants from "../utils/Constants";
@@ -232,7 +231,6 @@ export class NypsiClient extends Client {
     if (this.cluster.id != 0) return;
 
     runModerationChecks(this);
-    runMarketChecks(this);
     runCountdowns(this);
     runChristmas(this);
     openKarmaShop(this);
