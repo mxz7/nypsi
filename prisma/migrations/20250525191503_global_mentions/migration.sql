a9e0edfc9397a7b638329de66081ada4de31617f
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-05-25 20:15:21 +0100

    update schema

--- prisma/migrations/20250525191503_global_mentions/migration.sql
+++ prisma/migrations/20250525191503_global_mentions/migration.sql
@@ -1,5 +1,8 @@
 -- DropIndex
 DROP INDEX "Mention_guildId_idx";
 
+-- AlterTable
+ALTER TABLE "Preferences" ADD COLUMN     "mentionsGlobal" BOOLEAN NOT NULL DEFAULT false;
+
 -- CreateIndex
 CREATE INDEX "Mention_targetId_idx" ON "Mention"("targetId");
