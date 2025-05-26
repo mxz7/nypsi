1e6de8c9cb86c69ae572eb9674488129baa2c360
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-05-26 17:18:30 +0100

    what was the point in that empty string

--- src/commands/birthday.ts
+++ src/commands/birthday.ts
@@ -105,8 +105,7 @@ async function run(
       message.member,
       birthday
         ? `your birthday is ${dayjs(birthday).format("MMMM D, YYYY")}\n\n`
-        : "" +
-          "/**birthday set <YYYY-MM-DD>** *set your birthday*\n" +
+        : "/**birthday set <YYYY-MM-DD>** *set your birthday*\n" +
           "/**birthday toggle** *enable/disable your birthday from being announced in servers*\n" +
           "/**birthday channel <channel>** *set a channel to be used as the birthday announcement channel*\n" +
           "/**birthday disable** *disable birthday announcements in your server*",
