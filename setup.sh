464ef50e076e281364f33a0a9a44823fdb4b367a
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2025-05-29 11:47:33 -0400

    turns out -e is windows only

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ mkdir out
 cd src
 cd utils
 cd functions
-echo -e "// eslint-disable-next-line @typescript-eslint/no-unused-vars\nexport function a(b: string, c: string, d:string, e?: string) {} export function b(a: string): any {} export function c(a: string) {}" > anticheat.ts
+printf "// eslint-disable-next-line @typescript-eslint/no-unused-vars\nexport function a(b: string, c: string, d:string, e?: string) {} export function b(a: string): any {} export function c(a: string) {}" > anticheat.ts
 
 cd ..
 cd ..
