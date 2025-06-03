aead3862f3327d7513a95069bcfa80870b291d5a
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2025-06-03 05:35:36 -0400

    fix: only use needed amount of bobs (#1940)

--- src/utils/functions/economy/items/bob.ts
+++ src/utils/functions/economy/items/bob.ts
@@ -85,21 +85,25 @@ module.exports = new ItemUse(
     if (inventory.find((i) => i.item === "bob").amount < amount)
       return send({ embeds: [new ErrorEmbed("you dont have this many bobs")] });
 
-    await removeInventoryItem(message.member, "bob", amount);
-
     const breakdown: string[] = [];
 
+    let maxUsedAmount = 0;
+
     for (const item of crafting) {
-      const newDate = dayjs(item.finished).subtract(amount, "hour").toDate();
+      const remainingMs = item.finished.getTime() - Date.now();
+      const remainingHours = remainingMs / (1000 * 60 * 60);
+
+      const usableAmount = Math.min(amount, Math.ceil(remainingHours));
+      if (usableAmount <= 0) continue;
+
+      const newDate = dayjs(item.finished).subtract(usableAmount, "hour").toDate();
 
       let oldDateText = MStoTime(item.finished.getTime() - Date.now());
-
       if (Date.now() > item.finished.getTime()) {
         oldDateText = "done";
       }
 
       let newDateText = MStoTime(newDate.getTime() - Date.now());
-
       if (Date.now() > newDate.getTime()) {
         newDateText = "done";
       }
@@ -110,6 +114,10 @@ module.exports = new ItemUse(
         }: \`${oldDateText}\` → \`${newDateText}\``,
       );
 
+      if (usableAmount > maxUsedAmount) {
+        maxUsedAmount = usableAmount;
+      }
+
       await prisma.crafting.update({
         where: {
           id: item.id,
@@ -119,6 +127,11 @@ module.exports = new ItemUse(
         },
       });
     }
+
+    if (maxUsedAmount == 0)
+      return send({ embeds: [new ErrorEmbed("you are not currently crafting anything")] });
+
+    await removeInventoryItem(message.member, "bob", maxUsedAmount);
 
     getCraftingItems(message.member);
 
@@ -134,7 +147,7 @@ module.exports = new ItemUse(
       embeds: [
         new CustomEmbed(
           message.member,
-          `<:nypsi_bob:1078776552067694672> bob has removed ${amount} ${pluralize("hour", amount)} of crafting time from ${crafting.length} ${pluralize("item", crafting.length)}\n\n${breakdown.join("\n")}`,
+          `<:nypsi_bob:1078776552067694672> bob has removed ${maxUsedAmount} ${pluralize("hour", maxUsedAmount)} of crafting time from ${crafting.length} ${pluralize("item", crafting.length)}\n\n${breakdown.join("\n")}`,
         ),
       ],
     });
