716f57c3f0a41c2c4b3fcf79ea2dace8fd3dafc7
GrowlingGrizzly <52770460+GrowlingGrizzly@users.noreply.github.com>
2025-06-04 10:50:41 -0400

    ok.

--- src/utils/functions/economy/bakery.ts
+++ src/utils/functions/economy/bakery.ts
@@ -218,7 +218,7 @@ export async function runBakery(member: GuildMember) {
   let total = chosenAmount + passive;
 
   if (guildUpgrades.find((i) => i.upgradeId === "bakery")) {
-    if (percentChance(2 * guildUpgrades.find((i) => i.upgradeId === "bakery").amount))) {
+    if (percentChance(2 * guildUpgrades.find((i) => i.upgradeId === "bakery").amount)) {
       total = total * 2;
       earned.set("guild", Math.floor(total / 2));
       addStat(member, "bake-guild", Math.floor(total / 2));
