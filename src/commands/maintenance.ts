af45251ee44375f72b1ab169558d35e2b22a921e
max <29782397+mxz7@users.noreply.github.com>
2025-07-04 12:55:35 +0100

    add booster time back after maintenance

--- src/commands/maintenance.ts
+++ src/commands/maintenance.ts
@@ -1,17 +1,42 @@
 import { CommandInteraction, Message } from "discord.js";
+import prisma from "../init/database";
 import redis from "../init/redis";
 import { NypsiClient } from "../models/Client";
 import { Command, NypsiCommandInteraction, NypsiMessage } from "../models/Command";
 import { randomPresence } from "../utils/functions/presence";
 import { getAdminLevel } from "../utils/functions/users/admin";
+import dayjs = require("dayjs");
 
 const cmd = new Command("maintenance", "maintenance", "none").setPermissions(["bot owner"]);
 
 async function run(message: NypsiMessage | (NypsiCommandInteraction & CommandInteraction)) {
   if ((await getAdminLevel(message.member)) < 3) return;
 
-  if ((await redis.get("nypsi:maintenance")) == "t") {
+  if (await redis.exists("nypsi:maintenance")) {
+    const before = await redis.get("nypsi:maintenance");
     await redis.del("nypsi:maintenance");
+
+    if (parseInt(before)) {
+      const total = Date.now() - parseInt(before);
+
+      const boosters = await prisma.booster.findMany({
+        select: {
+          id: true,
+          expire: true,
+        },
+      });
+
+      for (const booster of boosters) {
+        await prisma.booster.update({
+          where: {
+            id: booster.id,
+          },
+          data: {
+            expire: dayjs(booster.expire).add(total, "millisecond").toDate(),
+          },
+        });
+      }
+    }
 
     const presence = await randomPresence();
 
@@ -26,7 +51,7 @@ async function run(message: NypsiMessage | (NypsiCommandInteraction & CommandInt
       { context: { presence } },
     );
   } else {
-    await redis.set("nypsi:maintenance", "t");
+    await redis.set("nypsi:maintenance", Date.now());
 
     (message.client as NypsiClient).cluster.send("maintenance_on");
     (message.client as NypsiClient).cluster.broadcastEval((c) => {
