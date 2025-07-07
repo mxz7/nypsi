04ab3733e76820696d77b232da9f86cb510e1ed3
max <29782397+mxz7@users.noreply.github.com>
2025-07-07 11:15:03 +0100

    fix exported formatting

--- src/utils/functions/transactions.ts
+++ src/utils/functions/transactions.ts
@@ -8,10 +8,10 @@ import dayjs = require("dayjs");
 export async function formatTransaction(transaction: Transaction, dest?: "discord") {
   return (
     `${dayjs(transaction.createdAt).format("YYYY-MM_DD HH:mm:ss")}: ` +
-    `${dest === "discord" && "`"}${transaction.sourceId}${dest === "discord" && "`"} (${await getLastKnownUsername(transaction.sourceId)}) -> ` +
-    `${dest === "discord" && "`"}${transaction.targetId}${dest === "discord" && "`"} (${await getLastKnownUsername(transaction.targetId)}) ` +
+    `${dest === "discord" ? "`" : ""}${transaction.sourceId}${dest === "discord" ? "`" : ""} (${await getLastKnownUsername(transaction.sourceId)}) -> ` +
+    `${dest === "discord" ? "`" : ""}${transaction.targetId}${dest === "discord" ? "`" : ""} (${await getLastKnownUsername(transaction.targetId)}) ` +
     `${transaction.type === "item" ? `${transaction.amount.toLocaleString()}x ${transaction.itemId}` : `$${transaction.amount.toLocaleString()}`} ` +
-    `${transaction.notes && `| ${transaction.notes}`}`
+    `${transaction.notes ? `| ${transaction.notes}` : ""}`
   );
 }
 
