f3bdb888dc1bd6756c91a2efa0d57a6827149de6
max <29782397+mxz7@users.noreply.github.com>
2025-07-07 09:55:25 +0100

    add transactions iterator

--- src/utils/functions/transactions.ts
+++ src/utils/functions/transactions.ts
@@ -1,4 +1,7 @@
-import { Transaction } from "@prisma/client";
+import { Prisma, Transaction } from "@prisma/client";
+import { createWriteStream } from "node:fs";
+import prisma from "../../init/database";
+import { logger } from "../logger";
 import { getLastKnownUsername } from "./users/tag";
 import dayjs = require("dayjs");
 
@@ -10,4 +13,45 @@ export async function formatTransaction(transaction: Transaction, dest?: "discor
     `${transaction.type === "item" ? `${transaction.amount.toLocaleString()}x ${transaction.itemId}` : `$${transaction.amount.toLocaleString()}`} ` +
     `${transaction.notes && `| ${transaction.notes}`}`
   );
+}
+
+async function* transactionIterator(query: Prisma.TransactionFindManyArgs["where"]) {
+  let currentData: Transaction[] = [];
+  let skip = 0;
+
+  do {
+    currentData = await prisma.transaction.findMany({
+      where: query,
+      orderBy: { id: "desc" },
+      take: 100,
+      skip,
+    });
+    skip += currentData.length;
+
+    yield currentData;
+  } while (currentData.length >= 100);
+}
+
+export async function exportTransactions(
+  query: Prisma.TransactionFindManyArgs["where"],
+): Promise<string> {
+  const fileName = `/tmp/tx-${Date.now()}.txt`;
+  const stream = createWriteStream(fileName);
+  const iterator = transactionIterator(query);
+
+  for await (const data of iterator) {
+    logger.debug(`transactions: received chunk (${data.length})`);
+    for (const transaction of data) {
+      stream.write((await formatTransaction(transaction)) + "\n");
+    }
+  }
+
+  stream.end();
+
+  return new Promise((resolve) => {
+    stream.on("finish", () => {
+      stream.close();
+      resolve(fileName);
+    });
+  });
 }
