da1e579725fd32906323879bd55edf744a8fca17
max <29782397+mxz7@users.noreply.github.com>
2025-07-08 13:04:24 +0100

    some functions

---
+++ src/utils/functions/economy/events.ts
@@ -0,0 +1,91 @@
+import dayjs = require("dayjs");
+import prisma from "../../../init/database";
+import { NypsiClient } from "../../../models/Client";
+import Constants from "../../Constants";
+import { getUserId, MemberResolvable } from "../member";
+
+export async function createEvent(
+  client: NypsiClient,
+  member: MemberResolvable,
+  type: string,
+  title: string,
+  target: string,
+  days: number,
+) {
+  const userId = getUserId(member);
+
+  const check = await prisma.event.findFirst({
+    where: {
+      completed: false,
+    },
+  });
+
+  if (check) {
+    return "event already in process";
+  }
+
+  await prisma.event.create({
+    data: {
+      ownerId: userId,
+      type,
+      title,
+      target,
+      expiresAt: dayjs()
+        .add(days, "day")
+        .set("hours", 0)
+        .set("minute", 0)
+        .set("second", 0)
+        .set("millisecond", 0)
+        .toDate(),
+    },
+  });
+
+  const targetChannel =
+    client.user.id === Constants.BOT_USER_ID
+      ? Constants.ANNOUNCEMENTS_CHANNEL_ID
+      : "819640200699052052"; // dev channel
+
+  const clusters = await client.cluster.broadcastEval(
+    (client, { channelId }) => {
+      const guild = client.channels.cache.get(channelId);
+
+      if (guild) return (client as unknown as NypsiClient).cluster.id;
+      return "not-found";
+    },
+    { context: { channelId: targetChannel } },
+  );
+
+  let cluster: number;
+
+  for (const i of clusters) {
+    if (i != "not-found") {
+      cluster = i;
+      break;
+    }
+  }
+
+  await client.cluster
+    .broadcastEval(
+      async (client, { content, channelId, cluster }) => {
+        if ((client as unknown as NypsiClient).cluster.id != cluster) return;
+
+        const channel = client.channels.cache.get(channelId);
+
+        if (!channel) return;
+
+        if (channel.isTextBased() && channel.isSendable()) {
+          await channel.send({ content });
+        }
+      },
+      {
+        context: {
+          content: `the **${title}** event has started!!\n` + `<@&${Constants.EVENTS_ROLE_ID}>`,
+          channelId: targetChannel,
+          cluster: cluster,
+        },
+      },
+    )
+    .then((res) => {
+      return res.filter((i) => Boolean(i));
+    });
+}
