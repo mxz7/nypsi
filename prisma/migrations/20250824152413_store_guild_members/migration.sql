03d99567aab54985c059bec52a3accc20a9e2aec
mxz7 <29782397+mxz7@users.noreply.github.com>
2025-08-24 16:24:28 +0100

    db migration

---
+++ prisma/migrations/20250824152413_store_guild_members/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "public"."GuildMember" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "guildId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "GuildMember_pkey" PRIMARY KEY ("guildId","userId")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."GuildMember" ADD CONSTRAINT "GuildMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."GuildMember" ADD CONSTRAINT "GuildMember_guildId_fkey" FOREIGN KEY ("guildId") REFERENCES "public"."Guild"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
