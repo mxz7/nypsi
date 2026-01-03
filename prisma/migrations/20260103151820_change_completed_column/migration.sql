bb57b2aafac071949f3e652b3f1fef138e2f48a8
max <29782397+mxz7@users.noreply.github.com>
2026-01-03 15:25:58 +0000

    change which columns to use for end

---
+++ prisma/migrations/20260103151820_change_completed_column/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `completed` on the `Event` table. All the data in the column will be lost.
+  - You are about to drop the column `completedAt` on the `Event` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Event" DROP COLUMN "completed",
+ADD COLUMN     "endedAt" TIMESTAMP(3);
+
+-- UpdateTable
+UPDATE "Event" SET "endedAt" = "completedAt";
+
+-- AlterTable
+ALTER TABLE "Event" DROP COLUMN "completedAt";
