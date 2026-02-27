260927bbc54c354727e7f6e42e2e817c821adec8
max <29782397+mxz7@users.noreply.github.com>
2026-02-27 23:35:20 +0000

    add staff notifications on heartbeat failures
    closes #2357

--- src/utils/functions/heartbeat.ts
+++ src/utils/functions/heartbeat.ts
@@ -1,10 +1,12 @@
 import { Cluster } from "discord-hybrid-sharding";
+import { WebhookClient } from "discord.js";
 import redis from "../../init/redis";
 import Constants from "../Constants";
 import { logger } from "../logger";
 import { dmQueueWorker } from "../queues/dms";
 
 const failedHeartbeats = new Map<number, number>();
+const webhook = new WebhookClient({ url: process.env.NYPSI_DYING_HOOK });
 
 export async function sendHeartbeat(cluster: Cluster) {
   if ((await redis.get(`${Constants.redis.nypsi.RESTART}:${cluster.id}`)) == "t") {
@@ -29,8 +31,14 @@ export async function addFailedHeartbeat(cluster: Cluster) {
   if (failedHeartbeats.has(cluster.id)) {
     if (failedHeartbeats.get(cluster.id) >= 5) {
       logger.info(`respawning cluster ${cluster.id} due to missing heartbeats`);
+      webhook.send({
+        content: `respawning cluster ${cluster.id} due to missing heartbeats <@&${Constants.STAFF_ROLE_ID}>`,
+      });
       await dmQueueWorker.pause();
       await cluster.respawn().then((c) => {
+        webhook.send({
+          content: `cluster ${cluster.id} respawned <@&${Constants.STAFF_ROLE_ID}>`,
+        });
         logger.debug(`${cluster.id} respawn promise resolved, sending ready event`);
         c.emit("ready");
         logger.debug(`${cluster.id} ready event sent`);
@@ -43,6 +51,12 @@ export async function addFailedHeartbeat(cluster: Cluster) {
   } else {
     failedHeartbeats.set(cluster.id, 1);
   }
+
+  const count = failedHeartbeats.get(cluster.id);
+
+  webhook.send({
+    content: `cluster ${cluster.id} missed heartbeat (${count}) <@&${Constants.STAFF_ROLE_ID}>`,
+  });
 }
 
 setInterval(() => {
