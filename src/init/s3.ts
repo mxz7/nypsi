d44f0aeb50a7447e307c52d1452472a94809c3d9
max <29782397+mxz7@users.noreply.github.com>
2026-02-28 12:06:04 +0000

    backblaze seems to support aws headers now

--- src/init/s3.ts
+++ src/init/s3.ts
@@ -3,42 +3,10 @@ import { S3Client } from "@aws-sdk/client-s3";
 const s3 = new S3Client({
   endpoint: process.env.S3_ENDPOINT,
   region: process.env.S3_REGION,
-  requestChecksumCalculation: "WHEN_REQUIRED",
-  responseChecksumValidation: "WHEN_REQUIRED",
   credentials: {
     accessKeyId: process.env.S3_KEY_ID,
     secretAccessKey: process.env.S3_KEY,
   },
 });
-
-// backblaze doesn't support these headers
-s3.middlewareStack.add(
-  (next) => async (args) => {
-    // @ts-expect-error stupid aws sdk I FUCKING HATE AWS
-    const headers = args.request.headers;
-
-    // List of unsupported checksum headers
-    const checksumHeaders = [
-      "x-amz-checksum-crc32",
-      "x-amz-checksum-crc32c",
-      "x-amz-checksum-crc64nvme",
-      "x-amz-checksum-sha1",
-      "x-amz-checksum-sha256",
-      "x-amz-checksum-algorithm",
-      "x-amz-checksum-mode",
-    ];
-
-    for (const header of checksumHeaders) {
-      delete headers[header];
-    }
-
-    return next(args);
-  },
-  {
-    step: "build",
-    name: "removeChecksumHeaders",
-    priority: "high",
-  },
-);
 
 export default s3;
