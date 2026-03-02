5b2a89ca167d5fa87e3e984c239917dd636b449f
max <29782397+mxz7@users.noreply.github.com>
2026-03-02 18:01:17 +0000

    refactor: use expect.soft

--- test/items.test.ts
+++ test/items.test.ts
@@ -10,70 +10,71 @@ const lootPools: Record<string, LootPool> = JSON.parse(
 
 for (const item of Object.values(items)) {
   test(item.id, () => {
-    expect(typeof item.id).toBe("string");
-    expect(typeof item.name).toBe("string");
-    expect(typeof item.emoji).toBe("string");
-    expect(typeof item.longDesc).toBe("string");
-    expect(typeof item.article).toBe("string");
-    expect(typeof item.in_crates).toBe("boolean");
-    expect(typeof item.role).toBe("string");
+    expect.soft(typeof item.id).toBe("string");
+    expect.soft(typeof item.name).toBe("string");
+    expect.soft(typeof item.emoji).toBe("string");
+    expect.soft(typeof item.longDesc).toBe("string");
+    expect.soft(typeof item.article).toBe("string");
+    expect.soft(typeof item.in_crates).toBe("boolean");
+    expect.soft(typeof item.role).toBe("string");
 
     if (item.role === "booster") {
-      expect(typeof item.stackable).toBe("boolean");
-      if (item.stackable) expect(typeof item.max).toBe("number");
-      expect(typeof item.boosterEffect).toBe("object");
-      expect(typeof item.boosterEffect.effect).toBe("number");
-      expect(typeof item.boosterEffect.time).toBe("number");
+      expect.soft(typeof item.stackable).toBe("boolean");
+      if (item.stackable) expect.soft(typeof item.max).toBe("number");
+      expect.soft(typeof item.boosterEffect).toBe("object");
+      expect.soft(typeof item.boosterEffect.effect).toBe("number");
+      expect.soft(typeof item.boosterEffect.time).toBe("number");
 
-      expect(Array.isArray(item.boosterEffect.boosts)).toBe(true);
+      expect.soft(Array.isArray(item.boosterEffect.boosts)).toBe(true);
 
       for (const effect of item.boosterEffect.boosts) {
-        expect(typeof effect).toBe("string");
+        expect.soft(typeof effect).toBe("string");
       }
     } else if (item.role === "scratch-card") {
-      expect(typeof item.clicks).toBe("number");
-    } else if (item.role === "car") expect(typeof item.speed).toBe("number");
-    else if (item.role === "ore") expect(typeof item.ingot).toBe("string");
-    else if (item.role === "worker-upgrade") expect(typeof item.worker_upgrade_id).toBe("string");
-    else if (item.role === "tag") expect(typeof item.tagId).toBe("string");
+      expect.soft(typeof item.clicks).toBe("number");
+    } else if (item.role === "car") expect.soft(typeof item.speed).toBe("number");
+    else if (item.role === "ore") expect.soft(typeof item.ingot).toBe("string");
+    else if (item.role === "worker-upgrade")
+      expect.soft(typeof item.worker_upgrade_id).toBe("string");
+    else if (item.role === "tag") expect.soft(typeof item.tagId).toBe("string");
 
     if (item.role === "scratch-card" || item.role === "crate") {
       for (const poolKey in item.loot_pools) {
-        expect(typeof poolKey).toBe("string");
-        expect(lootPools[poolKey]).toBeDefined();
-        expect(Number(item.loot_pools[poolKey])).toBeGreaterThan(0);
+        expect.soft(typeof poolKey).toBe("string");
+        expect.soft(lootPools[poolKey]).toBeDefined();
+        expect.soft(Number(item.loot_pools[poolKey])).toBeGreaterThan(0);
       }
     }
 
-    if (item.rarity) expect(typeof item.rarity).toBe("number");
-    if (item.booster_desc) expect(typeof item.booster_desc).toBe("string");
-    if (item.shortDesc) expect(typeof item.shortDesc).toBe("string");
-    if (item.buy) expect(typeof item.buy).toBe("number");
-    if (item.sell) expect(typeof item.sell).toBe("number");
-    if (item.aliases) expect(Array.isArray(item.aliases)).toBe(true);
-    if (item.plural) expect(typeof item.plural).toBe("string");
+    if (item.rarity) expect.soft(typeof item.rarity).toBe("number");
+    if (item.booster_desc) expect.soft(typeof item.booster_desc).toBe("string");
+    if (item.shortDesc) expect.soft(typeof item.shortDesc).toBe("string");
+    if (item.buy) expect.soft(typeof item.buy).toBe("number");
+    if (item.sell) expect.soft(typeof item.sell).toBe("number");
+    if (item.aliases) expect.soft(Array.isArray(item.aliases)).toBe(true);
+    if (item.plural) expect.soft(typeof item.plural).toBe("string");
 
     if (item.craft) {
-      expect(typeof item.craft).toBe("object");
-      expect(typeof item.craft.time).toBe("number");
-      expect(Array.isArray(item.craft.ingredients)).toBe(true);
+      expect.soft(typeof item.craft).toBe("object");
+      expect.soft(typeof item.craft.time).toBe("number");
+      expect.soft(Array.isArray(item.craft.ingredients)).toBe(true);
 
       for (const ingredient of item.craft.ingredients) {
-        expect(Boolean(items[ingredient.split(":")[0]])).toBe(true);
-        expect(Number(ingredient.split(":")[1])).toBeGreaterThan(0);
+        expect.soft(Boolean(items[ingredient.split(":")[0]])).toBe(true);
+        expect.soft(Number(ingredient.split(":")[1])).toBeGreaterThan(0);
       }
     }
 
-    if (item.plantId !== undefined) expect(typeof item.plantId).toBe("string");
-    if (item.unique !== undefined) expect(item.unique).toBe(true);
+    if (item.plantId !== undefined) expect.soft(typeof item.plantId).toBe("string");
+    if (item.unique !== undefined) expect.soft(item.unique).toBe(true);
 
-    if (item.default_count !== undefined) expect(typeof item.default_count).toBe("number");
-    if (item.account_locked !== undefined) expect(item.account_locked).toBe(true);
-    if (item.hidden !== undefined) expect(item.hidden).toBe(true);
-    if (item.upgrades) expect(typeof item.upgrades).toBe("string");
+    if (item.default_count !== undefined) expect.soft(typeof item.default_count).toBe("number");
+    if (item.account_locked !== undefined) expect.soft(item.account_locked).toBe(true);
+    if (item.hidden !== undefined) expect.soft(item.hidden).toBe(true);
+    if (item.upgrades) expect.soft(typeof item.upgrades).toBe("string");
 
     if (item.boosterEffect && item.boosterEffect.global !== undefined) {
-      expect(typeof item.boosterEffect.global).toBe("boolean");
+      expect.soft(typeof item.boosterEffect.global).toBe("boolean");
     }
   });
 }
