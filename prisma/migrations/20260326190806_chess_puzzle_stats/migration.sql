ee7593c65c39540ba1ce47e687b417d119654bd7
max <29782397+mxz7@users.noreply.github.com>
2026-03-26 20:01:05 +0000

    feat(db): add chess stats table

---
+++ prisma/migrations/20260326190806_chess_puzzle_stats/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "ChessPuzzleStats" (
+    "userId" TEXT NOT NULL,
+    "solved" INTEGER NOT NULL DEFAULT 0,
+    "failed" INTEGER NOT NULL DEFAULT 0,
+    "streak" INTEGER NOT NULL DEFAULT 0,
+    "bestStreak" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "ChessPuzzleStats_pkey" PRIMARY KEY ("userId")
+);
+
+-- AddForeignKey
+ALTER TABLE "ChessPuzzleStats" ADD CONSTRAINT "ChessPuzzleStats_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
