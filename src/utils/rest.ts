f75736c0f903c339cebb2409dd794ddaaed53e11
max <29782397+mxz7@users.noreply.github.com>
2026-04-18 14:08:16 +0100

    feat(rest): add rate limit log

--- src/utils/rest.ts
+++ src/utils/rest.ts
@@ -3,6 +3,10 @@ import { NypsiClient } from "../models/Client";
 
 const rest = new REST().setToken(process.env.BOT_TOKEN!);
 
+rest.on("rateLimited", (info) => {
+  console.warn(`rest: rate limited: ${info.route} ${info.timeToReset}ms until reset`, { ...info });
+});
+
 export function getRest(client?: NypsiClient): REST {
   if (client) {
     return client.rest;
