3fc125f339181386ac7efe0e670cb2603a830908
max <29782397+mxz7@users.noreply.github.com>
2026-04-18 11:10:52 +0100

    feat(mentions): add queue job data types

---
+++ src/types/workers/mentions.ts
@@ -0,0 +1,14 @@
+import { PermissionOverwrites } from "discord.js";
+
+export type Mention = "everyone" | `role:${string}` | `user:${string}`;
+
+export interface MentionJobData {
+  messageId: string;
+  messageUrl: string;
+  guildId: string;
+  channelId: string;
+  channelOverwrites: PermissionOverwrites[] | null;
+  roles: { id: string; permissions: string }[];
+  content: string;
+  mentions: Mention[];
+}
