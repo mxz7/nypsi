85a68cccaa80ef1b867555d26b9f5770ab30abb6
max <29782397+mxz7@users.noreply.github.com>
2026-05-06 16:48:19 +0100

    fix(db): chat conversation table

---
+++ prisma/migrations/20260506154747_add_ai_chats/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "AiChatConversation" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "AiChatConversation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AiChatMessage" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "conversationId" UUID NOT NULL,
+    "userQuery" TEXT NOT NULL,
+    "aiResponse" TEXT,
+    "model" TEXT NOT NULL,
+    "inputTokens" INTEGER,
+    "cachedInputTokens" INTEGER,
+    "outputTokens" INTEGER,
+
+    CONSTRAINT "AiChatMessage_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "AiChatConversation" ADD CONSTRAINT "AiChatConversation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AiChatMessage" ADD CONSTRAINT "AiChatMessage_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "AiChatConversation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
