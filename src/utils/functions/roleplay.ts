e8e60fe5067f7ed3a959942c4b0d36133b7ae923
max <29782397+mxz7@users.noreply.github.com>
2026-05-12 18:37:26 +0100

    fix(roleplay): check users exist before upserting

--- src/utils/functions/roleplay.ts
+++ src/utils/functions/roleplay.ts
@@ -1,10 +1,14 @@
 import prisma from "../../init/database";
+import { createUser, userExists } from "./economy/utils";
 
 export async function addRoleplayStat(
   userId: string,
   targetId: string,
   action: string,
 ): Promise<number> {
+  if (!(await userExists(userId))) await createUser(userId);
+  if (!(await userExists(targetId))) await createUser(targetId);
+
   const result = await prisma.roleplayStat.upsert({
     where: { userId_action_targetId: { userId, action, targetId } },
     update: { count: { increment: 1 } },
