ac0c812776dcca357cc2732e51f14446e458ad8a
max <29782397+mxz7@users.noreply.github.com>
2026-05-25 10:26:34 +0100

    fix(birthdays): use rest members to filter

--- src/scheduled/jobs/birthdays.ts
+++ src/scheduled/jobs/birthdays.ts
@@ -1,18 +1,21 @@
 import pAll = require("p-all");
+import dayjs from "dayjs";
+import { WebhookClient } from "discord.js";
 import prisma from "../../init/database";
-import { NypsiClient } from "../../models/Client";
 import { CustomEmbed } from "../../models/EmbedBuilders";
 import { Job } from "../../types/Jobs";
 import { addInventoryItem } from "../../utils/functions/economy/inventory";
 import { getItems } from "../../utils/functions/economy/utils";
-import { pluralize } from "../../utils/functions/string";
+import { getAllMembersRest } from "../../utils/functions/guilds/members";
+import { getOrdinalSuffix, pluralize } from "../../utils/functions/string";
 import { getTodaysBirthdays } from "../../utils/functions/users/birthday";
 import { addNotificationToQueue } from "../../utils/functions/users/notifications";
+import { logger } from "../../utils/logger";
 
 export default {
   name: "birthdays",
   cron: "0 1 * * *",
-  async run(log, manager) {
+  async run(log) {
     const birthdayMembers = await getTodaysBirthdays(false);
     const functions = [];
 
@@ -33,102 +36,54 @@ export default {
     await pAll(functions, { concurrency: 5 });
 
     if (birthdayMembers.length) {
-      const res = (
-        await manager.broadcastEval(
-          async (c, { birthdayMembers }) => {
-            const path = await import("path");
-            const dayjs = await import("dayjs");
-            const { WebhookClient } = await import("discord.js");
-            const { logger } = await import(path.join(process.cwd(), "dist", "utils", "logger.js"));
-            const { getBirthdayGuilds } = await import(
-              path.join(process.cwd(), "dist", "scheduled", "jobs", "birthdays.js")
-            );
-            const { getOrdinalSuffix } = await import(
-              path.join(process.cwd(), "dist", "utils", "functions", "string.js")
-            );
+      const guilds = await prisma.guild.findMany({
+        where: { birthdayHook: { not: null } },
+        select: { id: true, birthdayHook: true },
+      });
 
-            const client = c as unknown as NypsiClient;
+      let successCount = 0;
 
-            const guilds = await getBirthdayGuilds(Array.from(client.guilds.cache.keys()));
+      for (const guildData of guilds) {
+        try {
+          const hook = new WebhookClient({ url: guildData.birthdayHook });
+          const guildMemberIds = await getAllMembersRest(guildData.id, undefined, true);
 
-            let successCount = 0;
+          for (const member of birthdayMembers) {
+            if (!guildMemberIds.includes(member.id)) continue;
+            try {
+              const years = dayjs().diff(member.birthday, "years");
+              const msg = `it's <@${member.id}>'s ${member.birthday.getFullYear() === 69 ? "" : `**${years}${getOrdinalSuffix(years)}** `}birthday today!`;
 
-            for (const guildData of guilds) {
-              try {
-                const hook = new WebhookClient({ url: guildData.birthdayHook });
-                const guild = await client.guilds.fetch(guildData.id);
-
-                const members = await guild.members.fetch({
-                  user: birthdayMembers.map((i) => i.id),
+              await hook
+                .send({ content: msg })
+                .then(() => {
+                  successCount++;
+                })
+                .catch(() => {
+                  logger.warn(
+                    `failed to send ${member.id} birthday announcement in ${guildData.id}`,
+                  );
                 });
-
-                for (const member of members.values()) {
-                  try {
-                    if (
-                      member.user.username === "Deleted User" &&
-                      member.user.discriminator === "0000"
-                    ) {
-                      // deleted user
-                      continue;
-                    }
-
-                    const birthday = birthdayMembers.find((i) => i.id === member.id);
-
-                    if (!birthday) continue;
-
-                    // @ts-expect-error - dayjs is imported wrong apparently but it works fine
-                    const years = dayjs().diff(birthday.birthday, "years");
-
-                    const msg = `it's ${member.toString()}'s ${parseInt(birthday.birthday.split("-")[0]) == 69 ? "" : `**${years}${getOrdinalSuffix(years)}** `}birthday today!`;
-
-                    await hook
-                      .send({ content: msg })
-                      .then(() => {
-                        successCount++;
-                      })
-                      .catch(() => {
-                        logger.warn(
-                          `failed to send ${member.id} birthday announcement in ${guild.id}`,
-                        );
-                      });
-                  } catch (e) {
-                    logger.error(`error handling birthday in ${guildData.id} for ${member.id}`, {
-                      error: e,
-                    });
-                  }
-                }
-
-                hook.destroy();
-              } catch (e) {
-                logger.error(`error handling birthdays in ${guildData.id}`, {
-                  error: e,
-                });
-              }
+            } catch (e) {
+              logger.error(`error handling birthday in ${guildData.id} for ${member.id}`, {
+                error: e,
+              });
             }
+          }
 
-            return successCount;
-          },
-          { context: { birthdayMembers } },
-        )
-      ).reduce((acc, num) => acc + num, 0);
+          hook.destroy();
+        } catch (e) {
+          logger.error(`error handling birthdays in ${guildData.id}`, {
+            error: e,
+          });
+        }
+      }
 
-      if (res) log(`sent ${res} ${pluralize("birthday announcement", res)}`);
-
+      if (successCount)
+        log(`sent ${successCount} ${pluralize("birthday announcement", successCount)}`);
       log(
         `given birthday gifts to ${birthdayMembers.length} ${pluralize("user", birthdayMembers.length)}`,
       );
     }
   },
 } satisfies Job;
-
-export async function getBirthdayGuilds(ids: string[]) {
-  return await prisma.guild.findMany({
-    where: {
-      AND: [{ id: { in: ids } }, { birthdayHook: { not: null } }],
-    },
-    select: {
-      id: true,
-      birthdayHook: true,
-    },
-  });
-}
