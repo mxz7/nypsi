a292ee6e0cc15828928b34a78153dfdf439a2aeb
max <29782397+mxz7@users.noreply.github.com>
2026-07-04 10:43:39 +0100

    fix(lottery): higher chance for superdraw tickets

--- src/scheduled/jobs/lottery.ts
+++ src/scheduled/jobs/lottery.ts
@@ -249,13 +249,13 @@ async function deleteAllTickets(userIds: string[], isSuperDraw: boolean) {
 }
 
 function getSuperdrawChance(ticketAmount: number): number {
-  const maxChance = 0.1;
-  const minChance = 0.025;
+  const maxChance = 0.65;
+  const minChance = 0.1;
   const maxTicketsForMinChance = 1000;
 
   const clamped = Math.min(Math.max(ticketAmount, 1), maxTicketsForMinChance);
   const t = (clamped - 1) / (maxTicketsForMinChance - 1);
-  const progress = 1 - Math.pow(1 - t, 5);
+  const progress = 1 - Math.pow(1 - t, 3);
 
   return maxChance - (maxChance - minChance) * progress;
 }
