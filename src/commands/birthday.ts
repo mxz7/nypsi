55873c4aa42020dd2c1b2c22d50a74dc9df329f1
max <29782397+mxz7@users.noreply.github.com>
2026-08-17 15:42:18 +0100

    fix(birthday): use correct modal id

--- src/commands/birthday.ts
+++ src/commands/birthday.ts
@@ -329,7 +329,7 @@ async function run(
 
     if (!res) return;
 
-    const month = res.fields.getStringSelectValues("month")[0];
+    const month = res.fields.getStringSelectValues("select-month")[0];
     const day = res.fields.getTextInputValue("day").padStart(2, "0");
     const year = res.fields.getTextInputValue("year") || "0069";
 
